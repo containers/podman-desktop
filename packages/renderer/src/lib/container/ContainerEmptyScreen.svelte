406bfcc8129740b7c070b5ca76f2b4eb84ae3bbd
Daniel J Walsh <dwalsh@redhat.com>
2022-04-14 12:06:25 -0400

    fix: Use quay.io/podman/hello as the example podman container
    It should be just ubi8-micro, but I think this image is better.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- packages/renderer/src/lib/container/ContainerEmptyScreen.svelte
+++ packages/renderer/src/lib/container/ContainerEmptyScreen.svelte
@@ -24,7 +24,7 @@ onMount(async () => {
       <h1 class="pf-c-title pf-m-lg">No containers</h1>
       <div class="pf-c-empty-state__body">Run a first container using the following command line:</div>
       <div class="flex flex-row bg-gray-800 w-full items-center p-2 mt-2">
-        <div id="noContainerCommandLine">podman run redhat/ubi8-micro echo hello world</div>
+        <div id="noContainerCommandLine">podman run quay.io/podman/hello</div>
         <button title="Copy To Clipboard" class="mr-5" on:click="{() => copyRunInstructionToClipboard()}"
           ><Fa class="ml-3 h-5 w-5 cursor-pointer rounded-full text-3xl text-sky-800" icon="{faPaste}" /></button>
       </div>
