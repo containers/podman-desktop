5b462663f66fee342b8434e36ca977d3c673020a
Florent Benoit <fbenoit@redhat.com>
2022-06-13 11:00:15 +0200

    feat: handle boolean preferences
    Change-Id: I354adb4c34ae912f3c573df101051288989633e2
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/preferences/PreferencesRenderingItemFormat.svelte
+++ packages/renderer/src/lib/preferences/PreferencesRenderingItemFormat.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-import { invalid } from 'moment';
-
-import { onMount } from 'svelte';
-
 import {
   ConfigurationRegistry,
   IConfigurationPropertyRecordedSchema,
 } from '../../../../main/src/plugin/configuration-registry';
 
-let recordValue = '';
 let invalidEntry = false;
 let invalidText = undefined;
 export let showUpdate = false;
@@ -17,17 +12,26 @@ export let validRecord = () => {};
 
 export let record: IConfigurationPropertyRecordedSchema;
 
-onMount(async () => {
+let currentRecord: IConfigurationPropertyRecordedSchema;
+
+let recordValue;
+let checkboxValue: boolean = false;
+$: if (currentRecord !== record) {
   if (record.scope === ConfigurationRegistry.DEFAULT_SCOPE) {
-    try {
-      recordValue = await window.getConfigurationValue(record.id, record.scope);
-    } catch (err) {
-      console.error('Error getting configuration value', err);
-    }
+    window.getConfigurationValue(record.id, record.scope).then(value => {
+      recordValue = value;
+      if (recordValue === true) {
+        checkboxValue = true;
+      }
+    });
   } else if (record.default) {
     recordValue = record.default;
   }
-});
+
+  currentRecord = record;
+  invalidText = undefined;
+  invalidEntry = false;
+}
 
 function invalid() {
   // call the callback
@@ -77,6 +81,8 @@ function update(record: IConfigurationPropertyRecordedSchema) {
   let value: any = recordValue;
   if (record.type === 'number') {
     value = parseFloat(value);
+  } else if (record.type === 'boolean') {
+    value = checkboxValue;
   }
 
   // save the value
@@ -91,16 +97,30 @@ function update(record: IConfigurationPropertyRecordedSchema) {
 
 <div class="flex flex-row mb-2 px-4">
   <div class="flex flex-col mx-2 flex-1 pf-c-form__group-control">
-    <input
-      on:input="{event => checkValue(record, event)}"
-      class="pf-c-form-control"
-      name="{record.id}"
-      type="text"
-      bind:value="{recordValue}"
-      readonly="{!!record.readonly}"
-      id="input-standard"
-      aria-invalid="{invalidEntry}"
-      aria-label="{record.description}" />
+    {#if record.type === 'boolean'}
+      <input
+        on:input="{event => checkValue(record, event)}"
+        class="pf-c-check__input"
+        bind:checked="{checkboxValue}"
+        name="{record.id}"
+        type="checkbox"
+        readonly="{!!record.readonly}"
+        id="input-standard-{record.id}"
+        aria-invalid="{invalidEntry}"
+        aria-label="{record.description}" />
+    {:else}
+      <input
+        on:input="{event => checkValue(record, event)}"
+        class="pf-c-form-control"
+        name="{record.id}"
+        type="text"
+        bind:value="{recordValue}"
+        readonly="{!!record.readonly}"
+        id="input-standard-{record.id}"
+        aria-invalid="{invalidEntry}"
+        aria-label="{record.description}" />
+    {/if}
+
     {#if invalidEntry}
       <p class="pf-c-form__helper-text pf-m-error text:red" id="form-help-text-address-helper" aria-live="polite">
         {invalidText}.
