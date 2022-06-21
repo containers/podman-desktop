2f0719dd98441e369dccbe1cef4f1de5583cf386
Dev Kumar <devskumar99@gmail.com>
2022-06-21 12:04:04 -0400

    fix: dark mode for core values
    Signed-off-by: Dev Kumar <devskumar99@gmail.com>

--- website/src/pages/core-values/index.tsx
+++ website/src/pages/core-values/index.tsx
@@ -78,7 +78,7 @@ function FastAndLight() {
   function Simple() {
     return (
       <div>
-        <section className="text-gray-600 dark:bg-zinc-800 dark:text-gray-400 dark:bg-zinc-900 bg-zinc-100 body-font">
+        <section className="text-gray-600 dark:text-gray-400 dark:bg-zinc-900 bg-zinc-100 body-font">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">
