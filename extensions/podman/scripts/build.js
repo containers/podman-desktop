8f183f4564044e38549eec944d2fb11e03d3c68d
Florent Benoit <fbenoit@redhat.com>
2022-06-29 09:11:57 +0200

    feat: include all extensions by default Fixes https://github.com/containers/podman-desktop/issues/238
    Change-Id: Ic70a17c913f45f59cbfac52fc135d4688f479761
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- extensions/podman/scripts/build.js
+++ extensions/podman/scripts/build.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 /**********************************************************************
  * Copyright (C) 2022 Red Hat, Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,19 +28,16 @@ const builtinDirectory = path.resolve(__dirname, '../builtin');
 const unzippedDirectory = path.resolve(builtinDirectory, `${package.name}.cdix`);
 // remove the .cdix file before zipping
 if (fs.existsSync(destFile)) {
-    fs.rmSync(destFile);
+  fs.rmSync(destFile);
 }
 // remove the builtin folder before zipping
 if (fs.existsSync(builtinDirectory)) {
-    fs.rmSync(builtinDirectory, { recursive: true, force: true });
+  fs.rmSync(builtinDirectory, { recursive: true, force: true });
 }
-
 
 zipper.sync.zip(path.resolve(__dirname, '../')).compress().save(destFile);
 
-
-
 // create unzipped built-in
 mkdirp(unzippedDirectory).then(() => {
-        zipper.sync.unzip(destFile).save(unzippedDirectory);
+  zipper.sync.unzip(destFile).save(unzippedDirectory);
 });
