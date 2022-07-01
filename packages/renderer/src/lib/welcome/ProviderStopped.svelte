561a296d77a2e576991851ebf249c00aa212fb79
Florent Benoit <fbenoit@redhat.com>
2022-07-01 10:42:19 +0200

    feat: display stopped providers in onboarding page
    Change-Id: I4564d890e1360f72da34c0a470ae69f50414356d
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/welcome/ProviderStopped.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import type { ProviderInfo } from '../../../../main/src/plugin/api/provider-info';
+import ProviderLinks from './ProviderLinks.svelte';
+import ProviderLogo from './ProviderLogo.svelte';
+
+export let provider: ProviderInfo;
+</script>
+
+<div class="p-2 flex flex-col bg-zinc-700 rounded-lg">
+  <ProviderLogo provider="{provider}" />
+  <div class="flex flex-col items-center text-center">
+    <p class="text-xl text-gray-300">
+      {provider.name} is stopped
+    </p>
+    {#if provider.version}
+      <p class="text-base font-semibold text-gray-400">
+        version {provider.version}
+      </p>
+    {/if}
+  </div>
+  <ProviderLinks provider="{provider}" />
+</div>
