f410a40f89ee1d484763aefc1f1f5cf04462a4e6
Florent Benoit <fbenoit@redhat.com>
2022-08-16 18:11:49 +0200

    feat: use a common component by containers and images screen
    Change-Id: I0c64b715359cf2e8d49cb3a86dbb3f64db9a1fe2
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/ContainerList.svelte
+++ packages/renderer/src/lib/ContainerList.svelte
@@ -14,6 +14,7 @@ import { providerInfos } from '../stores/providers';
 import NoContainerEngineEmptyScreen from './image/NoContainerEngineEmptyScreen.svelte';
 import moment from 'moment';
 import type { Unsubscriber } from 'svelte/store';
+import NavPage from './ui/NavPage.svelte';
 
 const containerUtils = new ContainerUtils();
 let openChoiceModal = false;
@@ -144,104 +145,79 @@ function fromDockerfile(): void {
 }
 </script>
 
-<div class="flex flex-col min-h-full">
-  <div class="min-w-full flex-1">
-    <div class="flex">
-      <div class="pt-5 px-5">
-        <p class="text-xl">Containers</p>
-        <p class="text-sm text-gray-400">
-          Hover over a container to view action buttons; click to open up full details.
-        </p>
-      </div>
-    </div>
-    <div class="flex flex-row">
-      <div class="pt-2 px-5 lg:w-[35rem] w-[22rem]">
-        <div class="flex items-center bg-gray-700 text-gray-400">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5 ml-2 mr-2 "
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-          </svg>
-          <input
-            bind:value="{searchTerm}"
-            type="text"
-            name="containerSearchName"
-            placeholder="Search containers...."
-            class="w-full py-2 outline-none text-sm bg-gray-700" />
-        </div>
-      </div>
-      <div class="flex flex-1 justify-end">
-        <div class="py-5 px-5">
-          <button on:click="{() => toggleCreateContainer()}" class="pf-c-button pf-m-primary" type="button">
-            <span class="pf-c-button__icon pf-m-start">
-              <i class="fas fa-plus-circle" aria-hidden="true"></i>
-            </span>
-            Create container
-          </button>
-        </div>
-      </div>
-    </div>
-    <table
-      class="min-w-full divide-y divide-gray-800 border-t border-t-zinc-700"
-      class:hidden="{containers.length === 0}">
-      <tbody class="bg-zinc-800 divide-y divide-zinc-700">
-        {#each containers as container}
-          <tr class="group h-12 hover:bg-zinc-700">
-            <td class="px-4 whitespace-nowrap hover:cursor-pointer" on:click="{() => openDetailsContainer(container)}">
-              <div class="flex items-center">
-                <div class="flex-shrink-0 w-3 py-3">
-                  <ContainerIcon state="{container.state}" />
-                </div>
-                <div class="ml-4">
-                  <div class="flex flex-nowrap">
-                    <div class="text-sm text-gray-200 overflow-hidden text-ellipsis" title="{container.name}">
-                      {container.name}
-                    </div>
-                    <div class="pl-2 text-sm text-violet-400 overflow-hidden text-ellipsis" title="{container.image}">
-                      {container.image}
-                    </div>
+<NavPage
+  bind:searchTerm
+  title="containers"
+  subtitle="Hover over a container to view action buttons; click to open up full details.">
+  <button
+    slot="additional-actions"
+    on:click="{() => toggleCreateContainer()}"
+    class="pf-c-button pf-m-primary"
+    type="button">
+    <span class="pf-c-button__icon pf-m-start">
+      <i class="fas fa-plus-circle" aria-hidden="true"></i>
+    </span>
+    Create container
+  </button>
+
+  <table
+    slot="table"
+    class="min-w-full divide-y divide-gray-800 border-t border-t-zinc-700"
+    class:hidden="{containers.length === 0}">
+    <tbody class="bg-zinc-800 divide-y divide-zinc-700">
+      {#each containers as container}
+        <tr class="group h-12 hover:bg-zinc-700">
+          <td class="px-4 whitespace-nowrap hover:cursor-pointer" on:click="{() => openDetailsContainer(container)}">
+            <div class="flex items-center">
+              <div class="flex-shrink-0 w-3 py-3">
+                <ContainerIcon state="{container.state}" />
+              </div>
+              <div class="ml-4">
+                <div class="flex flex-nowrap">
+                  <div class="text-sm text-gray-200 overflow-hidden text-ellipsis" title="{container.name}">
+                    {container.name}
                   </div>
-                  <div class="flex flex-row text-xs font-extra-light text-gray-500">
-                    <div>{container.state}</div>
-                    <!-- Hide in case of single engines-->
-                    {#if multipleEngines}
-                      <div class="mx-2 px-2 inline-flex text-xs font-extralight rounded-sm bg-zinc-700 text-slate-400">
-                        {container.engineName}
-                      </div>
-                    {/if}
-                    <div class="pl-2 pr-2">{container.port}</div>
+                  <div class="pl-2 text-sm text-violet-400 overflow-hidden text-ellipsis" title="{container.image}">
+                    {container.image}
                   </div>
                 </div>
+                <div class="flex flex-row text-xs font-extra-light text-gray-500">
+                  <div>{container.state}</div>
+                  <!-- Hide in case of single engines-->
+                  {#if multipleEngines}
+                    <div class="mx-2 px-2 inline-flex text-xs font-extralight rounded-sm bg-zinc-700 text-slate-400">
+                      {container.engineName}
+                    </div>
+                  {/if}
+                  <div class="pl-2 pr-2">{container.port}</div>
+                </div>
               </div>
-            </td>
-            <td class="px-6 py-2 whitespace-nowrap w-10">
-              <div class="flex items-center">
-                <div class="ml-2 text-sm text-gray-400">{container.uptime}</div>
-              </div>
-            </td>
-            <td class="px-6 whitespace-nowrap">
-              <div class="flex flex-row justify-end opacity-0 group-hover:opacity-100 ">
-                <ContainerActions container="{container}" />
-              </div>
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+            </div>
+          </td>
+          <td class="px-6 py-2 whitespace-nowrap w-10">
+            <div class="flex items-center">
+              <div class="ml-2 text-sm text-gray-400">{container.uptime}</div>
+            </div>
+          </td>
+          <td class="px-6 whitespace-nowrap">
+            <div class="flex flex-row justify-end opacity-0 group-hover:opacity-100 ">
+              <ContainerActions container="{container}" />
+            </div>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+
+  <div slot="empty" class="min-h-full">
+    {#if providerConnections.length > 0}
+      <ContainerEmptyScreen slot="empty" containers="{$filtered}" />
+    {:else}
+      <NoContainerEngineEmptyScreen slot="empty" />
+    {/if}
   </div>
-  {#if providerConnections.length > 0}
-    <ContainerEmptyScreen containers="{$filtered}" />
-  {:else}
-    <NoContainerEngineEmptyScreen />
-  {/if}
-</div>
+</NavPage>
+
 {#if openChoiceModal}
   <Modal
     on:close="{() => {
--- packages/renderer/src/lib/ImagesList.svelte
+++ packages/renderer/src/lib/ImagesList.svelte
@@ -14,6 +14,7 @@ import { providerInfos } from '../stores/providers';
 import RunContainerModal from './image/RunContainerModal.svelte';
 import PushImageModal from './image/PushImageModal.svelte';
 import { ImageUtils } from './image/image-utils';
+import NavPage from './ui/NavPage.svelte';
 
 let searchTerm = '';
 $: searchPattern.set(searchTerm);
@@ -75,124 +76,96 @@ function openDetailsImage(image: ImageInfoUI) {
 }
 </script>
 
-<div class="flex flex-col min-h-full">
-  <div class="min-w-full flex-1">
-    <div class="flex">
-      <div class="pt-5 px-5">
-        <p class="text-xl">Images</p>
-        <p class="text-sm text-gray-400">Hover over an image to view action buttons.</p>
-      </div>
-    </div>
-    <div class="flex flex-row">
-      <div class="pt-2 px-5 lg:w-[35rem] w-[22rem]">
-        <div class="flex items-center bg-gray-700 text-gray-400">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5 ml-2 mr-2 "
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-          </svg>
-          <input
-            bind:value="{searchTerm}"
-            type="text"
-            name="containerSearchName"
-            placeholder="Search images...."
-            class="w-full py-2 outline-none text-sm bg-gray-700" />
-        </div>
-      </div>
-      <div class="flex flex-1 justify-end">
-        <div class="py-5 px-5 space-x-2 flex flex-nowrap">
-          <button on:click="{() => gotoPullImage()}" class="pf-c-button pf-m-primary" type="button">
-            <span class="pf-c-button__icon pf-m-start">
-              <i class="fas fa-arrow-circle-down" aria-hidden="true"></i>
-            </span>
-            Pull Image
-          </button>
-          <button on:click="{() => gotoBuildImage()}" class="pf-c-button pf-m-primary" type="button">
-            <span class="pf-c-button__icon pf-m-start">
-              <i class="fas fa-cube" aria-hidden="true"></i>
-            </span>
-            Build Image
-          </button>
-        </div>
-      </div>
-    </div>
-    <table class="min-w-full divide-y divide-gray-800 border-t border-t-zinc-700">
-      <tbody class="bg-zinc-800 divide-y divide-zinc-700">
-        {#each images as image}
-          <tr class="group h-12  hover:bg-zinc-700">
-            <td class="px-4 whitespace-nowrap w-10 hover:cursor-pointer" on:click="{() => openDetailsImage(image)}">
-              <div class="flex items-center">
-                <div class="flex-shrink-0 w-3 py-3">
-                  <Fa class="text-gray-400" icon="{faLayerGroup}" />
-                </div>
-                <div class="ml-4">
-                  <div class="flex flex-row items-center">
-                    <div class="text-sm text-gray-200">{image.name}</div>
-                  </div>
-                  <div class="flex flex-row items-center">
-                    <div class="text-xs text-violet-400">{image.shortId}</div>
-                    <div class="ml-1 text-xs font-extra-light text-gray-500">{image.tag}</div>
-                  </div>
-                  <div class="flex flex-row text-xs font-extra-light text-gray-500">
-                    <!-- Hide in case of single engine-->
-                    {#if multipleEngines}
-                      <div class="px-2 inline-flex text-xs font-extralight rounded-full bg-slate-900 text-slate-400">
-                        {image.engineName}
-                      </div>
-                    {/if}
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td class="px-6 py-2 whitespace-nowrap w-10">
-              <div class="flex items-center">
-                <div class="ml-2 text-sm text-gray-200">{image.humanCreationDate}</div>
-              </div>
-            </td>
-            <td class="px-6 py-2 whitespace-nowrap w-10">
-              <div class="flex">
-                <div class="w-full text-right text-sm text-gray-200">{image.humanSize}</div>
-              </div>
-            </td>
-            <td class="px-6 whitespace-nowrap">
-              <div class="flex opacity-0 flex-row justify-end group-hover:opacity-100">
-                <ImageActions
-                  image="{image}"
-                  onPushImage="{handlePushImageModal}"
-                  onRunContainerImage="{handleRunContainerFromImageModal}" />
-              </div>
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+<NavPage
+  bind:searchTerm
+  title="images"
+  subtitle="Hover over an image to view action buttons; click to open up full details.">
+  <div slot="additional-actions" class="space-x-2 flex flex-nowrap">
+    <button on:click="{() => gotoPullImage()}" class="pf-c-button pf-m-primary" type="button">
+      <span class="pf-c-button__icon pf-m-start">
+        <i class="fas fa-arrow-circle-down" aria-hidden="true"></i>
+      </span>
+      Pull Image
+    </button>
+    <button on:click="{() => gotoBuildImage()}" class="pf-c-button pf-m-primary" type="button">
+      <span class="pf-c-button__icon pf-m-start">
+        <i class="fas fa-cube" aria-hidden="true"></i>
+      </span>
+      Build Image
+    </button>
   </div>
-  {#if providerConnections.length > 0}
-    <ImageEmptyScreen images="{$filtered}" />
-  {:else}
-    <NoContainerEngineEmptyScreen />
-  {/if}
 
-  {#if runContainerFromImageModal}
-    <RunContainerModal
-      image="{runContainerFromImageInfo}"
-      closeCallback="{() => {
-        closeModals();
-      }}" />
-  {/if}
+  <table slot="table" class="min-w-full divide-y divide-gray-800 border-t border-t-zinc-700">
+    <tbody class="bg-zinc-800 divide-y divide-zinc-700">
+      {#each images as image}
+        <tr class="group h-12  hover:bg-zinc-700">
+          <td class="px-4 whitespace-nowrap w-10 hover:cursor-pointer" on:click="{() => openDetailsImage(image)}">
+            <div class="flex items-center">
+              <div class="flex-shrink-0 w-3 py-3">
+                <Fa class="text-gray-400" icon="{faLayerGroup}" />
+              </div>
+              <div class="ml-4">
+                <div class="flex flex-row items-center">
+                  <div class="text-sm text-gray-200">{image.name}</div>
+                </div>
+                <div class="flex flex-row items-center">
+                  <div class="text-xs text-violet-400">{image.shortId}</div>
+                  <div class="ml-1 text-xs font-extra-light text-gray-500">{image.tag}</div>
+                </div>
+                <div class="flex flex-row text-xs font-extra-light text-gray-500">
+                  <!-- Hide in case of single engine-->
+                  {#if multipleEngines}
+                    <div class="px-2 inline-flex text-xs font-extralight rounded-full bg-slate-900 text-slate-400">
+                      {image.engineName}
+                    </div>
+                  {/if}
+                </div>
+              </div>
+            </div>
+          </td>
+          <td class="px-6 py-2 whitespace-nowrap w-10">
+            <div class="flex items-center">
+              <div class="ml-2 text-sm text-gray-200">{image.humanCreationDate}</div>
+            </div>
+          </td>
+          <td class="px-6 py-2 whitespace-nowrap w-10">
+            <div class="flex">
+              <div class="w-full text-right text-sm text-gray-200">{image.humanSize}</div>
+            </div>
+          </td>
+          <td class="px-6 whitespace-nowrap">
+            <div class="flex opacity-0 flex-row justify-end group-hover:opacity-100">
+              <ImageActions
+                image="{image}"
+                onPushImage="{handlePushImageModal}"
+                onRunContainerImage="{handleRunContainerFromImageModal}" />
+            </div>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  <div slot="empty" class="min-h-full">
+    {#if providerConnections.length > 0}
+      <ImageEmptyScreen images="{$filtered}" />
+    {:else}
+      <NoContainerEngineEmptyScreen />
+    {/if}
 
-  {#if pushImageModal}
-    <PushImageModal
-      imageInfoToPush="{pushImageModalImageInfo}"
-      closeCallback="{() => {
-        closeModals();
-      }}" />
-  {/if}
-</div>
+    {#if runContainerFromImageModal}
+      <RunContainerModal
+        image="{runContainerFromImageInfo}"
+        closeCallback="{() => {
+          closeModals();
+        }}" />
+    {/if}
+
+    {#if pushImageModal}
+      <PushImageModal
+        imageInfoToPush="{pushImageModalImageInfo}"
+        closeCallback="{() => {
+          closeModals();
+        }}" />
+    {/if}
+  </div>
+</NavPage>
