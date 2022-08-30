13ef504eaa1d2edb7247de469345840d3574aeee
Yevhen Vydolob <yvydolob@redhat.com>
2022-08-30 12:09:37 +0100

    fix: WSL2 detection check (#430)
    * fix: fix WSL2 detection check
    
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>
    
    * change hyper-v detection
    
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>
    
    * remove redundant wsl output check
    
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>
    
    * Fix hyper-v check
    
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>
    
    * fix podman install package path resolution
    
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>
    
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

--- extensions/podman/src/podman-install.ts
+++ extensions/podman/src/podman-install.ts
@@ -244,7 +244,7 @@ abstract class BaseInstaller implements Installer {
       return path.resolve(__dirname, '..', 'assets');
     } else {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return path.resolve((process as any).resourcesPath, 'extensions', 'podman', 'builtin', 'podman.cdix', 'assets');
+      return path.resolve((process as any).resourcesPath, 'extensions', 'podman', 'assets');
     }
   }
 
@@ -430,11 +430,8 @@ class HyperVCheck extends BaseCheck {
   async execute(): Promise<extensionApi.CheckResult> {
     try {
       // set CurrentUICulture to force output in english
-      const res = await execPromise('powershell.exe', [
-        "[Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US';",
-        '(Get-Service vmcompute).DisplayName',
-      ]);
-      if (res === 'Hyper-V Host Compute Service') {
+      const res = await execPromise('powershell.exe', ['(Get-Service vmcompute).DisplayName']);
+      if (res.indexOf('Hyper-V') >= 0) {
         return this.createSuccessfulResult();
       }
     } catch (err) {
@@ -453,32 +450,32 @@ class WSL2Check extends BaseCheck {
 
   async execute(): Promise<extensionApi.CheckResult> {
     try {
-      // set WSL_UTF8 to force WSL2 output in UTF8, otherwise it will use utf16le
-      const res = await execPromise('wsl', ['-l', '-v'], { env: { WSL_UTF8: '1' } });
-      if (!res.startsWith('Usage: wsl.exe [Argument]')) {
-        return this.createSuccessfulResult();
+      const res = await execPromise('wsl', ['--status'], { env: { WSL_UTF8: '1' } });
+      const output = this.normalizeOutput(res);
+      if (!output) {
+        throw new Error();
       }
     } catch (err) {
-      if (typeof err === 'string') {
-        // this is workaround, wsl2 some time send output in utf16le, but we treat that as utf8,
-        // this code just eliminate every 'empty' character
-        let str = '';
-        for (let i = 0; i < err.length; i++) {
-          if (err.charCodeAt(i) !== 0) {
-            str += err.charAt(i);
-          }
-        }
+      return this.createFailureResult(
+        'WSL2 is not installed. Call "wsl --install" in terminal.',
+        'Install WSL',
+        'https://docs.microsoft.com/en-us/windows/wsl/install-manual',
+      );
+    }
 
-        if (str.indexOf('Windows Subsystem for Linux has no installed distributions.') !== -1) {
-          // WSL2 installed, it just doesn't have any distro installed.
-          return this.createSuccessfulResult();
-        }
+    return this.createSuccessfulResult();
+  }
+
+  private normalizeOutput(out: string): string {
+    // this is workaround, wsl2 some time send output in utf16le, but we treat that as utf8,
+    // this code just eliminate every 'empty' character
+    let str = '';
+    for (let i = 0; i < out.length; i++) {
+      if (out.charCodeAt(i) !== 0) {
+        str += out.charAt(i);
       }
     }
-    return this.createFailureResult(
-      'WSL2 is not installed. Call "wsl --install" in terminal.',
-      'Install WSL',
-      'https://docs.microsoft.com/en-us/windows/wsl/install-manual',
-    );
+
+    return str;
   }
 }
