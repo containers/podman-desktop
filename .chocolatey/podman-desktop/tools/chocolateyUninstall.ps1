7fb2eb73d900c9574f69b0cac2c07d028b277292
Florent Benoit <fbenoit@redhat.com>
2022-09-05 13:54:41 +0200

    chore: add publication scripts for brew, chocolatey and winget fixes https://github.com/containers/podman-desktop/issues/445 fixes https://github.com/containers/podman-desktop/issues/449
    Change-Id: I722b1c6061d1748209c1b4d25598cbc02b30476a
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ .chocolatey/podman-desktop/tools/chocolateyUninstall.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'podman-desktop'
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'PodmanDesktop'
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs = @{
+      packageName    = $packageName
+      fileType       = 'EXE'
+      silentArgs     = '/S'
+      validExitCodes = @(0)
+      file           = "$($_.UninstallString.Trim('"'))"
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$key.Count matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
---
+++ .chocolatey/podman-desktop/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$version = $env:VERSION
+$releases = 'https://github.com/containers/podman-desktop/releases/tag/v' + $version
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+        ".\podman-desktop.nuspec" = @{
+            "\<version\>.+" = "<version>$($Latest.Version)</version>"
+            "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
+    }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url64   = $download_page.links | ? href -match '-setup.exe$' | % href | select -First 1
+    $version = (Split-Path ( Split-Path $url64 ) -Leaf).Substring(1)
+
+    @{
+        URL64   = 'https://github.com' + $url64
+        Version = $version
+        ReleaseNotes = $releases
+    }
+}
+
+update -ChecksumFor 64
