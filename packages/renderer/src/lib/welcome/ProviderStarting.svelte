f7947874bda740bc9a9198e38d2001031c20bf17
Florent Benoit <fbenoit@redhat.com>
2022-09-27 16:57:49 +0200

    feat: adds starting state Will help for https://github.com/containers/podman-desktop/issues/334
    Change-Id: Ie282b08e56577c142346cb5a8606233d3ba16d83
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/welcome/ProviderStarting.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+import type { CheckStatus, ProviderInfo } from '../../../../main/src/plugin/api/provider-info';
+import ProviderLinks from './ProviderLinks.svelte';
+import ProviderLogo from './ProviderLogo.svelte';
+
+export let provider: ProviderInfo;
+</script>
+
+<div class="p-2 flex flex-col bg-zinc-700 rounded-lg">
+  <ProviderLogo provider="{provider}" />
+  <div class="flex flex-col items-center text-center">
+    <p class="text-xl text-gray-300">
+      {provider.name} is starting...
+    </p>
+    {#if provider.version}
+      <p class="text-base font-semibold text-gray-400">
+        version {provider.version}
+      </p>
+    {/if}
+    {#if provider.containerConnections.length > 0}
+      <div class="flex flex-row  text-xs text-gray-500 mt-4">
+        <p>
+          {provider.containerConnections.map(c => c.name).join(', ')}
+        </p>
+      </div>
+    {/if}
+  </div>
+  <ProviderLinks provider="{provider}" />
+</div>
