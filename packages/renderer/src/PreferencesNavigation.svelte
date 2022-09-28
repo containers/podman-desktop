a66f376d723731939ccab04fdd60a7032bc91cb7
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2022-09-28 11:39:42 +0300

    refactor: move settings navigation tree to main navigation area
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

---
+++ packages/renderer/src/PreferencesNavigation.svelte
@@ -0,0 +1,248 @@
+<script lang="ts">
+import { onMount } from 'svelte';
+import { Buffer } from 'buffer';
+import { extensionInfos } from './stores/extensions';
+import { configurationProperties } from './stores/configurationProperties';
+import { ConfigurationRegistry } from '../../main/src/plugin/configuration-registry';
+import type { ProviderInfo } from '../../main/src/plugin/api/provider-info';
+
+export let exitSettingsCallback: () => void;
+export let meta;
+
+let extensions, configProperties, providers;
+
+$: extensions = [];
+$: configProperties = new Map();
+$: providers = [];
+$: sectionExpanded = {};
+
+function toggleSection(provider: string) {
+  sectionExpanded[provider] = sectionExpanded[provider] === undefined ? true : !sectionExpanded[provider];
+}
+
+onMount(async () => {
+  await fetchProviderInfos();
+  extensionInfos.subscribe(value => {
+    extensions = value;
+  });
+  configurationProperties.subscribe(value => {
+    configProperties = value
+      .filter(property => property.scope === ConfigurationRegistry.DEFAULT_SCOPE)
+      .reduce(function (map, property) {
+        let [parentLeftId] = property.parentId.split('.');
+
+        if (map[parentLeftId] === undefined) {
+          map[parentLeftId] = [];
+        }
+
+        let children = map[parentLeftId].find(item => item.id === property.parentId);
+        if (children === undefined) {
+          map[parentLeftId].push({ id: property.parentId, title: property.title });
+        }
+        return map;
+      }, new Map());
+  });
+});
+
+async function fetchProviderInfos() {
+  providers = await window.getProviderInfos();
+}
+
+$: isCurrentPage = (pathParam: string): boolean => meta.url === pathParam;
+$: addExpandedClass = (section: string): string => (sectionExpanded[section] ? 'pf-m-expanded' : '');
+$: addCurrentClass = (pathParam: string): string =>
+  isCurrentPage(pathParam) ? 'dark:text-white pf-m-current' : 'dark:text-gray-400';
+$: isAriaExpanded = (section: string): string => (sectionExpanded[section] ? 'true' : 'false');
+$: addSectionHiddenClass = (section: string): string => (sectionExpanded[section] ? '' : 'hidden');
+$: addExpandableClass = (provider: ProviderInfo): string =>
+  provider.containerConnections.length > 0 ? 'pf-m-expandable' : '';
+$: addHiddenClass = (provider: ProviderInfo): string => (provider.containerConnections.length > 0 ? '' : 'hidden');
+</script>
+
+<nav
+  class="pf-c-nav z-0 group w-52 shadow flex-col justify-between flex transition-all duration-500 ease-in-out"
+  aria-label="Global">
+  <ul class="pf-c-nav__list h-full overflow-auto">
+    <!-- Resources configuration start -->
+    <li
+      class="pf-c-nav__item pf-m-expandable {addExpandedClass(
+        'resources',
+      )} dark:text-gray-400 hover:text-gray-300 cursor-pointer items-center">
+      <button
+        class="pf-c-nav__link"
+        id="configuration-section-resources"
+        aria-expanded="{isAriaExpanded('resources')}"
+        on:click="{() => toggleSection('resources')}">
+        Resources
+        <span class="pf-c-nav__toggle">
+          <span class="pf-c-nav__toggle-icon">
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+          </span>
+        </span>
+      </button>
+      <section class="pf-c-nav__subnav {addSectionHiddenClass('resources')}">
+        <ul class="pf-c-nav__list">
+          {#each providers as provider}
+            <li
+              class="pf-c-nav__item {addExpandableClass(provider)} {addExpandedClass(provider.name)} {addCurrentClass(
+                `/preferences/provider/${provider.internalId}`,
+              )}">
+              <a
+                class="pf-c-nav__link"
+                id="configuration-section-provider-{provider.name.toLowerCase()}"
+                aria-expanded="{isAriaExpanded(provider.name)}"
+                href="/preferences/provider/{provider.internalId}">
+                {provider.name}
+                <span class="pf-c-nav__toggle {addHiddenClass(provider)}">
+                  <span class="pf-c-nav__toggle-icon" on:click="{() => toggleSection(provider.name)}">
+                    <i class="fas fa-angle-right" aria-hidden="true"></i>
+                  </span>
+                </span>
+              </a>
+              <section class="pf-c-nav__subnav {addSectionHiddenClass(provider.name)}">
+                <ul class="pf-c-nav__list">
+                  {#each provider.containerConnections as connection}
+                    <li
+                      class="pf-c-nav__item {addCurrentClass(
+                        `/preferences/container-connection/${provider.internalId}/${Buffer.from(
+                          connection.endpoint.socketPath,
+                        ).toString('base64')}`,
+                      )}">
+                      <a
+                        href="/preferences/container-connection/{provider.internalId}/{Buffer.from(
+                          connection.endpoint.socketPath,
+                        ).toString('base64')}"
+                        id="configuration-section-provider-{provider.name.toLowerCase()}-{connection.name
+                          .toLowerCase()
+                          .replaceAll(' ', '_')}"
+                        class="pf-c-nav__link">{connection.name}</a>
+                    </li>
+                  {/each}
+                </ul>
+              </section>
+            </li>
+          {/each}
+        </ul>
+      </section>
+    </li>
+    <!-- Resources configuration end -->
+
+    <!-- Proxies configuration start -->
+    <li
+      class="pf-c-nav__item flex w-full justify-between {addCurrentClass(
+        '/preferences/proxies',
+      )} hover:text-gray-300 cursor-pointer items-center">
+      <a href="/preferences/proxies" id="configuration-section-proxy" class="pf-c-nav__link ">
+        <div class="flex items-center">
+          <span class="hidden md:block group-hover:block">Proxy</span>
+        </div>
+      </a>
+    </li>
+    <!-- Proxies configuration end -->
+
+    <!-- Registries configuration start -->
+    <li
+      class="pf-c-nav__item flex w-full justify-between {addCurrentClass(
+        '/preferences/registries',
+      )} hover:text-gray-300 cursor-pointer items-center">
+      <a href="/preferences/registries" id="configuration-section-registries" class="pf-c-nav__link">
+        <div class="flex items-center">
+          <span class="hidden md:block group-hover:block">Registries</span>
+        </div>
+      </a>
+    </li>
+    <!-- Registries configuration end -->
+
+    <!-- Extensions catalog configuration start -->
+    <li
+      class="pf-c-nav__item pf-m-expandable {addExpandedClass('extensionsCatalog')} {addCurrentClass(
+        '/preferences/extensions',
+      )} hover:text-gray-300 cursor-pointer items-center">
+      <button
+        class="pf-c-nav__link text-left"
+        id="configuration-section-extensions-catalog"
+        aria-expanded="{isAriaExpanded('extensionsCatalog')}"
+        on:click="{() => toggleSection('extensionsCatalog')}">
+        Extensions
+        <span class="pf-c-nav__toggle">
+          <span class="pf-c-nav__toggle-icon">
+            <i class="fas fa-angle-right" aria-hidden="true"></i>
+          </span>
+        </span>
+      </button>
+      <section class="pf-c-nav__subnav {addSectionHiddenClass('extensionsCatalog')}">
+        <ul class="pf-c-nav__list">
+          {#each extensions as extension}
+            <li class="pf-c-nav__item {addCurrentClass(`/preferences/extension/${extension.name}`)} ">
+              <a
+                href="/preferences/extension/{extension.name}"
+                id="configuration-section-extensions-catalog-{extension.name.toLowerCase()}"
+                class="pf-c-nav__link">{extension.name}</a>
+            </li>
+          {/each}
+        </ul>
+      </section>
+    </li>
+    <!-- Extensions catalog configuration end -->
+
+    <!-- Docker desktop extensions configuration start -->
+    <li
+      class="pf-c-nav__item flex w-full justify-between  {addCurrentClass(
+        '/preferences/ddExtensions',
+      )} hover:text-gray-300 cursor-pointer items-center">
+      <a href="/preferences/ddExtensions" id="configuration-section-docker-desktop-extensions" class="pf-c-nav__link ">
+        <div class="flex items-center">
+          <span class="hidden md:block group-hover:block">Docker Desktop Extensions</span>
+        </div>
+      </a>
+    </li>
+    <!-- Docker desktop extensions configuration start -->
+
+    <!-- Default configuration properties start -->
+    {#each Object.entries(configProperties) as [configSection, configItems]}
+      <li
+        class="pf-c-nav__item pf-m-expandable {addExpandedClass(configSection)} {addCurrentClass(
+          `/preferences/default/${configSection}`,
+        )} hover:text-gray-300 cursor-pointer items-center">
+        <a
+          class="pf-c-nav__link"
+          id="configuration-section-{configSection.toLowerCase()}"
+          aria-expanded="{isAriaExpanded(configSection)}"
+          href="/preferences/default/{configSection}">
+          <span class="hidden md:block group-hover:block mr-5 capitalize">{configSection}</span>
+          {#if configItems.length > 0}
+            <span class="pf-c-nav__toggle">
+              <span class="pf-c-nav__toggle-icon" on:click="{() => toggleSection(configSection)}">
+                <i class="fas fa-angle-right" aria-hidden="true"></i>
+              </span>
+            </span>
+          {/if}
+        </a>
+        <section class="pf-c-nav__subnav {addSectionHiddenClass(configSection)}">
+          <ul class="pf-c-nav__list">
+            {#each configItems as configItem}
+              <li class="pf-c-nav__item {addCurrentClass(`/preferences/default/${configItem.id}`)}">
+                <a
+                  href="/preferences/default/{configItem.id}"
+                  id="configuration-section-{configSection.toLowerCase()}-{configItem.title.toLowerCase()}"
+                  class="pf-c-nav__link">{configItem.title}</a>
+              </li>
+            {/each}
+          </ul>
+        </section>
+      </li>
+    {/each}
+    <!-- Default configuration properties end -->
+  </ul>
+
+  <ul class="pf-c-nav__list">
+    <li
+      class="pf-c-nav__item pf-c-nav__link flex w-full justify-between dark:text-gray-400 hover:text-gray-300 cursor-pointer items-center"
+      on:click="{exitSettingsCallback}">
+      <div class="flex items-center">
+        <i class="fa fa-angle-left"></i>
+        <span class="hidden md:block group-hover:block mx-2">Exit settings</span>
+      </div>
+    </li>
+  </ul>
+</nav>
--- packages/renderer/src/lib/preferences/PreferencesPage.svelte
+++ packages/renderer/src/lib/preferences/PreferencesPage.svelte
@@ -1,189 +1,39 @@
 <script lang="ts">
 import { configurationProperties } from '../../stores/configurationProperties';
-import { Buffer } from 'buffer';
 import { onMount } from 'svelte';
-import { Route, router } from 'tinro';
+import { Route } from 'tinro';
 import {
   ConfigurationRegistry,
   IConfigurationPropertyRecordedSchema,
 } from '../../../../main/src/plugin/configuration-registry';
 
-import TreeView from '../treeview/TreeView.svelte';
-import type { TreeViewDataItem } from '../treeview/TreeViewDataItem';
 import PreferencesRendering from './PreferencesRendering.svelte';
 import PreferencesContainerConnectionRendering from './PreferencesContainerConnectionRendering.svelte';
-import { providerInfos } from '../../stores/providers';
 import PreferencesProviderRendering from './PreferencesProviderRendering.svelte';
-import { extensionInfos } from '../../stores/extensions';
 import PreferencesExtensionRendering from './PreferencesExtensionRendering.svelte';
-import type { ExtensionInfo } from '../../../../main/src/plugin/api/extension-info';
 import PreferencesRegistriesRendering from './PreferencesRegistriesRendering.svelte';
 import PreferencesPageDockerExtensions from '../docker-extension/PreferencesPageDockerExtensions.svelte';
 import PreferencesProxiesRendering from './PreferencesProxiesRendering.svelte';
 
-let items: TreeViewDataItem[] = [];
-let extensions: ExtensionInfo[] = [];
 let properties: IConfigurationPropertyRecordedSchema[];
-
-function onSelectedItem(item: TreeViewDataItem) {
-  if (item.id.startsWith('provider@')) {
-    const providerId = item.id.split('@')[1];
-    router.goto(`/preferences/provider/${providerId}`);
-  } else if (item.id.startsWith('container-connection@')) {
-    // extract provider by the name before the @
-    const provider = item.id.split('@')[1];
-    const connection = item.id.split('@')[2];
-    router.goto(`/preferences/container-connection/${provider}/${connection}`);
-  } else if (item.id === 'extensions') {
-    router.goto(`/preferences/extensions`);
-  } else if (item.id === 'ddExtensions') {
-    router.goto(`/preferences/ddExtensions`);
-  } else if (item.id.startsWith('extension@')) {
-    // extract extension by the name before the @
-    const extensionId = item.id.split('@')[1];
-    router.goto(`/preferences/extension/${extensionId}`);
-  } else if (item.id === 'registries') {
-    router.goto('/preferences/registries');
-  } else if (item.id === 'proxies') {
-    router.goto('/preferences/proxies');
-  } else {
-    router.goto(`/preferences/default/${item.id}`);
-  }
-}
-
-async function buildTreeViewData(configProperties?: IConfigurationPropertyRecordedSchema[]): Promise<void> {
-  const treeViewDataItems: TreeViewDataItem[] = [];
-
-  if (!configProperties) {
-    if (properties) {
-      configProperties = properties;
-    } else {
-      return;
-    }
-  }
-
-  // map all the connections
-  const providers = await window.getProviderInfos();
-  const providerTreeViewDataItems = [];
-  providers.forEach(provider => {
-    const providerTreeViewDataItem = {
-      id: `provider@${provider.internalId}`,
-      name: provider.name,
-      children: [],
-    };
-    providerTreeViewDataItems.push(providerTreeViewDataItem);
-
-    // add under the provider the container connection links
-    provider.containerConnections.forEach(connection => {
-      providerTreeViewDataItem.children.push({
-        // encode socketpath as base64
-        id: `container-connection@${provider.internalId}@${Buffer.from(connection.endpoint.socketPath).toString(
-          'base64',
-        )}`,
-        name: `${connection.name}`,
-        children: [],
-      });
-    });
-  });
-
-  if (providerTreeViewDataItems.length > 0) {
-    treeViewDataItems.push({
-      id: 'providers',
-      name: 'Resources',
-      children: providerTreeViewDataItems,
-    });
-  }
-
-  // Add proxies
-  treeViewDataItems.push({
-    id: 'proxies',
-    name: 'Proxy',
-    children: [],
-  });
-
-  // Add registries
-  treeViewDataItems.push({
-    id: 'registries',
-    name: 'Registries',
-    children: [],
-  });
-
-  // then, add all features (using the default scope)
-  configProperties
-    .filter(property => property.scope === ConfigurationRegistry.DEFAULT_SCOPE)
-    .forEach(property => {
-      // split parent id from property id
-      const [parentLeftId] = property.parentId.split('.');
-      let parentItem = treeViewDataItems.find(item => item.id === parentLeftId);
-      if (!parentItem) {
-        parentItem = {
-          id: parentLeftId,
-          name: parentLeftId,
-          children: [],
-        };
-        treeViewDataItems.push(parentItem);
-      }
-      // add child if not existing
-      let childItem = parentItem.children.find(item => item.id === property.parentId);
-      if (!childItem) {
-        parentItem.children.push({
-          id: property.parentId,
-          name: property.title,
-          children: [],
-        });
-      }
-      properties = configProperties;
-    });
-
-  // now adds the extensions
-  const extensionsTreeViewDataItems = [];
-  extensions.forEach(extension => {
-    const extensionItem = {
-      id: `extension@${extension.name}`,
-      name: extension.name,
-      children: [],
-    };
-    extensionsTreeViewDataItems.push(extensionItem);
-  });
-  if (extensionsTreeViewDataItems.length > 0) {
-    treeViewDataItems.push({
-      id: 'extensions',
-      name: 'Extensions Catalog',
-      children: extensionsTreeViewDataItems,
-    });
-  }
-
-  // now adds the Docker Desktop Extensions
-  treeViewDataItems.push({
-    id: 'ddExtensions',
-    name: 'Docker Desktop Extensions',
-    children: [],
-  });
-
-  items = treeViewDataItems;
-}
+let defaultPrefPageId: string;
 
 onMount(async () => {
   configurationProperties.subscribe(value => {
-    buildTreeViewData(value);
-  });
-  providerInfos.subscribe(() => {
-    buildTreeViewData();
-  });
-  extensionInfos.subscribe(value => {
-    extensions = value;
-    buildTreeViewData();
+    properties = value;
+    [defaultPrefPageId] = value
+      .filter(property => property.scope === ConfigurationRegistry.DEFAULT_SCOPE)
+      .values()
+      .next()
+      .value.parentId.split('.');
   });
 });
 </script>
 
 <div class="flex h-full">
-  <div class="pr-2 w-[250px]">
-    <TreeView items="{items}" onSelect="{item => onSelectedItem(item)}" />
-  </div>
   <Route path="/">
-    {#if items.length > 0}
-      <PreferencesRendering key="{items[0].id}" properties="{properties}" />
+    {#if defaultPrefPageId !== undefined}
+      <PreferencesRendering key="{defaultPrefPageId}" properties="{properties}" />
     {:else}
       empty
     {/if}
