6d2485f397651e13c50a095a6dd4f1ef2ca09064
Florent Benoit <fbenoit@redhat.com>
2022-10-12 14:22:59 +0200

    feat: include title for Dashboard page
    Change-Id: I473d77fa5a1dd7c2ccd6c4f9e998d1bd0d4c2bc0
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/welcome/WelcomePage.svelte
+++ packages/renderer/src/lib/welcome/WelcomePage.svelte
@@ -8,6 +8,7 @@ import ProviderInstalled from './ProviderInstalled.svelte';
 import ProviderConfigured from './ProviderConfigured.svelte';
 import ProviderStopped from './ProviderStopped.svelte';
 import ProviderStarting from './ProviderStarting.svelte';
+import NavPage from '../ui/NavPage.svelte';
 
 $: providersNotInstalled = $providerInfos.filter(provider => provider.status === 'not-installed');
 $: providersInstalled = $providerInfos.filter(provider => provider.status === 'installed');
@@ -17,52 +18,54 @@ $: providersStarting = $providerInfos.filter(provider => provider.status === 'st
 $: providersStopped = $providerInfos.filter(provider => provider.status === 'stopped');
 </script>
 
-<div class="flex flex-col min-h-full">
-  <div class="min-w-full flex-1">
-    <div class="pt-5 px-5 space-y-5">
-      <!-- Provider is ready display a box to indicate some information -->
-      {#if providersReady.length > 0}
-        {#each providersReady as providerReady}
-          <ProviderReady provider="{providerReady}" />
-        {/each}
-      {/if}
+<NavPage searchEnabled="{false}" title="Dashboard" subtitle="&nbsp;">
+  <div slot="empty" class="flex flex-col min-h-full bg-zinc-700">
+    <div class="min-w-full flex-1">
+      <div class="pt-5 px-5 space-y-5">
+        <!-- Provider is ready display a box to indicate some information -->
+        {#if providersReady.length > 0}
+          {#each providersReady as providerReady}
+            <ProviderReady provider="{providerReady}" />
+          {/each}
+        {/if}
 
-      <!-- Provider is starting -->
-      {#if providersStarting.length > 0}
-        {#each providersStarting as providerStarting}
-          <ProviderStarting provider="{providerStarting}" />
-        {/each}
-      {/if}
+        <!-- Provider is starting -->
+        {#if providersStarting.length > 0}
+          {#each providersStarting as providerStarting}
+            <ProviderStarting provider="{providerStarting}" />
+          {/each}
+        {/if}
 
-      <!-- Provider is installed but not ready, it requires a user action
-        display a box to indicate how to make the provider ready -->
-      {#if providersInstalled.length > 0}
-        {#each providersInstalled as providerInstalled}
-          <ProviderInstalled provider="{providerInstalled}" />
-        {/each}
-      {/if}
+        <!-- Provider is installed but not ready, it requires a user action
+          display a box to indicate how to make the provider ready -->
+        {#if providersInstalled.length > 0}
+          {#each providersInstalled as providerInstalled}
+            <ProviderInstalled provider="{providerInstalled}" />
+          {/each}
+        {/if}
 
-      <!-- Provider is configured but not ready, it requires a user action
-        display a box to indicate how to make the provider ready -->
-      {#if providersConfigured.length > 0}
-        {#each providersConfigured as providerConfigured}
-          <ProviderConfigured provider="{providerConfigured}" />
-        {/each}
-      {/if}
+        <!-- Provider is configured but not ready, it requires a user action
+          display a box to indicate how to make the provider ready -->
+        {#if providersConfigured.length > 0}
+          {#each providersConfigured as providerConfigured}
+            <ProviderConfigured provider="{providerConfigured}" />
+          {/each}
+        {/if}
 
-      <!-- Provider is not installed, display a box to indicate how to install from the tool if possible -->
-      {#if providersNotInstalled.length > 0}
-        {#each providersNotInstalled as providerNotInstalled}
-          <ProviderNotInstalled provider="{providerNotInstalled}" />
-        {/each}
-      {/if}
+        <!-- Provider is not installed, display a box to indicate how to install from the tool if possible -->
+        {#if providersNotInstalled.length > 0}
+          {#each providersNotInstalled as providerNotInstalled}
+            <ProviderNotInstalled provider="{providerNotInstalled}" />
+          {/each}
+        {/if}
 
-      <!-- Provider is stopped -->
-      {#if providersStopped.length > 0}
-        {#each providersStopped as providerStopped}
-          <ProviderStopped provider="{providerStopped}" />
-        {/each}
-      {/if}
+        <!-- Provider is stopped -->
+        {#if providersStopped.length > 0}
+          {#each providersStopped as providerStopped}
+            <ProviderStopped provider="{providerStopped}" />
+          {/each}
+        {/if}
+      </div>
     </div>
   </div>
-</div>
+</NavPage>
