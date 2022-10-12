0ac4ebc27a7d7f6506076e1535ecbc4b140c4c09
Florent Benoit <fbenoit@redhat.com>
2022-10-12 14:22:20 +0200

    feat: allow to disable search bar
    Change-Id: I7802b52e6df39ec60eececddca2c4c517472c290
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/ui/NavPage.svelte
+++ packages/renderer/src/lib/ui/NavPage.svelte
@@ -2,6 +2,7 @@
 export let title: string;
 export let subtitle: string = '';
 export let searchTerm = '';
+export let searchEnabled: boolean = true;
 </script>
 
 <div class="flex flex-col min-h-full">
@@ -19,33 +20,35 @@ export let searchTerm = '';
         </div>
       </div>
     </div>
-    <div class="flex flex-row pb-4">
-      <div class="pt-2 pl-5 lg:w-[35rem] w-[22rem]">
-        <div class="flex items-center bg-gray-700 text-gray-400">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5 ml-2 mr-2 "
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-          </svg>
-          <input
-            bind:value="{searchTerm}"
-            type="text"
-            name="containerSearchName"
-            placeholder="Search {title}...."
-            class="w-full py-2 outline-none text-sm bg-gray-700" />
+    {#if searchEnabled}
+      <div class="flex flex-row pb-4">
+        <div class="pt-2 pl-5 lg:w-[35rem] w-[22rem]">
+          <div class="flex items-center bg-gray-700 text-gray-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-5 h-5 ml-2 mr-2 "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+            <input
+              bind:value="{searchTerm}"
+              type="text"
+              name="containerSearchName"
+              placeholder="Search {title}...."
+              class="w-full py-2 outline-none text-sm bg-gray-700" />
+          </div>
+        </div>
+        <div class="flex flex-1 pt-4 px-5">
+          <slot name="bottom-additional-actions">&nbsp;</slot>
         </div>
       </div>
-      <div class="flex flex-1 pt-4 px-5">
-        <slot name="bottom-additional-actions">&nbsp;</slot>
-      </div>
-    </div>
+    {/if}
 
     <slot name="table" />
   </div>
