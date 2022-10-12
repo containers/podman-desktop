c8f71636c2ccd7701b6bff1f960155c586b05d38
Florent Benoit <fbenoit@redhat.com>
2022-10-12 16:28:58 +0200

    feat: add rounded and change background color for the search item
    Change-Id: Ibc6b64a51f724fd3d59ab810b06cce7360238b59
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>
    Signed-off-by: Rychardt Rothwërkz <redwerkz@gmail.com>

--- packages/renderer/src/lib/ui/NavPage.svelte
+++ packages/renderer/src/lib/ui/NavPage.svelte
@@ -23,7 +23,7 @@ export let searchEnabled: boolean = true;
     {#if searchEnabled}
       <div class="flex flex-row pb-4">
         <div class="pt-2 pl-5 lg:w-[35rem] w-[22rem]">
-          <div class="flex items-center bg-gray-700 text-gray-400">
+          <div class="flex items-center bg-zinc-900 text-gray-400 rounded-sm">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="w-5 h-5 ml-2 mr-2 "
@@ -41,7 +41,7 @@ export let searchEnabled: boolean = true;
               type="text"
               name="containerSearchName"
               placeholder="Search {title}...."
-              class="w-full py-2 outline-none text-sm bg-gray-700" />
+              class="w-full py-2 outline-none text-sm bg-zinc-900 rounded-sm text-gray-400 placeholder-gray-400" />
           </div>
         </div>
         <div class="flex flex-1 pt-4 px-5">
