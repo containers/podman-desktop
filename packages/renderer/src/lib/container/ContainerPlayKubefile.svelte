e6eb4e1c8f275f6346b75c0541c894095b49d45b
Florent Benoit <fbenoit@redhat.com>
2022-10-13 11:40:55 +0200

    feat: update UI style (header, background color and field background color)
    Change-Id: Idf9e7705dde6614a0e814f84a72b96a21801925f
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/container/ContainerPlayKubefile.svelte
+++ packages/renderer/src/lib/container/ContainerPlayKubefile.svelte
@@ -7,6 +7,7 @@ let providerUnsubscribe: Unsubscriber;
 import { providerInfos } from '../../stores/providers';
 import MonacoEditor from '../editor/MonacoEditor.svelte';
 import NoContainerEngineEmptyScreen from '../image/NoContainerEngineEmptyScreen.svelte';
+import NavPage from '../ui/NavPage.svelte';
 import KubePlayIcon from './KubePlayIcon.svelte';
 
 let runStarted = false;
@@ -87,80 +88,88 @@ async function getKubernetesfileLocation() {
 {/if}
 
 {#if providerConnections.length > 0}
-  <div class="px-6 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8">
-    <h3 class="text-xl font-medium  :text-white">Run pod/containers from a Kubernetes .YAML file</h3>
-    <div hidden="{runStarted}">
-      <label for="containerFilePath" class="block mb-2 text-sm font-medium text-gray-300">Kubernetes .YAML file</label>
-      <input
-        on:click="{() => getKubernetesfileLocation()}"
-        name="containerFilePath"
-        id="containerFilePath"
-        bind:value="{kubernetesYamlFilePath}"
-        readonly
-        placeholder="Select .YAML file to run..."
-        class=" text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-        required />
-    </div>
-
-    <div hidden="{runStarted}">
-      {#if providerConnections.length > 1}
-        <label for="providerConnectionName" class="py-6 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >Container Engine
-          <select
-            class="border  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
-            name="providerChoice"
-            bind:value="{selectedProvider}">
-            {#each providerConnections as providerConnection}
-              <option value="{providerConnection}">{providerConnection.name}</option>
-            {/each}
-          </select>
-        </label>
-      {/if}
-      {#if providerConnections.length == 1}
-        <input type="hidden" name="providerChoice" readonly bind:value="{selectedProviderConnection.name}" />
-      {/if}
-    </div>
-
-    {#if !runFinished}
-      <button
-        on:click="{() => playKubeFile()}"
-        disabled="{hasInvalidFields || runStarted}"
-        class="w-full pf-c-button pf-m-primary"
-        type="button">
-        <div class="flex flex-row align-text-top justify-center items-center">
-          {#if runStarted}
-            <div class="mr-4">
-              <i class="pf-c-button__progress">
-                <span class="pf-c-spinner pf-m-md" role="progressbar">
-                  <span class="pf-c-spinner__clipper"></span>
-                  <span class="pf-c-spinner__lead-ball"></span>
-                  <span class="pf-c-spinner__tail-ball"></span>
-                </span>
-              </i>
-            </div>
-          {:else}
-            <KubePlayIcon />
-          {/if}
-
-          Run
+  <NavPage title="Run pod/containers from a Kubernetes .YAML file" searchEnabled="{false}">
+    <div slot="empty" class="bg-zinc-700 p-5 h-full">
+      <div class="bg-zinc-800 px-6 py-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8">
+        <div class="text-xl font-medium">Select file:</div>
+        <div hidden="{runStarted}">
+          <label for="containerFilePath" class="block mb-2 text-sm font-medium text-gray-300"
+            >Kubernetes .YAML file</label>
+          <input
+            on:click="{() => getKubernetesfileLocation()}"
+            name="containerFilePath"
+            id="containerFilePath"
+            bind:value="{kubernetesYamlFilePath}"
+            readonly
+            placeholder="Select .YAML file to run..."
+            class="w-full p-2 outline-none text-sm bg-zinc-900 rounded-sm text-gray-400 placeholder-gray-400"
+            required />
         </div>
-      </button>
-    {/if}
-    {#if runStarted}
-      <div class="text-gray-400 text-sm">
-        Please wait during the Play Kube and do not change screen. This process may take a few minutes to complete...
+
+        <div hidden="{runStarted}">
+          {#if providerConnections.length > 1}
+            <label
+              for="providerConnectionName"
+              class="py-6 block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Container Engine
+              <select
+                class="border  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
+                name="providerChoice"
+                bind:value="{selectedProvider}">
+                {#each providerConnections as providerConnection}
+                  <option value="{providerConnection}">{providerConnection.name}</option>
+                {/each}
+              </select>
+            </label>
+          {/if}
+          {#if providerConnections.length == 1}
+            <input type="hidden" name="providerChoice" readonly bind:value="{selectedProviderConnection.name}" />
+          {/if}
+        </div>
+
+        {#if !runFinished}
+          <button
+            on:click="{() => playKubeFile()}"
+            disabled="{hasInvalidFields || runStarted}"
+            class="w-full pf-c-button pf-m-primary"
+            type="button">
+            <div class="flex flex-row align-text-top justify-center items-center">
+              {#if runStarted}
+                <div class="mr-4">
+                  <i class="pf-c-button__progress">
+                    <span class="pf-c-spinner pf-m-md" role="progressbar">
+                      <span class="pf-c-spinner__clipper"></span>
+                      <span class="pf-c-spinner__lead-ball"></span>
+                      <span class="pf-c-spinner__tail-ball"></span>
+                    </span>
+                  </i>
+                </div>
+              {:else}
+                <KubePlayIcon />
+              {/if}
+
+              Run
+            </div>
+          </button>
+        {/if}
+        {#if runStarted}
+          <div class="text-gray-400 text-sm">
+            Please wait during the Play Kube and do not change screen. This process may take a few minutes to
+            complete...
+          </div>
+        {/if}
+        {#if runError}
+          <div class="text-red-500 text-sm">{runError}</div>
+        {/if}
+        {#if runFinished}
+          <button on:click="{() => goToContainerList()}" class="w-full pf-c-button pf-m-primary">Done</button>
+        {/if}
       </div>
-    {/if}
-    {#if runError}
-      <div class="text-red-500 text-sm">{runError}</div>
-    {/if}
-    {#if runFinished}
-      <button on:click="{() => goToContainerList()}" class="w-full pf-c-button pf-m-primary">Done</button>
-    {/if}
-  </div>
-  {#if playKubeResultRaw}
-    <div class=" h-full w-full px-6 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8">
-      <MonacoEditor content="{playKubeResultRaw}" language="json" />
+      {#if playKubeResultRaw}
+        <div class=" h-full w-full px-6 pb-4 space-y-6 lg:px-8 sm:pb-6 xl:pb-8">
+          <MonacoEditor content="{playKubeResultRaw}" language="json" />
+        </div>
+      {/if}
     </div>
-  {/if}
+  </NavPage>
 {/if}
