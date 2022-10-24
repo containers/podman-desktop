dc3ecd1f6c559731f89b7c1daf2f97eb9f60780d
Florent Benoit <fbenoit@redhat.com>
2022-10-24 13:09:33 +0200

    website: use Montserrat font as default font
    Change-Id: I762531176c41c50eabdd9ebe10f8b1016ac6f9cd
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
