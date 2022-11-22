641b1ee0021ffacf603be97d05286696f466fbfc
Charlie Drage <charlie@charliedrage.com>
2022-11-22 08:07:44 -0500

    feat: add kebab menu to pods (#877)
    Add kebab menu to pods
    
    ### What does this PR do?
    * Adds "kebab" menu to pods
    * Change "name" to text-left in column
    * Change "actions" to text-right in column
    * Update CSS to it's consistent between pods and containers list
    * Fixes minor issue of radius not appearing correctly for pod actions
    
    ### Screenshot/screencast of this PR
    
    <!-- Please include a screenshot or a screencast explaining what is doing this PR -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Please include any related issue from Podman Desktop repository (or from another issue tracker).
    -->
    
    N/A
    
    ### How to test this PR?
    
    `yarn watch` and try out viewing a pod
    
    <!-- Please explain steps to reproduce -->
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/renderer/src/lib/pod/PodActions.svelte
+++ packages/renderer/src/lib/pod/PodActions.svelte
@@ -6,9 +6,12 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import type { PodInfoUI } from './PodInfoUI';
 import { router } from 'tinro';
 import ListItemButtonIcon from '../ui/ListItemButtonIcon.svelte';
+import DropdownMenu from '../ui/DropdownMenu.svelte';
+import FlatMenu from '../ui/FlatMenu.svelte';
 
 export let pod: PodInfoUI;
 export let backgroundColor = 'bg-zinc-800';
+export let dropdownMenu: boolean = false;
 
 async function startPod(podInfoUI: PodInfoUI) {
   await window.startPod(podInfoUI.engineId, podInfoUI.id);
@@ -34,18 +37,16 @@ function openGenerateKube(): void {
 function deployToKubernetes(): void {
   router.goto(`/deploy-to-kube/${pod.id}/${pod.engineId}`);
 }
+// If dropdownMenu = true, we'll change style to the imported dropdownMenu style
+// otherwise, leave blank.
+let actionsStyle;
+if (dropdownMenu) {
+  actionsStyle = DropdownMenu;
+} else {
+  actionsStyle = FlatMenu;
+}
 </script>
 
-<ListItemButtonIcon
-  title="Generate Kube"
-  onClick="{() => openGenerateKube()}"
-  backgroundColor="{backgroundColor}"
-  icon="{faFileCode}" />
-<ListItemButtonIcon
-  title="Deploy to Kubernetes"
-  onClick="{() => deployToKubernetes()}"
-  backgroundColor="{backgroundColor}"
-  icon="{faRocket}" />
 <ListItemButtonIcon
   title="Start Pod"
   onClick="{() => startPod(pod)}"
@@ -59,12 +60,29 @@ function deployToKubernetes(): void {
   backgroundColor="{backgroundColor}"
   icon="{faStopCircle}" />
 <ListItemButtonIcon
-  title="Restart Pod"
-  onClick="{() => restartPod(pod)}"
-  backgroundColor="{backgroundColor}"
-  icon="{faArrowsRotate}" />
-<ListItemButtonIcon
   title="Delete Pod"
   onClick="{() => removePod(pod)}"
   backgroundColor="{backgroundColor}"
   icon="{faTrash}" />
+
+<!-- If dropdownMenu is true, use it, otherwise just show the regular buttons -->
+<svelte:component this="{actionsStyle}" backgroundColor="{backgroundColor}">
+  <ListItemButtonIcon
+    title="Generate Kube"
+    onClick="{() => openGenerateKube()}"
+    menu="{dropdownMenu}"
+    backgroundColor="{backgroundColor}"
+    icon="{faFileCode}" />
+  <ListItemButtonIcon
+    title="Deploy to Kubernetes"
+    onClick="{() => deployToKubernetes()}"
+    menu="{dropdownMenu}"
+    backgroundColor="{backgroundColor}"
+    icon="{faRocket}" />
+  <ListItemButtonIcon
+    title="Restart Pod"
+    onClick="{() => restartPod(pod)}"
+    menu="{dropdownMenu}"
+    backgroundColor="{backgroundColor}"
+    icon="{faArrowsRotate}" />
+</svelte:component>
