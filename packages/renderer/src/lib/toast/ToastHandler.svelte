497d7495e3f54cf76ec549bdffe61cbf39b09980
Florent BENOIT <fbenoit@redhat.com>
2023-01-11 19:39:52 +0100

    feat(dd-extension-api): Implement toast API (#1154)
    fixes https://github.com/containers/podman-desktop/issues/772
    
    Change-Id: I924f96229585fbd146fda19aa93386dae38a7d45
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/toast/ToastHandler.svelte
@@ -0,0 +1,58 @@
+<style>
+.svelte-toast-wrapper {
+  font-size: 0.8rem;
+  --toastMinHeight: 2rem;
+  --toastBorderRadius: 0.2rem;
+}
+</style>
+
+<script lang="ts">
+import { SvelteToast, toast } from '@zerodevx/svelte-toast';
+import { onDestroy, onMount } from 'svelte';
+
+let callback;
+
+onMount(() => {
+  callback = (object: { type: string; message: string }) => {
+    let theme: any = {};
+    if (object.type === 'success') {
+      theme = {
+        '--toastBackground': '#16a34a',
+        '--toastColor': '#bbf7d0',
+        '--toastBarBackground': '#14532d',
+      };
+    } else if (object.type === 'error') {
+      theme = {
+        '--toastBackground': 'red',
+        '--toastColor': 'white',
+        '--toastBarBackground': 'maroon',
+      };
+    } else if (object.type === 'warning') {
+      theme = {
+        '--toastBackground': 'yellow',
+        '--toastColor': 'black',
+        '--toastBarBackground': 'olive',
+      };
+    } else if (object.type === 'info') {
+      theme = {
+        '--toastBackground': 'blue',
+        '--toastColor': 'white',
+        '--toastBarBackground': 'navy',
+      };
+    }
+    toast.push(object.message, { pausable: true, theme });
+  };
+
+  window.events?.receive('toast:handler', (object: { type: string; message: string }) => {
+    callback(object);
+  });
+});
+
+onDestroy(() => {
+  callback = () => {};
+});
+</script>
+
+<div class="svelte-toast-wrapper">
+  <SvelteToast />
+</div>
