0d565fb73415135399645a6e9e2eb2c94feb8c36
Tim deBoer <git@tdeboer.ca>
2023-01-24 17:15:04 -0500

    fix: 'empty screen' icons
    Fix each of the four empty screens to use the correct SVG icon and reduce code duplication via EmptyScreen.
    
    Switched the order of the if statement in each of the lists because I thought this is more natural, and avoids passing array into each screen to confirm if it is empty.
    
    The volumes empty screen does not appear before or after this change because filtered.length == 1 when there are no visible volumes, will continue investigating via separate PR.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/image/ImageEmptyScreen.svelte
+++ packages/renderer/src/lib/image/ImageEmptyScreen.svelte
@@ -1,34 +1,10 @@
 <script lang="ts">
-import Fa from 'svelte-fa/src/fa.svelte';
-import { faPaste } from '@fortawesome/free-solid-svg-icons';
-import { onMount } from 'svelte';
-
-export let images: Array<unknown>;
-
-function copyPullInstructionToClipboard() {
-  const text = copyText?.innerText;
-  navigator.clipboard.writeText(text);
-}
-
-let copyText;
-
-onMount(async () => {
-  copyText = document.getElementById('noImageCommandLine') as HTMLElement;
-});
+import EmptyScreen from '../ui/EmptyScreen.svelte';
+import ImageIcon from '../images/ImageIcon.svelte';
 </script>
 
-<div class="h-full min-w-full flex flex-col" class:hidden="{images.length > 0}">
-  <div class="pf-c-empty-state h-full">
-    <div class="pf-c-empty-state__content">
-      <i class="fas fa-layer-group pf-c-empty-state__icon" aria-hidden="true"></i>
-
-      <h1 class="pf-c-title pf-m-lg">No images</h1>
-      <div class="pf-c-empty-state__body">Pull a first image using the following command line:</div>
-      <div class="flex flex-row bg-gray-800 w-full items-center justify-center p-2 mt-2">
-        <div id="noImageCommandLine">podman pull redhat/ubi8-micro</div>
-        <button title="Copy To Clipboard" class="mr-5" on:click="{() => copyPullInstructionToClipboard()}"
-          ><Fa class="ml-3 h-5 w-5 cursor-pointer rounded-full text-3xl text-sky-800" icon="{faPaste}" /></button>
-      </div>
-    </div>
-  </div>
-</div>
+<EmptyScreen
+  icon="{ImageIcon}"
+  title="No images"
+  message="Pull a first image using the following command line:"
+  commandline="podman pull redhat/ubi8-micro" />
