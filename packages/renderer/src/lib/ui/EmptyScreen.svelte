0b66aeb5cf501fbe6bd5779a588ce013619310bf
Tim deBoer <git@tdeboer.ca>
2023-01-24 15:17:21 -0500

    chore: create reusable 'empty' screen component (#1227)
    * chore: create reusable 'empty' screen component
    
    There are several 'empty' screens where we fall back to an icon, title/message, and optional command suggestion. This creates a reusable component for these screens to reduce copy/paste code and ensure consistency, and switches the No Container Engine screen to use it.
    
    This change by itself doesn't make much sense since it makes the No Container Engine screen two files instead of one and has command suggestion that it doesn't use, but the next PR(s) will reuse the component for several similar screens and has significant code reduction.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>
    
    * Improved binding and icon
    
    Improved binding and switching icon from slot to a property, as per review comments.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/ui/EmptyScreen.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+import Fa from 'svelte-fa/src/fa.svelte';
+import { faPaste } from '@fortawesome/free-solid-svg-icons';
+import { onMount, SvelteComponent } from 'svelte';
+
+export let icon: any;
+export let title: string = 'No title';
+export let message: string = 'Message';
+export let commandline: string = '';
+export let hidden: boolean = false;
+
+let fontAwesomeIcon = false;
+let processed = false;
+
+onMount(() => {
+  if (icon?.prefix === 'fas') {
+    fontAwesomeIcon = true;
+  }
+  processed = true;
+});
+
+function copyRunInstructionToClipboard() {
+  const text = copyTextDivElement?.innerText;
+  navigator.clipboard.writeText(text);
+}
+
+let copyTextDivElement: HTMLDivElement;
+</script>
+
+<div class="h-full min-w-full flex flex-col" class:hidden="{hidden}">
+  <div class="pf-c-empty-state h-full">
+    <div class="pf-c-empty-state__content">
+      <p class="pf-c-empty-state__body">
+        {#if processed}
+          {#if fontAwesomeIcon}
+            <Fa
+              icon="{icon}"
+              size="55"
+              class="cursor-pointer pf-c-empty-state__icon"
+              style="display:inline-block; text-align: center" />
+          {:else}
+            <svelte:component
+              this="{icon}"
+              size="60"
+              class="pf-c-empty-state__icon"
+              style="display:inline-block; text-align: center" />
+          {/if}
+        {/if}
+      </p>
+      <h1 class="pf-c-title pf-m-lg">{title}</h1>
+      <div class="pf-c-empty-state__body">{message}</div>
+      {#if commandline.length > 0}
+        <div class="flex flex-row bg-gray-800 w-full items-center p-2 mt-2">
+          <div bind:this="{copyTextDivElement}">{commandline}</div>
+          <button title="Copy To Clipboard" class="ml-5 mr-5" on:click="{() => copyRunInstructionToClipboard()}"
+            ><Fa class="h-5 w-5 cursor-pointer rounded-full text-3xl text-sky-800" icon="{faPaste}" /></button>
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
