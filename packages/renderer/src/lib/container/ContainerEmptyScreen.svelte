f6e1082b3c85002ed402ec58213c2a078f4b499b
Tim deBoer <git@tdeboer.ca>
2023-01-24 20:59:33 -0500

    Lint it one more time
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/container/ContainerEmptyScreen.svelte
+++ packages/renderer/src/lib/container/ContainerEmptyScreen.svelte
@@ -4,7 +4,7 @@ import ContainerIcon from '../images/ContainerIcon.svelte';
 </script>
 
 <EmptyScreen
-  icon={ContainerIcon}
+  icon="{ContainerIcon}"
   title="No containers"
   message="Run a first container using the following command line:"
-  commandline="podman run quay.io/podman/hello"/>
+  commandline="podman run quay.io/podman/hello" />
