d33c83c590b25161965bede62164cd03cc3ae43e
Tim deBoer <git@tdeboer.ca>
2023-02-24 15:15:14 -0800

    fix: Consistent summary pages
    Fix for issue #1540, inconsistent summary pages:
    - Pod details Summary tab moved first.
    - Container details details page (no, that's not a typo :) ) is moved first, renamed to Summary.
    - Moved container details summary into it's own svelte file to match other pages.
    - Formatting (font and spacing) changed to match other summary pages.
    
    Selecting a container or pod from the list pages still links directly into the /logs page instead of /summary, but leaving that since it is a separate issue.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/ContainerDetailsSummary.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+import type { ContainerInfoUI } from './container/ContainerInfoUI';
+import { getPanelDetailColor } from './color/color';
+
+export let container: ContainerInfoUI;
+</script>
+
+<div class="h-full" style="background-color: {getPanelDetailColor()}">
+  <div class="flex px-5 py-4 flex-col">
+    <div class="w-full">
+      <table class="h-2">
+        <tr>
+          <td class="pt-2 pr-2">Id</td>
+          <td class="pt-2 pr-2">{container.shortId}</td>
+        </tr>
+        <tr>
+          <td class="pt-2 pr-2">Command</td>
+          <td class="pt-2 pr-2">{container.command}</td>
+        </tr>
+        <tr>
+          <td class="pt-2 pr-2">State</td>
+          <td class="pt-2 pr-2">{container.state}</td>
+        </tr>
+        <tr>
+          <td class="pt-2 pr-2">Ports</td>
+          <td class="pt-2 pr-2" class:hidden="{container.hasPublicPort}">N/A</td>
+          <td class="pt-2 pr-2" class:hidden="{!container.hasPublicPort}">{container.port}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
