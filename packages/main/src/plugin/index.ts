68d8ee8236d6e16e268ba3c6f18f19565e3e5ce0
Florent Benoit <fbenoit@redhat.com>
2023-03-30 16:27:09 +0200

    chore: use a method to flag it as ready help for unit tests
    Change-Id: I579738585da99136fb4a881635f22e96431c722b
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/main/src/plugin/index.ts
+++ packages/main/src/plugin/index.ts
@@ -1364,12 +1364,16 @@ export class PluginSystem {
 
     await contributionManager.init();
 
+    this.markAsReady();
     await this.extensionLoader.start();
-    this.isReady = true;
     console.log('PluginSystem: initialization done.');
     apiSender.send('extension-system', `${this.isReady}`);
     autoStartConfiguration.start();
     return this.extensionLoader;
+  }
+
+  markAsReady(): void {
+    this.isReady = true;
   }
 
   getLogHandlerCreateConnection(channel: string, loggerId: string): LoggerWithEnd {
