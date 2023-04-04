9254d98e717211ecc2d5468afea4e4b0041db048
Tim deBoer <git@tdeboer.ca>
2023-04-04 18:35:11 -0400

    feat: Custom message box and dialog
    We're currently providing an extension API that uses the Electron dialog to display info/warning/error dialogs on screen. It's fine, but it would be better to use our own styling. We should also provide a more capable message box for future internal use.
    
    Notes:
    - Provides a new MessageBox component with a strict subset of the Electron MessageBox capability. This supports everything that an Electron dialog can do.
    - Why a message box and not just a dialog? The message box function and API is more capable and extendable for future needs (e.g. adding a detail message, default buttons, etc). Today it only supports what a dialog needs, but easier to extend later for things like default buttons, detail messages, etc. Some Electron API features (e.g. browserWindow, 'noLink') do not make sense to ever support.
    - Likewise, using the Electron Message box API internally allows for easier porting (e.g. if we decide to use this for our About dialog).
    - All of the existing extension API now routes to this new component. Unlike the previous attempt, this change does not affect extension API.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/main/src/plugin/message-box.ts
@@ -0,0 +1,115 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+import { Deferred } from './util/deferred';
+
+type DialogType = 'none' | 'info' | 'error' | 'question' | 'warning';
+
+/**
+ * Options to configure the behavior of the message box UI.
+ */
+export interface MessageBoxOptions {
+  /**
+   * The title of the message box.
+   */
+  title: string;
+  /**
+   * The primary message.
+   */
+  message: string;
+  /**
+   * Text for buttons.
+   */
+  buttons: string[];
+  /**
+   * The (optional) type, one of 'none' | 'info' | 'error' | 'question' | 'warning'.
+   */
+  type?: string;
+}
+
+export interface MessageBoxReturnValue {
+  response: number;
+  checkboxChecked: boolean | undefined;
+}
+
+export class MessageBox {
+  private callbackId = 0;
+
+  private callbacksMessageBox = new Map<number, Deferred<MessageBoxReturnValue>>();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(private apiSender: any) {}
+
+  async showMessageBox(options: MessageBoxOptions): Promise<MessageBoxReturnValue> {
+    // keep track of this request
+    this.callbackId++;
+
+    // create a promise that will be resolved when the frontend sends the result
+    const deferred = new Deferred<MessageBoxReturnValue>();
+
+    // store the callback that will resolve the promise
+    this.callbacksMessageBox.set(this.callbackId, deferred);
+
+    const data = {
+      id: this.callbackId,
+      title: options.title,
+      message: options.message,
+      buttons: options.buttons,
+      type: options.type,
+    };
+
+    // need to send the options to the frontend
+    this.apiSender.send('showMessageBox:open', data);
+
+    // return the promise
+    return deferred.promise;
+  }
+
+  async showDialog(type: DialogType, title: string, message: string, items: string[]): Promise<string | undefined> {
+    const result = await this.showMessageBox({
+      title: title,
+      message: message,
+      buttons: items,
+      type: type,
+    });
+    if (result) {
+      return items[result.response];
+    }
+
+    return undefined;
+  }
+
+  // this method is called by the frontend when the user selected a button
+  async onDidSelectButton(id: number, selectedIndex: number) {
+    // get the callback
+    const callback = this.callbacksMessageBox.get(id);
+
+    // if there is a callback
+    if (callback) {
+      // grab item
+      const val: MessageBoxReturnValue = {
+        response: selectedIndex,
+        checkboxChecked: false,
+      };
+      // resolve the promise
+      callback.resolve(val);
+    }
+
+    // remove the callback
+    this.callbacksMessageBox.delete(id);
+  }
+}
