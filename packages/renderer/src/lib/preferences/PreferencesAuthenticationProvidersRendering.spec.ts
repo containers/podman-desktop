97d208318475980ddcc7311b447926dc745d5466
Denis Golovin <dgolovin@redhat.com>
2023-04-13 22:50:38 -0700

    Implemented component tests
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

---
+++ packages/renderer/src/lib/preferences/PreferencesAuthenticationProvidersRendering.spec.ts
@@ -0,0 +1,85 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom';
+import { test, expect, vi } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/svelte';
+import { authenticationProviders } from '../../stores/authenticationProviders';
+import PreferencesAuthenticationProvidersRendering from './PreferencesAuthenticationProvidersRendering.svelte';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+test('Expect that page shows icon and message when no auth providers registered', async () => {
+  render(PreferencesAuthenticationProvidersRendering, {});
+  const noProvidersText = await screen.findAllByText('No authentication providers');
+  expect(noProvidersText).length(1);
+  expect(noProvidersText[0]).toBeInTheDocument();
+});
+
+test('Expect that page shows registered authentication providers without accounts as logged out', async () => {
+  authenticationProviders.set([
+      {
+        id: 'test',
+        displayName: 'Test Authentication Provider',
+        accounts:[],
+      }
+    ]
+  );
+  render(PreferencesAuthenticationProvidersRendering, {});
+  const providerText = await screen.findByText('Test Authentication Provider');
+  expect(providerText).toBeInTheDocument();
+  const loggedOut = await screen.findByText('Logged out');
+  expect(loggedOut).toBeInTheDocument();
+});
+
+const testProvidersInfo = [
+  {
+    id: 'test',
+    displayName: 'Test Authentication Provider',
+    accounts:[
+      {
+        id: 'test-account',
+        label: 'Test Account',
+      }  
+    ],
+  }
+];
+
+test('Expect that page shows registered authentication providers with account as logged in', async () => {
+  authenticationProviders.set(testProvidersInfo);
+  render(PreferencesAuthenticationProvidersRendering, {});
+  const providerText = await screen.findByText('Test Authentication Provider');
+  expect(providerText).toBeInTheDocument();
+  const loggedOut = await screen.findByText('Logged in');
+  expect(loggedOut).toBeInTheDocument();
+  const signoutButton = await screen.findByTitle('Sign out of Test Account');
+  expect(signoutButton).toBeInTheDocument();
+  expect(signoutButton).toBeEnabled();
+});
+
+test('Expect Sign Out button click calls window.requestAuthenticationProviderSignOut with provider and account ids', async () => {
+  authenticationProviders.set(testProvidersInfo);
+  render(PreferencesAuthenticationProvidersRendering, {});
+  const signoutButton = await screen.findByTitle('Sign out of Test Account');
+  const requestSignOutMock = vi.fn().mockImplementation(() => {});
+  (window as any).requestAuthenticationProviderSignOut = requestSignOutMock;
+  fireEvent.click(signoutButton);
+  expect(requestSignOutMock).toBeCalledWith('test', 'test-account');
+});
--- packages/renderer/src/lib/preferences/PreferencesAuthenticationProvidersRendering.svelte
+++ packages/renderer/src/lib/preferences/PreferencesAuthenticationProvidersRendering.svelte
@@ -41,7 +41,7 @@ import KeyIcon from '../images/KeyIcon.svelte';
                     </span>
                   </div>
                   {#if provider.accounts.length > 0}
-                    <button
+                    <button title="Sign out of {provider.accounts[0].label}"
                       class="pl-2 hover:cursor-pointer hover:text-white text-white"
                       on:click="{() =>
                         window.requestAuthenticationProviderSignOut(provider.id, provider.accounts[0].id)}">
