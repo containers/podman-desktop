89fb5556da283ed85ccc27fb340d087a9b6408ee
Denis Golovin <dgolovin@redhat.com>
2023-04-13 20:53:05 -0700

    Fix e2e test errors
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/stores/authenticationProviders.ts
+++ packages/renderer/src/stores/authenticationProviders.ts
@@ -31,4 +31,4 @@ export const authenticationProviders: Writable<readonly AuthenticationProviderIn
 
 // need to refresh when new registry are updated/deleted
 window.events?.receive('authentication-provider-update', fetchAuthenticationProvidersInfo);
-window.addEventListener('system-ready', fetchAuthenicationProvidersInfo);
+window.addEventListener('system-ready', fetchAuthenticationProvidersInfo);
