554b7fd49f231031843024673c2acb6af8c8ed66
Denis Golovin <dgolovin@redhat.com>
2023-04-19 21:45:09 -0700

    feat: UI to show message wiht links when provider has not connections
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

---
+++ packages/renderer/src/lib/preferences/PreferencesConnectionsEmptyRendering.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import LineWithLinks from './LineWithLinks.svelte';
+
+export let message: string;
+export let hidden: boolean;
+let messageLines: string[] = [];
+if (message) {
+  messageLines = message.split('\n');
+}
+</script>
+
+{#if !hidden}
+  <div class="ml-6 mt-2 text-sm text-gray-300">
+    {#each messageLines as line}
+      <LineWithLinks lineText="{line}" />
+    {/each}
+  </div>
+{/if}
