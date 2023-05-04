9595ceb460878524ecc4275d6982924d9dbedf1b
Florent Benoit <fbenoit@redhat.com>
2023-05-04 15:14:44 +0200

    feat: display featured extensions in the welcome page related to https://github.com/containers/podman-desktop/issues/1901
    Change-Id: I08b0eb85523652a474026ef2d8e7fb0d1f029964
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/welcome/WelcomePage.spec.ts
+++ packages/renderer/src/lib/welcome/WelcomePage.spec.ts
@@ -22,17 +22,23 @@ import '@testing-library/jest-dom';
 import { beforeAll, test, expect, vi } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/svelte';
 import WelcomePage from './WelcomePage.svelte';
-import type { Unsubscriber } from 'svelte/store';
+import { get, type Unsubscriber } from 'svelte/store';
 import { router } from 'tinro';
+import { featuredExtensionInfos } from '/@/stores/featuredExtensions';
+import type { FeaturedExtension } from '../../../../main/src/plugin/featured/featured-api';
 
 let routerUnsubscribe: Unsubscriber;
 let path: string;
+
+const getFeaturedExtensionsMock = vi.fn();
 
 // fake the window.events object
 beforeAll(() => {
   (window as any).getConfigurationValue = vi.fn();
   (window as any).updateConfigurationValue = vi.fn();
-
+  (window as any).getPodmanDesktopVersion = vi.fn();
+  (window as any).telemetryConfigure = vi.fn();
+  (window as any).getFeaturedExtensions = getFeaturedExtensionsMock;
   (window.events as unknown) = {
     receive: (_channel: string, func: any) => {
       func();
@@ -99,4 +105,53 @@ test('Expect that telemetry UI is visible when necessary', async () => {
   await render(WelcomePage, { showWelcome: true, showTelemetry: true });
   const checkbox = screen.getByRole('checkbox', { name: 'Enable telemetry' });
   expect(checkbox).toBeInTheDocument();
+});
+
+test('Expect that featured extensions are displayed', async () => {
+  const featuredExtension1: FeaturedExtension = {
+    builtin: true,
+    id: 'foo.bar',
+    displayName: 'FooBar',
+    description: 'Foobar description',
+    icon: 'data:image/png;base64,foobar',
+    categories: [],
+    fetchable: true,
+    fetchLink: 'oci-hello/world',
+    fetchVersion: '1.2.3',
+    installed: true,
+  };
+
+  const featuredExtension2: FeaturedExtension = {
+    builtin: true,
+    id: 'foo.baz',
+    displayName: 'FooBaz',
+    description: 'Foobaz description',
+    icon: 'data:image/png;base64,foobaz',
+    categories: [],
+    fetchable: false,
+    installed: false,
+  };
+
+  getFeaturedExtensionsMock.mockResolvedValue([featuredExtension1, featuredExtension2]);
+
+  // ask to update the featured Extensions store
+  window.dispatchEvent(new CustomEvent('system-ready'));
+
+  // wait store are populated
+  while (get(featuredExtensionInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+
+  await render(WelcomePage, { showWelcome: true });
+  const imageExt1 = screen.getByRole('img', { name: 'FooBar logo' });
+  // expect the image to be there
+  expect(imageExt1).toBeInTheDocument();
+  // expect image source is correct
+  expect(imageExt1).toHaveAttribute('src', 'data:image/png;base64,foobar');
+
+  const imageExt2 = screen.getByRole('img', { name: 'FooBaz logo' });
+  // expect the image to be there
+  expect(imageExt2).toBeInTheDocument();
+  // expect image source is correct
+  expect(imageExt2).toHaveAttribute('src', 'data:image/png;base64,foobaz');
 });
