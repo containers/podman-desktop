3a4023950918d92a818b2ddbb901c8fb42d33ba6
Florent Benoit <fbenoit@redhat.com>
2023-05-15 21:57:11 +0200

    fix: e2e test
    Wait for elements inside the page and check dashboard is displayed
    take screenshots into <root-folder>/tests/output/screenshots
    record video into <root-folder>/tests/output/videos
    
    fixes https://github.com/containers/podman-desktop/issues/2312
    fixes https://github.com/containers/podman-desktop/issues/1780
    
    Change-Id: I4b22e9fa21ce6fc6a85abefb7737c5ba8cbce173
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- vitest.config.js
+++ vitest.config.js
@@ -44,7 +44,7 @@ const config = {
     /**
      * A default timeout of 5000ms is sometimes not enough for playwright.
      */
-    testTimeout: 30_000,
+    testTimeout: 60_000,
     hookTimeout: 30_000,
   },
   resolve: {
