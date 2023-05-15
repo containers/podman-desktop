a386dc1fd064887e89a9993303f0a69c3d5666e4
Florent Benoit <fbenoit@redhat.com>
2023-05-15 21:57:11 +0200

    fix: e2e test
    Wait for elements inside the page and check dashboard is displayed
    take screenshots into <root-folder>/tests/output/screenshots
    record video into <root-folder>/tests/output/videos
    
    fixes https://github.com/containers/podman-desktop/issues/2312
    fixes https://github.com/containers/podman-desktop/issues/1780
    
    Change-Id: I4b22e9fa21ce6fc6a85abefb7737c5ba8cbce173
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- tests/src/e2e.spec.ts
+++ tests/src/e2e.spec.ts
@@ -1,22 +1,48 @@
-import type { ElectronApplication } from 'playwright';
+import type { BrowserWindow } from 'electron';
+import type { ElectronApplication, JSHandle, Page } from 'playwright';
 import { _electron as electron } from 'playwright';
 import { afterAll, beforeAll, expect, test } from 'vitest';
+import { expect as playExpect } from '@playwright/test';
+import { existsSync } from 'node:fs';
+import { rm } from 'node:fs/promises';
 
 let electronApp: ElectronApplication;
 
+let page: Page;
+
 beforeAll(async () => {
-  electronApp = await electron.launch({ args: ['.'] });
+  // remove all videos/screenshots
+  if (existsSync('tests/output')) {
+    console.log('Cleaning up output folder...');
+    await rm('tests/output', { recursive: true, force: true });
+  }
+
+  electronApp = await electron.launch({
+    args: ['.'],
+    recordVideo: {
+      dir: 'tests/output/videos',
+      size: {
+        width: 1050,
+        height: 700,
+      },
+    },
+  });
+
+  page = await electronApp.firstWindow();
 });
 
 afterAll(async () => {
   await electronApp.close();
 });
 
-test.skip('Main window state', async () => {
-  const windowState: { isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean } = await electronApp.evaluate(
-    ({ BrowserWindow }) => {
-      const mainWindow = BrowserWindow.getAllWindows()[0];
+test('Check welcome page is displayed and that we are redirected to the main page where dashboard page is there', async () => {
+  // Direct Electron console to Node terminal.
+  page.on('console', console.log);
 
+  const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
+
+  const windowState = await window.evaluate(
+    (mainWindow): Promise<{ isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean }> => {
       const getState = () => ({
         isVisible: mainWindow.isVisible(),
         isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
@@ -24,21 +50,38 @@ test.skip('Main window state', async () => {
       });
 
       return new Promise(resolve => {
+        /**
+         * The main window is created hidden, and is shown only when it is ready.
+         * See {@link ../packages/main/src/mainWindow.ts} function
+         */
         if (mainWindow.isVisible()) {
           resolve(getState());
-        } else mainWindow.once('ready-to-show', () => setTimeout(() => resolve(getState()), 0));
+        } else mainWindow.once('ready-to-show', () => resolve(getState()));
       });
     },
   );
 
-  expect(windowState.isCrashed, 'App was crashed').toBeFalsy();
-  expect(windowState.isVisible, 'Main window was not visible').toBeTruthy();
-  expect(windowState.isDevToolsOpened, 'DevTools was opened').toBeFalsy();
-});
+  await page.screenshot({ path: 'tests/output/screenshots/screenshot-welcome-page-init.png', fullPage: true });
 
-test('Main window web content', async () => {
-  const page = await electronApp.firstWindow();
-  const element = await page.$('#app', { strict: true });
-  expect(element, 'Cannot find root element').toBeDefined();
-  expect((await element.innerHTML()).trim(), 'Window content was empty').not.equal('');
+  // wait for the initial screen to be loaded
+  const goToPodmanDesktopButton = page.locator('button:text("Go to Podman Desktop")');
+  // wait for visibility
+  await goToPodmanDesktopButton.waitFor({ state: 'visible' });
+
+  await page.screenshot({ path: 'tests/output/screenshots/screenshot-welcome-page-display.png', fullPage: true });
+
+  // click on the button
+  await goToPodmanDesktopButton.click();
+
+  await page.screenshot({
+    path: 'tests/output/screenshots/screenshot-welcome-page-redirect-to-dashboard.png',
+    fullPage: true,
+  });
+
+  // check we have the dashboard page
+  const dashboardTitle = page.getByRole('heading', { name: 'Dashboard' });
+  await playExpect(dashboardTitle).toBeVisible();
+
+  expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
+  expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
 });
