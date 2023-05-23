e720728c418ca530a7de4fd5f67dedb6cd87878e
Tim deBoer <git@tdeboer.ca>
2023-05-23 17:10:26 -0400

    fix: refactor form and fix test
    Removed PF form and reworked the UI to match the build container page.
    
    Adding the tick() makes the log visible and breaks the test; it's kind of ugly but I couldn't find another option and exposed terminalIntialized so that the test doesn't need to include the log.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/image/PullImage.spec.ts
+++ packages/renderer/src/lib/image/PullImage.spec.ts
@@ -74,7 +74,7 @@ function setup() {
 describe('PullImage', () => {
   test('Expect that textbox is available and button is displayed', async () => {
     setup();
-    render(PullImage, {});
+    render(PullImage, { terminalIntialized: true });
 
     const entry = screen.getByPlaceholderText('Image name');
     expect(entry).toBeInTheDocument();
@@ -85,7 +85,7 @@ describe('PullImage', () => {
 
   test('Expect that whitespace does not enable button', async () => {
     setup();
-    render(PullImage, { imageToPull: '   ' });
+    render(PullImage, { imageToPull: '   ', terminalIntialized: true });
 
     const button = screen.getByRole('button', { name: buttonText });
     expect(button).toBeInTheDocument();
@@ -94,7 +94,7 @@ describe('PullImage', () => {
 
   test('Expect that valid entry enables button', async () => {
     setup();
-    render(PullImage, { imageToPull: 'some-valid-image' });
+    render(PullImage, { imageToPull: 'some-valid-image', terminalIntialized: true });
 
     const button = screen.getByRole('button', { name: buttonText });
     expect(button).toBeInTheDocument();
@@ -103,7 +103,7 @@ describe('PullImage', () => {
 
   test('Expect that valid entry enables button', async () => {
     setup();
-    render(PullImage);
+    render(PullImage, { terminalIntialized: true });
 
     const button = screen.getByRole('button', { name: buttonText });
     expect(button).toBeInTheDocument();
