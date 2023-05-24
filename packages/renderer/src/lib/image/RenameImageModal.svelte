5155ec804148c4d16090bdb3dd17e33981dbb497
Tucker Chapman <tucker.r.chapman@gmail.com>
2023-05-24 14:19:47 -0400

    Add form fields to modal
    Signed-off-by: Tucker Chapman <tuckerc@Tuckers-MacBook-Pro.local>
    Signed-off-by: Tucker Chapman <tucker.r.chapman@gmail.com>

--- packages/renderer/src/lib/image/RenameImageModal.svelte
+++ packages/renderer/src/lib/image/RenameImageModal.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { onMount } from 'svelte';
 import Modal from '../dialogs/Modal.svelte';
+import type {ImageInfoUI} from './ImageInfoUI';
 
 export let closeCallback: () => void;
 export let imageInfoToRename: ImageInfoUI;
@@ -11,6 +12,9 @@ onMount(async () => {
   const inspectInfo = await window.getImageInspect(imageInfoToRename.engineId, imageInfoToRename.id);
 
   imageTags = inspectInfo.RepoTags;
+  if (imageTags.length > 0) {
+    selectedImageTag = imageTags[0];
+  }
 });
 </script>
 
@@ -25,6 +29,31 @@ onMount(async () => {
       <button class="hover:text-gray-300 py-1" on:click="{() => closeCallback()}">
         <i class="fas fa-times" aria-hidden="true"></i>
       </button>
+    </div>
+    <div class="flex flex-col px-10 py-4 text-sm leading-5 space-y-5">
+      <div>
+        <label for="modalImageTag" class="block my-2 text-sm font-medium text-gray-400 dark:text-gray-400"
+          >Image Tag</label>
+        <select
+          class="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-charcoal-600 border-gray-900 placeholder-gray-700 text-white"
+          name="imageChoice"
+          bind:value="{selectedImageTag}">
+          {#each imageTags as imageTag}
+            <option value="{imageTag}">{imageTag}</option>
+          {/each}
+        </select>
+
+          <label for="newImageTag" class="block my-2 text-sm font-bold text-gray-400">New Image Tag</label>
+          <input
+            type="text"
+            value="{selectedImageTag}"
+            name="newImageTag"
+            id="newImageTag"
+            placeholder="Enter new image tag (e.g. quay.io/namespace/my-custom-image:latest)"
+            class="w-full p-2 outline-none text-sm bg-charcoal-600 rounded-sm text-gray-700 placeholder-gray-700"
+            required />
+      </div>
+
     </div>
   </div>
 </Modal>
