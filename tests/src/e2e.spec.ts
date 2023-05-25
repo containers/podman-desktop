6ad9cec04bd996b81fdca2d0b1fde7d8cebe4ed7
Ondrej Dockal <odockal@redhat.com>
2023-05-25 15:45:00 +0200

    chore(tests): extend e2e test case, include it in GA, fixes #1474
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- tests/src/e2e.spec.ts
+++ tests/src/e2e.spec.ts
@@ -1,20 +1,31 @@
 import type { BrowserWindow } from 'electron';
-import type { ElectronApplication, JSHandle, Page } from 'playwright';
+import type { ElectronApplication, JSHandle, Locator, Page } from 'playwright';
 import { _electron as electron } from 'playwright';
 import { afterAll, beforeAll, expect, test } from 'vitest';
 import { expect as playExpect } from '@playwright/test';
-import { existsSync } from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 import { rm } from 'node:fs/promises';
+import * as os from 'node:os'
+import * as path from 'node:path';
 
 let electronApp: ElectronApplication;
 
 let page: Page;
+let userHome = os.homedir();
+const navBarItems = ['Dashboard', 'Containers', 'Images', 'Pods', 'Volumes', 'Settings'];
 
 beforeAll(async () => {
   // remove all videos/screenshots
   if (existsSync('tests/output')) {
     console.log('Cleaning up output folder...');
     await rm('tests/output', { recursive: true, force: true });
+  }
+  
+  const settingsPath = path.join(userHome, '.local', 'share', 'containers', 'podman-desktop', 'configuration', 'settings.json');
+  // clean up settings to show initial welcome screen
+  if (existsSync(settingsPath)) {
+    console.log('Removing settings.json to get initial state');
+    await rm(settingsPath, {});
   }
 
   electronApp = await electron.launch({
@@ -35,17 +46,16 @@ afterAll(async () => {
   await electronApp.close();
 });
 
-test.skip('Check welcome page is displayed and that we are redirected to the main page where dashboard page is there', async () => {
+test('Check the Welcome page is displayed', async () => {
   // Direct Electron console to Node terminal.
-  page.on('console', console.log);
+  // page.on('console', console.log);
 
   const window: JSHandle<BrowserWindow> = await electronApp.browserWindow(page);
 
   const windowState = await window.evaluate(
-    (mainWindow): Promise<{ isVisible: boolean; isDevToolsOpened: boolean; isCrashed: boolean }> => {
+    (mainWindow): Promise<{ isVisible: boolean; isCrashed: boolean }> => {
       const getState = () => ({
         isVisible: mainWindow.isVisible(),
-        isDevToolsOpened: mainWindow.webContents.isDevToolsOpened(),
         isCrashed: mainWindow.webContents.isCrashed(),
       });
 
@@ -54,16 +64,36 @@ test.skip('Check welcome page is displayed and that we are redirected to the mai
          * The main window is created hidden, and is shown only when it is ready.
          * See {@link ../packages/main/src/mainWindow.ts} function
          */
+
+        mainWindow.webContents.closeDevTools();
         if (mainWindow.isVisible()) {
           resolve(getState());
         } else mainWindow.once('ready-to-show', () => resolve(getState()));
       });
     },
   );
+  expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
+  expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
 
   await page.screenshot({ path: 'tests/output/screenshots/screenshot-welcome-page-init.png', fullPage: true });
 
+  const welcomeMessage = page.locator('text=/Welcome to Podman Desktop.*/');
+  await playExpect(welcomeMessage).toBeVisible();
+});
+
+test('Telemetry checkbox is present, set to true, consent can be changed', async () => {
+
   // wait for the initial screen to be loaded
+  const telemetryConsent = page.getByText('Telemetry');
+  expect(telemetryConsent).not.undefined;
+  expect(await telemetryConsent.isChecked()).to.be.true;
+
+  await telemetryConsent.click();
+  expect(await telemetryConsent.isChecked()).to.be.false;
+});
+
+test('Redirection from Welcome page to Dashboard works', async () => {
+  
   const goToPodmanDesktopButton = page.locator('button:text("Go to Podman Desktop")');
   // wait for visibility
   await goToPodmanDesktopButton.waitFor({ state: 'visible' });
@@ -81,7 +111,36 @@ test.skip('Check welcome page is displayed and that we are redirected to the mai
   // check we have the dashboard page
   const dashboardTitle = page.getByRole('heading', { name: 'Dashboard' });
   await playExpect(dashboardTitle).toBeVisible();
+});
 
-  expect(windowState.isCrashed, 'The app has crashed').toBeFalsy();
-  expect(windowState.isVisible, 'The main window was not visible').toBeTruthy();
+test('Verify main UI elements are present - Application tray', async () => {
+  const appTray = page.locator('.place-self-end');
+  expect(appTray).not.undefined;
+  const help = appTray.getByTitle('Help');
+  playExpect(help).toBeVisible();
+  const tasks = appTray.getByTitle('Tasks');
+  playExpect(tasks).toBeVisible();
+  const feedback = appTray.getByTitle('Share your feedback');
+  playExpect(feedback).toBeVisible();
+  const version = appTray.getByTitle(/Using version .*/);
+  console.log(`version: ${await version.innerText()}`);
+  playExpect(version).toBeVisible();
+});
+
+test('Verify main UI elements are present - Kind and Compose installation', async () => {
+  const appTray = page.locator('.place-self-end').locator('xpath=..');
+  expect(appTray).not.undefined;
+  const kind = appTray.getByText('Kind');
+  playExpect(kind).toBeVisible();
+  const compose = appTray.getByText('Compose');
+  playExpect(compose).toBeVisible();
+});
+
+test('Verify main UI elements are present - Navigation Bar items', async () => {
+  const navBar = page.getByLabel('Global');
+  expect(navBar).not.undefined;
+  for (const item of navBarItems) {
+    const locator = navBar.getByText(new RegExp(item));
+    expect(locator).toBeDefined();
+  }
 });
