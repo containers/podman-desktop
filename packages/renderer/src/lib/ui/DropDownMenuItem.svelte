6bbc1d12fb3c6941e65de012e76253974590bb34
Máirín Duffy <duffy@redhat.com>
2023-05-30 10:48:44 -0400

    Refining colors/look&feel of action menu
    - s/violet/purple for colors
    - rounding edges of hover highlight of dropdown menu items (had been
      square and the corners were drawing over the rounded edges of the
    dropdownmenu ring)
    - removing opacity from ring and adjusting color values to match at full
      opacity
    - for improved purple text contrast in highlighted menu item, changed
      highlight background color to black and default menu item color to a
    lighter charcoal gray
    
    Signed-off-by: Máirín Duffy <duffy@redhat.com>

--- packages/renderer/src/lib/ui/DropDownMenuItem.svelte
+++ packages/renderer/src/lib/ui/DropDownMenuItem.svelte
@@ -10,7 +10,7 @@ export let onClick: () => void = () => {};
 
 {#if !hidden}
   <!-- Use a div + onclick so there's no "blind spots" for when clicking-->
-  <div class="p-3 hover:text-violet-400 text-gray-400 hover:cursor-pointer" role="none" on:click="{onClick}">
+  <div class="p-3 hover:bg-black hover:text-purple-500 hover:rounded-md text-gray-400 hover:cursor-pointer" role="none" on:click="{onClick}">
     <span title="{title}" class="group flex items-center text-sm no-underline whitespace-nowrap" tabindex="-1">
       <Fa class="h-4 w-4 text-md mr-2" icon="{icon}" />
       {title}
