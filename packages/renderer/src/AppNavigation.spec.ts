dd98b3a51c7c00790fce57424cfc114352d63f04
Ondrej Dockal <odockal@redhat.com>
2023-06-05 13:49:37 -0700

    chore: add aria labels into navigation items (#2742)
    * chore: add aria labels into navigation items
    
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>

---
+++ packages/renderer/src/AppNavigation.spec.ts
@@ -0,0 +1,53 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom';
+import { beforeAll, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import AppNavigation from './AppNavigation.svelte';
+
+// fake the window.events object
+beforeAll(() => {
+  (window.events as unknown) = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    receive: (_channel: string, func: any) => {
+      func();
+    },
+  };
+});
+
+test('Test rendering of the navigation bar with empty items', () => {
+  render(AppNavigation, {
+    meta: {
+      url: '/',
+    },
+  });
+
+  const dasboard = screen.getByRole('link', { name: 'Dashboard' });
+  expect(dasboard).toBeInTheDocument();
+  const containers = screen.getByRole('link', { name: 'Containers' });
+  expect(containers).toBeInTheDocument();
+  const pods = screen.getByRole('link', { name: 'Pods' });
+  expect(pods).toBeInTheDocument();
+  const images = screen.getByRole('link', { name: 'Images' });
+  expect(images).toBeInTheDocument();
+  const volumes = screen.getByRole('link', { name: 'Volumes' });
+  expect(volumes).toBeInTheDocument();
+  const settings = screen.getByRole('link', { name: 'Settings' });
+  expect(settings).toBeInTheDocument();
+});
