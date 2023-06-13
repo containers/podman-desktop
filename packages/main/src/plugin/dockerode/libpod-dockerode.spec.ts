ff569b387ea8b989451ca0e42ff650c4fb72a80b
Jeff MAURY <jmaury@redhat.com>
2023-06-13 09:42:58 +0200

    fix: Deleting a running pod generates an error
    Fixes #2413
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ packages/main/src/plugin/dockerode/libpod-dockerode.spec.ts
@@ -0,0 +1,57 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import { afterEach, beforeAll, beforeEach, test } from 'vitest';
+import { type LibPod, LibpodDockerode } from '/@/plugin/dockerode/libpod-dockerode.js';
+import Dockerode from 'dockerode';
+import { rest } from 'msw';
+import { type SetupServer, setupServer } from 'msw/node';
+
+beforeAll(() => {
+  const libpod = new LibpodDockerode();
+  libpod.enhancePrototypeWithLibPod();
+});
+
+let server: SetupServer;
+
+beforeEach(() => {
+  server = setupServer();
+  server.listen();
+});
+
+afterEach(() => server.close());
+
+test('Check force is not given with default remove pod options', async () => {
+  server.use(
+    rest.delete('http://localhost/v4.2.0/libpod/pods/dummy', (req, res, ctx) => {
+      return res(ctx.status(req.url.searchParams.has('force') ? 500 : 200));
+    }),
+  );
+  const api = new Dockerode({ protocol: 'http', host: 'localhost' });
+  await (api as unknown as LibPod).removePod('dummy');
+});
+
+test('Check force is given with remove pod options', async () => {
+  server.use(
+    rest.delete('http://localhost/v4.2.0/libpod/pods/dummy', (req, res, ctx) => {
+      return res(ctx.status(req.url.searchParams.has('force') ? 200 : 500));
+    }),
+  );
+  const api = new Dockerode({ protocol: 'http', host: 'localhost' });
+  await (api as unknown as LibPod).removePod('dummy', { force: true });
+});
