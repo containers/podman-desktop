21b51a9085a4972fadfd174573a0e28bdd0a8cd3
lstocchi <lstocchi@redhat.com>
2023-06-13 18:20:33 +0200

    add test
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- packages/renderer/src/lib/preferences/PreferencesResourcesRendering.svelte
+++ packages/renderer/src/lib/preferences/PreferencesResourcesRendering.svelte
@@ -40,7 +40,7 @@ $: providerInstallationInProgress = new Map<string, boolean>();
 
 let isStatusUpdated = false;
 let displayInstallModal = false;
-let installModalProvider: { provider: ProviderInfo, displayName: string };
+let providerToBeInstalled: { provider: ProviderInfo; displayName: string };
 let doExecuteAfterInstallation: () => void;
 $: preflightChecks = [];
 
@@ -58,11 +58,13 @@ onMount(() => {
     providers = providerInfosValue;
     const connectionNames = [];
     providers.forEach(provider => {
-      if (installModalProvider 
-          && doExecuteAfterInstallation 
-          && provider.name === installModalProvider.provider.name 
-          && (provider.status === 'ready' || provider.status === 'installed')) {
-        installModalProvider = undefined;
+      if (
+        providerToBeInstalled &&
+        doExecuteAfterInstallation &&
+        provider.name === providerToBeInstalled.provider.name &&
+        (provider.status === 'ready' || provider.status === 'installed')
+      ) {
+        providerToBeInstalled = undefined;
         doExecuteAfterInstallation();
       }
 
@@ -231,10 +233,10 @@ async function startConnectionProvider(
 
 async function doCreateNew(provider: ProviderInfo, displayName: string) {
   displayInstallModal = false;
-  if (provider.status == 'not-installed') {
+  if (provider.status === 'not-installed') {
     providerInstallationInProgress.set(provider.name, true);
     providerInstallationInProgress = providerInstallationInProgress;
-    installModalProvider = { provider, displayName };
+    providerToBeInstalled = { provider, displayName };
     doExecuteAfterInstallation = () => router.goto(`/preferences/provider/${provider.internalId}`);
     performInstallation(provider);
   } else {
@@ -263,7 +265,7 @@ async function performInstallation(provider: ProviderInfo) {
         currentCheck = status;
         if (currentCheck.successful === false) {
           preflightChecks = [...checksStatus, currentCheck];
-        }        
+        }
       },
     });
   } catch (err) {
@@ -343,16 +345,16 @@ function openLink(e: MouseEvent, url: string): void {
                       class="pf-c-button pf-m-primary"
                       aria-label="Create new {providerDisplayName}"
                       type="button"
-                      on:click="{() => doCreateNew(provider, providerDisplayName)}">                      
-                        {#if providerInstallationInProgress.get(provider.name) === true}                        
-                          <i class="pf-c-button__progress">
-                            <span class="pf-c-spinner pf-m-md" role="progressbar">
-                              <span class="pf-c-spinner__clipper"></span>
-                              <span class="pf-c-spinner__lead-ball"></span>
-                              <span class="pf-c-spinner__tail-ball"></span>
-                            </span>
-                          </i>
-                        {/if}                      
+                      on:click="{() => doCreateNew(provider, providerDisplayName)}">
+                      {#if providerInstallationInProgress.get(provider.name) === true}
+                        <i class="pf-c-button__progress">
+                          <span class="pf-c-spinner pf-m-md" role="progressbar">
+                            <span class="pf-c-spinner__clipper"></span>
+                            <span class="pf-c-spinner__lead-ball"></span>
+                            <span class="pf-c-spinner__tail-ball"></span>
+                          </span>
+                        </i>
+                      {/if}
                       {buttonTitle} ...
                     </button>
                   </Tooltip>
@@ -446,66 +448,61 @@ function openLink(e: MouseEvent, url: string): void {
       {/each}
     {/if}
   </div>
-  {#if displayInstallModal && installModalProvider}
-  <Modal on:close="{() => hideInstallModal()}">
-    <div
-      class="inline-block w-full overflow-hidden text-left transition-all transform bg-charcoal-600 z-50 rounded-xl shadow-xl shadow-neutral-900">
-      
+  {#if displayInstallModal && providerToBeInstalled}
+    <Modal on:close="{() => hideInstallModal()}">
+      <div
+        class="inline-block w-full overflow-hidden text-left transition-all transform bg-charcoal-600 z-50 rounded-xl shadow-xl shadow-neutral-900"
+        aria-label="install provider">
         <div class="flex items-center justify-between px-5 py-4 mb-4">
-          <h1 class="text-md font-semibold">Create a new {installModalProvider.displayName}</h1>
-  
+          <h1 class="text-md font-semibold">Create a new {providerToBeInstalled.displayName}</h1>
           <button class="hover:text-gray-300 px-2 py-1" on:click="{() => hideInstallModal()}">
             <i class="fas fa-times" aria-hidden="true"></i>
           </button>
         </div>
-      <div class="overflow-y-auto px-4 pb-4">        
-        <div class="flex flex-col rounded-lg">
-          <div class="mx-auto max-w-[250px] mb-5">
-            <ProviderLogo provider="{installModalProvider.provider}" />
-          </div>
-          <div class="flex flex-row mx-auto text-md">
-            Some system requirements are missing.
-          </div>
-          <div class="flex flex-col min-h-[150px] mt-5 mx-auto py-4 px-10 rounded-md bg-charcoal-800">
-            {#each preflightChecks as preCheck}
-              <div class="flex flex-row mb-2 mx-auto">
-                <Fa icon="{faCircleXmark}" class="text-red-500 mt-0.5" />
-                <div class="flex flex-col ml-1 text-sm">
-                  {#if preCheck.description}                  
-                    <span class="w-full">{preCheck.description}</span>
-                    {#if preCheck.docLinks}
-                      <div class="flex flex-row mt-0.5">
-                        <span class="mr-1">See:</span>
-                        {#each preCheck.docLinks as link}
-                          <a href="{link.url}" target="_blank" class="mr-1" on:click="{e => openLink(e, link.url)}">{link.title}</a>
-                        {/each}
-                      </div>              
+        <div class="overflow-y-auto px-4 pb-4">
+          <div class="flex flex-col rounded-lg">
+            <div class="mx-auto max-w-[250px] mb-5">
+              <ProviderLogo provider="{providerToBeInstalled.provider}" />
+            </div>
+            <div class="flex flex-row mx-auto text-md">Some system requirements are missing.</div>
+            <div class="flex flex-col min-h-[150px] mt-5 mx-auto py-4 px-10 rounded-md bg-charcoal-800">
+              {#each preflightChecks as preCheck}
+                <div class="flex flex-row mb-2 mx-auto">
+                  <Fa icon="{faCircleXmark}" class="text-red-500 mt-0.5" />
+                  <div class="flex flex-col ml-1 text-sm">
+                    {#if preCheck.description}
+                      <span class="w-full">{preCheck.description}</span>
+                      {#if preCheck.docLinks}
+                        <div class="flex flex-row mt-0.5">
+                          <span class="mr-1">See:</span>
+                          {#each preCheck.docLinks as link}
+                            <a href="{link.url}" target="_blank" class="mr-1" on:click="{e => openLink(e, link.url)}"
+                              >{link.title}</a>
+                          {/each}
+                        </div>
+                      {/if}
+                    {:else}
+                      {preCheck.name}
                     {/if}
-                  
-                  {:else}
-                    {preCheck.name}          
-                {/if}
-
+                  </div>
                 </div>
-                
-              </div>
-            {/each}
+              {/each}
+            </div>
+            <div class="text-xs text-gray-800 mt-2 text-center">
+              Be sure that your system fulfills all the requirements above before proceeding
+            </div>
+            <div class="flex flex-row justify-end w-full pt-2">
+              <button aria-label="Cancel" class="text-xs hover:underline mr-3" on:click="{() => hideInstallModal()}"
+                >Cancel</button>
+              <button
+                class="pf-c-button pf-m-primary"
+                type="button"
+                on:click="{() => doCreateNew(providerToBeInstalled.provider, providerToBeInstalled.displayName)}"
+                >Next</button>
+            </div>
           </div>
-          <div class="text-xs text-gray-800 mt-2  text-center">
-            Be sure that your system fulfills all the requirements above before proceeding
-          </div>
-          <div class="flex flex-row justify-end w-full pt-2">
-            <button aria-label="Cancel" class="text-xs hover:underline mr-3" on:click="{() => hideInstallModal()}"
-              >Cancel</button>
-            <button
-              class="pf-c-button pf-m-primary"
-              type="button"
-              on:click="{() => doCreateNew(installModalProvider.provider, installModalProvider.displayName)}">Next</button>
-          </div>
-        </div>        
+        </div>
       </div>
-    </div>
-  </Modal>
+    </Modal>
   {/if}
-
 </SettingsPage>
