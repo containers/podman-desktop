951f9bab1bee49995045c991a8d545be3c666c31
Florent Benoit <fbenoit@redhat.com>
2023-06-15 16:18:35 +0200

    chore: fix ban-ts-comment eslint rule
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/editor/MonacoEditor.svelte
+++ packages/renderer/src/lib/editor/MonacoEditor.svelte
@@ -15,7 +15,6 @@ export let content = '';
 export let language = 'json';
 
 onMount(async () => {
-  // @ts-ignore
   self.MonacoEnvironment = {
     getWorker: function (_moduleId: any, label: string) {
       if (label === 'json') {
@@ -23,6 +22,7 @@ onMount(async () => {
       }
       return new editorWorker();
     },
+    createTrustedTypesPolicy: () => undefined,
   };
 
   Monaco = await import('monaco-editor');
