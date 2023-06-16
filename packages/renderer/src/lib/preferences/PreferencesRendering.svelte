4dd3f6be3c633188989060fd4375e84d1b4c9472
Tim deBoer <git@tdeboer.ca>
2023-06-16 14:13:02 -0400

    chore: Move preferences search into header
    Follow up from PR #2863: the search bar for preferences should be in the header and always visible.
    
    I started to add a search capability to SettingsPage similar to NavPage, but I don't think the extra code and complexity is worth it when this is the only page that has search within Settings. This just adds a slot to the header, uses it in preferences, and makes the necessary formatting changes.
    
    Minor tweak to merge pt/mt + pb into a py.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/preferences/PreferencesRendering.svelte
+++ packages/renderer/src/lib/preferences/PreferencesRendering.svelte
@@ -48,7 +48,7 @@ function updateSearchValue(event: any) {
 
 <Route path="/" breadcrumb="{key}">
   <SettingsPage title="Preferences">
-    <div class="bg-charcoal-900">
+    <div class="mt-4" slot="header">
       <div
         class="flex items-center text-gray-700 rounded-sm rounded-lg border-2 border-charcoal-900 focus-within:border-violet-500">
         <input
@@ -73,12 +73,12 @@ function updateSearchValue(event: any) {
         </svg>
       </div>
     </div>
-    <div class="flex flex-col min-w-full rounded-md">
+    <div class="flex flex-col min-w-full rounded-md space-y-5">
       {#if matchingRecords.size === 0}
-        <div class="mt-5">No Settings Found</div>
+        <div>No Settings Found</div>
       {:else}
         {#each [...matchingRecords.keys()].sort((a, b) => a.localeCompare(b)) as configSection}
-          <div class="mt-5">
+          <div>
             <div class="first-letter:uppercase">{matchingRecords.get(configSection).at(0).title}</div>
             {#each matchingRecords.get(configSection) as configItem}
               <div class="bg-charcoal-600 rounded-md mt-2 ml-2">
