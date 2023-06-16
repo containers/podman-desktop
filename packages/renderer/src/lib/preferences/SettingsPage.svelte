0d6010597ed96caa4d7b9ddcd69ccfe0dcf38789
Tim deBoer <git@tdeboer.ca>
2023-06-16 14:13:02 -0400

    chore: Move preferences search into header
    Follow up from PR #2863: the search bar for preferences should be in the header and always visible.
    
    I started to add a search capability to SettingsPage similar to NavPage, but I don't think the extra code and complexity is worth it when this is the only page that has search within Settings. This just adds a slot to the header, uses it in preferences, and makes the necessary formatting changes.
    
    Minor tweak to merge pt/mt + pb into a py.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/preferences/SettingsPage.svelte
+++ packages/renderer/src/lib/preferences/SettingsPage.svelte
@@ -3,13 +3,15 @@ export let title: string;
 </script>
 
 <div class="flex flex-col min-w-full h-full">
-  <div class="min-w-full pt-4 pb-4 px-5">
+  <div class="min-w-full px-5 py-4">
     <div>
       <p class="capitalize text-xl">{title}</p>
       <p class="text-sm text-gray-700"><slot name="subtitle"><br /></slot></p>
     </div>
+
+    <slot name="header" />
   </div>
-  <div class="flex flex-col min-w-full h-full mt-4 pb-4 px-5 overflow-y-auto">
+  <div class="flex flex-col min-w-full h-full px-5 py-4 overflow-y-auto">
     <slot />
   </div>
 </div>
