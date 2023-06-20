833fa39b921a82b270c54720111e43e93a8ade9d
Florent Benoit <fbenoit@redhat.com>
2023-06-20 17:12:59 +0200

    chore: add aria role/label for error message component
    It is easier to test (then add a test file)
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/ui/ErrorMessage.svelte
+++ packages/renderer/src/lib/ui/ErrorMessage.svelte
@@ -18,6 +18,6 @@ export let icon = false;
     class="text-red-500 p-1 flex flex-row items-center {$$props.class}"
     class:opacity-0="{error === undefined || error === ''}">
     <Fa size="18" class="cursor-pointer text-red-500" icon="{faExclamationCircle}" />
-    <div class="ml-2">{error}</div>
+    <div role="alert" aria-label="Error Message Content" class="ml-2">{error}</div>
   </div>
 {/if}
