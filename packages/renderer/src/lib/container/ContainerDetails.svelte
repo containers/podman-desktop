ca0ba4a14ac967f8f5bdb2aa6b12e8a9fc6c8cd5
Tim deBoer <git@tdeboer.ca>
2023-06-23 11:45:18 -0400

    fix: detailspage, resizing & consistency
    The four details pages each had a little uniqueness, and some copy/paste errors
    (e.g. issue #2986 - the close button goes to the wrong place). They also had
    layout issues when the window is narrower (issue 2322).
    
    This creates a common DetailsPage component to make sure the pages are consistent,
    and applies it to each page. There are two features that are only used by one page
    right now (e.g. images have a title detail in purple; containers has statistics
    detail on the right) but otherwise everything in the same.
    
    Fixed a few layout issues in the component to improve narrow layouts (e.g.
    extraneous spacing, moved tabs to new row) and fixed the close action. There is
    still some work that could be done to reduce some of the class attributes that are
    still required in each page or simplify further, but I think this is a good pass.
    
    Fixes #2322.
    Fixes #2986.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/container/ContainerDetails.svelte
+++ packages/renderer/src/lib/container/ContainerDetails.svelte
@@ -15,6 +15,7 @@ import ContainerDetailsSummary from './ContainerDetailsSummary.svelte';
 import ContainerDetailsInspect from './ContainerDetailsInspect.svelte';
 import ContainerDetailsKube from './ContainerDetailsKube.svelte';
 import ContainerStatistics from './ContainerStatistics.svelte';
+import DetailsPage from '../ui/DetailsPage.svelte';
 import DetailsTab from '../ui/DetailsTab.svelte';
 import ErrorMessage from '../ui/ErrorMessage.svelte';
 
@@ -46,85 +47,56 @@ function errorCallback(errorMessage: string): void {
 </script>
 
 {#if container}
-  <Route path="/*">
-    <div class="w-full h-full">
-      <div class="flex h-full flex-col">
-        <div class="flex w-full flex-row">
-          <div class="w-full px-5 pt-5">
-            <div class="flex flew-row items-center">
-              <a
-                class="text-violet-400 text-base hover:no-underline"
-                href="/containers"
-                title="Go back to containers list">Containers</a>
-              <div class="text-xl mx-2 text-gray-700">></div>
-              <div class="text-sm font-extralight text-gray-700">Container Details</div>
-            </div>
-            <div class="text-lg flex flex-row items-start pt-1">
-              <div class="pr-3 pt-1">
-                <StatusIcon icon="{ContainerIcon}" status="{container.state}" />
-              </div>
-              <div class="text-lg flex flex-col">
-                <div class="mr-2">{container.name}</div>
-                <div class="mr-2 pb-4 text-small text-gray-900" title="{container.image}">{container.shortImage}</div>
-              </div>
-            </div>
-            <section class="pf-c-page__main-tabs pf-m-limit-width">
-              <div class="pf-c-page__main-body">
-                <div class="pf-c-tabs pf-m-page-insets" id="open-tabs-example-tabs-list">
-                  <ul class="pf-c-tabs__list">
-                    <DetailsTab title="Summary" url="summary" />
-                    <DetailsTab title="Logs" url="logs" />
-                    <DetailsTab title="Inspect" url="inspect" />
-
-                    {#if container.engineType === 'podman' && container.groupInfo.type === ContainerGroupInfoTypeUI.STANDALONE}
-                      <DetailsTab title="Kube" url="kube" />
-                    {/if}
-                    <DetailsTab title="Terminal" url="terminal" />
-                  </ul>
-                </div>
-              </div>
-            </section>
-          </div>
-          <div class="flex flex-col px-5 pt-5">
-            <div class="flex justify-end">
-              <div class="flex items-center w-5">
-                {#if container.actionError}
-                  <ErrorMessage error="{container.actionError}" icon />
-                {:else}
-                  <div>&nbsp;</div>
-                {/if}
-              </div>
-              <ContainerActions
-                inProgressCallback="{(flag, state) => inProgressCallback(flag, state)}"
-                errorCallback="{error => errorCallback(error)}"
-                container="{container}"
-                detailed="{true}" />
-            </div>
-            <div class="flex my-2 w-full justify-end">
-              <ContainerStatistics container="{container}" />
-            </div>
-          </div>
-          <a href="/containers" title="Close Details" class="mt-2 mr-2 text-gray-900"
-            ><i class="fas fa-times" aria-hidden="true"></i></a>
-        </div>
-        <div class="h-full bg-charcoal-900">
-          <Route path="/summary" breadcrumb="Summary">
-            <ContainerDetailsSummary container="{container}" />
-          </Route>
-          <Route path="/logs" breadcrumb="Logs">
-            <ContainerDetailsLogs container="{container}" />
-          </Route>
-          <Route path="/inspect" breadcrumb="Inspect">
-            <ContainerDetailsInspect container="{container}" />
-          </Route>
-          <Route path="/kube" breadcrumb="Kube">
-            <ContainerDetailsKube container="{container}" />
-          </Route>
-          <Route path="/terminal" breadcrumb="Terminal">
-            <ContainerDetailsTerminal container="{container}" />
-          </Route>
-        </div>
+  <DetailsPage
+    name="Container Details"
+    title="{container.name}"
+    subtitle="{container.shortImage}"
+    parentName="Containers"
+    parentURL="/containers">
+    <StatusIcon slot="icon" icon="{ContainerIcon}" status="{container.state}" />
+    <div slot="actions" class="flex justify-end">
+      <div class="flex items-center w-5">
+        {#if container.actionError}
+          <ErrorMessage error="{container.actionError}" icon />
+        {:else}
+          <div>&nbsp;</div>
+        {/if}
       </div>
+      <ContainerActions
+        inProgressCallback="{(flag, state) => inProgressCallback(flag, state)}"
+        errorCallback="{error => errorCallback(error)}"
+        container="{container}"
+        detailed="{true}" />
     </div>
-  </Route>
+    <div slot="detail" class="flex py-2 w-full justify-end">
+      <ContainerStatistics container="{container}" />
+    </div>
+    <div slot="tabs" class="pf-c-tabs__list">
+      <DetailsTab title="Summary" url="summary" />
+      <DetailsTab title="Logs" url="logs" />
+      <DetailsTab title="Inspect" url="inspect" />
+
+      {#if container.engineType === 'podman' && container.groupInfo.type === ContainerGroupInfoTypeUI.STANDALONE}
+        <DetailsTab title="Kube" url="kube" />
+      {/if}
+      <DetailsTab title="Terminal" url="terminal" />
+    </div>
+    <span slot="content">
+      <Route path="/summary" breadcrumb="Summary">
+        <ContainerDetailsSummary container="{container}" />
+      </Route>
+      <Route path="/logs" breadcrumb="Logs">
+        <ContainerDetailsLogs container="{container}" />
+      </Route>
+      <Route path="/inspect" breadcrumb="Inspect">
+        <ContainerDetailsInspect container="{container}" />
+      </Route>
+      <Route path="/kube" breadcrumb="Kube">
+        <ContainerDetailsKube container="{container}" />
+      </Route>
+      <Route path="/terminal" breadcrumb="Terminal">
+        <ContainerDetailsTerminal container="{container}" />
+      </Route>
+    </span>
+  </DetailsPage>
 {/if}
--- packages/renderer/src/lib/image/ImageDetails.svelte
+++ packages/renderer/src/lib/image/ImageDetails.svelte
@@ -11,6 +11,7 @@ import ImageDetailsInspect from './ImageDetailsInspect.svelte';
 import ImageDetailsHistory from './ImageDetailsHistory.svelte';
 import ImageDetailsSummary from './ImageDetailsSummary.svelte';
 import PushImageModal from './PushImageModal.svelte';
+import DetailsPage from '../ui/DetailsPage.svelte';
 import DetailsTab from '../ui/DetailsTab.svelte';
 
 export let imageID: string;
@@ -44,68 +45,34 @@ onMount(() => {
 </script>
 
 {#if image}
-  <Route path="/*">
-    <div class="w-full h-full">
-      <div class="flex h-full flex-col">
-        <div class="flex w-full flex-row">
-          <div class="w-full px-5 pt-5">
-            <div class="flex flew-row items-center">
-              <a class="text-violet-400 text-base hover:no-underline" href="/images" title="Go back to images list"
-                >Images</a>
-              <div class="text-xl mx-2 text-gray-700">></div>
-              <div class="text-sm font-extralight text-gray-700">Image Details</div>
-            </div>
-            <div class="flex flex-row items-start pt-1">
-              <div class="pr-3 pt-1">
-                <StatusIcon icon="{ImageIcon}" status="{image.inUse ? 'USED' : 'UNUSED'}" />
-              </div>
-              <div class="text-lg flex flex-col">
-                <div class="flex flex-row">
-                  <div class="mr-2">{image.name}</div>
-                  <div class="text-base text-violet-400">{image.shortId}</div>
-                </div>
-                <div class="mr-2 pb-4 text-small text-gray-900">{image.tag}</div>
-              </div>
-            </div>
-
-            <section class="pf-c-page__main-tabs pf-m-limit-width">
-              <div class="pf-c-page__main-body">
-                <div class="pf-c-tabs pf-m-page-insets" id="open-tabs-example-tabs-list">
-                  <ul class="pf-c-tabs__list">
-                    <DetailsTab title="Summary" url="summary" />
-                    <DetailsTab title="History" url="history" />
-                    <DetailsTab title="Inspect" url="inspect" />
-                  </ul>
-                </div>
-              </div>
-            </section>
-          </div>
-          <div class="flex flex-col px-5 pt-5">
-            <div class="flex justify-end">
-              <ImageActions
-                image="{image}"
-                onPushImage="{handlePushImageModal}"
-                detailed="{true}"
-                dropdownMenu="{false}" />
-            </div>
-          </div>
-          <a href="/containers" title="Close Details" class="mt-2 mr-2 text-gray-900"
-            ><i class="fas fa-times" aria-hidden="true"></i></a>
-        </div>
-        <div class="h-full bg-charcoal-900">
-          <Route path="/history" breadcrumb="History">
-            <ImageDetailsHistory image="{image}" />
-          </Route>
-          <Route path="/inspect" breadcrumb="Inspect">
-            <ImageDetailsInspect image="{image}" />
-          </Route>
-          <Route path="/summary" breadcrumb="Summary">
-            <ImageDetailsSummary image="{image}" />
-          </Route>
-        </div>
-      </div>
+  <DetailsPage
+    name="Image Details"
+    title="{image.name}"
+    titleDetail="{image.shortId}"
+    subtitle="{image.tag}"
+    parentName="Images"
+    parentURL="/images">
+    <StatusIcon slot="icon" icon="{ImageIcon}" status="{image.inUse ? 'USED' : 'UNUSED'}" />
+    <div slot="actions" class="flex justify-end">
+      <ImageActions image="{image}" onPushImage="{handlePushImageModal}" detailed="{true}" dropdownMenu="{false}" />
     </div>
-  </Route>
+    <div slot="tabs" class="pf-c-tabs__list">
+      <DetailsTab title="Summary" url="summary" />
+      <DetailsTab title="History" url="history" />
+      <DetailsTab title="Inspect" url="inspect" />
+    </div>
+    <span slot="content">
+      <Route path="/history" breadcrumb="History">
+        <ImageDetailsHistory image="{image}" />
+      </Route>
+      <Route path="/inspect" breadcrumb="Inspect">
+        <ImageDetailsInspect image="{image}" />
+      </Route>
+      <Route path="/summary" breadcrumb="Summary">
+        <ImageDetailsSummary image="{image}" />
+      </Route>
+    </span>
+  </DetailsPage>
 {/if}
 
 {#if pushImageModal}
