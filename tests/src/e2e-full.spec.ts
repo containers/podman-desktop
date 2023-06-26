0097b9b97c1b424c36015278d8d2778e8eaad24b
Tamara Babalova <tbabalov@redhat.com>
2023-06-26 16:06:23 +0200

    format fix
    Signed-off-by: Tamara Babalova <tbabalov@redhat.com>

--- tests/src/e2e-full.spec.ts
+++ tests/src/e2e-full.spec.ts
@@ -27,13 +27,13 @@ beforeAll(async () => {
   });
 
   page = await electronApp.firstWindow();
-  
+
   page.on('console', console.log);
 
-  const checkLoader = page.getByRole('heading', {name: 'Initializing...'});
-  await playExpect(checkLoader).toHaveCount(0, {timeout: 10000});
+  const checkLoader = page.getByRole('heading', { name: 'Initializing...' });
+  await playExpect(checkLoader).toHaveCount(0, { timeout: 10000 });
 
-  if(await page.locator('button:text("Go to Podman Desktop")').count()>0){
+  if ((await page.locator('button:text("Go to Podman Desktop")').count()) > 0) {
     await page.locator('button:text("Go to Podman Desktop")').click();
   }
 });
@@ -42,38 +42,37 @@ afterAll(async () => {
   await electronApp.close();
 });
 
-test('Pull and check image', async() => {
-    const images = page.getByRole('link', { name: 'Images' });
-    await playExpect(images).toBeVisible();
-    await images.click();
-  
-    let checkPage = page.getByRole('heading', {name: 'images', exact: true});
-    await playExpect(checkPage).toBeVisible();
-  
-    const pullImageButton = page.locator('button:text("Pull an image")');
-    await pullImageButton.waitFor({state: 'visible'});
-    await pullImageButton.click();
-  
-    checkPage = page.getByRole('heading', {name: 'Pull Image From a Registry'});
-    await playExpect(checkPage).toBeVisible();
-    
-    const imageInput = page.getByLabel("imageName");
-    await imageInput.fill("quay.io/centos7/httpd-24-centos7");
-  
-    const pullButton = page.locator('button:text("Pull image")');
-    await pullButton.waitFor({state: 'visible'});
-    await pullButton.click();
-  
-    const doneButton = page.locator('button:text("Done")');
-    await doneButton.waitFor({state: 'visible'});
-    await doneButton.click();
-  
-    const imageRow = page.locator('tr:has-text("quay.io/centos7/httpd-24-centos7")');
-    await imageRow.waitFor({state: 'visible'});
-    await imageRow.click();
+test('Pull and check image', async () => {
+  const images = page.getByRole('link', { name: 'Images' });
+  await playExpect(images).toBeVisible();
+  await images.click();
 
-    await playExpect(page.getByText('Summary')).toBeVisible();
-    await playExpect(page.getByText('History')).toBeVisible();
-    await playExpect(page.getByText('Inspect')).toBeVisible();
-  });
-  
+  let checkPage = page.getByRole('heading', { name: 'images', exact: true });
+  await playExpect(checkPage).toBeVisible();
+
+  const pullImageButton = page.locator('button:text("Pull an image")');
+  await pullImageButton.waitFor({ state: 'visible' });
+  await pullImageButton.click();
+
+  checkPage = page.getByRole('heading', { name: 'Pull Image From a Registry' });
+  await playExpect(checkPage).toBeVisible();
+
+  const imageInput = page.getByLabel('imageName');
+  await imageInput.fill('quay.io/centos7/httpd-24-centos7');
+
+  const pullButton = page.locator('button:text("Pull image")');
+  await pullButton.waitFor({ state: 'visible' });
+  await pullButton.click();
+
+  const doneButton = page.locator('button:text("Done")');
+  await doneButton.waitFor({ state: 'visible' });
+  await doneButton.click();
+
+  const imageRow = page.locator('tr:has-text("quay.io/centos7/httpd-24-centos7")');
+  await imageRow.waitFor({ state: 'visible' });
+  await imageRow.click();
+
+  await playExpect(page.getByText('Summary')).toBeVisible();
+  await playExpect(page.getByText('History')).toBeVisible();
+  await playExpect(page.getByText('Inspect')).toBeVisible();
+});
