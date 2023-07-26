4c52dd5b9a6c5c759f6d401f112996b8046bbf7e
Charlie Drage <charlie@charliedrage.com>
2023-07-26 09:59:39 -0400

    feat: add inspect tab to compose (#3316)
    ### What does this PR do?
    
    * Adds the inspect tab to Compose which is an array of "container
      inspect" from docker / podman.
    * Inspect is JSON array of containers, see
      https://github.com/docker/compose/issues/4155 for inspiration (they
      don't actually have a `compose inspect` command..)
    * Adds tests
    
    ### Screenshot/screencast of this PR
    
    <!-- Please include a screenshot or a screencast explaining what is doing this PR -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Please include any related issue from Podman Desktop repository (or from another issue tracker).
    -->
    
    Closes https://github.com/containers/podman-desktop/issues/3192
    
    ### How to test this PR?
    
    1. Deploy a compose example
    2. Click on the compose group
    3. Select the inspect tab
    
    <!-- Please explain steps to reproduce -->
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/renderer/src/lib/compose/ComposeDetails.spec.ts
+++ packages/renderer/src/lib/compose/ComposeDetails.spec.ts
@@ -3,6 +3,11 @@ import { test, expect, vi } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/svelte';
 import ComposeDetails from './ComposeDetails.svelte';
 import { mockBreadcrumb } from '../../stores/breadcrumb';
+import type { ContainerInspectInfo } from '@podman-desktop/api';
+import { containersInfos } from '../../stores/containers';
+import { get } from 'svelte/store';
+
+const listContainersMock = vi.fn();
 
 vi.mock('xterm', () => {
   return {
@@ -11,6 +16,10 @@ vi.mock('xterm', () => {
 });
 
 beforeAll(() => {
+  const onDidUpdateProviderStatusMock = vi.fn();
+  (window as any).onDidUpdateProviderStatus = onDidUpdateProviderStatusMock;
+  onDidUpdateProviderStatusMock.mockImplementation(() => Promise.resolve());
+
   (window.events as unknown) = {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     receive: (_channel: string, func: any) => {
@@ -21,14 +30,124 @@ beforeAll(() => {
   (window as any).matchMedia = vi.fn().mockReturnValue({
     addListener: vi.fn(),
   });
-  (window as any).telemetryPage = vi.fn().mockReturnValue({
-    sendPageView: vi.fn(),
-  });
   (window as any).ResizeObserver = vi.fn().mockReturnValue({ observe: vi.fn(), unobserve: vi.fn() });
   (window as any).initializeProvider = vi.fn().mockResolvedValue([]);
+  (window as any).getContainerInspect = vi.fn().mockResolvedValue(containerInspectInfo);
+  (window as any).listNetworks = vi.fn().mockResolvedValue([]);
+  (window as any).listContainers = listContainersMock;
+  (window as any).logsContainer = vi.fn();
+  (window as any).listViewsContributions = vi.fn();
+  (window as any).telemetryPage = vi.fn().mockResolvedValue(undefined);
   (window as any).generatePodmanKube = vi.fn();
   mockBreadcrumb();
 });
+
+const containerInspectInfo: ContainerInspectInfo = {
+  engineId: '',
+  engineName: '',
+  Id: '',
+  Created: '',
+  Path: '',
+  Args: [],
+  State: {
+    Status: '',
+    Running: false,
+    Paused: false,
+    Restarting: false,
+    OOMKilled: false,
+    Dead: false,
+    Pid: 0,
+    ExitCode: 0,
+    Error: '',
+    StartedAt: '',
+    FinishedAt: '',
+    Health: {
+      Status: '',
+      FailingStreak: 0,
+      Log: [],
+    },
+  },
+  Image: '',
+  ResolvConfPath: '',
+  HostnamePath: '',
+  HostsPath: '',
+  LogPath: '',
+  Name: '',
+  RestartCount: 0,
+  Driver: '',
+  Platform: '',
+  MountLabel: '',
+  ProcessLabel: '',
+  AppArmorProfile: '',
+  HostConfig: {
+    PortBindings: {
+      9090: [
+        {
+          HostPort: 8383,
+          HostIp: '',
+        },
+      ],
+    },
+  },
+  GraphDriver: {
+    Name: '',
+    Data: {
+      DeviceId: '',
+      DeviceName: '',
+      DeviceSize: '',
+    },
+  },
+  Mounts: [],
+  Config: {
+    Hostname: '',
+    Domainname: '',
+    User: '',
+    AttachStdin: false,
+    AttachStdout: false,
+    AttachStderr: false,
+    ExposedPorts: {},
+    Tty: false,
+    OpenStdin: false,
+    StdinOnce: false,
+    Env: [],
+    Cmd: [],
+    Image: '',
+    Volumes: {},
+    WorkingDir: '',
+    Entrypoint: '',
+    OnBuild: undefined,
+    Labels: {},
+  },
+  NetworkSettings: {
+    Bridge: '',
+    SandboxID: '',
+    HairpinMode: false,
+    LinkLocalIPv6Address: '',
+    LinkLocalIPv6PrefixLen: 0,
+    Ports: {},
+    SandboxKey: '',
+    SecondaryIPAddresses: undefined,
+    SecondaryIPv6Addresses: undefined,
+    EndpointID: '',
+    Gateway: '',
+    GlobalIPv6Address: '',
+    GlobalIPv6PrefixLen: 0,
+    IPAddress: '',
+    IPPrefixLen: 0,
+    IPv6Gateway: '',
+    MacAddress: '',
+    Networks: {},
+    Node: {
+      ID: '',
+      IP: '',
+      Addr: '',
+      Name: '',
+      Cpus: 0,
+      Memory: 0,
+      Labels: undefined,
+    },
+  },
+};
 
 test('Simple test that compose logs are clickable and loadable', async () => {
   render(ComposeDetails, { composeName: 'foobar', engineId: 'engine' });
@@ -44,6 +163,50 @@ test('Simple test that compose logs are clickable and loadable', async () => {
 test('Simple test that compose name is displayed', async () => {
   render(ComposeDetails, { composeName: 'foobar', engineId: 'engine' });
   expect(screen.getByText('foobar')).toBeInTheDocument();
+});
+
+test('Compose details inspect is clickable and loadable', async () => {
+  const mockedContainers = [
+    {
+      Id: 'sha256:1234567890123',
+      Image: 'sha256:123',
+      Names: ['foo'],
+      Status: 'Running',
+      engineId: 'podman',
+      engineName: 'podman',
+      Labels: {
+        'com.docker.compose.project': 'foobar',
+      },
+    },
+    {
+      Id: 'sha256:1234567890123',
+      Image: 'sha256:123',
+      Names: ['foo2'],
+      Status: 'Running',
+      engineId: 'podman',
+      engineName: 'podman',
+      Labels: {
+        'com.docker.compose.project': 'foobar',
+      },
+    },
+  ];
+
+  listContainersMock.mockResolvedValue(mockedContainers);
+
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  // wait store are populated
+  while (get(containersInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+
+  render(ComposeDetails, { composeName: 'foobar', engineId: 'podman' });
+
+  // Click on the inspect href that it renders correctly / displays the correct data
+  const inspectHref = screen.getByRole('link', { name: 'Inspect' });
+  await fireEvent.click(inspectHref);
 });
 
 test('Test that compose kube tab is clickable and loadable', async () => {
