16521993119ca0c2718fc1af2784ef42262098da
Charlie Drage <charlie@charliedrage.com>
2023-08-02 15:36:56 -0400

    feat: add starting / stopping call backs for compose group (#3390)
    ### What does this PR do?
    
    When deleting / starting a group of containers, the starting / stopping
    callbacks weren't correctly added. This PR adds the feature of
    "STARTING" and "STOPPING" showing.
    
    Unsure how to correctly add tests, but instead I have added new tests in
    a new file for Compose. I have put this in a new file as when using a
    group of compose containers in ContainerList.spec.ts, it is not
    correctly cleared up after each test. Using afterEach and clearing mocks
    does not seem to work. So it will go in a new file temporarily.
    
    ### Screenshot/screencast of this PR
    
    <!-- Please include a screenshot or a screencast explaining what is doing this PR -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Please include any related issue from Podman Desktop repository (or from another issue tracker).
    -->
    
    Closes https://github.com/containers/podman-desktop/issues/3123
    
    ### How to test this PR?
    
    1. Deploy a Docker Compose example.
    2. Stop / start the group. It should now show STARTING or STOPPING.
    
    <!-- Please explain steps to reproduce -->
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

---
+++ packages/renderer/src/lib/ContainerListCompose.spec.ts
@@ -0,0 +1,136 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import '@testing-library/jest-dom';
+import { beforeAll, test, expect, vi } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/svelte';
+import ContainerList from './ContainerList.svelte';
+import { containersInfos } from '../stores/containers';
+import { get } from 'svelte/store';
+import { providerInfos } from '../stores/providers';
+
+const listContainersMock = vi.fn();
+const getProviderInfosMock = vi.fn();
+
+const listPodsMock = vi.fn();
+
+const kubernetesListPodsMock = vi.fn();
+
+const deleteContainersByLabelMock = vi.fn();
+
+// fake the window.events object
+beforeAll(() => {
+  const onDidUpdateProviderStatusMock = vi.fn();
+  (window as any).onDidUpdateProviderStatus = onDidUpdateProviderStatusMock;
+  onDidUpdateProviderStatusMock.mockImplementation(() => Promise.resolve());
+  listPodsMock.mockImplementation(() => Promise.resolve([]));
+  kubernetesListPodsMock.mockImplementation(() => Promise.resolve([]));
+  (window as any).listContainers = listContainersMock;
+  (window as any).listPods = listPodsMock;
+  (window as any).kubernetesListPods = kubernetesListPodsMock;
+  (window as any).getProviderInfos = getProviderInfosMock;
+  (window as any).deleteContainersByLabel = deleteContainersByLabelMock;
+
+  (window.events as unknown) = {
+    receive: (_channel: string, func: any) => {
+      func();
+    },
+  };
+});
+
+async function waitRender(customProperties: object): Promise<void> {
+  const result = render(ContainerList, { ...customProperties });
+  // wait that result.component.$$.ctx[2] is set
+  while (result.component.$$.ctx[2] === undefined) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+}
+
+test('Expect no container engines being displayed', async () => {
+  render(ContainerList);
+  const noEngine = screen.getByRole('heading', { name: 'No Container Engine' });
+  expect(noEngine).toBeInTheDocument();
+});
+
+test('Delete a group of compose containers succesfully', async () => {
+  getProviderInfosMock.mockResolvedValue([
+    {
+      name: 'podman',
+      status: 'started',
+      internalId: 'podman-internal-id',
+      containerConnections: [
+        {
+          name: 'podman-machine-default',
+          status: 'started',
+        },
+      ],
+    },
+  ]);
+
+  const groupName = 'compose-group';
+  const mockedContainers = [
+    {
+      Id: 'container1',
+      Image: 'docker.io/kindest/node:foobar',
+      Labels: { 'com.docker.compose.project': groupName },
+      Names: ['container1'],
+      State: 'RUNNING',
+    },
+    {
+      Id: 'container2',
+      Image: 'docker.io/kindest/node:foobar',
+      Labels: { 'com.docker.compose.project': groupName },
+      Names: ['container2'],
+      State: 'RUNNING',
+    },
+  ];
+  listContainersMock.mockResolvedValue(mockedContainers);
+
+  // Send over custom events to simulate PD being started
+  window.dispatchEvent(new CustomEvent('extensions-already-started'));
+  window.dispatchEvent(new CustomEvent('provider-lifecycle-change'));
+  window.dispatchEvent(new CustomEvent('tray:update-provider'));
+
+  // Wait for the store to get populated
+  while (get(containersInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+
+  while (get(providerInfos).length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+  await waitRender({});
+
+  // Find the 'Delete Compose' button for the compose group
+  const deleteButton = screen.getByRole('button', { name: 'Delete Compose' });
+  expect(deleteButton).toBeInTheDocument();
+
+  // Click on it
+  await fireEvent.click(deleteButton);
+
+  // wait deleteContainerMock is called
+  while (deleteContainersByLabelMock.mock.calls.length === 0) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+
+  // Expect deleteContainerMock to be called / successfully clicked
+  expect(deleteContainersByLabelMock).toBeCalledWith(undefined, 'com.docker.compose.project', groupName);
+  expect(deleteContainersByLabelMock).toBeCalledTimes(1);
+});
