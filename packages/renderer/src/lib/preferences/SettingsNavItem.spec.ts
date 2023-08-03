c00ffbb4092faaac8820632c9b361cbce510c761
Tim deBoer <git@tdeboer.ca>
2023-08-03 15:10:29 -0400

    chore: settings nav item
    I tried the same thing for preferences navigation as I did for the main nav bar in PR #3404:
    a SettingsNavItem component makes PreferencesNavigation far simpler.
    
    Matching the PF styling was relatively straight-forward, but I have changed five things:
    - bg-color corrected to match our design: charcoal-700.
    - Ditto for selection bg-color: charcoal-300.
    - This makes the hover color indistinguishable, so I went with charcoal-500 for now.
    - PF sections (e.g. Extensions) are never selected when expanded. IMHO this is a defect and
      fixed here as per the screen cap.
    - PF has an abnormally large right margin on child elements, I reduced this so there is
      less wrapping (see OpenShift Local in the screen cap).
    
    PF has a nice fa-angle animation when you expand/collapse the section which I haven't tried
    to replicate, but in my highly-biased opinion that should not hold this PR up.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/preferences/SettingsNavItem.spec.ts
@@ -0,0 +1,99 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import '@testing-library/jest-dom';
+import { test, expect } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import SettingsNavItem from './SettingsNavItem.svelte';
+
+function renderIt(title: string, href: string, meta: any, section?: boolean, child?: boolean): void {
+  render(SettingsNavItem, { title: title, href: href, meta: meta, section: section, child: child });
+}
+
+test('Expect correct role and href', async () => {
+  const title = 'Resources';
+  const href = '/test';
+  renderIt(title, href, { url: href });
+
+  const element = screen.getByLabelText(title);
+  expect(element).toBeInTheDocument();
+  expect(element).toHaveAttribute('href', href);
+});
+
+test('Expect selection styling', async () => {
+  const title = 'Resources';
+  const href = '/test';
+  renderIt(title, href, { url: href });
+
+  const element = screen.getByLabelText(title);
+  expect(element).toBeInTheDocument();
+  expect(element.firstChild).toBeInTheDocument();
+  expect(element.firstChild).toHaveClass('border-purple-500');
+});
+
+test('Expect not to have selection styling', async () => {
+  const title = 'Resources';
+  renderIt(title, '/test', { url: '/elsewhere' });
+
+  const element = screen.getByLabelText(title);
+  expect(element).toBeInTheDocument();
+  expect(element.firstChild).toBeInTheDocument();
+  expect(element.firstChild).not.toHaveClass('border-purple-500');
+  expect(element.firstChild).toHaveClass('border-charcoal-600');
+});
+
+test('Expect child styling', async () => {
+  const title = 'Resources';
+  const href = '/test';
+  renderIt(title, href, { url: href }, false, true);
+
+  const element = screen.getByLabelText(title);
+  expect(element).toBeInTheDocument();
+  expect(element.firstChild).toBeInTheDocument();
+  expect(element.firstChild).toHaveClass('leading-none');
+});
+
+test('Expect section styling', async () => {
+  const title = 'Extensions';
+  const href = '/test';
+  renderIt(title, href, { url: href }, true, false);
+
+  const element = screen.getByLabelText(title);
+  expect(element).toBeInTheDocument();
+  expect(element.firstChild).toBeInTheDocument();
+  expect(element.firstChild.childNodes[2]).toBeInTheDocument();
+  expect(element.firstChild.childNodes[2]).toHaveClass('fas');
+});
+
+test('Expect sections expand', async () => {
+  const title = 'Extensions';
+  const href = '/test';
+  renderIt(title, href, { url: href }, true, false);
+
+  const element = screen.getByLabelText(title);
+  expect(element).toBeInTheDocument();
+  expect(element.firstChild).toBeInTheDocument();
+  expect(element.firstChild.childNodes[2]).toBeInTheDocument();
+  expect(element.firstChild.childNodes[2]).toHaveClass('fa-angle-right');
+
+  await fireEvent.click(element);
+
+  expect(element.firstChild.childNodes[2]).toHaveClass('fa-angle-down');
+});
