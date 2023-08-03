0e1cd5099f6dd015f2506bef28ebb3b637448956
Tim deBoer <git@tdeboer.ca>
2023-08-03 15:10:29 -0400

    chore: settings nav item
    I tried the same thing for preferences navigation as I did for the main nav bar in PR #3404:
    a SettingsNavItem component makes PreferencesNavigation far simpler.
    
    Matching the PF styling was relatively straight-forward, but I have changed five things:
    - bg-color corrected to match our design: charcoal-700.
    - Ditto for selection bg-color: charcoal-300.
    - This makes the hover color indistinguishable, so I went with charcoal-500 for now.
    - PF sections (e.g. Extensions) are never selected when expanded. IMHO this is a defect and
      fixed here as per the screen cap.
    - PF has an abnormally large right margin on child elements, I reduced this so there is
      less wrapping (see OpenShift Local in the screen cap).
    
    PF has a nice fa-angle animation when you expand/collapse the section which I haven't tried
    to replicate, but in my highly-biased opinion that should not hold this PR up.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/preferences/SettingsNavItem.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+import type { TinroRouteMeta } from 'tinro';
+
+export let title: string;
+export let href: string;
+export let meta: TinroRouteMeta;
+export let section = false;
+export let expanded = false;
+export let child = false;
+
+let selected: boolean;
+$: selected = meta.url === href;
+</script>
+
+<a class="no-underline" href="{href}" aria-label="{title}" on:click="{() => (expanded = !expanded)}">
+  <div
+    class="flex w-full pr-1 justify-between items-center cursor-pointer border-l-[4px] border-charcoal-600"
+    class:text-white="{selected}"
+    class:py-3="{!child}"
+    class:pl-3="{!child}"
+    class:py-2="{child}"
+    class:pl-4="{child}"
+    class:leading-none="{child}"
+    class:text-sm="{child}"
+    class:font-extralight="{child}"
+    class:font-semibold="{!child}"
+    class:bg-charcoal-300="{selected}"
+    class:border-purple-500="{selected}"
+    class:text-gray-400="{!selected}"
+    class:hover:text-gray-300="{!selected}"
+    class:hover:bg-charcoal-500="{!selected}"
+    class:hover:border-charcoal-500="{!selected}">
+    <span class="block group-hover:block" class:capitalize="{!child}">{title}</span>
+    {#if section}
+      <i class="fas {expanded ? 'fa-angle-down' : 'fa-angle-right'} text-lg px-2" aria-hidden="true"></i>
+    {/if}
+  </div>
+</a>
