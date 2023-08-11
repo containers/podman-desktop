37f4e0b0e615915b5167ead1b60c5eec9145e265
Jeff MAURY <jmaury@redhat.com>
2023-08-11 09:48:51 +0200

    fix: kube event error when switching context
    Fixes #3035
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- packages/main/src/plugin/kubernetes-client.ts
+++ packages/main/src/plugin/kubernetes-client.ts
@@ -220,7 +220,8 @@ export class KubernetesClient {
           '/api/v1/namespaces/' + ns + '/pods',
           {},
           () => this.apiSender.send('pod-event'),
-          (err: unknown) => console.error('Kube event error', err),
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          () => {},
         )
         .then(req => (this.kubeWatcher = req))
         .catch((err: unknown) => console.error('Kube event error', err));
