5274273bee44b93a59f01e0bfd93668c27952b68
Tim deBoer <git@tdeboer.ca>
2023-08-14 20:11:41 -0400

    chore: link component (#3533)
    We have several different styles of 'links' today:
    - blue ones with underline hover (e.g. deploy to kube 'restricted')
    - blue ones with light hover (e.g. deploy to kube 'Open OpenShift')
    - purples ones with dark hover (e.g. deploy to kube routes, dashboard)
    - purples ones with no hover (e.g. back link on detail, form pages)
    - white ones with hover underline hover (e.g. dialog cancel buttons)
    
    To make things more consistent, and obvious to the user what is
    clickable, we should reuse a Link component. This PR creates a simple
    component with an optional icon and either href or on:click support
    for simplicity & ease of migrating from the existing a/div/spans
    (which will be done via other PRs).
    
    I tried making the Link a simpler <Button type="link">, but buttons
    have padding and complexity that makes this more complex and hard to
    directly reuse in either direction. IMHO it makes sense to keep Link
    and Button independent but share styling; had a brief chat with
    Mairin that it may make sense to change link Buttons to purple for
    consistency.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/ui/Link.spec.ts
@@ -0,0 +1,77 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import '@testing-library/jest-dom';
+import { test, expect, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import Link from './Link.svelte';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
+
+test('Check link styling', async () => {
+  render(Link);
+
+  // check for one element of the styling
+  const link = screen.getByRole('link');
+  expect(link).toBeInTheDocument();
+  expect(link).toHaveClass('text-purple-400');
+  expect(link).toHaveClass('hover:underline');
+});
+
+test('Check icon styling', async () => {
+  render(Link, { icon: faRocket });
+
+  // check for the fa SVG child
+  const link = screen.getByRole('link');
+  expect(link).toBeInTheDocument();
+  expect(link.firstChild).toBeInTheDocument();
+  expect(link.firstChild.firstChild).toBeInTheDocument();
+  expect(link.firstChild.firstChild).toHaveClass('svelte-fa');
+});
+
+test('Check href action', async () => {
+  const urlMock = vi.fn();
+  (window as any).openExternal = urlMock;
+  render(Link, { href: 'test' });
+
+  // check href link
+  const link = screen.getByRole('link');
+  expect(link).toBeInTheDocument();
+  expect(urlMock).not.toHaveBeenCalled();
+
+  fireEvent.click(link);
+
+  expect(urlMock).toBeCalledTimes(1);
+});
+
+test('Check on:click action', async () => {
+  const comp = render(Link);
+
+  const clickMock = vi.fn();
+  comp.component.$on('click', clickMock);
+
+  // check on:click
+  const link = screen.getByRole('link');
+  expect(link).toBeInTheDocument();
+  expect(clickMock).not.toHaveBeenCalled();
+
+  fireEvent.click(link);
+
+  expect(clickMock).toBeCalledTimes(1);
+});
