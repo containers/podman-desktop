1ebe226618901eb4dc1f965966f675f7fe4d11fb
Charlie Drage <charlie@charliedrage.com>
2023-09-01 15:47:03 -0400

    feat: onboarding add link micromark for commands
    ### What does this PR do?
    
    * Adds the ability to define
      `:link[command]{command=podman.onboarding.checkPodmanRequirements}`
      within `package.json` onboarding section and have it show up as a link
      that's clickable to a command that's executed. This helps add the
      ability for commands such as an "install version quickpick" to be used
    * Added comments due to no comments in markdown section.
    * You can also still use
      `:link[command]{href=https://foobar.com}` similar to how button
      micromark does
    
    ### Screenshot/screencast of this PR
    
    <!-- Please include a screenshot or a screencast
    explaining what is doing this PR -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop/issues/3710
    
    ### How to test this PR?
    
    Add `:link[command]{command=podman.onboarding.checkPodmanRequirements}`
    or a different test command to a "section" part of onboarding in
    `package.json` of podman and click on the link. It'll run the command.
    
    <!-- Please explain steps to reproduce -->
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/renderer/src/lib/markdown/micromark-button-directive.ts
+++ packages/renderer/src/lib/markdown/micromark-button-directive.ts
@@ -22,47 +22,54 @@
 /**
  * Allow to generate a custom button markdown directive
  * syntax is the following:
+ * :button[Name of the button]{command=command.example title="tooltip text"}
+ * or
  * :button[Name of the button]{href=http://my-link title="tooltip text"}
  */
-
 /**
  * @this {import('micromark-util-types').CompileContext}
  * @type {import('micromark-extension-directive').Handle}
  */
 export function button(d: any) {
+  // Make sure it's not part of a text directive
   if (d.type !== 'textDirective') {
     return false;
   }
 
-  // create button html if it has command attribute
+  // Make sure {command=example} has been passed in
   if (d.attributes && 'command' in d.attributes) {
+    // Make this a button if it's a command
     this.tag(
       '<button class="px-4 py-[6px] rounded-[4px] text-white text-[13px] whitespace-nowrap bg-purple-600 hover:bg-purple-500 no-underline" data-command="' +
         this.encode(d.attributes.command) +
         '">',
     );
 
+    // Add the "progress" spinner
+    // TODO: Remove cloudfoundry to tailwind
     this.tag(
       '<i class="pf-c-button__progress" style="position: relative; margin-right:5px; display: none;"><span class="pf-c-spinner pf-m-sm" role="progressbar"><span class="pf-c-spinner__clipper">' +
         '</span><span class="pf-c-spinner__lead-ball"></span><span class="pf-c-spinner__tail-ball"></span></span></i>',
     );
 
+    // Add any labels and close the button
     this.raw(d.label || '');
     this.tag('</button>');
   } else {
-    // it is a link
+    // If href is passed in, make this an anchor tag but make it look like a button
     this.tag(
       '<a class="px-4 py-[6px] rounded-[4px] text-white text-[13px] whitespace-nowrap bg-purple-600 hover:bg-purple-500 no-underline"',
     );
 
+    // Href & title
     if (d.attributes && 'href' in d.attributes) {
       this.tag(' href="' + this.encode(d.attributes.href) + '"');
     }
-
     if (d.attributes && 'title' in d.attributes) {
       this.tag(' title="' + this.encode(d.attributes.title) + '"');
     }
 
+    // Add the closing tags + labels (if any)
     this.tag('>');
     this.raw(d.label || '');
     this.tag('</a>');
