aedf7578929859b0f89e1f9bbf4d06923c23ccab
Tim deBoer <git@tdeboer.ca>
2023-09-13 08:56:46 -0400

    fix: initial action state in details pages
    While testing issue #3529 I noticed that when you're in the pod or container
    details page, all actions change the state to STARTING regardless of what the
    action says the state should be changed to. If you were actually deleting the
    object this means there's often a little blip where it looks like it's trying
    to start before the state is corrected.
    
    Fixed, now matches the correct/identical code that's in the corresponding
    *List pages.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/container/ContainerDetails.svelte
+++ packages/renderer/src/lib/container/ContainerDetails.svelte
@@ -67,7 +67,7 @@ onMount(() => {
 function inProgressCallback(inProgress: boolean, state: string | undefined): void {
   container.actionInProgress = inProgress;
   if (state && inProgress) {
-    container.state = 'STARTING';
+    container.state = state;
   }
 }
 
