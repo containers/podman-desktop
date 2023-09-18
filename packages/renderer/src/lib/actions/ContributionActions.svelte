6fe1fabfe917e73387241a3b4a785d2bb235f503
axel7083 <42176370+axel7083@users.noreply.github.com>
2023-09-18 14:55:40 +0200

    test: adding test for ContributionActions Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/actions/ContributionActions.svelte
+++ packages/renderer/src/lib/actions/ContributionActions.svelte
@@ -3,6 +3,38 @@ import type { Menu } from '../../../../main/src/plugin/menu-registry';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import ListItemButtonIcon from '../ui/ListItemButtonIcon.svelte';
 
+function isSerializable(value: any): boolean {
+  switch (typeof value) {
+    case 'string':
+    case 'number':
+    case 'boolean':
+    case 'object':
+      return true;
+    default:
+      return false;
+  }
+}
+
+function removeNonSerializableProperties<T>(obj: T): T {
+  if (typeof obj !== 'object' || obj === undefined) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => removeNonSerializableProperties(item)) as any;
+  }
+
+  const result: Partial<T> = {};
+
+  for (const key in obj) {
+    if (isSerializable(obj[key])) {
+      result[key] = removeNonSerializableProperties(obj[key]);
+    }
+  }
+
+  return result as T;
+}
+
 export let args: unknown[];
 
 export let dropdownMenu = false;
@@ -12,7 +44,7 @@ export let onError: (errorMessage: string) => void;
 
 async function executeContribution(menu: Menu): Promise<void> {
   try {
-    await window.executeCommand(menu.command, ...args);
+    await window.executeCommand(menu.command, ...removeNonSerializableProperties(args));
   } catch (err) {
     onError(`Error while executing ${menu.title}: ${String(err)}`);
   }
