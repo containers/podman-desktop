f6ade40d12615461f642376a56a702b524b5657d
axel7083 <42176370+axel7083@users.noreply.github.com>
2023-09-18 13:35:50 +0200

    revert: restoring [] Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/ContainerList.svelte
+++ packages/renderer/src/lib/ContainerList.svelte
@@ -540,7 +540,7 @@ function errorCallback(container: ContainerInfoUI, errorMessage: string): void {
                       name: containerGroup.name,
                       engineId: containerGroup.engineId,
                       engineType: containerGroup.engineType,
-                      containers: containerGroup.containers,
+                      containers: [],
                     }}"
                     dropdownMenu="{true}"
                     inProgressCallback="{(containers, flag, state) =>
