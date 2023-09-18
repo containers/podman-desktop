079ba468ccef762181dcb2531ab427f5573aaab0
axel7083 <42176370+axel7083@users.noreply.github.com>
2023-09-18 14:55:40 +0200

    test: adding test for ContributionActions Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/actions/ContributionActions.svelte
+++ packages/renderer/src/lib/actions/ContributionActions.svelte
@@ -4,6 +4,38 @@ import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import ListItemButtonIcon from '../ui/ListItemButtonIcon.svelte';
 import { removeNonSerializableProperties } from '/@/lib/actions/ActionUtils';
 
+function isSerializable(value: any): boolean {
+  switch (typeof value) {
+    case 'string':
+    case 'number':
+    case 'boolean':
+    case 'object':
+      return true;
+    default:
+      return false;
+  }
+}
+
+function removeNonSerializableProperties<T>(obj: T): T {
+  if (typeof obj !== 'object' || obj === undefined) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => removeNonSerializableProperties(item)) as any;
+  }
+
+  const result: Partial<T> = {};
+
+  for (const key in obj) {
+    if (isSerializable(obj[key])) {
+      result[key] = removeNonSerializableProperties(obj[key]);
+    }
+  }
+
+  return result as T;
+}
+
 export let args: unknown[];
 
 export let dropdownMenu = false;
