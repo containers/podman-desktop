8f8d9b1cab146730573ba4317ead748f7840103f
Fabrice Flore-Thébault <ffloreth@redhat.com>
2023-09-18 09:35:08 +0200

    Update .electron-builder.config.cjs
    Signed-off-by: Fabrice Flore-Thébault <ffloreth@redhat.com>

--- .electron-builder.config.cjs
+++ .electron-builder.config.cjs
@@ -132,7 +132,7 @@ const config = {
     ],
   },
   linux: {
-    category: 'Development',
+    category: ['Development', 'Utility'],
     icon: './buildResources/icon-512x512.png',
     target: ['flatpak', 'tar.gz'],
   },
