2ad5590a3eb77d5415ad238d068e45c3c111acc0
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2023-09-20 11:33:35 +0200

    fix: context update should trigger check on step completion (#3972) (#3973)
    * fix: context update should trigger check on step completion (#3972)
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * chore: move code to external ts and add tests
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    ---------
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- packages/renderer/src/lib/onboarding/onboarding-utils.ts
+++ packages/renderer/src/lib/onboarding/onboarding-utils.ts
@@ -15,6 +15,11 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
+
+import type { OnboardingInfo, OnboardingStep, OnboardingStatus } from '../../../../main/src/plugin/api/onboarding';
+import type { ContextUI } from '../context/context';
+import { ContextKeyExpr } from '../context/contextKey';
+
 export const SCOPE_ONBOARDING = 'onboarding';
 
 export const STATUS_COMPLETED = 'completed';
@@ -22,3 +27,116 @@ export const STATUS_SKIPPED = 'skipped';
 
 export const ON_COMMAND_PREFIX = 'onCommand:';
 export const ONBOARDING_CONTEXT_PREFIX = 'onboardingContext:';
+
+export interface ActiveOnboardingStep {
+  onboarding: OnboardingInfo;
+  step: OnboardingStep;
+}
+
+/*
+ *   it update the step status and, if it is the last step with a completed state, the onboarding status in the backend.
+ */
+export async function updateOnboardingStepStatus(
+  onboarding: OnboardingInfo,
+  step: OnboardingStep,
+  status: OnboardingStatus,
+) {
+  step.status = status;
+  await window.updateStepState(status, onboarding.extension, step.id);
+  // if the completed step is the last one, we mark the onboarding as completed
+  // the last step should have a completed state by default
+  const lastCompletedStep = onboarding.steps.findLast(s => s.state === 'completed');
+  if (lastCompletedStep?.id === step.id) {
+    onboarding.status = STATUS_COMPLETED;
+    await window.updateStepState(STATUS_COMPLETED, onboarding.extension);
+  }
+}
+
+/**
+ * it verifies if a step must be marked as completed by checking that the step does not depend on any completion event or, if any, that that they have
+ * been satisfied.
+ */
+export function isStepCompleted(
+  activeStep: ActiveOnboardingStep,
+  executedCommands: string[],
+  globalContext?: ContextUI,
+): boolean {
+  return (
+    !activeStep.step.completionEvents ||
+    activeStep.step.completionEvents.length === 0 ||
+    activeStep.step.completionEvents.every(cmp => {
+      // check if command has been executed
+      if (cmp.startsWith(ON_COMMAND_PREFIX) && executedCommands.includes(cmp.replace(ON_COMMAND_PREFIX, ''))) {
+        return true;
+      }
+
+      // check if cmp string is an onContext event, check the value from context
+      if (cmp.startsWith(ONBOARDING_CONTEXT_PREFIX)) {
+        if (!globalContext) {
+          return false;
+        }
+        cmp = cmp.replace(ONBOARDING_CONTEXT_PREFIX, `${activeStep.onboarding.extension}.${SCOPE_ONBOARDING}.`);
+        const completionEventDeserialized = ContextKeyExpr.deserialize(cmp);
+        return completionEventDeserialized?.evaluate(globalContext);
+      }
+
+      return false;
+    })
+  );
+}
+
+/*
+ *   it checks if all onboardings i nthe list have been completed
+ */
+export function isOnboardingsSetupCompleted(onboardings: OnboardingInfo[]): boolean {
+  for (const onboarding of onboardings) {
+    if (!isOnboardingCompleted(onboarding)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ *   it checks if the setup of the onboarding is complete
+ */
+export function isOnboardingCompleted(onboarding: OnboardingInfo): boolean {
+  if (!onboarding.status) {
+    return false;
+  }
+  for (const step of onboarding.steps) {
+    if (!step.status) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ *   it normalizes the values containing the prefix "onboardingContext:". It is replaces with <extension>.onboarding
+ */
+export function normalizeOnboardingWhenClause(when: string, extension: string): string {
+  return when.replaceAll(ONBOARDING_CONTEXT_PREFIX, `${extension}.${SCOPE_ONBOARDING}.`);
+}
+
+/*
+ *   it clean the context of all onboardings in the list
+ */
+export async function cleanSetup(onboardings: OnboardingInfo[], globalContext: ContextUI) {
+  // reset onboarding on backend
+  await window.resetOnboarding(onboardings.map(onboarding => onboarding.extension));
+  // clean ui context
+  const contextValues = globalContext.collectAllValues();
+  onboardings.forEach(onboarding => {
+    // remove context key added during the onboarding
+    for (const key in contextValues) {
+      if (key.startsWith(`${onboarding.extension}.${SCOPE_ONBOARDING}`)) {
+        globalContext.removeValue(key);
+      }
+    }
+    onboarding.status = undefined;
+    onboarding.steps.forEach(step => {
+      step.status = undefined;
+    });
+  });
+}
