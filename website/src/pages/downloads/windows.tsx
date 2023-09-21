6ee8d1075dae6545cb184bdc1458f94f5c13472c
Fabrice Flore-Thébault <ffloreth@redhat.com>
2023-09-21 11:36:02 +0200

    docs: avoid redirects in website
    Signed-off-by: Fabrice Flore-Thébault <ffloreth@redhat.com>

--- website/src/pages/downloads/windows.tsx
+++ website/src/pages/downloads/windows.tsx
@@ -183,7 +183,7 @@ export function WindowsDownloads(): JSX.Element {
 
             <Link
               className="underline inline-flex dark:text-white text-purple-500 hover:text-purple-200 py-2 px-6 font-semibold text-md"
-              to="/docs/Installation/windows-install">
+              to="/docs/installation/windows-install">
               <FontAwesomeIcon size="1x" icon={faWindows} className="mr-2" />
               Package Managers Guide
             </Link>
