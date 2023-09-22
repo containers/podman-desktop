1eb73f28294e21a0e61a90eedc0ef66f924a869d
axel7083 <42176370+axel7083@users.noreply.github.com>
2023-09-22 12:07:01 +0200

    fix: bad rebasing Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/ContainerList.svelte
+++ packages/renderer/src/lib/ContainerList.svelte
@@ -541,7 +541,7 @@ function errorCallback(container: ContainerInfoUI, errorMessage: string): void {
                       name: containerGroup.name,
                       engineId: containerGroup.engineId,
                       engineType: containerGroup.engineType,
-                      containers: [],
+                      containers: containerGroup.containers,
                     }}"
                     dropdownMenu="{true}"
                     inProgressCallback="{(containers, flag, state) =>
