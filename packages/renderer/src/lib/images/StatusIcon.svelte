b4c67356f8c296999071e048dabedc13b692f518
Tim deBoer <git@tdeboer.ca>
2023-09-25 12:16:30 -0400

    chore: deleting status icon
    Currently there is no indication in the status icon when something is being deleted.
    We're adding text to the age column (#4056), but the design preference is that this
    is re-enforced by the status icon changing into a spinner.
    
    This adds the DELETING status, using a correctly sized Spinner component with no
    border. Added a test, and some missing tests for other states.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/images/StatusIcon.svelte
+++ packages/renderer/src/lib/images/StatusIcon.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 import StarIcon from './StarIcon.svelte';
+import Spinner from '../ui/Spinner.svelte';
 
-// status: one of RUNNING, STARTING, USED, CREATED, or DEGRADED
+// status: one of RUNNING, STARTING, USED, CREATED, DELETING, or DEGRADED
 // any other status will result in a standard outlined box
 export let status = '';
 export let icon: any = undefined;
@@ -15,12 +16,14 @@ $: solid = status === 'RUNNING' || status === 'STARTING' || status === 'USED' ||
     class:bg-green-400="{status === 'RUNNING' || status === 'USED'}"
     class:bg-green-600="{status === 'STARTING'}"
     class:bg-amber-600="{status === 'DEGRADED'}"
-    class:border-2="{!solid}"
+    class:border-2="{!solid && status !== 'DELETING'}"
     class:border-gray-700="{!solid}"
     class:text-gray-700="{!solid}"
     role="status"
     title="{status}">
-    {#if typeof icon === 'string'}
+    {#if status === 'DELETING'}
+      <Spinner size="1.4em" />
+    {:else if typeof icon === 'string'}
       <span class="{icon}" aria-hidden="true"></span>
     {:else}
       <svelte:component this="{icon}" size="20" solid="{solid}" />
