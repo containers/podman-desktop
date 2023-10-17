da1a3df2a0753503d5ca7d1f7d2b9a81f6199c4f
Philippe Martin <phmartin@redhat.com>
2023-10-17 13:40:35 +0200

    Revert "fix: define a getParameters instead"
    This reverts commit 64a4028110930b6c98a0909e710a0e1681b366da.
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/routes.ts
+++ packages/renderer/src/routes.ts
@@ -1,23 +1,21 @@
-interface parameters {
-  kind: string;
-  name: string;
-  engineId: string;
-}
-
 export class PodDetailsRoute {
   static path(): string {
     return '/pods/:kind/:name/:engineId/*';
   }
 
-  static getRoute({ kind, name, engineId }: parameters, other?: string): string {
+  static getRoute({ kind, name, engineId }: { kind: string; name: string; engineId: string }, other?: string): string {
     return `/pods/${encodeURI(kind)}/${encodeURI(name)}/${encodeURIComponent(engineId)}${other}`;
   }
 
-  static getParameters(params: Record<string, string>): parameters {
-    return {
-      kind: decodeURI(params.kind),
-      name: decodeURI(params.name),
-      engineId: decodeURIComponent(params.engineId),
-    };
+  static getKind(params: Record<string, string>): string {
+    return decodeURI(params.kind);
+  }
+
+  static getName(params: Record<string, string>): string {
+    return decodeURI(params.name);
+  }
+
+  static getEngineId(params: Record<string, string>): string {
+    return decodeURIComponent(params.engineId);
   }
 }
