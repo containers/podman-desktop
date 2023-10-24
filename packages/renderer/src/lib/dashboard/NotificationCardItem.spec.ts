f6044a09066efc869b352272f29ab931397d5baa
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2023-10-24 02:29:51 +0200

    feat: add notifications box in the dashboard (#4125) (#4348)
    * feat: add notification registry (#4125)
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * feat: extend api so extensions can push notification
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * fix: use existing showNotification function
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * fix: use only notificationOptions interface
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * feat: add notifications box in the dashboard (#4125)
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * fix: clean after rebase
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    * Update notification body
    
    Co-authored-by: Charlie Drage <charlie@charliedrage.com>
    Signed-off-by: Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
    
    * fix: add comments and update title
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    
    ---------
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>
    Signed-off-by: Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
    Co-authored-by: Charlie Drage <charlie@charliedrage.com>

---
+++ packages/renderer/src/lib/dashboard/NotificationCardItem.spec.ts
@@ -0,0 +1,55 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+import '@testing-library/jest-dom/vitest';
+import { test, expect, vi, beforeAll } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import type { NotificationCard } from '../../../../main/src/plugin/api/notification';
+import NotificationCardItem from './NotificationCardItem.svelte';
+
+const removeNotificationMock = vi.fn();
+
+beforeAll(() => {
+  (window as any).removeNotification = removeNotificationMock;
+});
+
+test('Expect notification card to show notification title, description and close button', async () => {
+  const notification: NotificationCard = {
+    id: 1,
+    extensionId: 'extension',
+    title: 'title',
+    body: 'description',
+    type: 'info',
+  };
+  render(NotificationCardItem, {
+    notification,
+  });
+
+  const titleDiv = screen.getByLabelText('Notification title');
+  const descriptionDiv = screen.getByLabelText('Notification description');
+  expect(titleDiv).toBeInTheDocument();
+  expect(descriptionDiv).toBeInTheDocument();
+  expect(titleDiv.textContent).toEqual('title');
+  expect(descriptionDiv.textContent).toContain('description');
+
+  const deleteButton = screen.getByRole('button', { name: 'Delete notification 1' });
+  expect(deleteButton).toBeInTheDocument();
+
+  await fireEvent.click(deleteButton);
+
+  expect(removeNotificationMock).toBeCalled();
+});
---
+++ packages/renderer/src/lib/dashboard/NotificationsBox.spec.ts
@@ -0,0 +1,114 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+import '@testing-library/jest-dom/vitest';
+import { test, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import { notificationQueue } from '/@/stores/notifications';
+import NotificationsBox from './NotificationsBox.svelte';
+import type { NotificationCard } from '../../../../main/src/plugin/api/notification';
+
+test('Expect notifications box to be hidden if there are no notifications in the queue', async () => {
+  notificationQueue.set([]);
+  render(NotificationsBox);
+
+  const noNotificationsDiv = screen.queryByLabelText('Notifications');
+  expect(noNotificationsDiv).not.toBeInTheDocument();
+});
+
+test('Expect to show only the first three elements from the notifications queue', async () => {
+  const notification1: NotificationCard = {
+    id: 1,
+    extensionId: 'extension',
+    title: '1',
+    body: '1',
+    type: 'info',
+    highlight: true,
+  };
+  const notification2: NotificationCard = {
+    id: 2,
+    extensionId: 'extension',
+    title: '2',
+    body: '2',
+    type: 'info',
+    highlight: true,
+  };
+  const notification3: NotificationCard = {
+    id: 3,
+    extensionId: 'extension',
+    title: '3',
+    body: '3',
+    type: 'info',
+    highlight: true,
+  };
+  const notification4: NotificationCard = {
+    id: 4,
+    extensionId: 'extension',
+    title: '4',
+    body: '4',
+    type: 'info',
+    highlight: true,
+  };
+  notificationQueue.set([notification1, notification2, notification3, notification4]);
+  render(NotificationsBox);
+
+  const titleDivs = screen.getAllByLabelText('Notification title');
+  expect(titleDivs.length).toBe(3);
+  expect(titleDivs[0].textContent).toEqual('1');
+  expect(titleDivs[1].textContent).toEqual('2');
+  expect(titleDivs[2].textContent).toEqual('3');
+});
+
+test('Expect to show only the notification highlighted', async () => {
+  const notification1: NotificationCard = {
+    id: 1,
+    extensionId: 'extension',
+    title: '1',
+    body: '1',
+    type: 'info',
+  };
+  const notification2: NotificationCard = {
+    id: 2,
+    extensionId: 'extension',
+    title: '2',
+    body: '2',
+    type: 'info',
+    highlight: true,
+  };
+  const notification3: NotificationCard = {
+    id: 3,
+    extensionId: 'extension',
+    title: '3',
+    body: '3',
+    type: 'info',
+  };
+  const notification4: NotificationCard = {
+    id: 4,
+    extensionId: 'extension',
+    title: '4',
+    body: '4',
+    type: 'info',
+    highlight: true,
+  };
+  notificationQueue.set([notification1, notification2, notification3, notification4]);
+  render(NotificationsBox);
+
+  const titleDivs = screen.getAllByLabelText('Notification title');
+  expect(titleDivs.length).toBe(2);
+  expect(titleDivs[0].textContent).toEqual('2');
+  expect(titleDivs[1].textContent).toEqual('4');
+});
