7942fc5bb95257b24c15703256a855cad673e907
axel7083 <42176370+axel7083@users.noreply.github.com>
2023-10-30 14:20:31 +0100

    feat: adding optional abort controller to dockerode api (#4364)
    * feat: adding optional abort controller to (some) functions of container-registry.ts
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/main/src/plugin/container-registry.ts
+++ packages/main/src/plugin/container-registry.ts
@@ -289,7 +289,7 @@ export class ContainerProviderRegistry {
   }
 
   // do not use inspect information
-  async listSimpleContainers(): Promise<SimpleContainerInfo[]> {
+  async listSimpleContainers(abortController?: AbortController): Promise<SimpleContainerInfo[]> {
     let telemetryOptions = {};
     const containers = await Promise.all(
       Array.from(this.internalProviders.values()).map(async provider => {
@@ -299,7 +299,7 @@ export class ContainerProviderRegistry {
             return [];
           }
 
-          const containers = await providerApi.listContainers({ all: true });
+          const containers = await providerApi.listContainers({ all: true, abortSignal: abortController?.signal });
           return Promise.all(
             containers.map(async container => {
               const containerInfo: SimpleContainerInfo = {
@@ -328,9 +328,13 @@ export class ContainerProviderRegistry {
   }
 
   // listSimpleContainers by matching label and key
-  async listSimpleContainersByLabel(label: string, key: string): Promise<SimpleContainerInfo[]> {
+  async listSimpleContainersByLabel(
+    label: string,
+    key: string,
+    abortController?: AbortController,
+  ): Promise<SimpleContainerInfo[]> {
     // Get all the containers using listSimpleContainers
-    const containers = await this.listSimpleContainers();
+    const containers = await this.listSimpleContainers(abortController);
 
     // Find all the containers that match the label + key
     return containers.filter(container => {
@@ -817,10 +821,10 @@ export class ContainerProviderRegistry {
     return this.getMatchingEngine(engineId).getImage(imageId);
   }
 
-  async stopContainer(engineId: string, id: string): Promise<void> {
+  async stopContainer(engineId: string, id: string, abortController?: AbortController): Promise<void> {
     let telemetryOptions = {};
     try {
-      return this.getMatchingContainer(engineId, id).stop();
+      return this.getMatchingContainer(engineId, id).stop({ abortSignal: abortController?.signal });
     } catch (error) {
       telemetryOptions = { error: error };
       throw error;
@@ -873,13 +877,17 @@ export class ContainerProviderRegistry {
     imageTag: string,
     callback: (name: string, data: string) => void,
     authInfo?: containerDesktopAPI.ContainerAuthInfo,
+    abortController?: AbortController,
   ): Promise<void> {
     let telemetryOptions = {};
     try {
       const engine = this.getMatchingEngine(engineId);
       const image = engine.getImage(imageTag);
       const authconfig = authInfo || this.imageRegistry.getAuthconfigForImage(imageTag);
-      const pushStream = await image.push({ authconfig });
+      const pushStream = await image.push({
+        authconfig,
+        abortSignal: abortController?.signal,
+      });
       pushStream.on('end', () => {
         callback('end', '');
       });
@@ -907,6 +915,7 @@ export class ContainerProviderRegistry {
     providerContainerConnectionInfo: ProviderContainerConnectionInfo | containerDesktopAPI.ContainerProviderConnection,
     imageName: string,
     callback: (event: PullEvent) => void,
+    abortController?: AbortController,
   ): Promise<void> {
     let telemetryOptions = {};
     try {
@@ -914,6 +923,7 @@ export class ContainerProviderRegistry {
       const matchingEngine = this.getMatchingEngineFromConnection(providerContainerConnectionInfo);
       const pullStream = await matchingEngine.pull(imageName, {
         authconfig,
+        abortSignal: abortController?.signal,
       });
       // eslint-disable-next-line @typescript-eslint/ban-types
       let resolve: () => void;
@@ -988,11 +998,11 @@ export class ContainerProviderRegistry {
     }
   }
 
-  async deleteContainer(engineId: string, id: string): Promise<void> {
+  async deleteContainer(engineId: string, id: string, abortController?: AbortController): Promise<void> {
     let telemetryOptions = {};
     try {
       // use force to delete it even it is running
-      return this.getMatchingContainer(engineId, id).remove({ force: true });
+      return this.getMatchingContainer(engineId, id).remove({ force: true, abortSignal: abortController?.signal });
     } catch (error) {
       telemetryOptions = { error: error };
       throw error;
@@ -1001,7 +1011,7 @@ export class ContainerProviderRegistry {
     }
   }
 
-  async startContainer(engineId: string, id: string): Promise<void> {
+  async startContainer(engineId: string, id: string, abortController?: AbortController): Promise<void> {
     let telemetryOptions = {};
     try {
       const engine = this.internalProviders.get(engineId);
@@ -1010,8 +1020,7 @@ export class ContainerProviderRegistry {
       if (engine) {
         await this.attachToContainer(engine, container);
       }
-
-      return container.start();
+      return container.start({ abortSignal: abortController?.signal } as unknown as Dockerode.ContainerStartOptions);
     } catch (error) {
       telemetryOptions = { error: error };
       throw error;
@@ -1183,7 +1192,7 @@ export class ContainerProviderRegistry {
     }
   }
 
-  async restartContainer(engineId: string, id: string): Promise<void> {
+  async restartContainer(engineId: string, id: string, abortController?: AbortController): Promise<void> {
     let telemetryOptions = {};
     try {
       const engine = this.internalProviders.get(engineId);
@@ -1193,7 +1202,7 @@ export class ContainerProviderRegistry {
         await this.attachToContainer(engine, container);
       }
 
-      return container.restart();
+      return container.restart({ abortSignal: abortController?.signal });
     } catch (error) {
       telemetryOptions = { error: error };
       throw error;
@@ -1205,11 +1214,16 @@ export class ContainerProviderRegistry {
   // Find all containers that match the against the given project name to
   // the label com.docker.compose.project (for example)
   // and then restart all the containers that have the following label AND key
-  async restartContainersByLabel(engineId: string, label: string, key: string): Promise<void> {
+  async restartContainersByLabel(
+    engineId: string,
+    label: string,
+    key: string,
+    abortController?: AbortController,
+  ): Promise<void> {
     let telemetryOptions = {};
     try {
       // Get all the containers using listSimpleContainers
-      const containers = await this.listSimpleContainers();
+      const containers = await this.listSimpleContainers(abortController);
 
       // Find all the containers that are using projectLabel and match the projectName
       const containersMatchingProject = containers.filter(container => {
@@ -1256,7 +1270,12 @@ export class ContainerProviderRegistry {
   }
 
   // Stop containers that match a label + key
-  async stopContainersByLabel(engineId: string, label: string, key: string): Promise<void> {
+  async stopContainersByLabel(
+    engineId: string,
+    label: string,
+    key: string,
+    abortController?: AbortController,
+  ): Promise<void> {
     let telemetryOptions = {};
     try {
       // Get all the containers using listSimpleContainers
@@ -1271,7 +1290,7 @@ export class ContainerProviderRegistry {
       const containerIds = containersMatchingProject.map(container => container.Id);
 
       // Stop all the containers
-      await Promise.all(containerIds.map(containerId => this.stopContainer(engineId, containerId)));
+      await Promise.all(containerIds.map(containerId => this.stopContainer(engineId, containerId, abortController)));
     } catch (error) {
       telemetryOptions = { error: error };
       throw error;
@@ -1281,7 +1300,12 @@ export class ContainerProviderRegistry {
   }
 
   // Delete all containers that match a certain label and key
-  async deleteContainersByLabel(engineId: string, label: string, key: string): Promise<void> {
+  async deleteContainersByLabel(
+    engineId: string,
+    label: string,
+    key: string,
+    abortController?: AbortController,
+  ): Promise<void> {
     let telemetryOptions = {};
     try {
       // Get all the containers using listSimpleContainers
@@ -1294,7 +1318,7 @@ export class ContainerProviderRegistry {
       const containerIds = containersMatchingProject.map(container => container.Id);
 
       // Delete all the containers in containerIds
-      await Promise.all(containerIds.map(containerId => this.deleteContainer(engineId, containerId)));
+      await Promise.all(containerIds.map(containerId => this.deleteContainer(engineId, containerId, abortController)));
     } catch (error) {
       telemetryOptions = { error: error };
       throw error;
@@ -1303,7 +1327,12 @@ export class ContainerProviderRegistry {
     }
   }
 
-  async logsContainer(engineId: string, id: string, callback: (name: string, data: string) => void): Promise<void> {
+  async logsContainer(
+    engineId: string,
+    id: string,
+    callback: (name: string, data: string) => void,
+    abortController?: AbortController,
+  ): Promise<void> {
     let telemetryOptions = {};
     let firstMessage = true;
     const container = this.getMatchingContainer(engineId, id);
@@ -1312,6 +1341,7 @@ export class ContainerProviderRegistry {
         follow: true,
         stdout: true,
         stderr: true,
+        abortSignal: abortController?.signal,
       })
       .then(containerStream => {
         containerStream.on('end', () => {
@@ -1338,6 +1368,7 @@ export class ContainerProviderRegistry {
     command: string[],
     onStdout: (data: Buffer) => void,
     onStderr: (data: Buffer) => void,
+    abortController?: AbortController,
   ): Promise<void> {
     const container = this.getMatchingContainer(engineId, id);
 
@@ -1349,7 +1380,7 @@ export class ContainerProviderRegistry {
       Tty: false,
     });
 
-    const execStream = await exec.start({ hijack: true, stdin: false });
+    const execStream = await exec.start({ hijack: true, stdin: false, abortSignal: abortController?.signal });
 
     const wrappedAsStream = (redirect: (data: Buffer) => void): Writable => {
       return new Writable({
@@ -1868,6 +1899,7 @@ export class ContainerProviderRegistry {
     imageName: string,
     selectedProvider: ProviderContainerConnectionInfo,
     eventCollect: (eventName: 'stream' | 'error' | 'finish', data: string) => void,
+    abortController?: AbortController,
   ): Promise<unknown> {
     let telemetryOptions = {};
     try {
@@ -1899,6 +1931,7 @@ export class ContainerProviderRegistry {
           registryconfig,
           dockerfile: relativeContainerfilePath,
           t: imageName,
+          abortSignal: abortController?.signal,
         })) as unknown as Stream;
       } catch (error: unknown) {
         console.log('error in buildImage', error);
