0d8ebef08bc41dfc1ee1b30859c15ef86bdca3f7
Denis Golovin <dgolovin@redhat.com>
2023-11-06 21:09:53 -0800

    feat: add extension/kubctl-cli to register kubectl CLI Tool
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

---
+++ extensions/kubectl-cli/scripts/build.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+const zipper = require('zip-local');
+const path = require('path');
+const package = require('../package.json');
+const { mkdirp } = require('mkdirp');
+const fs = require('fs');
+const byline = require('byline');
+const cp = require('copyfiles');
+
+const destFile = path.resolve(__dirname, `../${package.name}.cdix`);
+const builtinDirectory = path.resolve(__dirname, '../builtin');
+const zipDirectory = path.resolve(builtinDirectory, `${package.name}.cdix`);
+const extFiles = path.resolve(__dirname, '../.extfiles');
+const fileStream = fs.createReadStream(extFiles, { encoding: 'utf8' });
+
+const includedFiles = [];
+
+// remove the .cdix file before zipping
+if (fs.existsSync(destFile)) {
+  fs.rmSync(destFile);
+}
+// remove the builtin folder before zipping
+if (fs.existsSync(builtinDirectory)) {
+  fs.rmSync(builtinDirectory, { recursive: true, force: true });
+}
+
+byline(fileStream)
+  .on('data', line => {
+    includedFiles.push(line);
+  })
+  .on('error', () => {
+    throw new Error('Error reading .extfiles');
+  })
+  .on('end', () => {
+    includedFiles.push(zipDirectory);
+    mkdirp.sync(zipDirectory);
+    console.log(`Copying files to ${zipDirectory}`);
+    cp(includedFiles, error => {
+      if (error) {
+        throw new Error('Error copying files', error);
+      }
+      console.log(`Zipping files to ${destFile}`);
+      zipper.sync.zip(zipDirectory).compress().save(destFile);
+    });
+  });
---
+++ extensions/kubectl-cli/src/extension.ts
@@ -0,0 +1,33 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import { cli } from '@podman-desktop/api';
+
+export function activate() {
+  cli.createCliTool({
+    markdownDescription:
+      "A command line tool used to run commands against Kubernetes clusters. It does this by authenticating with the Master Node of your cluster and making API calls to do a variety of management actions. If you're just getting started with Kubernetes, prepare to be spending a lot of time with kubectl.",
+    name: 'kubectl',
+    displayName: 'kubectl',
+    images: {
+      icon: 'icon.png',
+    },
+    version: '',
+    path: '',
+  });
+}
