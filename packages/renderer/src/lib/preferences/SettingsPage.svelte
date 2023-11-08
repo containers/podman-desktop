187571e1c51d5683ce8bf27afda7201e261523d3
Ondrej Dockal <odockal@redhat.com>
2023-11-08 15:27:58 +0100

    chore(ui,tests): add ARIA functionality elements into SettingsPage
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- packages/renderer/src/lib/preferences/SettingsPage.svelte
+++ packages/renderer/src/lib/preferences/SettingsPage.svelte
@@ -2,8 +2,8 @@
 export let title: string;
 </script>
 
-<div class="flex flex-col min-w-full h-full">
-  <div class="min-w-full px-5 py-4">
+<div class="flex flex-col min-w-full h-full" role="region" aria-label="{title}">
+  <div class="min-w-full px-5 py-4" role="region" aria-label="header">
     <div class="flex flex-row">
       <div class="grow">
         <p class="capitalize text-xl">{title}</p>
@@ -14,7 +14,7 @@ export let title: string;
 
     <slot name="header" />
   </div>
-  <div class="flex flex-row min-w-full h-full px-5 py-4 overflow-y-auto">
+  <div class="flex flex-row min-w-full h-full px-5 py-4 overflow-y-auto" role="region" aria-label="content">
     <div class="flex flex-col grow max-w-[905px] mx-auto">
       <slot />
     </div>
