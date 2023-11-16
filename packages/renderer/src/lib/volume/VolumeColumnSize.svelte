2261150d065442e5d385ee6b50b6c742c4335230
Tim deBoer <git@tdeboer.ca>
2023-11-16 14:40:23 -0500

    feat: volumes using table component
    Switches the Volume page to the new table component. Columns are extracted
    as expected, and I've pulled out the engine into an Environment column to
    match the Pods page.
    
    I made all the columns sortable. The raw size had to be made visible from
    VolumeInfoUI because the regular column is humanized and not sortable.
    Status and size are 'reverse sorted' because you typically want to see
    used volumes and bigger things first.
    
    Based on the Table PR #4545. Ignore everything in /table/* and once that
    has merged I'll rebase.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/volume/VolumeColumnSize.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import type { VolumeInfoUI } from './VolumeInfoUI';
+
+export let object: VolumeInfoUI;
+</script>
+
+<div class="text-sm text-gray-700">
+  {object.humanSize}
+</div>
---
+++ packages/renderer/src/lib/volume/VolumeColumnStatus.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import StatusIcon from '../images/StatusIcon.svelte';
+import VolumeIcon from '../images/VolumeIcon.svelte';
+import type { VolumeInfoUI } from './VolumeInfoUI';
+
+export let object: VolumeInfoUI;
+</script>
+
+<StatusIcon icon="{VolumeIcon}" status="{object.inUse ? 'USED' : 'UNUSED'}" />
