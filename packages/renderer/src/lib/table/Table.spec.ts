15dbe75e034c1c105a642afce8278fd63dd15381
Tim deBoer <git@tdeboer.ca>
2023-11-16 11:00:34 -0500

    feat: table component
    This is a clean branch of the table component after previous review comments
    in PR #4545.
    
    Each page passes an array of objects and Columns to the Table component, which
    is responsible for creating the basic layout and calling back to the Column
    renderer to render each cell in the table. The Table uses grid layout and
    provides enough abstraction that we can implement additional features later.
    (e.g. optional columns, column reordering, or resizing. grouping. etc)
    
    The Table provides sorting & sort indicators, and tests are included.
    Support for 'object containers/grouping' in order to support Container list
    will be done separately since this is already a big PR.
    
    Fixes #4365.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/table/Table.spec.ts
@@ -0,0 +1,87 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+import { test, expect } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+
+import TestTable from './TestTable.svelte';
+
+test('Expect basic table layout', async () => {
+  // render the component
+  render(TestTable, {});
+
+  // 3 people = header + 3 rows
+  const rows = await screen.findAllByRole('row');
+  expect(rows).toBeDefined();
+  expect(rows.length).toBe(4);
+
+  // first data row should contain John and his age
+  expect(rows[1].textContent).toContain('John');
+  expect(rows[1].textContent).toContain('57');
+
+  // second data row should contain Henry and his age
+  expect(rows[2].textContent).toContain('Henry');
+  expect(rows[2].textContent).toContain('27');
+
+  // last data row should contain Charlie and his age
+  expect(rows[3].textContent).toContain('Charlie');
+  expect(rows[3].textContent).toContain('43');
+});
+
+test('Expect sorting by name works', async () => {
+  render(TestTable, {});
+
+  const nameCol = screen.getByText('Name');
+  expect(nameCol).toBeInTheDocument();
+
+  let rows = await screen.findAllByRole('row');
+  expect(rows).toBeDefined();
+  expect(rows.length).toBe(4);
+  expect(rows[1].textContent).toContain('John');
+  expect(rows[2].textContent).toContain('Henry');
+  expect(rows[3].textContent).toContain('Charlie');
+
+  await fireEvent.click(nameCol);
+
+  rows = await screen.findAllByRole('row');
+  expect(rows[1].textContent).toContain('Charlie');
+  expect(rows[2].textContent).toContain('Henry');
+  expect(rows[3].textContent).toContain('John');
+});
+
+test('Expect sorting by age works', async () => {
+  render(TestTable, {});
+
+  const ageCol = screen.getByText('Age');
+  expect(ageCol).toBeInTheDocument();
+
+  let rows = await screen.findAllByRole('row');
+  expect(rows).toBeDefined();
+  expect(rows.length).toBe(4);
+  expect(rows[1].textContent).toContain('John');
+  expect(rows[2].textContent).toContain('Henry');
+  expect(rows[3].textContent).toContain('Charlie');
+
+  await fireEvent.click(ageCol);
+
+  rows = await screen.findAllByRole('row');
+  expect(rows[1].textContent).toContain('Henry');
+  expect(rows[2].textContent).toContain('Charlie');
+  expect(rows[3].textContent).toContain('John');
+});
