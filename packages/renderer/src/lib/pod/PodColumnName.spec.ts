7df942f96c09ebe8e0a41364be8f31cb78444deb
Tim deBoer <git@tdeboer.ca>
2023-11-21 21:27:29 -0500

    feat: pods table component
    Switches the Pods page to the new table component. Columns and actions
    are extracted as expected. All the columns are sortable.
    
    Table component allows update events to be passed through so that pod
    actions (or other future table actions) can trigger the UI to refresh.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/pod/PodColumnName.spec.ts
@@ -0,0 +1,68 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+import { test, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+
+import PodColumnName from './PodColumnName.svelte';
+import type { PodInfoUI } from './PodInfoUI';
+import { router } from 'tinro';
+import { fireEvent } from '@testing-library/dom';
+
+const pod: PodInfoUI = {
+  id: 'pod-id',
+  shortId: 'short-id',
+  name: 'my-pod',
+  engineId: 'podman',
+  engineName: '',
+  status: '',
+  age: '',
+  created: '',
+  selected: false,
+  containers: [],
+  kind: 'podman',
+};
+
+test('Expect simple column styling', async () => {
+  render(PodColumnName, { object: pod });
+
+  const text = screen.getByText(pod.name);
+  expect(text).toBeInTheDocument();
+  expect(text).toHaveClass('text-sm');
+  expect(text).toHaveClass('text-gray-300');
+
+  const id = screen.getByText(pod.shortId);
+  expect(id).toBeInTheDocument();
+  expect(id).toHaveClass('text-xs');
+  expect(id).toHaveClass('text-violet-400');
+});
+
+test('Expect clicking works', async () => {
+  render(PodColumnName, { object: pod });
+
+  const text = screen.getByText(pod.name);
+  expect(text).toBeInTheDocument();
+
+  // test click
+  const routerGotoSpy = vi.spyOn(router, 'goto');
+
+  fireEvent.click(text);
+
+  expect(routerGotoSpy).toBeCalledWith('/pods/podman/my-pod/podman/logs');
+});
---
+++ packages/renderer/src/lib/pod/PodColumnName.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { router } from 'tinro';
+import type { PodInfoUI } from './PodInfoUI';
+
+export let object: PodInfoUI;
+
+function openDetailsPod(pod: PodInfoUI) {
+  router.goto(`/pods/${encodeURI(pod.kind)}/${encodeURI(pod.name)}/${encodeURIComponent(pod.engineId)}/logs`);
+}
+</script>
+
+<button class="hover:cursor-pointer flex flex-col" on:click="{() => openDetailsPod(object)}">
+  <div class="text-sm text-gray-300">{object.name}</div>
+  <div class="text-xs text-violet-400">{object.shortId}</div>
+</button>
