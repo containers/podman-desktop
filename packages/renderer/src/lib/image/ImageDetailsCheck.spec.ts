06bffb90c116df250933a0c5ba5b19cb6d2fb20d
Philippe Martin <phmartin@redhat.com>
2023-11-23 11:23:24 +0100

    fix: tests Signed-off-by: Philippe Martin <phmartin@redhat.com>
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/image/ImageDetailsCheck.spec.ts
+++ packages/renderer/src/lib/image/ImageDetailsCheck.spec.ts
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/vitest';
-import { beforeAll, expect, test, vi } from 'vitest';
+import { beforeAll, beforeEach, expect, test, vi } from 'vitest';
 import ImageDetailsCheck from './ImageDetailsCheck.svelte';
 import { fireEvent, render, screen } from '@testing-library/svelte';
 import type { ImageChecks } from '@podman-desktop/api';
@@ -11,12 +11,15 @@ const cancelTokenSpy = vi.fn();
 
 const tokenID = 70735;
 beforeAll(() => {
-  vi.clearAllMocks();
   (window as any).getCancellableTokenSource = getCancellableTokenSourceMock;
   getCancellableTokenSourceMock.mockImplementation(() => tokenID);
   (window as any).imageCheck = imageCheckMock;
   (window as any).cancelToken = cancelTokenSpy;
   (window as any).telemetryTrack = vi.fn();
+});
+
+beforeEach(() => {
+  vi.clearAllMocks();
 });
 
 test('expect to display wait message before to receive results', async () => {
@@ -98,6 +101,92 @@ test('expect to abort when clicking the Abort button', async () => {
   expect(cancelTokenSpy).toHaveBeenCalledWith(tokenID);
 });
 
+test('expect to abort when destroying the component', async () => {
+  imageCheckerProviders.set([
+    {
+      id: 'provider1',
+      label: 'Image Checker',
+    },
+  ]);
+
+  imageCheckMock.mockImplementation(async () => {
+    // never returns results
+    return new Promise(() => {});
+  });
+
+  const result = render(ImageDetailsCheck, {
+    image: {
+      id: '123456',
+      shortId: '123',
+      name: 'an-image',
+      engineId: 'podman',
+      engineName: 'Podman',
+      tag: 'a-tag',
+      createdAt: 123,
+      age: '1 day',
+      humanSize: '1Mb',
+      base64RepoTag: Buffer.from('<none>', 'binary').toString('base64'),
+      selected: false,
+      inUse: false,
+    },
+  });
+
+  await vi.waitFor(async () => {
+    screen.getByRole('button', { name: 'Abort' });
+  });
+
+  result.unmount();
+
+  expect(cancelTokenSpy).toHaveBeenCalledWith(tokenID);
+});
+
+test('expect to not abort again when destroying the component after abort', async () => {
+  imageCheckerProviders.set([
+    {
+      id: 'provider1',
+      label: 'Image Checker',
+    },
+  ]);
+
+  imageCheckMock.mockImplementation(async () => {
+    // never returns results
+    return new Promise(() => {});
+  });
+
+  const result = render(ImageDetailsCheck, {
+    image: {
+      id: '123456',
+      shortId: '123',
+      name: 'an-image',
+      engineId: 'podman',
+      engineName: 'Podman',
+      tag: 'a-tag',
+      createdAt: 123,
+      age: '1 day',
+      humanSize: '1Mb',
+      base64RepoTag: Buffer.from('<none>', 'binary').toString('base64'),
+      selected: false,
+      inUse: false,
+    },
+  });
+
+  await vi.waitFor(async () => {
+    const abortBtn = screen.getByRole('button', { name: 'Abort' });
+    await fireEvent.click(abortBtn);
+  });
+
+  vi.waitFor(() => {
+    const msg = screen.getByText(content => content.includes('Check aborted'));
+    expect(msg).toBeInTheDocument();
+  });
+
+  expect(cancelTokenSpy).toHaveBeenCalledWith(tokenID);
+
+  result.unmount();
+
+  expect(cancelTokenSpy).toHaveBeenCalledTimes(1);
+});
+
 test('expect to display results from image checker provider', async () => {
   imageCheckerProviders.set([
     {
@@ -138,4 +227,50 @@ test('expect to display results from image checker provider', async () => {
     const cell = screen.getByText('check1');
     expect(cell).toBeInTheDocument();
   });
+});
+
+test('expect to not abort when destroying te component after displaying results from image checker provider', async () => {
+  imageCheckerProviders.set([
+    {
+      id: 'provider1',
+      label: 'Image Checker',
+    },
+  ]);
+
+  imageCheckMock.mockResolvedValue({
+    checks: [
+      {
+        name: 'check1',
+        status: 'failed',
+        markdownDescription: 'an error for check1',
+        severity: 'critical',
+      },
+    ],
+  } as ImageChecks);
+
+  const result = render(ImageDetailsCheck, {
+    image: {
+      id: '123456',
+      shortId: '123',
+      name: 'an-image',
+      engineId: 'podman',
+      engineName: 'Podman',
+      tag: 'a-tag',
+      createdAt: 123,
+      age: '1 day',
+      humanSize: '1Mb',
+      base64RepoTag: Buffer.from('<none>', 'binary').toString('base64'),
+      selected: false,
+      inUse: false,
+    },
+  });
+
+  vi.waitFor(() => {
+    const cell = screen.getByText('check1');
+    expect(cell).toBeInTheDocument();
+  });
+
+  result.unmount();
+
+  expect(cancelTokenSpy).not.toHaveBeenCalled();
 });
