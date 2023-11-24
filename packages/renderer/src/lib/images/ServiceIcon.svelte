d0e4f414b064dc09fbbfea6e24f921f67b0fbf16
Tim deBoer <git@tdeboer.ca>
2023-11-24 09:07:39 -0500

    chore: service icon
    Adding icons for services, in both outline and solid form, as required by upcoming issue.
    
    Part of #4368.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/images/ServiceIcon.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+export let size = '1em';
+export let solid = false;
+
+let boxStyle = 'fill: currentColor;';
+if (!solid) {
+  boxStyle = 'stroke-miterlimit: 10; stroke-width: 1; stroke: currentColor; stroke-opacity: 1;';
+}
+</script>
+
+<svg
+  width="{size}"
+  height="{size}"
+  class="{$$props.class}"
+  style="{$$props.style}"
+  version="1.1"
+  xml:space="preserve"
+  viewBox="-0.5 -0.5 20.03 15">
+  <!-- four boxes -->
+  <g style="{boxStyle}">
+    <path d="M5.472,0.000L13.558,0.000L13.558,3.531L5.472,3.531Z"></path>
+    <path d="M0.000,10.469L5.103,10.469L5.103,14.000L0.000,14.000Z"></path>
+    <path d="M6.964,10.469L12.067,10.469L12.067,14.000L6.964,14.000Z"></path>
+    <path d="M13.927,10.469L19.030,10.469L19.030,14.000L13.927,14.000Z"></path>
+  </g>
+  <!-- lines -->
+  <g
+    style="fill: none; stroke-linejoin: round; stroke-miterlimit: 10; stroke-width: 0.7; stroke: currentColor; stroke-opacity: 1;">
+    <path d="M9.515,3.531L9.515,7.000L2.555,7.000L2.555,10.464"></path>
+    <path d="M9.515,3.531L9.515,7.000L16.475,7.000L16.475,10.464"></path>
+    <path d="M9.500,3.534L9.500,7.002L9.530,7.002L9.530,10.466"></path>
+  </g>
+</svg>
