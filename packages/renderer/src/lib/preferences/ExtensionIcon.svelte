6b8e7045ef4e759adac486915a956543c1e9e5e8
Tim deBoer <git@tdeboer.ca>
2023-12-03 21:26:45 -0500

    feat: extension icons
    After seeing Luca's PR #5089 I felt like I had to do this. :) Uses the
    extension's icon in the Settings > Extensions page, using an almost
    identical component to ProviderLogo. If the extension has no icon, the
    puzzle piece is still used.
    
    I've put the component in the /preferences folder since that's the only
    place it is being used for now.
    
    The puzzle pieces used purple vs gray to show whether the extension is
    running, I've switched to using a brightness filter instead, since it
    is consistent between the two. Tests included.
    
    Fixes #5100.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/preferences/ExtensionIcon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import Fa from 'svelte-fa';
+import type { ExtensionInfo } from '../../../../main/src/plugin/api/extension-info';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+
+export let extension: ExtensionInfo;
+
+let icon: string | undefined = undefined;
+$: {
+  if (extension.icon) {
+    if (typeof extension.icon === 'string') {
+      icon = extension.icon;
+    } else if (extension.icon.dark) {
+      // for now use dark theme
+      icon = extension.icon.dark;
+    }
+  }
+}
+$: fade = extension.state !== 'started' ? ' brightness-50' : '';
+</script>
+
+{#if icon}
+  <img src="{icon}" alt="{extension.name}" class="max-w-10 max-h-10 {fade}" />
+{:else}
+  <Fa alt="{extension.name}" class="h-10 w-10 rounded-full text-violet-600 {fade}" size="25" icon="{faPuzzlePiece}" />
+{/if}
