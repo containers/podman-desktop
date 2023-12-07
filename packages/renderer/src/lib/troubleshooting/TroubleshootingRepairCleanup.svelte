3f7aade3755d5dc8e1ebd2a274c1f4ff621bfa31
Florent Benoit <fbenoit@redhat.com>
2023-12-07 22:13:12 +0100

    feat: add cleanup UI
    fixes https://github.com/containers/podman-desktop/issues/5022
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/troubleshooting/TroubleshootingRepairCleanup.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+import { faBroom, faWarning } from '@fortawesome/free-solid-svg-icons';
+import type { ProviderInfo } from '../../../../main/src/plugin/api/provider-info';
+import Button from '../ui/Button.svelte';
+import Fa from 'svelte-fa';
+
+export let providers: ProviderInfo[] = [];
+let providerIdsWithCleanup: string[] = [];
+$: providerIdsWithCleanup = providers.filter(provider => provider.cleanupSupport).map(provider => provider.internalId);
+
+let cleanupInProgress = false;
+
+let cleanupFailures: string[] = [];
+
+async function openCleanupDialog(): Promise<void> {
+  let message = 'This action may delete data. Proceed ?';
+
+  const result = await window.showMessageBox({
+    title: 'Cleanup',
+    message: message,
+    buttons: ['Yes', 'No'],
+  });
+
+  if (result?.response === 0) {
+    cleanup();
+  }
+}
+
+async function cleanup() {
+  try {
+    cleanupInProgress = true;
+    cleanupFailures = [];
+    await window.cleanupProviders(providerIdsWithCleanup, Symbol('cleanup-provider'), (_key, _eventName, args) => {
+      // log into the console as no UI is available to display logs
+      console.log('cleanup event', args);
+    });
+  } catch (e) {
+    console.error(e);
+    cleanupFailures.push(String(e));
+  } finally {
+    cleanupInProgress = false;
+  }
+}
+</script>
+
+<div class="flex flex-row items-center">
+  <div>
+    <div class="text-gray-700 flex flex-row items-center">Clean / Purge data</div>
+    <div class="text-gray-900 text-xs flex flex-row items-center">
+      <Fa class="pr-1" size="9" icon="{faWarning}" />Proceeding with this action may result in data loss, including
+      existing volumes, containers, images, etc.
+    </div>
+  </div>
+
+  <div class="flex flex-1 justify-end">
+    <Button
+      type="danger"
+      on:click="{() => openCleanupDialog()}"
+      disabled="{cleanupInProgress}"
+      inProgress="{cleanupInProgress}"
+      aria-label="Cleanup"
+      icon="{faBroom}">Cleanup</Button>
+  </div>
+
+  <div>
+    {#if cleanupFailures.length > 0}
+      <div class="text-red-500 text-xs flex flex-row items-center" role="alert" aria-label="error">
+        <Fa class="pr-1" size="9" icon="{faWarning}" />{cleanupFailures.length} failures
+      </div>
+    {/if}
+  </div>
+</div>
