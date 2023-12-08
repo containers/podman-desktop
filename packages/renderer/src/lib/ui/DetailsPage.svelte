1fe9f4358b78b4f13966ae87bd5023ded42763a4
Tim deBoer <git@tdeboer.ca>
2023-12-08 13:09:34 -0500

    chore: adjust details heading and summary post-pf
    While testing another PR I noticed that the DetailsPage header was taking
    up a lot of vertical space. The Summary tabs are especially poor and the
    rows are really spaced out. I checked and sure enough, we were adding
    extra padding to compensate for PatternFly's narrow line-height, so when
    we removed PF these gaps became noticeable.
    
    This PR removes all unnecessary padding from each of the Summary pages,
    and makes minor changes to padding in Tab and DetailsPage to return to
    the original design.
    
    While I was there I added an if around the tabs, so if we ever have a
    details page without tabs there won't be an empty div adding padding
    and a line.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/ui/DetailsPage.svelte
+++ packages/renderer/src/lib/ui/DetailsPage.svelte
@@ -22,7 +22,7 @@ function handleKeydown(e: KeyboardEvent) {
 <svelte:window on:keydown="{handleKeydown}" />
 
 <div class="flex flex-col w-full h-full shadow-pageheader">
-  <div class="flex flex-row w-full h-fit px-5 py-4">
+  <div class="flex flex-row w-full h-fit px-5 pt-4 pb-2">
     <div class="flex flex-col w-full h-fit">
       <div class="flex flew-row items-center text-sm text-gray-700">
         <Link class="text-sm" aria-label="back" internalRef="{$lastPage.path}" title="Go back to {$lastPage.name}"
@@ -42,7 +42,7 @@ function handleKeydown(e: KeyboardEvent) {
             <h1 aria-label="{title}" class="text-xl leading-tight">{title}</h1>
             <div class="text-violet-400 ml-2 leading-normal" class:hidden="{!titleDetail}">{titleDetail}</div>
           </div>
-          <div class="pt-1">
+          <div>
             <span class="text-sm leading-none text-gray-900" class:hidden="{!subtitle}">{subtitle}</span>
             <slot name="subtitle" />
           </div>
@@ -60,9 +60,11 @@ function handleKeydown(e: KeyboardEvent) {
       </div>
     </div>
   </div>
-  <div class="flex flex-row px-2 border-b border-charcoal-400">
-    <slot name="tabs" />
-  </div>
+  {#if $$slots.tabs}
+    <div class="flex flex-row px-2 border-b border-charcoal-400">
+      <slot name="tabs" />
+    </div>
+  {/if}
   <div class="h-full bg-charcoal-900 min-h-0">
     <slot name="content" />
   </div>
