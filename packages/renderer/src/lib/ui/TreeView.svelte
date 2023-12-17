f3b5eef29e4431463b55b12fe1a733ff2c508131
Philippe Martin <phmartin@redhat.com>
2023-12-17 18:26:36 +0100

    feat: colors and removed files Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/ui/TreeView.svelte
+++ packages/renderer/src/lib/ui/TreeView.svelte
@@ -22,9 +22,32 @@ export let tree: fileNode<any>;
 export let margin = 0;
 export let root = true;
 
-$: label = tree.name + getLink(tree.data);
+$: label = isRemoved(tree) ? tree.name.substring(4) : tree.name;
 $: children = tree.children;
 $: file = tree.data;
+$: colorClass = getColor(tree.name, tree.data);
+
+function getColor(name: string, data: any) {
+  if (!data) {
+    return '';
+  }
+  if (name.startsWith('.wh.')) {
+    return 'text-red-500';
+  }
+  if (data.type === 'SymbolicLink') {
+    return 'text-sky-300';
+  }
+  if (data.type === 'Directory') {
+    return 'text-sky-500';
+  }
+  if (isExecutable(data.mode)) {
+    return 'text-green-500';
+  }
+}
+
+function isRemoved(data: any): boolean {
+  return data?.name.startsWith('.wh.');
+}
 
 $: expanded = _expansionState.get(label) || false;
 const toggleExpansion = () => {
@@ -48,6 +71,10 @@ function getModeString(type: string, mode: number): string {
   );
 }
 
+function isExecutable(mode: number): boolean {
+  return (mode & 0o111) !== 0;
+}
+
 function getHumanSize(size: number): string {
   let u = '';
   if (size > 1024) {
@@ -69,7 +96,7 @@ function getLink(file: any): string {
   if (!file) {
     return '';
   }
-  if (file.type === 'SymbolicLink' || file.type === 'Link') {
+  if (file.type === 'SymbolicLink') {
     return ' → ' + file.linkpath;
   }
   return '';
@@ -81,13 +108,13 @@ function getLink(file: any): string {
     <svelte:self root="{false}" margin="{margin + 2}" tree="{child}" />
   {/each}
 {:else}
-  <div>{tree.data ? getModeString(tree.data.type, tree.data.mode) : ''}</div>
-  <div class="text-right">{tree.data ? tree.data.uid + ':' + tree.data.gid : ''}</div>
-  <div class="text-right">{tree.data ? getHumanSize(tree.data.size) : ''}</div>
+  <div class="font-mono">{tree.data && !isRemoved(tree) ? getModeString(tree.data.type, tree.data.mode) : ''}</div>
+  <div class="text-right">{tree.data && !isRemoved(tree) ? tree.data.uid + ':' + tree.data.gid : ''}</div>
+  <div class="text-right">{tree.data && !isRemoved(tree) ? getHumanSize(tree.data.size) : ''}</div>
   {#if children.size || (file && file.type === 'Directory')}
-    <button class="{`text-left ml-${margin}`}" on:click="{toggleExpansion}">
+    <button class="{`text-left ml-${margin} ${colorClass}`}" on:click="{toggleExpansion}">
       <span class="arrow mr-1" class:arrowDown="{arrowDown}">&gt;</span>
-      {label}
+      {label}<span class="text-gray-900 text-sm">{getLink(tree.data)}</span>
     </button>
     {#if expanded}
       {#each children as [_, child]}
@@ -95,9 +122,9 @@ function getLink(file: any): string {
       {/each}
     {/if}
   {:else}
-    <div>
+    <div class="{`${colorClass}`}">
       <span class="{`no-arrow ml-${margin}`}"></span>
-      {label}
+      {label}<span class="text-gray-900 text-sm">{getLink(tree.data)}</span>
     </div>
   {/if}
 {/if}
