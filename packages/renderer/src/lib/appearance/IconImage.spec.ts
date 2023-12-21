f5c3c7caad1da6266dbdae95258b9a8ca3ce0c84
Florent BENOIT <fbenoit@redhat.com>
2023-12-21 15:00:32 +0100

    chore: allow to have empty alt field (sometimes we don't have) (#5354)
    * chore: allow to have empty alt field (sometimes we don't have)
    
    also fix unit tests as it was testing another class
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/appearance/IconImage.spec.ts
+++ packages/renderer/src/lib/appearance/IconImage.spec.ts
@@ -19,8 +19,8 @@
 /* eslint-disable no-null/no-null */
 import '@testing-library/jest-dom/vitest';
 import { test, vi, expect, beforeEach } from 'vitest';
-import { render, type RenderResult } from '@testing-library/svelte';
-import Appearance from './Appearance.svelte';
+import { render } from '@testing-library/svelte';
+import IconImage from './IconImage.svelte';
 import { AppearanceSettings } from '../../../../main/src/plugin/appearance-settings';
 
 const getConfigurationValueMock = vi.fn();
@@ -35,81 +35,52 @@ beforeEach(() => {
   });
 });
 
-function getRootElement(container: HTMLElement): HTMLElement {
-  // get root html element
-  let rootElement: HTMLElement | null = container;
-  while (rootElement?.parentElement) {
-    rootElement = container.parentElement;
-  }
-  return rootElement as HTMLElement;
-}
-
-function getRootElementClassesValue(container: HTMLElement): string | undefined {
-  return getRootElement(container).classList.value;
-}
-
-async function awaitRender(): Promise<RenderResult<Appearance>> {
-  const result = render(Appearance);
-  // wait end of asynchrounous onMount
-  // wait 200ms
-  await new Promise(resolve => setTimeout(resolve, 200));
-
-  return result;
-}
-
-// temporary as only dark mode is supported as rendering for now
-// it should return empty later
-test('Expect dark mode using system when OS is set to light', async () => {
-  (window as any).matchMedia = vi.fn().mockReturnValue({
-    matches: false,
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-  });
-
-  getConfigurationValueMock.mockResolvedValue(AppearanceSettings.SystemEnumValue);
-
-  const { container } = await awaitRender();
-
-  const val = getRootElementClassesValue(container);
-
-  // expect to have class being "dark" as for now we force dark mode in system mode
-  expect(val).toBe('dark');
-});
-
-test('Expect dark mode using system when OS is set to dark', async () => {
-  (window as any).matchMedia = vi.fn().mockReturnValue({
-    matches: true,
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-  });
-
-  getConfigurationValueMock.mockResolvedValue(AppearanceSettings.SystemEnumValue);
-
-  const { container } = await awaitRender();
-  // expect to have class being "dark" as OS is using dark
-  expect(getRootElementClassesValue(container)).toBe('dark');
-});
-
-test('Expect light mode using light configuration', async () => {
-  getConfigurationValueMock.mockResolvedValue(AppearanceSettings.LightEnumValue);
-
-  const { container } = await awaitRender();
-
-  // expect to have class being ""  as we should be in light mode
-  expect(getRootElementClassesValue(container)).toBe('');
-
-  // expect to have color-scheme: light
-  expect(getRootElement(container)).toHaveStyle('color-scheme: light');
-});
-
-test('Expect dark mode using dark configuration', async () => {
+test('Expect valid source and alt text with dark mode', async () => {
   getConfigurationValueMock.mockResolvedValue(AppearanceSettings.DarkEnumValue);
 
-  const { container } = await awaitRender();
+  const image = render(IconImage, { image: { light: 'light.png', dark: 'dark.png' }, alt: 'this is alt text' });
 
-  // expect to have class being "dark" as we should be in light mode
-  expect(getRootElementClassesValue(container)).toBe('dark');
+  // wait for image to be loaded
+  await new Promise(resolve => setTimeout(resolve, 200));
 
-  // expect to have color-scheme: dark
-  expect(getRootElement(container)).toHaveStyle('color-scheme: dark');
+  // grab image element
+  const imageElement = image.getByRole('img');
+
+  // expect to have valid source
+  expect(imageElement).toHaveAttribute('src', 'dark.png');
+  expect(imageElement).toHaveAttribute('alt', 'this is alt text');
+});
+
+test('Expect valid source and alt text with light mode', async () => {
+  getConfigurationValueMock.mockResolvedValue(AppearanceSettings.LightEnumValue);
+
+  const image = render(IconImage, { image: { light: 'light.png', dark: 'dark.png' }, alt: 'this is alt text' });
+
+  // wait for image to be loaded
+  await new Promise(resolve => setTimeout(resolve, 200));
+
+  // grab image element
+  const imageElement = image.getByRole('img');
+
+  // expect to have valid source
+  expect(imageElement).toHaveAttribute('src', 'light.png');
+  expect(imageElement).toHaveAttribute('alt', 'this is alt text');
+});
+
+test('Expect no alt attribute if missing and default image', async () => {
+  getConfigurationValueMock.mockResolvedValue(AppearanceSettings.LightEnumValue);
+
+  const image = render(IconImage, { image: 'image.png' });
+
+  // wait for image to be loaded
+  await new Promise(resolve => setTimeout(resolve, 200));
+
+  // grab image element
+  const imageElement = image.getByRole('img');
+
+  // expect to have valid source
+  expect(imageElement).toHaveAttribute('src', 'image.png');
+
+  // alt should be missing
+  expect(imageElement).not.toHaveAttribute('alt');
 });
