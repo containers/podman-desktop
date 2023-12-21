342783d6f9f3dc865fa12a036559e01c16104af8
Tim deBoer <git@tdeboer.ca>
2023-12-21 15:59:26 -0500

    feat: nav sections
    Nav sections component. Every section starts collapsed and can be expanded
    to show a set of child nav items. We didn't have a complete design for
    this to I've tried a few things and ran them past Mairin briefly. State
    changes are animated, and selection styling updated to use purple outlines
    instead of left border.
    
    Switched Docker Desktop extensions to use a section (and so that there
    is something to easily test here), using a new puzzle icon from Mairin
    (FA only has a solid puzzle). Kubernetes will be done in another PR.
    
    This doesn't directly solve the problem that you could have so many icons
    that it doesn't fit onscreen, but as we have more sections they start
    closed and you can at least close most of them to help make sure the
    thing you need is visible.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/ui/NavSection.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import Tooltip from './Tooltip.svelte';
+import Fa from 'svelte-fa';
+import { cubicOut } from 'svelte/easing';
+
+export let expanded: boolean = false;
+export let tooltip: string;
+
+function fadeSlide(node: any, { delay = 0, duration = 400, easing = cubicOut }) {
+  const style = getComputedStyle(node);
+  const opacity = +style.opacity;
+  const height = parseFloat(style.height);
+  const paddingTop = parseFloat(style.paddingTop);
+  const paddingBottom = parseFloat(style.paddingBottom);
+
+  return {
+    delay,
+    duration,
+    easing,
+    css: (t: number) =>
+      `overflow: hidden;` +
+      `opacity: ${Math.min(t, 1) * opacity};` +
+      `height: ${t * height}px;` +
+      `padding-top: ${t * paddingTop}px;` +
+      `padding-bottom: ${t * paddingBottom}px;`,
+  };
+}
+</script>
+
+<div class="flex flex-col justify-center items-center mx-1 bg-charcoal-600 rounded my-1">
+  {#if expanded}
+    <div class="inline-block pt-0.5">
+      <div transition:fadeSlide="{{ duration: 500 }}">
+        <slot />
+      </div>
+    </div>
+  {/if}
+
+  <button class="inline-block flex flex-col justify-center items-center" on:click="{() => (expanded = !expanded)}">
+    <Tooltip class="flex flex-col justify-center items-center pb-2" tip="{tooltip}" right>
+      {#if !expanded}
+        <div class="pt-2 pb-0.5" transition:fadeSlide="{{ duration: 500 }}">
+          <slot name="icon" />
+        </div>
+      {/if}
+      <Fa size="12" icon="{expanded ? faChevronUp : faChevronDown}" />
+    </Tooltip>
+  </button>
+</div>
---
+++ packages/renderer/src/lib/ui/NavSectionTest.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import NavSection from './NavSection.svelte';
+
+let expanded: boolean;
+</script>
+
+Is it expanded: <div data-testid="expand">{expanded}</div>
+
+<NavSection tooltip="Test Section" bind:expanded="{expanded}">
+  <div data-testid="icon" slot="icon">The icon</div>
+  <div data-testid="content">Content</div>
+</NavSection>
