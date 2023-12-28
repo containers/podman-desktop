0c9b5f512db941ff6a41c086d95f201c7bcb5262
Philippe Martin <phmartin@redhat.com>
2023-12-28 12:45:30 +0100

    test: fix unit tests Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/main/src/plugin/file-tree.spec.ts
+++ packages/main/src/plugin/file-tree.spec.ts
@@ -25,23 +25,28 @@ interface typ {
 
 test('add paths to filetree', () => {
   const tree = new FileTree<typ>('tree1');
-  tree.addPath('A', { path: 'A-path' });
-  tree.addPath('a/', { path: 'a/-path' });
-  tree.addPath('a/b/c/d.txt', { path: 'a/b/c/d.txt-path' });
-  tree.addPath('a/b/c/e.txt', { path: 'a/b/c/e.txt-path' });
+  tree.addPath('A', { path: 'A-path' }, 5);
+  tree.addPath('a/', { path: 'a/-path' }, 0);
+  tree.addPath('a/b/c/d.txt', { path: 'a/b/c/d.txt-path' }, 3);
+  tree.addPath('a/b/c/e.txt', { path: 'a/b/c/e.txt-path' }, 4);
 
+  expect(tree.size).toBe(12);
   expect(tree.root.children).toHaveLength(2);
   expect(tree.root.children.get('A')!.children).toHaveLength(0);
   expect(tree.root.children.get('A')!.data!.path).toBe('A-path');
+  expect(tree.root.children.get('A')!.size).toBe(5);
 
   expect(tree.root.children.get('a')!.children).toHaveLength(1);
   expect(tree.root.children.get('a')!.data!.path).toBe('a/-path');
+  expect(tree.root.children.get('a')!.size).toBe(7);
 
   expect(tree.root.children.get('a')!.children.get('b')!.children).toHaveLength(1);
   expect(tree.root.children.get('a')!.children.get('b')!.data).toBeUndefined();
+  expect(tree.root.children.get('a')!.children.get('b')!.size).toBe(7);
 
   expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children).toHaveLength(2);
   expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.data).toBeUndefined();
+  expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.size).toBe(7);
 
   expect(
     tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children.get('d.txt')!.children,
@@ -49,6 +54,7 @@ test('add paths to filetree', () => {
   expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children.get('d.txt')!.data!.path).toBe(
     'a/b/c/d.txt-path',
   );
+  expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children.get('d.txt')!.size).toBe(3);
 
   expect(
     tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children.get('e.txt')!.children,
@@ -56,4 +62,5 @@ test('add paths to filetree', () => {
   expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children.get('e.txt')!.data!.path).toBe(
     'a/b/c/e.txt-path',
   );
+  expect(tree.root.children.get('a')!.children.get('b')!.children.get('c')!.children.get('e.txt')!.size).toBe(4);
 });
