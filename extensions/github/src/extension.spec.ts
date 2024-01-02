4ea033c118a67aa28b65114ef027fe7ec58b2558
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-02 14:55:04 +0100

    test: more unit tests
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- extensions/github/src/extension.spec.ts
+++ extensions/github/src/extension.spec.ts
@@ -1,7 +1,7 @@
-import { expect, test, vi } from 'vitest';
-import { GitHubProvider } from './extension';
+import { beforeEach, expect, test, vi } from 'vitest';
+import { activate, getGitHubAccessToken, GitHubProvider } from './extension';
 import * as extensionApi from '@podman-desktop/api';
-import type { Configuration } from '@podman-desktop/api';
+import type { AuthenticationProvider, AuthenticationProviderOptions, Configuration } from '@podman-desktop/api';
 
 function fakeConfiguration(accessToken?: string): Configuration {
   return {
@@ -17,15 +17,23 @@ function fakeConfiguration(accessToken?: string): Configuration {
   };
 }
 
+const fireMock = vi.hoisted(() => vi.fn());
 vi.mock('@podman-desktop/api', async () => {
   return {
     EventEmitter: vi.fn().mockReturnValue({
-      fire: vi.fn(),
+      fire: fireMock,
     }),
     configuration: {
       getConfiguration: vi.fn(),
     },
+    authentication: {
+      registerAuthenticationProvider: vi.fn(),
+    },
   };
+});
+
+beforeEach(() => {
+  vi.clearAllMocks();
 });
 
 vi.mock('@octokit/rest', () => {
@@ -37,6 +45,16 @@ vi.mock('@octokit/rest', () => {
   return {
     Octokit: vi.fn().mockReturnValue({ users: users }),
   };
+});
+
+test('expect token configuration undefined', async () => {
+  vi.spyOn(extensionApi.configuration, 'getConfiguration').mockReturnValue(fakeConfiguration());
+  expect(getGitHubAccessToken()).toBeUndefined();
+});
+
+test('expect token configuration defined', async () => {
+  vi.spyOn(extensionApi.configuration, 'getConfiguration').mockReturnValue(fakeConfiguration('dummy'));
+  expect(getGitHubAccessToken()).toBe('dummy');
 });
 
 test('error: cannot create session: empty access token', async () => {
@@ -56,6 +74,7 @@ test('error: cannot create session: empty access token', async () => {
 });
 
 test('valid access token', async () => {
+  // Configured access token
   vi.spyOn(extensionApi.configuration, 'getConfiguration').mockReturnValue(fakeConfiguration('dummy'));
 
   const authProvider = new GitHubProvider();
@@ -64,4 +83,64 @@ test('valid access token', async () => {
   const session = await authProvider.createSession([]);
   expect(session).toBeDefined();
   expect(session.accessToken).toBe('dummy');
+  expect(fireMock).toHaveBeenNthCalledWith(1, {
+    added: [session],
+  });
+});
+
+test('expect createSession to be called automatically', async () => {
+  // We have a configured access token value
+  vi.spyOn(extensionApi.configuration, 'getConfiguration').mockReturnValue(fakeConfiguration('dummy'));
+
+  let provider: AuthenticationProvider | undefined = undefined;
+  vi.spyOn(extensionApi.authentication, 'registerAuthenticationProvider').mockImplementation(
+    (_id: string, _label: string, nProvider: AuthenticationProvider, _options?: AuthenticationProviderOptions) => {
+      provider = nProvider;
+      return undefined;
+    },
+  );
+
+  await activate();
+  expect(provider).toBeDefined();
+  expect(await provider.getSessions([])).toHaveLength(1);
+});
+
+test('expect createSession not to be called automatically', async () => {
+  // We DO NOT have a configured access token value
+  vi.spyOn(extensionApi.configuration, 'getConfiguration').mockReturnValue(fakeConfiguration());
+
+  let provider: AuthenticationProvider | undefined = undefined;
+  vi.spyOn(extensionApi.authentication, 'registerAuthenticationProvider').mockImplementation(
+    (_id: string, _label: string, nProvider: AuthenticationProvider, _options?: AuthenticationProviderOptions) => {
+      provider = nProvider;
+      return undefined;
+    },
+  );
+
+  await activate();
+  expect(provider).toBeDefined();
+  expect(await provider.getSessions([])).toHaveLength(0);
+});
+
+test('expect session removed properly', async () => {
+  vi.spyOn(extensionApi.configuration, 'getConfiguration').mockReturnValue(fakeConfiguration('dummy'));
+
+  const authProvider = new GitHubProvider();
+
+  // createSession with access token configured
+  const session = await authProvider.createSession([]);
+  // Ensure the session is created and event fired
+  expect(await authProvider.getSessions()).toHaveLength(1);
+  expect(fireMock).toHaveBeenNthCalledWith(1, {
+    added: [session],
+  });
+
+  // Reset the fire mock
+  fireMock.mockReset();
+  await authProvider.removeSession(session.id);
+
+  expect(await authProvider.getSessions()).toHaveLength(0);
+  expect(fireMock).toHaveBeenNthCalledWith(1, {
+    removed: [session],
+  });
 });
