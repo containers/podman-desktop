1e84e10a8958bbddb8b5df4847a6cac45826d471
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-04 12:13:05 +0100

    test: ensure the text and icon is visible when dropdownMenu on and groupContributions off
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/image/ImageActions.spec.ts
+++ packages/renderer/src/lib/image/ImageActions.spec.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/vitest';
 import { test, expect, vi, beforeAll } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/svelte';
+import { render, screen, waitFor, fireEvent } from '@testing-library/svelte';
 import ImageActions from '/@/lib/image/ImageActions.svelte';
 import type { ImageInfoUI } from '/@/lib/image/ImageInfoUI';
 
@@ -18,8 +18,8 @@ const fakedImage: ImageInfoUI = {
   name: 'dummy',
 } as unknown as ImageInfoUI;
 
-test('Expect no dropdown', async () => {
-  // Since we only have one contributions, we do not use a dropdown
+test('Expect no dropdown when one contribution and dropdownMenu off', async () => {
+  // Since we only have one contribution, we do not use a dropdown
   getContributedMenusMock.mockImplementation(_context =>
     Promise.resolve([{ command: 'valid-command', title: 'dummy-contrib' }]),
   );
@@ -42,7 +42,7 @@ test('Expect no dropdown', async () => {
   });
 });
 
-test('Expect dropdown', async () => {
+test('Expect contribution in dropdown when several contributions and dropdownMenu off', async () => {
   // Since we have more than one contribution we group them in a dropdown
   getContributedMenusMock.mockImplementation(_context =>
     Promise.resolve([
@@ -67,5 +67,36 @@ test('Expect dropdown', async () => {
     expect(button).toBeDefined();
     expect(button.parentElement).toBeDefined();
     expect(button.parentElement?.classList?.length).toBeGreaterThan(0);
+  });
+});
+
+test('Expect no dropdown when several contributions and dropdownMenu mode on', async () => {
+  // Simulate with multiple contributions
+  getContributedMenusMock.mockImplementation(_context =>
+    Promise.resolve([
+      { command: 'valid-command', title: 'dummy-contrib' },
+      { command: 'valid-command-2', title: 'dummy-contrib-2' },
+    ]),
+  );
+
+  render(ImageActions, {
+    onPushImage: vi.fn(),
+    onRenameImage: vi.fn(),
+    image: fakedImage,
+    dropdownMenu: true, // We are showing all actions in a Dropdown
+    detailed: true,
+    groupContributions: false, // we do not group them since we are in a dropdown
+  });
+
+  expect(getContributedMenusMock).toHaveBeenCalled();
+
+  await fireEvent.click(screen.getByLabelText('kebab menu'));
+
+  await waitFor(() => {
+    const button = screen.getByTitle('dummy-contrib');
+    expect(button).toBeDefined();
+    expect(button.firstChild?.nodeName.toLowerCase()).toBe('svg');
+    expect(button.lastChild?.nodeName.toLowerCase()).toBe('span');
+    expect(button.lastChild?.textContent).toBe('dummy-contrib');
   });
 });
