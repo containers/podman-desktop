49b6bfcbee3f8802429c6579d1f7296a6e3ea034
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-04 13:53:19 +0100

    test: ensuring the showMessageBox is called when an error occurs
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/image/ImageActions.spec.ts
+++ packages/renderer/src/lib/image/ImageActions.spec.ts
@@ -1,14 +1,42 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
 import '@testing-library/jest-dom/vitest';
 import { test, expect, vi, beforeAll } from 'vitest';
 import { render, screen, waitFor, fireEvent } from '@testing-library/svelte';
 import ImageActions from '/@/lib/image/ImageActions.svelte';
 import type { ImageInfoUI } from '/@/lib/image/ImageInfoUI';
 
+const showMessageBoxMock = vi.fn();
 const getContributedMenusMock = vi.fn();
 
-beforeAll(() => {
-  (window as any).getContributedMenus = getContributedMenusMock;
+vi.mock('./image-utils', () => {
+  return {
+    ImageUtils: vi.fn().mockImplementation(() => ({
+      deleteImage: vi.fn().mockImplementation(() => Promise.reject(new Error('Cannot delete image in test'))),
+    })),
+  };
+});
 
+beforeAll(() => {
+  (window as any).showMessageBox = showMessageBoxMock;
+
+  (window as any).getContributedMenus = getContributedMenusMock;
   (window as any).hasAuthconfigForImage = vi.fn();
   (window as any).hasAuthconfigForImage.mockImplementation(() => Promise.resolve(false));
 });
@@ -17,6 +45,24 @@ const fakedImage: ImageInfoUI = {
   id: 'dummy',
   name: 'dummy',
 } as unknown as ImageInfoUI;
+
+
+test('Expect showMessageBox to be called when error occurs', async () => {
+  render(ImageActions, {
+    onPushImage: vi.fn(),
+    onRenameImage: vi.fn(),
+    image: {
+      name: 'dummy',
+    } as unknown as ImageInfoUI,
+  });
+  const button = screen.getByTitle('Delete Image');
+  expect(button).toBeDefined();
+  await fireEvent.click(button);
+
+  await waitFor(() => {
+    expect(showMessageBoxMock).toHaveBeenCalledOnce();
+  });
+});
 
 test('Expect no dropdown when one contribution and dropdownMenu off', async () => {
   // Since we only have one contribution, we do not use a dropdown
