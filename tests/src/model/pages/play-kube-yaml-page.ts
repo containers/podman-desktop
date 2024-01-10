4ab933f3f753409eec9786dfbfde17b46005ba58
Vladimir Lazar <vlazar@redhat.com>
2024-01-10 14:25:54 +0200

    chore(tests): wait to finish Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/src/model/pages/play-kube-yaml-page.ts
+++ tests/src/model/pages/play-kube-yaml-page.ts
@@ -44,7 +44,7 @@ export class PlayKubeYamlPage extends BasePage {
 
     await this.yamlPathInput.fill(pathToYaml);
     await this.playButton.click();
-    await waitUntil(async () => await this.doneButton.isEnabled(), 30000, 500);
+    await waitUntil(async () => await this.doneButton.isEnabled(), 60000, 500);
     await this.doneButton.click();
     return new PodsPage(this.page);
   }
