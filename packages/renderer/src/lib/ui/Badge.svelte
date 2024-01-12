c97dced7fde9cb1a7a6b61095d1fc3e4e5082f32
Florent Benoit <fbenoit@redhat.com>
2024-01-12 09:47:06 -0500

    feat: add UI badge component
    fixes https://github.com/containers/podman-desktop/issues/5521
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/ui/Badge.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { onMount } from 'svelte';
+import { AppearanceUtil } from '/@/lib/appearance/appearance-util';
+
+export let color: string | { light: string; dark: string } = 'bg-gray-500';
+export let label: string = '';
+
+let customStyle: string = '';
+let customClass: string = '';
+
+onMount(async () => {
+  const appearanceUtil = new AppearanceUtil();
+
+  // get the color
+  let singleColor = await appearanceUtil.getImage(color);
+  if (!singleColor) {
+    singleColor = '';
+  }
+
+  if (singleColor?.startsWith('#')) {
+    customStyle = `background-color: ${singleColor};`;
+  } else {
+    customClass = singleColor ?? '';
+  }
+});
+</script>
+
+<div class="text-black text-xs me-2 px-1 py-0.5 rounded select-none {customClass}" style="{customStyle}">{label}</div>
