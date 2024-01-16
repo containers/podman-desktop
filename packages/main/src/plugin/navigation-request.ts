67eef84ece07509f8e1dc1237a50e08116b3b01b
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-16 15:42:27 +0100

    Update packages/main/src/plugin/navigation-request.ts
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/main/src/plugin/navigation-request.ts
+++ packages/main/src/plugin/navigation-request.ts
@@ -2,5 +2,5 @@ import type { NavigationPage } from '/@/plugin/navigation-page.js';
 
 export interface NavigationRequest {
   page: NavigationPage;
-  parameters?: Record<string, string>;
+  parameters?: { [key: string]: string };
 }
