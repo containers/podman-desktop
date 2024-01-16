04429b1c98d5c0ea44939df54a61764470b30d21
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-16 15:51:06 +0100

    fix: removing useless comments
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/extension-api/src/extension-api.d.ts
+++ packages/extension-api/src/extension-api.d.ts
@@ -2704,33 +2704,33 @@ declare module '@podman-desktop/api' {
   }
 
   export namespace navigation {
-    // Navigate to the Containers page in Podman-Desktop
+    // Navigate to the Containers page
     export function navigateToContainers(): Promise<void>;
-    // Navigate to the Container page in Podman-Desktop
+    // Navigate to the Container page
     export function navigateToContainer(id: string): Promise<void>;
-    // Navigate to the Container logs tab in Podman-Desktop
+    // Navigate to the Container logs tab
     export function navigateToContainerLogs(id: string): Promise<void>;
-    // Navigate to the Container inspect tab in Podman-Desktop
+    // Navigate to the Container inspect tab
     export function navigateToContainerInspect(id: string): Promise<void>;
-    // Navigate to the Container terminal tab in Podman-Desktop
+    // Navigate to the Container terminal tab
     export function navigateToContainerTerminal(id: string): Promise<void>;
 
-    // Navigate to the Images page in Podman-Desktop
+    // Navigate to the Images page
     export function navigateToImages(): Promise<void>;
-    // Navigate to a specific image referenced by id, engineId and tag in Podman-Desktop
+    // Navigate to a specific image referenced by id, engineId and tag
     export function navigateToImage(id: string, engineId: string, tag: string): Promise<void>;
 
-    // Navigate to the Volumes page in Podman-Desktop
+    // Navigate to the Volumes page
     export function navigateToVolumes(): Promise<void>;
-    // Navigate to a specific volume in Podman-Desktop
+    // Navigate to a specific volume
     export function navigateToVolume(name: string, engineId: string): Promise<void>;
 
-    // Navigate to the Pods page in Podman-Desktop
+    // Navigate to the Pods page
     export function navigateToPods(): Promise<void>;
-    // Navigate to a specific pod referenced by kind, name and engineId in Podman-Desktop
+    // Navigate to a specific pod referenced by kind, name and engineId
     export function navigateToPod(kind: string, name: string, engineId: string): Promise<void>;
 
-    // Navigate to a specific contribution (aka extension page) referenced by name in Podman-Desktop
+    // Navigate to a specific contribution (aka extension page) referenced by name
     export function navigateToContribution(name: string): Promise<void>;
   }
 }
