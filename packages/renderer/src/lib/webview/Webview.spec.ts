c6136f5eb9d2d893e7ebcbd3ef689faa37b1d2f0
Florent Benoit <fbenoit@redhat.com>
2024-01-19 00:58:06 +0100

    feat: add webview in the UI
    part of https://github.com/containers/podman-desktop/issues/5140
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/webview/Webview.spec.ts
@@ -0,0 +1,171 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import '@testing-library/jest-dom/vitest';
+import { test, expect, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import Webview from './Webview.svelte';
+import { webviews } from '/@/stores/webviews';
+import type { WebviewInfo } from '../../../../main/src/plugin/api/webview-info';
+
+const makeDefaultWebviewVisibleMock = vi.fn();
+const getWebviewPreloadPathMock = vi.fn();
+const getWebviewRegistryHttpPortMock = vi.fn();
+const messages = new Map<string, (args: any) => void>();
+
+// webviews for the store
+const webviewTestList: WebviewInfo[] = [
+  {
+    id: 'webviewId1',
+    uuid: 'uuid1',
+    viewType: 'podman-desktop',
+    sourcePath: '/path/to/source',
+    icon: 'icon1',
+    name: 'Podman Desktop1',
+    html: '<html>newCode1</html>',
+    state: { stateData1: 'stateData1' },
+  },
+  {
+    id: 'webviewId2',
+    uuid: 'uuid2',
+    viewType: 'podman-desktop',
+    sourcePath: '/path/to/source',
+    icon: 'icon2',
+    name: 'Podman Desktop2',
+    html: '<html>newCode2</html>',
+    state: { stateData1: 'stateData2' },
+  },
+];
+
+beforeEach(() => {
+  vi.resetAllMocks();
+  (window as any).makeDefaultWebviewVisible = makeDefaultWebviewVisibleMock;
+  (window as any).getWebviewPreloadPath = getWebviewPreloadPathMock;
+  (window as any).getWebviewRegistryHttpPort = getWebviewRegistryHttpPortMock;
+
+  (window.events as unknown) = {
+    receive: vi.fn().mockImplementation((channel, func) => {
+      messages.set(channel, func);
+      return {
+        dispose: vi.fn(),
+      };
+    }),
+  };
+
+  // provide preload path
+  getWebviewPreloadPathMock.mockResolvedValue('/path/to/preload');
+
+  // provide registry port
+  getWebviewRegistryHttpPortMock.mockResolvedValue(5678);
+
+  webviews.set(webviewTestList);
+});
+
+async function waitRender(customProperties: object): Promise<void> {
+  const result = render(Webview, { ...customProperties });
+  // wait for path, port and webviewInfo object to defined
+  while (result.component.$$.ctx[0] === undefined) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+  while (result.component.$$.ctx[1] === undefined) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+  while (result.component.$$.ctx[2] === undefined) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+}
+
+test('check we have webview being displayed', async () => {
+  // render the first webview
+  await waitRender({ id: webviewTestList[0].id });
+
+  // get webview element
+  const webview = screen.getByRole('document', { name: 'Webview Podman Desktop1' });
+  expect(webview).toBeInTheDocument();
+
+  // check webview attributes
+  expect(webview).toHaveAttribute('httpreferrer', 'http://uuid1.webview.localhost:5678');
+  expect(webview).toHaveAttribute('preload', '/path/to/preload');
+  expect(webview).toHaveAttribute('src', 'http://uuid1.webview.localhost:5678?webviewId=webviewId1');
+});
+
+test('check post message', async () => {
+  // render the first webview
+  await waitRender({ id: webviewTestList[0].id });
+
+  // get webview element
+  const webview = screen.getByRole('document', { name: 'Webview Podman Desktop1' });
+  expect(webview).toBeInTheDocument();
+
+  // add a send method to the webview to check if it is called
+  const sendToWebviewMock = vi.fn();
+  (webview as any).send = sendToWebviewMock;
+
+  // send a post message event
+  messages.get('webview-post-message')?.({ id: webviewTestList[0].id, message: 'my-custom-message' });
+
+  // wait
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  //  check if send method has been called
+  expect(sendToWebviewMock).toHaveBeenCalledWith('webview-post-message', { message: 'my-custom-message' });
+});
+
+test('check update html', async () => {
+  // render the first webview
+  await waitRender({ id: webviewTestList[0].id });
+
+  // get webview element
+  const webview = screen.getByRole('document', { name: 'Webview Podman Desktop1' });
+  expect(webview).toBeInTheDocument();
+
+  // add a send method to the webview to check if it is called
+  const sendToWebviewMock = vi.fn();
+  (webview as any).send = sendToWebviewMock;
+
+  // now check for html update
+  messages.get('webview-update:html')?.({ id: webviewTestList[0].id, html: '<html>newCode1</html>' });
+  // wait
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  //  check if send method has been called
+  expect(sendToWebviewMock).toHaveBeenCalledWith('webview-update-html', '<html>newCode1</html>');
+});
+
+test('check open devtools', async () => {
+  // render the first webview
+  await waitRender({ id: webviewTestList[0].id });
+
+  // get webview element
+  const webview = screen.getByRole('document', { name: 'Webview Podman Desktop1' });
+  expect(webview).toBeInTheDocument();
+
+  //create a mock for openDevtools
+  const openDevtoolsMock = vi.fn();
+  (webview as any).openDevTools = openDevtoolsMock;
+
+  // now check for open devtools
+  messages.get('dev-tools:open-webview')?.(webviewTestList[0].id);
+  // wait
+  await new Promise(resolve => setTimeout(resolve, 100));
+
+  //  check if send method has been called
+  expect(openDevtoolsMock).toHaveBeenCalled();
+});
---
+++ packages/renderer/src/lib/webview/Webviews.spec.ts
@@ -0,0 +1,74 @@
+/**********************************************************************
+ * Copyright (C) 2023 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import '@testing-library/jest-dom/vitest';
+import { test, expect, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import Webviews from './Webviews.svelte';
+import { webviews } from '/@/stores/webviews';
+import type { WebviewInfo } from '../../../../main/src/plugin/api/webview-info';
+import type { TinroRouteMeta } from 'tinro';
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+test('check we have list of webviews in navigation bar', async () => {
+  // register webviews to the store
+  const webviewTestList: WebviewInfo[] = [
+    {
+      id: 'webviewId1',
+      uuid: 'uuid1',
+      viewType: 'podman-desktop',
+      sourcePath: '/path/to/source',
+      icon: 'icon1',
+      name: 'Podman Desktop1',
+      html: '<html>newCode1</html>',
+      state: { stateData1: 'stateData1' },
+    },
+    {
+      id: 'webviewId2',
+      uuid: 'uuid2',
+      viewType: 'podman-desktop',
+      sourcePath: '/path/to/source',
+      icon: 'icon2',
+      name: 'Podman Desktop2',
+      html: '<html>newCode2</html>',
+      state: { stateData1: 'stateData2' },
+    },
+  ];
+
+  webviews.set(webviewTestList);
+  const fakeMeta = { url: '/webviews' } as unknown as TinroRouteMeta;
+  render(Webviews, { meta: fakeMeta });
+
+  const links = screen.getAllByRole('link');
+
+  // check we have 2 links
+  expect(links).toHaveLength(2);
+
+  // now check detail of each link
+  expect(links[0]).toHaveTextContent('Podman Desktop1');
+  expect(links[1]).toHaveTextContent('Podman Desktop2');
+
+  // check link href
+  expect(links[0]).toHaveAttribute('href', '/webviews/webviewId1');
+  expect(links[1]).toHaveAttribute('href', '/webviews/webviewId2');
+});
