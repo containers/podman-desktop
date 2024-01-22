6c791976a7ba0a7e1dc90b943416e121a13f99d4
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2024-01-23 01:22:05 +0200

    feat: open kubernetes route
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- packages/renderer/src/lib/pod/PodActions.spec.ts
+++ packages/renderer/src/lib/pod/PodActions.spec.ts
@@ -23,16 +23,26 @@ import PodActions from './PodActions.svelte';
 import type { PodInfoUI } from './PodInfoUI';
 import type { ContainerInfo, Port } from '@podman-desktop/api';
 
-const pod: PodInfoUI = {
+const podmanPod: PodInfoUI = {
   id: 'pod',
   containers: [{ Id: 'pod' }],
   status: 'RUNNING',
   kind: 'podman',
 } as PodInfoUI;
 
+const kubernetesPod: PodInfoUI = {
+  id: 'pod',
+  name: 'name',
+  containers: [{ Id: 'pod' }],
+  status: 'RUNNING',
+  kind: 'kubernetes',
+} as PodInfoUI;
+
 const listContainersMock = vi.fn();
 const getContributedMenusMock = vi.fn();
 const updateMock = vi.fn();
+const kubernetesGetCurrentNamespaceMock = vi.fn();
+const kubernetesReadNamespacedPodMock = vi.fn();
 
 beforeEach(() => {
   (window as any).kubernetesDeletePod = vi.fn();
@@ -41,13 +51,18 @@ beforeEach(() => {
   (window as any).stopPod = vi.fn();
   (window as any).restartPod = vi.fn();
   (window as any).removePod = vi.fn();
+  (window as any).kubernetesGetCurrentNamespace = kubernetesGetCurrentNamespaceMock;
+  (window as any).kubernetesReadNamespacedPod = kubernetesReadNamespacedPodMock;
 
   listContainersMock.mockResolvedValue([
     { Id: 'pod', Ports: [{ PublicPort: 8080 } as Port] as Port[] } as ContainerInfo,
   ]);
 
+  kubernetesGetCurrentNamespaceMock.mockResolvedValue('ns');
+  kubernetesReadNamespacedPodMock.mockResolvedValue({ metadata: { labels: { app: 'foo' } } });
+
   (window as any).getContributedMenus = getContributedMenusMock;
-  getContributedMenusMock.mockImplementation(() => Promise.resolve([]));
+  getContributedMenusMock.mockResolvedValue([]);
 });
 
 afterEach(() => {
@@ -58,59 +73,112 @@ afterEach(() => {
 test('Expect no error and status starting pod', async () => {
   listContainersMock.mockResolvedValue([]);
 
-  const { component } = render(PodActions, { pod });
+  const { component } = render(PodActions, { pod: podmanPod });
   component.$on('update', updateMock);
 
   // click on start button
   const startButton = screen.getByRole('button', { name: 'Start Pod' });
   await fireEvent.click(startButton);
 
-  expect(pod.status).toEqual('STARTING');
-  expect(pod.actionError).toEqual('');
+  expect(podmanPod.status).toEqual('STARTING');
+  expect(podmanPod.actionError).toEqual('');
   expect(updateMock).toHaveBeenCalled();
 });
 
 test('Expect no error and status stopping pod', async () => {
   listContainersMock.mockResolvedValue([]);
 
-  const { component } = render(PodActions, { pod });
+  const { component } = render(PodActions, { pod: podmanPod });
   component.$on('update', updateMock);
 
   // click on stop button
   const stopButton = screen.getByRole('button', { name: 'Stop Pod' });
   await fireEvent.click(stopButton);
 
-  expect(pod.status).toEqual('STOPPING');
-  expect(pod.actionError).toEqual('');
+  expect(podmanPod.status).toEqual('STOPPING');
+  expect(podmanPod.actionError).toEqual('');
   expect(updateMock).toHaveBeenCalled();
 });
 
 test('Expect no error and status restarting pod', async () => {
   listContainersMock.mockResolvedValue([]);
 
-  const { component } = render(PodActions, { pod });
+  const { component } = render(PodActions, { pod: podmanPod });
   component.$on('update', updateMock);
 
   // click on restart button
   const restartButton = screen.getByRole('button', { name: 'Restart Pod' });
   await fireEvent.click(restartButton);
 
-  expect(pod.status).toEqual('RESTARTING');
-  expect(pod.actionError).toEqual('');
+  expect(podmanPod.status).toEqual('RESTARTING');
+  expect(podmanPod.actionError).toEqual('');
   expect(updateMock).toHaveBeenCalled();
 });
 
 test('Expect no error and status deleting pod', async () => {
   listContainersMock.mockResolvedValue([]);
 
-  const { component } = render(PodActions, { pod });
+  const { component } = render(PodActions, { pod: podmanPod });
   component.$on('update', updateMock);
 
   // click on delete button
   const deleteButton = screen.getByRole('button', { name: 'Delete Pod' });
   await fireEvent.click(deleteButton);
 
-  expect(pod.status).toEqual('DELETING');
-  expect(pod.actionError).toEqual('');
+  expect(podmanPod.status).toEqual('DELETING');
+  expect(podmanPod.actionError).toEqual('');
   expect(updateMock).toHaveBeenCalled();
+});
+
+test('Expect kubernetes route to be displayed', async () => {
+  const routeName = 'route.name';
+  const routeHost = 'host.local';
+  const openExternalSpy = vi.fn();
+
+  (window as any).kubernetesListRoutes = function () {
+    return { items: [{ metadata: { labels: { app: 'foo' }, name: routeName }, spec: { host: routeHost } }] };
+  };
+  (window as any).openExternal = openExternalSpy;
+
+  render(PodActions, { pod: kubernetesPod });
+
+  const openRouteButton = await screen.findByRole('button', { name: `Open ${routeName}` });
+  expect(openRouteButton).toBeVisible();
+
+  await fireEvent.click(openRouteButton);
+
+  expect(openExternalSpy).toHaveBeenCalledWith(`http://${routeHost}`);
+});
+
+test('Expect kubernetes route to be displayed but disabled', async () => {
+  (window as any).kubernetesListRoutes = function () {
+    return { items: [] };
+  };
+
+  render(PodActions, { pod: kubernetesPod });
+
+  const openRouteButton = await screen.findByRole('button', { name: `Open Browser` });
+  expect(openRouteButton).toBeVisible();
+  expect(openRouteButton).toBeDisabled();
+});
+
+test('Expect kubernetes routes kebab menu to be displayed', async () => {
+  (window as any).kubernetesListRoutes = function () {
+    return {
+      items: [
+        { metadata: { labels: { app: 'foo' }, name: 'route1.name' }, spec: { host: 'host1.local' } },
+        { metadata: { labels: { app: 'foo' }, name: 'route2.name' }, spec: { host: 'host2.local' } },
+      ],
+    };
+  };
+
+  render(PodActions, { pod: kubernetesPod });
+
+  const openRouteButton = await screen.findByRole('button', { name: 'Open Kubernetes Routes' });
+  expect(openRouteButton).toBeVisible();
+
+  await fireEvent.click(openRouteButton);
+
+  const routesDropDownMenu = await screen.findByTitle('Drop Down Menu Items');
+  expect(routesDropDownMenu).toBeVisible();
 });
