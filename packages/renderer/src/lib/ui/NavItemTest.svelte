f1d18b237f6d0f7c783b681cb8bf89e93c0b846e
Tim deBoer <git@tdeboer.ca>
2024-01-22 14:35:45 -0500

    chore: improve nav sections
    Improving a number of issues with the new nav sections. Here are the
    changes in behaviour/UI:
    - Webviews are added to the same section as the Docker extensions,
      and it is renamed to just Extensions. (This reduces the point of
      separating Webviews.svelte a bit - maybe future chore to have an
      an Extensions.svelte for both?)
    - If there are no items, the section is not shown.
    - Sections are now expanded by default, but collapsed by default
      if there are more than 3 items. (this is arbitrary, but 4+ felt
      like a lot to me)
    - If there is only one item, the button to collapse is greyed out
      and disabled. I tried it without this, but even after you fix
      the height issue IMHO it was really confusing what it is doing
      - 'why are you giving me a button to hide the nav item??'
      This felt like the right balance between showing that
      there is a section and what it is (you can still hover over the
      collapse button to see the tooltip) without confusing users.
    
    Technical notes:
    - The only way to count the number of items in a section I had to
      use the context. The nice side benefit of this is that we no
      longer need to tell NavItems that they are inSection - if the
      context exists, they are.
    - Why 'expanded && $count < 2'? Two reasons:
      1) when you collapse the item count (correctly) goes to zero
         and you always want to be able to expand.
      2) if there is one item and a second is added dynamically.
         the collapse button/styling will be removed.
    
    Fixes #5618.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/ui/NavItemTest.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import type { TinroRouteMeta } from 'tinro';
+import NavItem from './NavItem.svelte';
+import { setContext } from 'svelte';
+import { writable } from 'svelte/store';
+
+let meta: TinroRouteMeta = { url: '/test' } as TinroRouteMeta;
+
+// define context so that item thinks we're in a section
+setContext('nav-items', writable(0));
+</script>
+
+<NavItem tooltip="Dashboard" href="/test" bind:meta="{meta}"></NavItem>
