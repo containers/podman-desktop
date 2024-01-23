89c3b2a4c1202b466ee842b41b81530e42d28952
Florent Benoit <fbenoit@redhat.com>
2024-01-23 13:04:10 +0100

    chore: polish for platform selection
    fixes https://github.com/containers/podman-desktop/issues/5601
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/image/BuildImageFromContainerfileCard.svelte
+++ packages/renderer/src/lib/image/BuildImageFromContainerfileCard.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import Fa from 'svelte-fa';
 import Checkbox from '../ui/Checkbox.svelte';
 import Tooltip from '../ui/Tooltip.svelte';
@@ -10,7 +10,7 @@ export let badge: string = '';
 export let isDefault: boolean = false;
 export let checked: boolean = false;
 export let value: string = '';
-export let icon: any;
+export let icon: any | undefined = '';
 let iconType: 'fontAwesome' | 'unknown' | undefined = undefined;
 
 export let additionalItem: boolean = false;
@@ -19,6 +19,8 @@ let additionalValue: string = '';
 let displayValueFieldInput = false;
 
 let inputHtmlElement: HTMLInputElement | undefined;
+
+let badges: string[] = [];
 
 const dispatch = createEventDispatcher();
 
@@ -60,6 +62,13 @@ onMount(() => {
     iconType = 'unknown';
   }
 
+  if (badge) {
+    badges = [badge];
+  }
+  if (isDefault) {
+    badges.push('Default');
+  }
+
   if (isDefault) {
     dispatch('card', { mode: 'add', value: value });
   }
@@ -67,46 +76,49 @@ onMount(() => {
 </script>
 
 <button
-  class="rounded-md p-2 min-w-32 w-32 cursor-pointer hover:bg-charcoal-700 {checked
+  class="rounded-md bg-charcoal-700 p-2 min-w-48 w-48 min-h-24 cursor-pointer hover:bg-charcoal-500 {checked
     ? 'border-dustypurple-700'
-    : 'border-gray-700'} border-2 flex flex-row"
+    : 'border-charcoal-700'} border-2 flex flex-col"
   aria-label="{value}"
   on:click|preventDefault="{() => handleClick()}">
-  <div>
-    {#if !additionalItem}
-      <Checkbox bind:checked="{checked}" on:click="{() => handleClick()}" />
-    {:else}
-      <div>&nbsp;</div>
-    {/if}
-  </div>
-  <div class="mr-2 text-gray-700">
-    <div class="flex flex-row">
-      <div class="ml-1">
-        {#if iconType === 'fontAwesome'}
-          <Fa class="text-gray-700 cursor-pointer" icon="{icon}" size="24" />
-        {:else if iconType === 'unknown'}
-          <svelte:component this="{icon}" class="text-gray-700 cursor-pointer" size="24" />
-        {/if}
-      </div>
-
-      {#if isDefault}
-        <Tooltip tip="Default platform of your computer">
-          <Fa size="14" class="text-dustypurple-700 cursor-pointer" icon="{faSun}" />
-        </Tooltip>
+  <div class="flex flex-row">
+    <div class="flex flex-col">
+      {#if !additionalItem}
+        <Checkbox bind:checked="{checked}" on:click="{() => handleClick()}" />
+      {:else}
+        <Fa class="text-dustypurple-700 cursor-pointer" icon="{faPlusCircle}" size="24" />
       {/if}
     </div>
-    <div class="text-sm text-left truncate w-24">{title}</div>
+    <div class="ml-2 text-sm text-left break-normal w-36">{title}</div>
+    {#if isDefault}
+      <Tooltip tip="Default platform of your computer">
+        <Fa size="7" class="text-dustypurple-700 cursor-pointer" icon="{faCircle}" />
+      </Tooltip>
+    {/if}
+  </div>
+  <div class="flex flex-row grow w-full mt-2 items-end">
     <div class="flex">
-      {#if badge}
-        <div class="bg-dustypurple-700 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded">{badge}</div>
+      {#if badges.length > 0}
+        {#each badges as badge}
+          <div class="text-gray-700 border-gray-700 border text-xs font-medium me-2 px-2.5 py-0.5 rounded-xl">
+            {badge}
+          </div>
+        {/each}
       {/if}
       {#if displayValueFieldInput}
         <input
           type="text"
-          class="w-24 outline-none text-sm bg-dustypurple-900 rounded-sm text-gray-700 placeholder-gray-700"
+          class="w-40 outline-none text-sm bg-dustypurple-900 rounded-xs text-gray-700 placeholder-gray-700"
           bind:value="{additionalValue}"
           bind:this="{inputHtmlElement}"
           on:keydown="{handleKeydownAdditionalField}" />
+      {/if}
+    </div>
+    <div class="flex grow justify-end">
+      {#if iconType === 'fontAwesome'}
+        <Fa class="text-white cursor-pointer" icon="{icon}" size="24" />
+      {:else if iconType === 'unknown'}
+        <svelte:component this="{icon}" class="text-gray-900 cursor-pointer" size="24" />
       {/if}
     </div>
   </div>
