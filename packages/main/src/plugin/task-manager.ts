19e90caae1f66eabc536d1cdd0e8ba2e293ac84f
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-25 15:30:43 +0100

    fix: relative path Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/main/src/plugin/task-manager.ts
+++ packages/main/src/plugin/task-manager.ts
@@ -19,7 +19,7 @@
 import type { ApiSenderType } from './api.js';
 import type { NotificationInfo } from './api/notification.js';
 import type { NotificationTask, StatefulTask, Task } from './api/task.js';
-import type { StatusBarRegistry } from '/@/plugin/statusbar/statusbar-registry.js';
+import type { StatusBarRegistry } from './statusbar/statusbar-registry.js';
 
 /**
  * Contribution manager to provide the list of external OCI contributions
