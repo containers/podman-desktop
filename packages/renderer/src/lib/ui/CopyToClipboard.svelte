a45791b8b51076ad4c6253563fcf102be022a113
Thiago Mendes <tzig@tutanota.de>
2024-01-26 13:11:25 +0100

    chore: add generic copy button with text
    Add a generic copy button to copy the displayed text to the clipboard.
    Signed-off-by: Thiago Mendes <tzig@tutanota.de>

---
+++ packages/renderer/src/lib/ui/CopyToClipboard.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Fa from 'svelte-fa';
+import Tooltip from './Tooltip.svelte';
+import { faPaste } from '@fortawesome/free-solid-svg-icons';
+
+export let clipboardData: string;
+export let title: string;
+
+function copyTextToClipboard() {
+  window.clipboardWriteText(clipboardData);
+}
+</script>
+
+<div class="float-right">
+  <Tooltip tip="Copy to Clipboard" bottom>
+    <button
+      title="Copy To Clipboard"
+      class="ml-5 {$$props.class || ''}"
+      aria-label="Copy To Clipboard"
+      on:click="{() => copyTextToClipboard()}">
+      <Fa icon="{faPaste}" />
+    </button>
+  </Tooltip>
+</div>
+<div class="mt-1 my-auto text-xs truncate {$$props.class || ''}" aria-label="{title} copy to clipboard" title="{title}">
+  {title}
+</div>
