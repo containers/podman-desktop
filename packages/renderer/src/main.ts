c758ab63d16ab475eb4d76f5e696afe08b0263f5
Florent Benoit <fbenoit@redhat.com>
2024-02-11 23:28:36 +0100

    chore: allow to handle svelte v5
    svelte v5 is no longer instantiating classes
    https://svelte-5-preview.vercel.app/docs/breaking-changes#components-are-no-longer-classes
    
    use code to be compliant with both v4 and v5
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,10 +1,17 @@
+import * as svelte from 'svelte';
 import Loader from './Loader.svelte';
 
 const target = document.getElementById('app');
 let app;
 if (target) {
-  app = new Loader({
-    target,
-  });
+  // handle svelte v5
+  if ((svelte as any).createRoot) {
+    app = (svelte as any).createRoot(Loader, { target });
+  } else {
+    // v4 usage
+    app = new Loader({
+      target,
+    });
+  }
 }
 export default app;
