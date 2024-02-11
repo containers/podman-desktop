191072adb4bbc08677fbbc4d81bda88030dc43c8
Florent Benoit <fbenoit@redhat.com>
2024-02-11 23:12:04 +0100

    chore: a table should not use tr without first a tbody
    you have this kind of error with svelte v5, so better to fix it now
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/container/ContainerDetailsSummary.svelte
+++ packages/renderer/src/lib/container/ContainerDetailsSummary.svelte
@@ -7,23 +7,25 @@ export let container: ContainerInfoUI;
 <div class="flex px-5 py-4 flex-col h-full overflow-auto">
   <div class="w-full">
     <table class="h-2">
-      <tr>
-        <td class="pr-2">Id</td>
-        <td>{container.shortId}</td>
-      </tr>
-      <tr class:hidden="{!container.command}">
-        <td class="pr-2">Command</td>
-        <td>{container.command}</td>
-      </tr>
-      <tr>
-        <td class="pr-2">State</td>
-        <td>{container.state}</td>
-      </tr>
-      <tr>
-        <td class="pr-2">Ports</td>
-        <td class:hidden="{container.hasPublicPort}">N/A</td>
-        <td class:hidden="{!container.hasPublicPort}">{container.portsAsString}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <td class="pr-2">Id</td>
+          <td>{container.shortId}</td>
+        </tr>
+        <tr class:hidden="{!container.command}">
+          <td class="pr-2">Command</td>
+          <td>{container.command}</td>
+        </tr>
+        <tr>
+          <td class="pr-2">State</td>
+          <td>{container.state}</td>
+        </tr>
+        <tr>
+          <td class="pr-2">Ports</td>
+          <td class:hidden="{container.hasPublicPort}">N/A</td>
+          <td class:hidden="{!container.hasPublicPort}">{container.portsAsString}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </div>
