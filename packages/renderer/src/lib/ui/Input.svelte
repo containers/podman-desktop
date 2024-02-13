b53f9d973d7036b5ea8b7cc837ca852d9618c44c
Tim deBoer <git@tdeboer.ca>
2024-02-13 14:52:00 -0500

    chore: input errors, use input component in run image
    Uses the Input component in the Run Image form. This is the first
    time that we have inputs with 'errors', so I've added support to
    Input for an error. When there's an error the purple changes to
    red, there's a red X on the right, and the error message is listed
    underneath the input.
    
    Another part of #3234.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/ui/Input.svelte
+++ packages/renderer/src/lib/ui/Input.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleExclamation, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { createEventDispatcher } from 'svelte';
 import Fa from 'svelte-fa';
 
@@ -11,6 +11,7 @@ export let readonly: boolean = false;
 export let required: boolean = false;
 export let clearable: boolean = false;
 export let disabled: boolean = false;
+export let error: string | undefined = undefined;
 
 export let element: HTMLInputElement | undefined = undefined;
 
@@ -29,45 +30,58 @@ async function onClear() {
 }
 </script>
 
-<div
-  class="flex flex-row items-center px-1 py-1 group bg-charcoal-500 border-[1px] border-charcoal-500 {$$props.class ||
-    ''}"
-  class:hover:bg-charcoal-900="{!readonly && !disabled}"
-  class:focus-within:bg-charcoal-900="{!readonly && !disabled}"
-  class:hover:rounded-md="{!readonly && !disabled}"
-  class:focus-within:rounded-md="{!readonly && !disabled}"
-  class:border-b-purple-500="{!readonly && !disabled}"
-  class:hover:border-purple-400="{!readonly && !disabled}"
-  class:focus-within:border-purple-500="{!readonly && !disabled}"
-  class:border-b-charcoal-100="{readonly || disabled}">
-  <slot name="left" />
-  <input
-    bind:this="{element}"
-    on:input
-    class="grow px-1 outline-0 bg-charcoal-500 text-sm placeholder:text-gray-700 overflow-hidden"
-    class:text-white="{!disabled}"
-    class:text-gray-700="{disabled}"
-    class:group-hover:bg-charcoal-900="{!readonly && !disabled}"
-    class:group-focus-within:bg-charcoal-900="{!readonly && !disabled}"
-    class:group-hover-placeholder:text-gray-900="{!readonly && !disabled}"
-    name="{name}"
-    type="text"
-    disabled="{disabled}"
-    readonly="{readonly}"
-    required="{required}"
-    placeholder="{placeholder}"
-    id="{id}"
-    aria-label="{$$props['aria-label']}"
-    aria-invalid="{$$props['aria-invalid']}"
-    bind:value="{value}" />
-  {#if clearable}
-    <button
-      class="px-1 cursor-pointer text-gray-700 group-hover:text-gray-900 group-focus-within:text-gray-900"
-      class:hidden="{!value || readonly || disabled}"
-      aria-label="clear"
-      on:click="{onClear}">
-      <Fa icon="{faXmark}" />
-    </button>
+<div class="flex flex-col w-full">
+  <div
+    class="flex flex-row w-full items-center px-1 py-1 group bg-charcoal-500 border-[1px] border-charcoal-500 {$$props.class ||
+      ''}"
+    class:hover:bg-charcoal-900="{!readonly && !disabled}"
+    class:focus-within:bg-charcoal-900="{!readonly && !disabled}"
+    class:hover:rounded-md="{!readonly && !disabled}"
+    class:focus-within:rounded-md="{!readonly && !disabled}"
+    class:border-b-purple-500="{!readonly && !disabled && !error}"
+    class:border-b-red-500="{!readonly && !disabled && error}"
+    class:hover:border-purple-400="{!readonly && !disabled && !error}"
+    class:hover:border-red-400="{!readonly && !disabled && error}"
+    class:focus-within:border-purple-500="{!readonly && !disabled && !error}"
+    class:focus-within:border-red-500="{!readonly && !disabled && error}"
+    class:border-b-charcoal-100="{readonly || disabled}">
+    <slot name="left" />
+    <input
+      bind:this="{element}"
+      on:input
+      class="grow px-1 outline-0 bg-charcoal-500 text-sm placeholder:text-gray-700 overflow-hidden"
+      class:text-white="{!disabled}"
+      class:text-gray-700="{disabled}"
+      class:group-hover:bg-charcoal-900="{!readonly && !disabled}"
+      class:group-focus-within:bg-charcoal-900="{!readonly && !disabled}"
+      class:group-hover-placeholder:text-gray-900="{!readonly && !disabled}"
+      name="{name}"
+      type="text"
+      disabled="{disabled}"
+      readonly="{readonly}"
+      required="{required}"
+      placeholder="{placeholder}"
+      id="{id}"
+      aria-label="{$$props['aria-label']}"
+      aria-invalid="{$$props['aria-invalid']}"
+      bind:value="{value}" />
+    {#if error}
+      <span class="px-1 text-red-500" aria-label="error">
+        <Fa icon="{faCircleExclamation}" />
+      </span>
+    {/if}
+    {#if clearable}
+      <button
+        class="px-1 cursor-pointer text-gray-700 group-hover:text-gray-900 group-focus-within:text-gray-900"
+        class:hidden="{!value || readonly || disabled}"
+        aria-label="clear"
+        on:click="{onClear}">
+        <Fa icon="{faXmark}" />
+      </button>
+    {/if}
+    <slot name="right" />
+  </div>
+  {#if error && error.length > 0}
+    <span class="text-sm text-red-500">{error}</span>
   {/if}
-  <slot name="right" />
 </div>
