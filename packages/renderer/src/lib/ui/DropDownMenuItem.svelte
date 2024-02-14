e6a9f4058c03c0890f9f8757006bb14cbc13299c
Florent Benoit <fbenoit@redhat.com>
2024-02-14 10:13:15 +0100

    feat: allow to customize icons in contributed actions
    it's done by providing the icon font
    
    fixes https://github.com/containers/podman-desktop/issues/5562
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/ui/DropDownMenuItem.svelte
+++ packages/renderer/src/lib/ui/DropDownMenuItem.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import Fa from 'svelte-fa';
 
 export let title: string;
-export let icon: IconDefinition;
+export let icon: IconDefinition | string;
 export let enabled = true;
 export let hidden = false;
 export let onClick: () => void = () => {};
@@ -16,7 +16,11 @@ const disabledClasses = 'text-gray-900 bg-charcoal-800';
   <!-- Use a div + onclick so there's no "blind spots" for when clicking-->
   <div class="{`p-3 ${enabled ? enabledClasses : disabledClasses}`}" role="none" on:click="{onClick}">
     <span title="{title}" class="group flex items-center text-sm no-underline whitespace-nowrap" tabindex="-1">
-      <Fa class="h-4 w-4 text-md" icon="{icon}" />
+      {#if typeof icon === 'string'}
+        <span role="img" aria-label="{title}" class="{icon} h-4 w-4"></span>
+      {:else}
+        <Fa class="h-4 w-4 text-md" icon="{icon}" />
+      {/if}
       {#if title}<span class="ml-2">{title}</span>{/if}
     </span>
   </div>
