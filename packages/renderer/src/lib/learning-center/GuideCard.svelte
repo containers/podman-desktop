e01af8e7900f6506eb6116e3e5787cfcdab01492
Denis Golovin <dgolovin@redhat.com>
2024-02-14 23:41:04 -0800

    feat: send telemetry for guide opened form learning center
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/lib/learning-center/GuideCard.svelte
+++ packages/renderer/src/lib/learning-center/GuideCard.svelte
@@ -4,6 +4,13 @@ import Button from '../ui/Button.svelte';
 export let guide: Guide;
 export let width = 300;
 export let height = 300;
+
+async function openGuide(guide: Guide): Promise<void> {
+  await window.openExternal(guide.url);
+  window.telemetryTrack('openLearningCenterGuide', {
+    guideId: guide.id,
+  });
+}
 </script>
 
 <div
@@ -18,9 +25,7 @@ export let height = 300;
     <p class="line-clamp-4 px-4 pt-4 text-base text-gray-700">{guide.description}</p>
   </dif>
   <div class="flex justify-center items-end flex-1 pt-4">
-    <Button
-      class="justify-self-center self-end text-md"
-      on:click="{() => window.openExternal(guide.url)}"
-      title="Get started">Get started</Button>
+    <Button class="justify-self-center self-end text-md" on:click="{() => openGuide(guide)}" title="Get started"
+      >Get started</Button>
   </div>
 </div>
