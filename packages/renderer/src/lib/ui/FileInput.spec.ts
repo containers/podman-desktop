cddb572c887ea8c8fdac6d74e569ea5664ceaacc
Tim deBoer <git@tdeboer.ca>
2024-02-15 16:10:07 -0500

    feat: file input
    We have Inputs next to Browse buttons in many places, all with slight
    variations in UI or browsing. This creates a new FileInput component that
    uses a regular Input component with a folder icon on the right to browse
    for files. Exposes OpenDialogOptions to make it easy to customize what
    the file dialog should filter for.
    
    Uses the new FileInput for file/folder preferences so that there's something
    to test. There are several other file/browse buttons elsewhere, but they
    would be migrated separately. Likely also a candidate for svelte-ui package,
    but that should be after we become comfortable with/use it.
    
    Fixes #7197.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/ui/FileInput.spec.ts
@@ -0,0 +1,60 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+
+import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
+import { beforeAll, expect, test, vi } from 'vitest';
+
+import FileInput from './FileInput.svelte';
+
+const openDialogMock = vi.fn();
+
+beforeAll(() => {
+  (window as any).openDialog = openDialogMock;
+});
+
+test('Expect clicking the button opens file dialog', async () => {
+  openDialogMock.mockResolvedValue({ canceled: true });
+
+  render(FileInput, {});
+
+  const browseButton = screen.getByRole('button');
+  expect(browseButton).toBeInTheDocument();
+  await userEvent.click(browseButton);
+
+  expect(openDialogMock).toHaveBeenCalled();
+});
+
+test('Expect value to change when selecting via file dialog', async () => {
+  const filename = 'somefile';
+  openDialogMock.mockResolvedValue([filename]);
+
+  render(FileInput, {});
+
+  const browseButton = screen.getByRole('button');
+  expect(browseButton).toBeInTheDocument();
+  await userEvent.click(browseButton);
+
+  expect(openDialogMock).toHaveBeenCalled();
+
+  const input = screen.getByRole('textbox');
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveValue(filename);
+});
---
+++ packages/renderer/src/lib/ui/FileInput.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import type { OpenDialogOptions } from '@podman-desktop/api';
+import { Button, Input } from '@podman-desktop/ui-svelte';
+
+export let placeholder: string | undefined = undefined;
+export let id: string | undefined = undefined;
+export let name: string | undefined = undefined;
+export let value: string | undefined = undefined;
+export let options: OpenDialogOptions;
+export let readonly: boolean = false;
+export let required: boolean = false;
+
+async function openDialog() {
+  const result = await window.openDialog(options);
+  if (result?.[0]) {
+    value = result[0];
+  }
+}
+</script>
+
+<div class="flex flex-row grow space-x-1.5">
+  <Input
+    name="{name}"
+    id="{id}"
+    class="{$$props.class || ''}"
+    bind:value="{value}"
+    placeholder="{placeholder}"
+    readonly="{readonly}"
+    required="{required}"
+    aria-label="{$$props['aria-label']}"
+    aria-invalid="{$$props['aria-invalid']}">
+  </Input>
+  <Button aria-label="browse" icon="{faFolderOpen}" on:click="{openDialog}" />
+</div>
