6c66fbc6459ba7f063e3dc9ce510f7373d697e06
Jeff MAURY <jmaury@redhat.com>
2024-02-15 12:00:00 +0100

    fix: add definition for EndpointSettings
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- packages/extension-api/src/extension-api.d.ts
+++ packages/extension-api/src/extension-api.d.ts
@@ -2256,9 +2256,85 @@ declare module '@podman-desktop/api' {
     Retries?: number;
   }
 
+  interface EndpointIPAMConfig {
+    IPv4Address?: string;
+    IPv6Address?: string;
+    LinkLocalIPs?: string[];
+  }
+
+  interface EndpointSettings {
+    /**
+     * EndpointIPAMConfig represents an endpoint's IPAM configuration.
+     */
+    IPAMConfig?: EndpointIPAMConfig;
+
+    Links?: string[];
+
+    /**
+     * MAC address for the endpoint on this network. The network driver might ignore this parameter.
+     */
+    MacAddress?: string;
+
+    Aliases?: string[];
+
+    /**
+     * Unique ID of the network.
+     */
+    NetworkID?: string;
+
+    /**
+     * Unique ID for the service endpoint in a Sandbox.
+     */
+    EndpointID?: string;
+
+    /**
+     * Gateway address for this network.
+     */
+    Gateway?: string;
+
+    /**
+     * IPv4 address.
+     */
+    IPAddress?: string;
+
+    /**
+     * Mask length of the IPv4 address.
+     */
+    IPPrefixLen?: number;
+
+    /**
+     * IPv6 gateway address.
+     */
+    IPv6Gateway?: string;
+
+    /**
+     * Global IPv6 address.
+     */
+    GlobalIPv6Address?: string;
+
+    /**
+     * Mask length of the global IPv6 address.
+     */
+    GlobalIPv6PrefixLen?: number;
+
+    /**
+     * DriverOpts is a mapping of driver options and values. These options are passed directly to the driver and are driver specific.
+     */
+    DriverOpts?: { [key: string]: string };
+
+    /**
+     * List of all DNS names an endpoint has on a specific network. This list is based on the container name, network
+     * aliases, container short ID, and hostname.
+     *
+     * These DNS names are non-fully qualified but can contain several dots. You can get fully qualified DNS names by
+     * appending ```.<network-name>```. For instance, if container name is ```my.ctr``` and the network is named
+     * ```testnet```, ```DNSNames``` will contain ```my.ctr``` and the FQDN will be ```my.ctr.testnet```.
+     */
+    DNSNames?: string[];
+  }
+
   interface NetworkingConfig {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    EndpointsConfig?: { [key: string]: {} };
+    EndpointsConfig?: { [key: string]: EndpointSettings };
   }
 
   export interface PodmanContainerCreateOptions {
