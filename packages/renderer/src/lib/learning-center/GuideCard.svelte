32ce4bc1114a5b40a549b0fa3cb686dc1c98581a
Denis Golovin <dgolovin@redhat.com>
2024-02-19 16:47:37 -0800

    fix: add GuideCard component tests
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/lib/learning-center/GuideCard.svelte
+++ packages/renderer/src/lib/learning-center/GuideCard.svelte
@@ -15,15 +15,15 @@ async function openGuide(guide: Guide): Promise<void> {
 
 <div
   class="flex flex-col flex-1 bg-charcoal-600 pb-4 rounded-lg hover:bg-zinc-700 min-w-[{width}px] min-h-[{height}px]">
-  <dif class="pt-4 flex flex-col">
+  <div class="pt-4 flex flex-col">
     <div class="px-4">
       <img src="{`data:image/png;base64,${guide.icon}`}" class="h-[48px]" alt="{guide.id}" />
     </div>
     <div class="px-4 pt-4 text-nowrap text-gray-400">
       {guide.title}
     </div>
-    <p class="line-clamp-4 px-4 pt-4 text-base text-gray-700">{guide.description}</p>
-  </dif>
+    <div class="line-clamp-4 px-4 pt-4 text-base text-gray-700">{guide.description}</div>
+  </div>
   <div class="flex justify-center items-end flex-1 pt-4">
     <Button class="justify-self-center self-end text-md" on:click="{() => openGuide(guide)}" title="Get started"
       >Get started</Button>
