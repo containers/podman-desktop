ba3c648c1826104c7544dd867a77dd0ca3150885
Tim deBoer <git@tdeboer.ca>
2024-03-04 10:01:03 -0500

    chore: add test for hidden
    Add test to confirm that hidden buttons are not visible.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/ui/Button.spec.ts
+++ packages/renderer/src/lib/ui/Button.spec.ts
@@ -133,3 +133,13 @@ test('Check selected tab button styling', async () => {
   expect(button).toHaveClass('text-white');
   expect(button).toHaveClass('border-purple-500');
 });
+
+test('Check hidden button', async () => {
+  render(Button, { hidden: true });
+
+  // check that the button is in fact hidden
+  const button = screen.getByRole('button', { hidden: true });
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveAttribute('hidden');
+  expect(button).not.toBeVisible();
+});
