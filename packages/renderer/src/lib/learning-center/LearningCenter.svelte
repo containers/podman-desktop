34962bf312e72172e96c0f0969b0a56fffc39b47
Philippe Martin <phmartin@redhat.com>
2024-03-06 10:49:30 +0100

    fix: do not use Expandable component + chevrons Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/learning-center/LearningCenter.svelte
+++ packages/renderer/src/lib/learning-center/LearningCenter.svelte
@@ -3,9 +3,10 @@ import { onMount } from 'svelte';
 import Carousel from '../carousel/Carousel.svelte';
 import GuideCard from './GuideCard.svelte';
 import type { Guide } from '../../../../main/src/plugin/learning-center/learning-center-api';
-import Expandable from '../ui/Expandable.svelte';
+import { fadeSlide } from '../ui/animations';
 
 let guides: Guide[] = [];
+let expanded: boolean = true;
 
 onMount(async () => {
   guides = await window.listGuides();
@@ -13,9 +14,25 @@ onMount(async () => {
 </script>
 
 <div class="flex flex-1 flex-col bg-charcoal-800 p-5 rounded-lg">
-  <Expandable title="Learning Center">
-    <Carousel cards="{guides}" let:card>
-      <GuideCard guide="{card}" />
-    </Carousel>
-  </Expandable>
+  <div>
+    <button on:click="{() => (expanded = !expanded)}" class="">
+      <div class="flex flex-row space-x-2 items-center">
+        {#if expanded}
+          <i class="fas fa-chevron-down"></i>
+        {:else}
+          <i class="fas fa-chevron-right"></i>
+        {/if}
+        <span>Learning Center</span>
+      </div>
+    </button>
+  </div>
+  {#if expanded}
+    <div role="region" class="mt-5">
+      <div transition:fadeSlide="{{ duration: 500 }}">
+        <Carousel cards="{guides}" let:card>
+          <GuideCard guide="{card}" />
+        </Carousel>
+      </div>
+    </div>
+  {/if}
 </div>
