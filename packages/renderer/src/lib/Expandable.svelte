c546cd08c2fcab5e674e3863f27292badde16049
Philippe Martin <phmartin@redhat.com>
2024-03-06 04:45:47 +0100

    feat: add Expandable component Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/renderer/src/lib/Expandable.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { fadeSlide } from './ui/animations';
+
+export let title: string;
+export let expanded: boolean = true;
+</script>
+
+<div>
+  <button on:click="{() => (expanded = !expanded)}" class="">
+    <div class="flex flex-row space-x-2 items-center">
+      {#if expanded}
+        <i class="fas fa-chevron-up"></i>
+      {:else}
+        <i class="fas fa-chevron-down"></i>
+      {/if}
+      <span>{title}</span>
+    </div>
+  </button>
+</div>
+{#if expanded}
+  <div role="region" class="mt-5">
+    <div transition:fadeSlide="{{ duration: 500 }}">
+      <slot />
+    </div>
+  </div>
+{/if}
