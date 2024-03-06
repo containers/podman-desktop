2d3a652637a60c669879512fd3a9697c6c35e0b6
Philippe Martin <phmartin@redhat.com>
2024-03-06 05:00:10 +0100

    refactor: use Expandable for Carousel Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/learning-center/LearningCenter.svelte
+++ packages/renderer/src/lib/learning-center/LearningCenter.svelte
@@ -3,6 +3,7 @@ import { onMount } from 'svelte';
 import Carousel from '../carousel/Carousel.svelte';
 import GuideCard from './GuideCard.svelte';
 import type { Guide } from '../../../../main/src/plugin/learning-center/learning-center-api';
+import Expandable from '../Expandable.svelte';
 
 let guides: Guide[] = [];
 
@@ -11,11 +12,10 @@ onMount(async () => {
 });
 </script>
 
-<div class="flex flex-1 flex-col">
-  <p class="text-lg first-letter:uppercase font-bold pb-5">Learning center:</p>
-  <div class="flex flex-1 flex-col bg-charcoal-800 p-5 rounded-lg">
+<div class="flex flex-1 flex-col bg-charcoal-800 p-5 rounded-lg">
+  <Expandable title="Learning Center">
     <Carousel cards="{guides}" let:card>
       <GuideCard guide="{card}" />
     </Carousel>
-  </div>
+  </Expandable>
 </div>
