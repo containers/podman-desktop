83ca7250b0a9698e683c426db48892a1a7c4bde1
Florent Benoit <fbenoit@redhat.com>
2024-03-13 16:40:53 +0100

    chore: allow to provide other class parameters on the component
    usually we need to provide some classes to place the component as expected
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/ui/Badge.spec.ts
+++ packages/renderer/src/lib/ui/Badge.spec.ts
@@ -89,3 +89,13 @@ test('Should display badge with dark color', async () => {
   // check color being there in the style
   expect(label).toHaveStyle('background-color: rgb(0, 255, 0)');
 });
+
+test('Should display badge with custom class', async () => {
+  render(Badge, { label: 'customLabel', class: 'text-right' });
+
+  // expect to see label inside the label
+  const label = screen.getByText('customLabel');
+  expect(label).toBeInTheDocument();
+
+  await vi.waitFor(() => expect(label).toHaveClass('text-right'));
+});
