326ccba4cd1cc92b2432f94ea2942f94bb8b1e44
Florent Benoit <fbenoit@redhat.com>
2024-03-13 08:55:54 +0100

    chore: store combining PD and DD extensions
    fixes https://github.com/containers/podman-desktop/issues/6341
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/stores/all-installed-extensions.ts
@@ -0,0 +1,60 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+// Combine PD and DD extensions being installed
+import { derived, type Readable } from 'svelte/store';
+import { extensionInfos } from './extensions';
+import { contributions } from './contribs';
+import type { ExtensionInfo } from '../../../main/src/plugin/api/extension-info';
+
+export interface CombinedExtensionInfoUI extends ExtensionInfo {
+  type: 'dd' | 'pd';
+}
+
+export const combinedInstalledExtensions: Readable<CombinedExtensionInfoUI[]> = derived(
+  [contributions, extensionInfos],
+  ([$contributions, $extensionInfos]) => {
+    // Combine PD and DD extensions being installed
+
+    // add type to each extension
+    const pdExtensions: CombinedExtensionInfoUI[] = $extensionInfos.map(ext => ({ ...ext, type: 'pd' }));
+
+    // now convert the contributions to the same type
+    const ddExtensions: CombinedExtensionInfoUI[] = $contributions.map(ext => {
+      let displayName = ext.displayName;
+      if (!displayName) {
+        displayName = ext.name;
+      }
+      return {
+        ...ext,
+        displayName,
+        type: 'dd',
+        state: 'started',
+        removable: true,
+        path: ext.storagePath,
+        readme: ext.readme ?? '',
+      };
+    });
+
+    const allExtensions = [...pdExtensions, ...ddExtensions];
+
+    // sort by display name
+    allExtensions.sort((a, b) => a.displayName.localeCompare(b.displayName));
+    return allExtensions;
+  },
+);
