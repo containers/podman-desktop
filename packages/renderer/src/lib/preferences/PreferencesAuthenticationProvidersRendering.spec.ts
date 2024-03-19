01f6ba0ccffee6337006f61982e9e6ca42cfb930
Denis Golovin <dgolovin@users.noreply.github.com>
2024-03-19 05:40:24 -0700

    feat: add sign in button for auth providers w/ the only auth session request (#6446)
    * feat: add sign in button for auth providers w/ the only auth session request
    
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>

--- packages/renderer/src/lib/preferences/PreferencesAuthenticationProvidersRendering.spec.ts
+++ packages/renderer/src/lib/preferences/PreferencesAuthenticationProvidersRendering.spec.ts
@@ -43,7 +43,6 @@ test('Expect that page shows registered authentication providers without account
       id: 'test',
       displayName: 'Test Authentication Provider',
       accounts: [],
-      sessionRequests: [],
     },
   ]);
   render(PreferencesAuthenticationProvidersRendering, {});
@@ -89,7 +88,7 @@ test('Expect Sign Out button click calls window.requestAuthenticationProviderSig
   expect(requestSignOutMock).toBeCalledWith('test', 'test-account');
 });
 
-const testProividersInfoWithoutSessionRequests = [
+const testProvidersInfoWithoutSessionRequests = [
   {
     id: 'test',
     displayName: 'Test Authentication Provider',
@@ -98,14 +97,14 @@ const testProividersInfoWithoutSessionRequests = [
   },
 ];
 
-test('Expect Sign in menu item to be hidden when there are no session requests', async () => {
-  authenticationProviders.set(testProividersInfoWithoutSessionRequests);
+test('Expect Sign in button to be hidden when there are no session requests', async () => {
+  authenticationProviders.set(testProvidersInfoWithoutSessionRequests);
   render(PreferencesAuthenticationProvidersRendering, {});
   const menuButton = screen.queryAllByRole('button');
   expect(menuButton.length).equals(0); // no menu button
 });
 
-const testProividersInfoWithSessionRequests = [
+const testProvidersInfoWithSessionRequests = [
   {
     id: 'test',
     displayName: 'Test Authentication Provider',
@@ -122,26 +121,67 @@ const testProividersInfoWithSessionRequests = [
   },
 ];
 
-test('Expect Sign in menu item to be visible when there are session requests', async () => {
-  authenticationProviders.set(testProividersInfoWithSessionRequests);
-  render(PreferencesAuthenticationProvidersRendering, {});
-  const menuButton = screen.getAllByRole('button');
-  expect(menuButton.length).equals(1); // menu button
-  await fireEvent.click(menuButton[0]);
-  render(PreferencesAuthenticationProvidersRendering, {});
-  const menuItems = screen.getAllByText('Sign in to use Extension Label');
-  expect(menuItems.length).equals(1);
+test('Expect Sign In button to be visible when there is only one session request', async () => {
+  authenticationProviders.set(testProvidersInfoWithSessionRequests);
   const requestSignInMock = vi.fn();
   (window as any).requestAuthenticationProviderSignIn = requestSignInMock;
-  await fireEvent.click(menuItems[0]);
+  render(PreferencesAuthenticationProvidersRendering, {});
+  const menuButton = screen.getByRole('button');
+  const tooltip = screen.getByText('Sign in to use Extension Label');
+  expect(tooltip).toBeInTheDocument();
+  await fireEvent.click(menuButton);
   expect(requestSignInMock).toBeCalled();
 });
 
+const testProvidersInfoWithMultipleSessionRequests = [
+  {
+    id: 'test',
+    displayName: 'Test Authentication Provider',
+    accounts: [],
+    sessionRequests: [
+      {
+        id: 'ext:test1',
+        providerId: 'test',
+        extensionId: 'ext1',
+        extensionLabel: 'Extension1 Label',
+        scopes: ['scope1', 'scope2'],
+      },
+      {
+        id: 'ext:test2',
+        providerId: 'test',
+        extensionId: 'ext2',
+        extensionLabel: 'Extension2 Label',
+        scopes: ['scope1', 'scope2'],
+      },
+    ],
+  },
+];
+
+test('Expect Sign In popup menu to be visible when there is more than one session request', async () => {
+  authenticationProviders.set(testProvidersInfoWithMultipleSessionRequests);
+  (window as any).requestAuthenticationProviderSignIn = vi.fn();
+  render(PreferencesAuthenticationProvidersRendering, {});
+  const menuButton = screen.getByRole('button');
+  await fireEvent.click(menuButton);
+  // test sign in with extension1
+  const menuItem1 = screen.getByText('Sign in to use Extension1 Label');
+  const requestSignInMock = vi.fn();
+  (window as any).requestAuthenticationProviderSignIn = requestSignInMock;
+  await fireEvent.click(menuItem1);
+  expect(requestSignInMock).toBeCalledWith('ext:test1');
+  // test sign in with extension2
+  requestSignInMock.mockReset();
+  await fireEvent.click(menuButton);
+  const menuItem2 = screen.getByText('Sign in to use Extension2 Label');
+  await fireEvent.click(menuItem2);
+  expect(requestSignInMock).toBeCalledWith('ext:test2');
+});
+
 test('Expects default icon to be used when provider has no images option', async () => {
-  authenticationProviders.set(testProividersInfoWithSessionRequests);
+  authenticationProviders.set(testProvidersInfoWithSessionRequests);
   render(PreferencesAuthenticationProvidersRendering, {});
   screen.getByRole('img', {
-    name: `Default icon for ${testProividersInfoWithSessionRequests[0].displayName} provider`,
+    name: `Default icon for ${testProvidersInfoWithSessionRequests[0].displayName} provider`,
   });
 });
 
@@ -159,7 +199,7 @@ test('Expects images.icon option to be used when no themes are present', () => {
   ];
   authenticationProviders.set(providerWithImageIcon);
   render(PreferencesAuthenticationProvidersRendering, {});
-  screen.getByRole('img', { name: `Icon for ${testProividersInfoWithSessionRequests[0].displayName} provider` });
+  screen.getByRole('img', { name: `Icon for ${testProvidersInfoWithSessionRequests[0].displayName} provider` });
 });
 
 test('Expects images.icon.dark option to be used when themes are present', () => {
@@ -180,6 +220,6 @@ test('Expects images.icon.dark option to be used when themes are present', () =>
   authenticationProviders.set(providerWithImageIcon);
   render(PreferencesAuthenticationProvidersRendering, {});
   screen.getByRole('img', {
-    name: `Dark color theme icon for ${testProividersInfoWithSessionRequests[0].displayName} provider`,
+    name: `Dark color theme icon for ${testProvidersInfoWithSessionRequests[0].displayName} provider`,
   });
 });
