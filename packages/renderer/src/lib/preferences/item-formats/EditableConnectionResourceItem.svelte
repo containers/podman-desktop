135c4c356e1e278d70df724a865322bf0bbf096e
GLEF1X <glebgar567@gmail.com>
2024-03-30 22:04:01 -0400

    fix: corrected validation rules for disk size and memory Signed-off-by: GLEF1X <glebgar567@gmail.com>

--- packages/renderer/src/lib/preferences/item-formats/EditableConnectionResourceItem.svelte
+++ packages/renderer/src/lib/preferences/item-formats/EditableConnectionResourceItem.svelte
@@ -13,16 +13,20 @@ let recordValue: DisplayConfigurationValue | undefined;
 $: recordValue = getDisplayConfigurationValue(record, value);
 
 function onChangeInput(_recordId: string, _value: number) {
-  innerOnSave(_recordId, _value);
+  const displayConfigurationValue = getDisplayConfigurationValue(record, value);
+  if (displayConfigurationValue) {
+    onSave(_recordId, normalizeValue(_value));
+  }
 }
 
 function onCancel(_recordId: string, originalValue: number) {
-  innerOnSave(_recordId, originalValue);
+  onSave(_recordId, originalValue);
 }
 
 interface DisplayConfigurationValue {
   value: number;
   format?: string;
+  exponent?: number;
 }
 
 function getDisplayConfigurationValue(
@@ -31,13 +35,14 @@ function getDisplayConfigurationValue(
 ): DisplayConfigurationValue | undefined {
   if (configurationKey.format === 'memory' || configurationKey.format === 'diskSize') {
     const fileSizeItem = value
-      ? filesize(value as number)
-      : filesize(getNormalizedDefaultNumberValue(configurationKey));
-    const fileSizeItems = fileSizeItem.split(' ');
-    // the value returned consists of a number and its format (e.g 20 GB). We return the number as value and the format as description for the editableItem component
+      ? filesize(value as number, { output: 'object' })
+      : filesize(getNormalizedDefaultNumberValue(configurationKey), { output: 'object' });
+    // the value returned consists of a number and its format (e.g 20 GB) and its exponent(2 for MB, 3 for GB, etc).
+    // We return the number as value and the format as description for the editableItem component
     return {
-      value: Number(fileSizeItems[0]),
-      format: fileSizeItems[1],
+      value: Number(fileSizeItem.value),
+      format: fileSizeItem.symbol,
+      exponent: fileSizeItem.exponent,
     };
   } else if (configurationKey.format === 'cpu') {
     return {
@@ -48,56 +53,48 @@ function getDisplayConfigurationValue(
 }
 
 function getFileSizeValue(fileSizeItem: string): number {
-  return parseInt(fileSizeItem.split(' ')[0]);
+  return parseFloat(fileSizeItem.split(' ')[0]);
 }
 
 function normalizeDiskAndMemoryConfigurationKey(
   configurationKey: IConfigurationPropertyRecordedSchema,
+  recordValue?: DisplayConfigurationValue,
 ): IConfigurationPropertyRecordedSchema {
   const configurationKeyClone = Object.assign({}, configurationKey);
-  // if configurationKey is memory or diskSize let's convert the values in bytes to GB so, in case of errors, the message is displayed correctly to the user
-  // instad of having "the value must be less than 16000000000" will be ".... less than 16"
-  if (configurationKey.format === 'memory' || configurationKey.format === 'diskSize') {
+  // if configurationKey is memory or diskSize let's convert the values in bytes to the most appropriate unit so, in case of errors, the message is displayed correctly to the user
+  // instead of having "the value must be less than 16000000000" will be ".... less than 16"
+  if ((configurationKey.format === 'memory' || configurationKey.format === 'diskSize') && recordValue?.exponent) {
     configurationKeyClone.maximum =
       typeof configurationKey.maximum === 'number'
-        ? getFileSizeValue(filesize(configurationKey.maximum))
+        ? getFileSizeValue(filesize(configurationKey.maximum, { exponent: recordValue.exponent }))
         : configurationKey.maximum;
     configurationKeyClone.minimum = configurationKey.minimum
-      ? getFileSizeValue(filesize(configurationKey.minimum))
+      ? getFileSizeValue(filesize(configurationKey.minimum, { exponent: recordValue.exponent }))
       : configurationKey.minimum;
     configurationKeyClone.default =
       typeof configurationKey.maximum === 'number' && configurationKey.default
-        ? getFileSizeValue(filesize(configurationKey.default))
+        ? getFileSizeValue(filesize(configurationKey.default, { exponent: recordValue.exponent }))
         : configurationKey.default;
     return configurationKeyClone;
   }
   return configurationKey;
 }
 
-function innerOnSave(_recordId: string, _value: number) {
-  // convert value to byte to be saved
+function normalizeValue(originalValue: number): number {
   const displayConfigurationValue = getDisplayConfigurationValue(record, value);
-  if (displayConfigurationValue) {
-    switch (displayConfigurationValue.format) {
-      case 'GB': {
-        _value = _value * 1000 * 1000 * 1000;
-        break;
-      }
-      case 'MB': {
-        _value = _value * 1000 * 1000;
-        break;
-      }
-    }
-    onSave(_recordId, _value);
+  if (!displayConfigurationValue) {
+    return originalValue;
   }
+  return originalValue * Math.pow(1000, displayConfigurationValue.exponent || 0);
 }
 </script>
 
 {#if record && recordValue}
   <EditableItem
-    record="{normalizeDiskAndMemoryConfigurationKey(record)}"
+    record="{normalizeDiskAndMemoryConfigurationKey(record, recordValue)}"
     value="{recordValue.value}"
     description="{recordValue.format}"
     onCancel="{onCancel}"
-    onChange="{onChangeInput}" />
+    onChange="{onChangeInput}"
+    normalizeOriginalValue="{normalizeValue}" />
 {/if}
