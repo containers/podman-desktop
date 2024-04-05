1506b5f1a081f67e100fdd675be0ce72eb6ec816
emma kidney <ekidney@redhat.com>
2024-04-05 17:14:17 +0100

    chore: changing font sizes and weights in the provider card on the dashboard
    Signed-off-by: emma kidney <ekidney@redhat.com>

--- packages/renderer/src/lib/ui/ProviderStatus.svelte
+++ packages/renderer/src/lib/ui/ProviderStatus.svelte
@@ -12,7 +12,7 @@ interface connectionStatusStyle {
   | 'configured'
   | 'unknown';*/
 const roundIconStyle = 'my-auto w-3 h-3 rounded-full';
-const labelStyle = 'my-auto ml-1 font-bold text-[9px]';
+const labelStyle = 'my-auto ml-1 text-base font-medium';
 const statusesStyle = new Map<string, connectionStatusStyle>([
   [
     'ready',
