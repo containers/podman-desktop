506d59993321eda80c431c288cade39cf126ee86
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-04-08 16:25:11 +0200

    fix: remove code comment
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/recommendation/ExtensionBanners.svelte
+++ packages/renderer/src/lib/recommendation/ExtensionBanners.svelte
@@ -3,7 +3,6 @@ import ExtensionBanner from '/@/lib/recommendation/ExtensionBanner.svelte';
 import { extensionBannerInfos } from '/@/stores/extensionBanners';
 </script>
 
-<span>length {$extensionBannerInfos.length}</span>
 {#each $extensionBannerInfos as banner (banner.extensionId)}
   <ExtensionBanner banner="{banner}" />
 {/each}
