ebd0d7d64de445ac3f35a997531fe74df4279c63
Charlie Drage <charlie@charliedrage.com>
2024-04-08 14:05:53 -0400

    chore: add isManifest function to determine a manifest
    ### What does this PR do?
    
    Adds a guessIsManifest function to try and determine if an image is a
    manifest or not:
    
    - By checking the repotags and digests
    - Checking labels does not exist, as labels are not added when creating
      a manifest
    - VirtualSize is under 1MB as a manifest list is just text that points
      to other images
    
    We are unable to check the history (manifest commonly has no history) as
    Dockerode API does not support getting the history of the image.
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    N/A
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes  https://github.com/containers/podman-desktop/issues/6690
    
    ### How to test this PR?
    
    <!-- Please explain steps to verify the functionality,
    do not forget to provide unit/component tests -->
    
    - [X] Tests are covering the bug fix or the new feature
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

---
+++ packages/main/src/plugin/util/manifest.spec.ts
@@ -0,0 +1,120 @@
+/**********************************************************************
+ *
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import { describe, expect, test } from 'vitest';
+
+import type { ImageInfo } from '../api/image-info.js';
+import { guessIsManifest } from './manifest.js';
+
+describe('guessIsManifest function', () => {
+  test('correctly identifies a manifest image', () => {
+    const manifestImage: ImageInfo = {
+      Id: 'manifestImage',
+      Labels: {},
+      engineId: 'engine1',
+      engineName: 'podman',
+      ParentId: '',
+      RepoTags: ['manifestTag'],
+      RepoDigests: ['manifestDigest'],
+      Created: 0,
+      Size: 0,
+      VirtualSize: 500000, // 500KB, less than the 1MB threshold
+      SharedSize: 0,
+      Containers: 0,
+    };
+
+    expect(guessIsManifest(manifestImage)).toBe(true);
+  });
+
+  test('returns false if VirtualSize is over 1MB', () => {
+    const largeImage: ImageInfo = {
+      Id: 'largeImage',
+      Labels: {},
+      engineId: 'engine2',
+      engineName: 'podman',
+      ParentId: '',
+      RepoTags: ['largeImageTag'],
+      RepoDigests: ['largeImageDigest'],
+      Created: 0,
+      Size: 0,
+      VirtualSize: 2000000, // 2MB
+      SharedSize: 0,
+      Containers: 0,
+    };
+
+    expect(guessIsManifest(largeImage)).toBe(false);
+  });
+
+  test('returns false if Labels is not empty', () => {
+    const labeledImage: ImageInfo = {
+      Id: 'labeledImage',
+      Labels: { key: 'value' },
+      engineId: 'engine3',
+      engineName: 'podman',
+      ParentId: '',
+      RepoTags: ['labeledImageTag'],
+      RepoDigests: ['labeledImageDigest'],
+      Created: 0,
+      Size: 0,
+      VirtualSize: 500000, // 500KB
+      SharedSize: 0,
+      Containers: 0,
+    };
+
+    expect(guessIsManifest(labeledImage)).toBe(false);
+  });
+
+  test('returns false if RepoTags is undefined or empty', () => {
+    const noTagImage: ImageInfo = {
+      Id: 'noTagImage',
+      Labels: {},
+      engineId: 'engine4',
+      engineName: 'podman',
+      ParentId: '',
+      RepoTags: [],
+      RepoDigests: ['noTagImageDigest'],
+      Created: 0,
+      Size: 0,
+      VirtualSize: 500000, // 500KB
+      SharedSize: 0,
+      Containers: 0,
+    };
+
+    expect(guessIsManifest(noTagImage)).toBe(false);
+  });
+
+  test('returns false if RepoDigests is undefined or empty', () => {
+    const noDigestImage: ImageInfo = {
+      Id: 'noDigestImage',
+      Labels: {},
+      engineId: 'engine5',
+      engineName: 'podman',
+      ParentId: '',
+      RepoTags: ['noDigestImageTag'],
+      RepoDigests: [],
+      Created: 0,
+      Size: 0,
+      VirtualSize: 500000, // 500KB
+      SharedSize: 0,
+      Containers: 0,
+    };
+
+    expect(guessIsManifest(noDigestImage)).toBe(false);
+  });
+});
---
+++ packages/main/src/plugin/util/manifest.ts
@@ -0,0 +1,47 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import type { ImageInfo } from '../api/image-info.js';
+
+const GUESSED_MANIFEST_SIZE = 1000000; // 1MB
+
+// Function to safely "guess" if the image is actually a manifest
+// IMPORTANT NOTE: 
+// This is because there is no clear way to now what a manifest
+// is in the Dockerode API / Podman API (yet). This is a workaround
+// until we have a better way to determine if an image is a manifest
+// See issue: https://github.com/containers/podman/issues/22184
+//
+// We will "safely" determine if an image is a manifest by checking:
+// - VirtualSize is under 1MB, as the manifest is usually very small and only contains text
+// - Labels is empty, as the manifest usually doesn't have any labels
+// - RepoTags always exists, as the manifest has a tag associated
+// - RepoDigests always exists as well, as the manifest has a digest associated
+// all of these conditions must be met to (safely) determine if the image is a manifest
+//
+// We will check this within ImageInfo
+export function guessIsManifest(image: ImageInfo): boolean {
+  return Boolean(
+    image.RepoTags &&
+      image.RepoDigests &&
+      image.RepoTags.length > 0 &&
+      image.RepoDigests.length > 0 &&
+      (!image.Labels || Object.keys(image.Labels).length === 0) &&
+      image.VirtualSize < GUESSED_MANIFEST_SIZE,
+  );
+}
