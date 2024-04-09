2d6ea98ef2d94549b2439ea5b2f3f321d399e21c
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-04-09 14:45:55 +0200

    fix(UI): moving extension banners above featured
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/dashboard/DashboardPage.svelte
+++ packages/renderer/src/lib/dashboard/DashboardPage.svelte
@@ -99,8 +99,8 @@ function getInitializationContext(id: string): InitializationContext {
           {/each}
         {/if}
         <LearningCenter />
-        <FeaturedExtensions />
         <ExtensionBanners />
+        <FeaturedExtensions />
       </div>
     </div>
   </div>
