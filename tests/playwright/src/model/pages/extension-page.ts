9321f73cf9ac37637d4b2068499f986d98251159
Vladimir Lazar <106525396+cbr7@users.noreply.github.com>
2024-04-16 13:44:03 +0200

    chore(test): incorrect elemented called on enable (#6807)
    Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/model/pages/extension-page.ts
+++ tests/playwright/src/model/pages/extension-page.ts
@@ -49,7 +49,7 @@ export class ExtensionPage extends SettingsPage {
   async enableExtension(): Promise<this> {
     if ((await this.status.innerText()) === 'ACTIVE') return this;
 
-    await this.disableButton.click();
+    await this.enableButton.click();
     await playExpect(this.status).toHaveText('ACTIVE');
     return this;
   }
