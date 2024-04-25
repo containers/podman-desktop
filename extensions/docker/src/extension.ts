bd18f7b4852f2e03c45526eb6d7a0b76b96d39db
Florent Benoit <fbenoit@redhat.com>
2024-04-25 11:56:03 +0200

    chore: update typescript-eslint to v7.7.1 Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- extensions/docker/src/extension.ts
+++ extensions/docker/src/extension.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2022 Red Hat, Inc.
+ * Copyright (C) 2022-2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -192,7 +192,7 @@ function initProvider(extensionContext: extensionApi.ExtensionContext): void {
   containerProviderConnection = {
     name: 'Docker',
     type: 'docker',
-    status: () => providerState,
+    status: (): extensionApi.ProviderConnectionStatus => providerState,
     endpoint: {
       socketPath,
     },
