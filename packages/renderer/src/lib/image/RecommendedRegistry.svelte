115f03e5230a72f6c05025eebfd281936c4cb71b
Florent Benoit <fbenoit@redhat.com>
2024-04-25 11:09:36 +0200

    fixup! feat: suggest extensions if building/pulling from some registries report errors Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/image/RecommendedRegistry.svelte
+++ packages/renderer/src/lib/image/RecommendedRegistry.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+import { handleNavigation } from '/@/navigation';
 import { recommendedRegistries } from '/@/stores/recommendedRegistries';
 
+import { NavigationPage } from '../../../../main/src/plugin/navigation/navigation-page';
 import type { RecommendedRegistry } from '../../../../main/src/plugin/recommendations/recommendations-api';
 import FeaturedExtensionDownload from '../featured/FeaturedExtensionDownload.svelte';
+import Button from '../ui/Button.svelte';
 
 export let imageError: string | undefined;
 
@@ -15,6 +18,10 @@ $: registriesFilteredByIds = $recommendedRegistries.filter(reg => imageName?.inc
 $: recommendedRegistriesToInstall = registriesFilteredByIds.filter(registry =>
   registry.errors.some(registryMatchingError => imageError?.includes(registryMatchingError)),
 );
+
+function goToAuthPage() {
+  handleNavigation(NavigationPage.AUTHENTICATION);
+}
 </script>
 
 {#each recommendedRegistriesToInstall as registry}
@@ -28,5 +35,9 @@ $: recommendedRegistriesToInstall = registriesFilteredByIds.filter(registry =>
         href="/extensions/details/{registry.extensionId}">{registry.name} extension</a>
       to manage {registry.id}
     </p>
+
+    {#if registry.isInstalled}
+      <Button on:click="{() => goToAuthPage()}">Sign in...</Button>
+    {/if}
   </div>
 {/each}
