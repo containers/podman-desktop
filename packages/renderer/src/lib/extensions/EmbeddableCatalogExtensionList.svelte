662a07f22197ce70b83b3f8c18695088e206b010
Florent BENOIT <fbenoit@redhat.com>
2024-04-25 11:34:15 +0200

    chore: add embeddable version of catalog list (#6935)
    * chore: add embeddable version of catalog list
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/extensions/EmbeddableCatalogExtensionList.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { derived, type Readable } from 'svelte/store';
+
+import { combinedInstalledExtensions } from '/@/stores/all-installed-extensions';
+import { catalogExtensionInfos } from '/@/stores/catalog-extensions';
+import { featuredExtensionInfos } from '/@/stores/featuredExtensions';
+
+import type { CatalogExtensionInfoUI } from './catalog-extension-info-ui';
+import CatalogExtensionList from './CatalogExtensionList.svelte';
+import { ExtensionsUtils } from './extensions-utils';
+
+// restricted category to display
+export let category: string | undefined = undefined;
+
+// show installed extensions
+export let showInstalled: boolean = true;
+
+const extensionsUtils = new ExtensionsUtils();
+
+const catalogExtensions: Readable<CatalogExtensionInfoUI[]> = derived(
+  [catalogExtensionInfos, featuredExtensionInfos, combinedInstalledExtensions],
+  ([$catalogExtensionInfos, $featuredExtensionInfos, $combinedInstalledExtensions]) => {
+    if (category) {
+      const filteredCategory = category;
+      $catalogExtensionInfos = $catalogExtensionInfos.filter(catalogExtension =>
+        catalogExtension.categories.includes(filteredCategory),
+      );
+    }
+    if (!showInstalled) {
+      $catalogExtensionInfos = $catalogExtensionInfos.filter(
+        catalogExtension =>
+          !$combinedInstalledExtensions.some(installedExtension => installedExtension.id === catalogExtension.id),
+      );
+    }
+
+    return extensionsUtils.extractCatalogExtensions(
+      $catalogExtensionInfos,
+      $featuredExtensionInfos,
+      $combinedInstalledExtensions,
+    );
+  },
+);
+</script>
+
+<div class="flex bg-charcoal-700 text-left">
+  <CatalogExtensionList catalogExtensions="{$catalogExtensions}" />
+</div>
