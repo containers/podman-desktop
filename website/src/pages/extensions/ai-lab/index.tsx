d264a1fdae5b570a452803ee3dbe6d84c043c7b6
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-02 14:24:41 +0200

    feat: adding extension ai lab page (#91)
    * feat: adding extension ai lab page
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * Update website/src/pages/extensions/ai-lab/index.tsx
    
    Co-authored-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>

---
+++ website/src/pages/extensions/ai-lab/index.tsx
@@ -0,0 +1,62 @@
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import React from 'react';
+import TailWindThemeSelector from '@site/src/components/TailWindThemeSelector';
+import { GradientText } from '@site/src/components/GradientText';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <Layout title={siteConfig.title} description="Extensibility">
+      <TailWindThemeSelector />
+
+      <section className="text-gray-900 dark:text-gray-700 body-font">
+        <div className="container mx-auto flex px-5 py-6 items-center justify-center flex-col">
+          <div className="text-center lg:w-2/3 w-full">
+            {/* header section */}
+            <h1 className="sm:text-4xl text-3xl lg:text-6xl mb-4 font-bold text-charcoal-500 dark:text-white">
+              Run{' '}
+              <GradientText gradientAngle={90} colorFrom="#7D2D79" colorTo="#6d48bf" content="Large Language Models" />{' '}
+              locally with Podman AI Lab
+            </h1>
+            <p>
+              Podman AI Lab is the easiest way to work with Large Language Models (LLMs) on your local developer workstation. Find a
+              catalog of recipes, leverage a curated list of open source models, experiment and compare the models. Get
+              ahead of the curve and take your development to new heights wth Podman AI Lab!
+            </p>
+            {/* buttons section */}
+            <div className="mt-4 flex justify-center items-center gap-x-4">
+              <Link
+                className="items-center mt-auto no-underline hover:no-underline inline-flex text-white hover:text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-500 rounded-xl text-md font-semibold"
+                to={'#'}>
+                <FontAwesomeIcon size="1x" icon={faRocket} className="mr-2" />
+                Get started
+              </Link>
+
+              <Link
+                className="items-center mt-auto no-underline hover:no-underline inline-flex text-charcoal-500 dark:text-white border-0 py-2 px-6 focus:outline-none text-md font-semibold"
+                to={'https://github.com/containers/podman-desktop-extension-ai-lab'}>
+                Learn more on GitHub
+                <FontAwesomeIcon size="1x" icon={faArrowRight} className="ml-2" />
+              </Link>
+            </div>
+
+            {/* Application image */}
+            <div className="my-12 from-purple-500 bg-gradient-to-r to-fuschia-500 rounded-xl">
+              <img
+                className="p-8"
+                src={useBaseUrl('img/extensions/ai-lab/model-service-details.png')}
+                alt={'AI-Lab Model service page'}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
