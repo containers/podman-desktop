607ceb29039a1969bc18de390fa107bc3c90d41b
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-02 12:24:51 +0200

    chore: bump @types/dockerode version to 3.3.29 (#7042)
    * chore: bump dockerode version to 3.3.29
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * Update yarn.lock
    
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * Update package.json
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: yarn lock
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: removing type
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>

--- packages/main/src/plugin/container-registry.ts
+++ packages/main/src/plugin/container-registry.ts
@@ -20,7 +20,7 @@ import * as crypto from 'node:crypto';
 import { EventEmitter } from 'node:events';
 import * as fs from 'node:fs';
 import path from 'node:path';
-import { type Stream, Writable } from 'node:stream';
+import { Writable } from 'node:stream';
 import { pipeline } from 'node:stream/promises';
 
 import type * as containerDesktopAPI from '@podman-desktop/api';
@@ -2344,7 +2344,7 @@ export class ContainerProviderRegistry {
         options.containerFile = options.containerFile.replace(/\\/g, '/');
       }
 
-      let streamingPromise: Stream;
+      let streamingPromise: NodeJS.ReadableStream;
       try {
         const buildOptions: ImageBuildOptions = {
           registryconfig,
@@ -2384,10 +2384,7 @@ export class ContainerProviderRegistry {
         if (options?.pull) {
           buildOptions.pull = options.pull;
         }
-        streamingPromise = (await matchingContainerProviderApi.buildImage(
-          tarStream,
-          buildOptions,
-        )) as unknown as Stream;
+        streamingPromise = await matchingContainerProviderApi.buildImage(tarStream, buildOptions);
       } catch (error: unknown) {
         console.log('error in buildImage', error);
         const errorMessage = error instanceof Error ? error.message : '' + error;
