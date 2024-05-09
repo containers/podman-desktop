5b738b008c457cd981cb23b192654e43564ca73d
Tim deBoer <git@tdeboer.ca>
2024-05-09 16:50:19 -0400

    chore: more use of slide toggle
    We have toggle switches in several places, but none of them were
    the same code. This updates the existing SlideToggle to support a
    few more cases (properties, aria, label on left or right) and adds
    colors for light mode. The component is then reused for Settings >
    Preferences and Settings > Proxies.
    
    I had to make a couple judgement calls to make things consistent:
    - The switch (circle) is bigger than before in Preferences.
    - The proxy text gets dimmer when disabled/off, to match what was
      happening in preferences.
    - I added missing hover states, making each one step brighter to
      match Buttons, except I made the off-hover state purple to match
      other controls to let you know you can click on it.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/preferences/item-formats/BooleanItem.svelte
+++ packages/renderer/src/lib/preferences/item-formats/BooleanItem.svelte
@@ -1,34 +1,29 @@
 <script lang="ts">
 import type { IConfigurationPropertyRecordedSchema } from '../../../../../main/src/plugin/configuration-registry';
+import SlideToggle from '../../ui/SlideToggle.svelte';
 
 export let record: IConfigurationPropertyRecordedSchema;
 export let checked = false;
 export let onChange = async (_id: string, _value: boolean) => {};
 let invalidEntry = false;
 
-function onInput(event: Event) {
+function onChecked(state: boolean) {
   invalidEntry = false;
-  const target = event.target as HTMLInputElement;
-  if (record.id && target.checked !== checked) {
-    onChange(record.id, target.checked).catch((_: unknown) => (invalidEntry = true));
+  if (record.id && state !== checked) {
+    onChange(record.id, state).catch((_: unknown) => (invalidEntry = true));
   }
 }
 </script>
 
-<label class="relative inline-flex items-center cursor-pointer">
-  <span class="text-xs {checked ? 'text-white' : 'text-gray-700'} mr-3">{checked ? 'Enabled' : 'Disabled'}</span>
-  <input
-    on:input="{onInput}"
-    class="sr-only peer"
-    bind:checked="{checked}"
-    name="{record.id}"
-    type="checkbox"
-    readonly="{!!record.readonly}"
-    disabled="{!!record.readonly}"
-    id="input-standard-{record.id}"
-    aria-invalid="{invalidEntry}"
-    aria-label="{record.description || record.markdownDescription}" />
-  <div
-    class="w-8 h-[20px] bg-gray-900 rounded-full peer peer-checked:after:translate-x-full after:bg-charcoal-600 after:content-[''] after:absolute after:top-[4px] after:left-[61px] after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-violet-600">
-  </div>
-</label>
+<SlideToggle
+  id="input-standard-{record.id}"
+  name="{record.id}"
+  left
+  bind:checked="{checked}"
+  on:checked="{event => onChecked(event.detail)}"
+  readonly="{!!record.readonly}"
+  disabled="{!!record.readonly}"
+  aria-invalid="{invalidEntry}"
+  aria-label="{record.description || record.markdownDescription}">
+  <span class="text-xs">{checked ? 'Enabled' : 'Disabled'}</span>
+</SlideToggle>
