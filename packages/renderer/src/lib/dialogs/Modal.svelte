44c5ffaea832c1b6a010e03f72e358afd9aa92e4
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-10 11:32:37 +0200

    chore: adding tests for modal svelte component
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/dialogs/Modal.svelte
+++ packages/renderer/src/lib/dialogs/Modal.svelte
@@ -31,8 +31,10 @@ if (previously_focused) {
 
 <svelte:window on:keydown="{handle_keydown}" />
 
-<button class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-60 bg-blend-multiply z-40" on:click="{close}"
-></button>
+<button
+  aria-label="close"
+  class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-60 bg-blend-multiply z-40"
+  on:click="{close}"></button>
 
 <div
   class="absolute top-1/2 left-1/2 z-50 rounded translate-x-[-50%] translate-y-[-50%] overflow-auto w-[calc(200vw-4em)] max-w-[42em] max-h-[calc(100vh-4em)]"
