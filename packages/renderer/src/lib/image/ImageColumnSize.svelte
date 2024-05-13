67863665bc94763115a2b9c19d465194114c4bd5
Tim deBoer <git@tdeboer.ca>
2024-05-13 15:40:56 -0400

    chore: replace image name/age with simplecolumn
    Remove these columns since they can be replaced by SimpleColumn.
    
    Fixes #7193.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/image/ImageColumnSize.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-import type { ImageInfoUI } from './ImageInfoUI';
-
-export let object: ImageInfoUI;
-</script>
-
-<div class="text-sm text-gray-700">
-  {object.humanSize}
-</div>
