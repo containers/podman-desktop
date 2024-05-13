d86e5ec7c2ff0c1f81a5b70113662e2a4a750122
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-13 09:53:11 +0200

    feat: migrate MessageBox to modal component
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/dialogs/MessageBox.svelte
+++ packages/renderer/src/lib/dialogs/MessageBox.svelte
@@ -5,6 +5,7 @@ import { Button, type ButtonType } from '@podman-desktop/ui-svelte';
 import { onDestroy, onMount } from 'svelte';
 import Fa from 'svelte-fa';
 
+import Modal from '/@/lib/dialogs/Modal.svelte';
 import CloseButton from '/@/lib/ui/CloseButton.svelte';
 
 import { tabWithinParent } from './dialog-utils';
@@ -126,49 +127,41 @@ function getButtonType(b: boolean): ButtonType {
 <svelte:window on:keydown="{handleKeydown}" />
 
 {#if display}
-  <!-- Create overlay-->
-  <div class="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-60 bg-blend-multiply h-full grid z-50">
-    <div
-      class="flex flex-col place-self-center w-[550px] rounded-xl bg-charcoal-800 shadow-xl shadow-black"
-      role="dialog"
-      aria-labelledby="{title}"
-      aria-label="{title}"
-      bind:this="{messageBox}">
-      <div class="flex items-center justify-between pl-4 pr-3 py-3 space-x-2 text-gray-400">
-        {#if type === 'error'}
-          <Fa class="h-4 w-4 text-red-500" icon="{faCircleExclamation}" />
-        {:else if type === 'warning'}
-          <Fa class="h-4 w-4 text-amber-400" icon="{faTriangleExclamation}" />
-        {:else if type === 'info'}
-          <div class="flex">
-            <Fa class="h-4 w-4 place-content-center" icon="{faCircle}" />
-            <Fa class="h-4 w-4 place-content-center -ml-4 mt-px text-xs" icon="{faInfo}" />
-          </div>
-        {:else if type === 'question'}
-          <Fa class="h-4 w-4" icon="{faCircleQuestion}" />
-        {/if}
-        <h1 class="grow text-lg font-bold capitalize">{title}</h1>
+  <Modal>
+    <div class="flex items-center justify-between pl-4 pr-3 py-3 space-x-2 text-gray-400">
+      {#if type === 'error'}
+        <Fa class="h-4 w-4 text-red-500" icon="{faCircleExclamation}" />
+      {:else if type === 'warning'}
+        <Fa class="h-4 w-4 text-amber-400" icon="{faTriangleExclamation}" />
+      {:else if type === 'info'}
+        <div class="flex">
+          <Fa class="h-4 w-4 place-content-center" icon="{faCircle}" />
+          <Fa class="h-4 w-4 place-content-center -ml-4 mt-px text-xs" icon="{faInfo}" />
+        </div>
+      {:else if type === 'question'}
+        <Fa class="h-4 w-4" icon="{faCircleQuestion}" />
+      {/if}
+      <h1 class="grow text-lg font-bold capitalize">{title}</h1>
 
-        <CloseButton on:click="{() => clickButton(cancelId >= 0 ? cancelId : undefined)}" />
-      </div>
-
-      <div class="max-h-80 overflow-auto">
-        <div class="px-10 py-4 text-sm text-gray-500 leading-5" aria-label="Dialog Message">{message}</div>
-
-        {#if detail}
-          <div class="px-10 pb-4 text-sm text-gray-500 leading-5" aria-label="Dialog Details">{detail}</div>
-        {/if}
-      </div>
-
-      <div class="px-5 py-5 mt-2 flex flex-row w-full justify-end space-x-5">
-        {#each buttonOrder as i}
-          {#if i === cancelId}
-            <Button type="link" aria-label="Cancel" on:click="{() => clickButton(i)}">Cancel</Button>
-          {:else}
-            <Button type="{getButtonType(defaultId === i)}" on:click="{() => clickButton(i)}">{buttons[i]}</Button>
-          {/if}
-        {/each}
-      </div>
+      <CloseButton on:click="{() => clickButton(cancelId >= 0 ? cancelId : undefined)}" />
     </div>
-  </div>
+
+    <div class="max-h-80 overflow-auto">
+      <div class="px-10 py-4 text-sm text-gray-500 leading-5" aria-label="Dialog Message">{message}</div>
+
+      {#if detail}
+        <div class="px-10 pb-4 text-sm text-gray-500 leading-5" aria-label="Dialog Details">{detail}</div>
+      {/if}
+    </div>
+
+    <div class="px-5 py-5 mt-2 flex flex-row w-full justify-end space-x-5">
+      {#each buttonOrder as i}
+        {#if i === cancelId}
+          <Button type="link" aria-label="Cancel" on:click="{() => clickButton(i)}">Cancel</Button>
+        {:else}
+          <Button type="{getButtonType(defaultId === i)}" on:click="{() => clickButton(i)}">{buttons[i]}</Button>
+        {/if}
+      {/each}
+    </div>
+  </Modal>
 {/if}
