1af435bdb56b55ab1db0b6d92409d917e8cdff11
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-16 08:25:05 +0200

    fix(website): adding recipes details
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- website/src/pages/extensions/ai-lab/index.tsx
+++ website/src/pages/extensions/ai-lab/index.tsx
@@ -1,12 +1,69 @@
 import Link from '@docusaurus/Link';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { faArrowRight, faRocket } from '@fortawesome/free-solid-svg-icons';
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import {
+  faArrowRight,
+  faFileLines,
+  faMagnifyingGlassChart,
+  faMessage,
+  faMicrophone,
+  faRocket,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { GradientText } from '@site/src/components/GradientText';
+import { getGradient, GradientText } from '@site/src/components/GradientText';
 import TailWindThemeSelector from '@site/src/components/TailWindThemeSelector';
 import Layout from '@theme/Layout';
 import React from 'react';
+
+interface Recipe {
+  id: string;
+  name: string;
+  content: string;
+  icon: IconDefinition;
+  colorFrom: string;
+  colorTo: string;
+  iconColor: string;
+}
+
+const RECIPES: Recipe[] = [
+  {
+    id: 'chatbot',
+    name: 'LLM Chatbot',
+    content: 'Experiment prompting locally',
+    icon: faMessage,
+    colorFrom: '#9bfacc',
+    colorTo: '#fff',
+    iconColor: '#36f8a7',
+  },
+  {
+    id: 'summarizer',
+    name: 'Text Summarizer',
+    content: 'Get insight on large text corpus',
+    icon: faFileLines,
+    colorFrom: '#9cf4fd',
+    colorTo: '#fff',
+    iconColor: '#2fecfc',
+  },
+  {
+    id: 'speech-to-text',
+    name: 'Speech to text',
+    content: 'Get transcript from any audio file',
+    icon: faMicrophone,
+    colorFrom: '#f4acce',
+    colorTo: '#fff',
+    iconColor: '#e54b95',
+  },
+  {
+    id: 'obj-detection',
+    name: 'Object Detection',
+    content: 'Get started with computer vision',
+    icon: faMagnifyingGlassChart,
+    colorFrom: '#fbe696',
+    colorTo: '#fff',
+    iconColor: '#fccf44',
+  },
+];
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
@@ -47,19 +104,47 @@ export default function Home(): JSX.Element {
             </div>
 
             {/* Application video */}
-            <div className="my-12 rounded-xl border-black border-8">
-              <video controls>
-                <source
-                  src="https://github.com/containers/podman-desktop-media/raw/ai-lab/videos/homepage/ai-lab-hero.mp4"
-                  type="video/mp4"
-                />
-                <img
-                  src={useBaseUrl('img/extensions/ai-lab/model-service-details.png')}
-                  alt={'AI-Lab Model service page'}
-                />
-              </video>
+            <div className="my-12 rounded-xl from-purple-500 bg-gradient-to-r to-fuschia-500">
+              <div className="p-8">
+                <video className="rounded-xl w-full h-full" controls>
+                  <source
+                    src="https://github.com/containers/podman-desktop-media/raw/ai-lab/videos/homepage/ai-lab-hero.mp4"
+                    type="video/mp4"
+                  />
+                  <img
+                    src={useBaseUrl('img/extensions/ai-lab/model-service-details.png')}
+                    alt={'AI-Lab Model service page'}
+                  />
+                </video>
+              </div>
             </div>
           </div>
+          {/* Recipes sections */}
+          <div className="space-y-3 mb-5 lg:w-2/3 w-full items-center justify-center flex flex-col">
+            <h2 className="sm:text-2xl text-3xl lg:text-4xl font-bold text-charcoal-500 dark:text-white text-center">
+              Experiment with free{' '}
+              <GradientText gradientAngle={90} colorFrom="#7D2D79" colorTo="#6d48bf" content="Open Source" /> recipes
+            </h2>
+            <div className="w-full grid grid-cols-2 gap-2 lg:grid-cols-4">
+              {RECIPES.map(recipe => (
+                <div
+                  className="px-8 py-4 rounded-xl from-purple-500 from-70% bg-gradient-to-t to-white flex flex-col items-center text-black text-center"
+                  style={{
+                    background: getGradient(recipe.colorFrom, recipe.colorTo),
+                    border: `1px solid ${recipe.colorFrom}`,
+                  }}
+                  key={recipe.id}>
+                  <FontAwesomeIcon className="mb-2" color={recipe.iconColor} size="lg" icon={recipe.icon} />
+                  <div className="text-xl font-black text-black">{recipe.name}</div>
+                  <div className="text-xs font-semibold text-charcoal-100 mb-2">{recipe.content}</div>
+                </div>
+              ))}
+            </div>
+            <h2 className="sm:text-1xl text-2xl lg:text-3xl mb-4 font-bold text-charcoal-500 dark:text-white text-center w-full">
+              All built to run <span className="text-bold">locally</span> on your laptop
+            </h2>
+          </div>
+          {/* Videos sections */}
           <div className="lg:w-2/3 w-full">
             <div className="space-y-8">
               <div className="grid grid-cols-2 gap-4">
