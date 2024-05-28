bce9439e0cea6540275ce930dbcad76739742f1d
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-28 11:45:26 +0200

    chore(ui): migrate star icon to ui package (#7322)
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/images/StatusIcon.svelte
+++ packages/renderer/src/lib/images/StatusIcon.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 import { Spinner } from '@podman-desktop/ui-svelte';
-
-import StarIcon from './StarIcon.svelte';
+import { StarIcon } from '@podman-desktop/ui-svelte/icons';
 
 // status: one of RUNNING, STARTING, USED, CREATED, DELETING, or DEGRADED
 // any other status will result in a standard outlined box
