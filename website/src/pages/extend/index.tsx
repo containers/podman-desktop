2451c782ee1ab4bd4d3a7c389cd43716b43248ac
Charlie Drage <charlie@charliedrage.com>
2024-05-28 18:48:44 -0400

    docs: update extend page with more links, updated info
    ### What does this PR do?
    
    * Updates the extend page with links to our main developing
      documentation, publishing, how to install, etc.
    * Fixes the paragraphs which auto-wrap, instead of the odd centering
    * Puts PD extension first, before docker provider.
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop/issues/5783
    
    ### How to test this PR?
    
    <!-- Please explain steps to verify the functionality,
    do not forget to provide unit/component tests -->
    
    N/A, view netlify
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- website/src/pages/extend/index.tsx
+++ website/src/pages/extend/index.tsx
@@ -1,5 +1,7 @@
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { faCertificate, faCloudArrowDown, faGears, faRocket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TailWindThemeSelector from '@site/src/components/TailWindThemeSelector';
 import Layout from '@theme/Layout';
 import ThemedImage from '@theme/ThemedImage';
@@ -16,38 +18,38 @@ export default function Home(): JSX.Element {
         <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
           <div className="text-center lg:w-2/3 w-full">
             <h1 className="title-font sm:text-4xl text-3xl lg:text-6xl mb-4 font-medium text-gray-900 dark:text-white">
-              Extend capabilities with Docker Desktop extensions.
+              Extensibility Documentation and Resources
+            </h1>
+            <p>Explore our comprehensive guides on extending Podman Desktop:</p>
+            <div className="text-center">
+              <ul className="list-none pl-0 mt-2">
+                <li>
+                  <FontAwesomeIcon icon={faRocket} className="mr-2" />
+                  <a href={useBaseUrl('/docs/extensions/developing')}>Developing an Extension</a>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCloudArrowDown} className="mr-2" />
+                  <a href={useBaseUrl('/docs/extensions/install')}>Installing an Extension</a>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCertificate} className="mr-2" />
+                  <a href={useBaseUrl('/docs/extensions/publish')}>Publishing an Extension</a>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faGears} className="mr-2" />
+                  <a href={useBaseUrl('/api')}>API Reference</a>
+                </li>
+              </ul>
+            </div>
+            <h1 className="mt-24 title-font sm:text-4xl text-3xl lg:text-6xl mb-4 font-medium text-gray-900 dark:text-white">
+              Extend capabilities with Podman Desktop extensions.
             </h1>
             <p>
-              Podman Desktop is able to use Docker Desktop UI plug-ins by adding a wrapper to intercept the API calls.
+              Podman Desktop leverages a variety of extensions to manage different container engines effectively.
+              Discover and add new extensions to adapt Podman Desktop to your specific needs, whether it's managing
+              containers or integrating new tools. This flexibility supports a broad range of container engines, such as
+              Podman, Docker, or Lima, enhancing your development workflow.
             </p>
-            <p>By adding a Docker Desktop extension, you can extend the capabilities of Podman Desktop.</p>
-
-            <ThemedImage
-              className="py-4"
-              alt="Extend with Docker Desktop extensions"
-              sources={{
-                light: useBaseUrl('img/extend/extend-dd-light.png'),
-                dark: useBaseUrl('img/extend/extend-dd-dark.png'),
-              }}
-            />
-
-            <ThemedImage
-              className="py-4"
-              alt="Reuse Docker Desktop extensions"
-              sources={{
-                light: useBaseUrl('img/ddextensions/dd-support.png'),
-                dark: useBaseUrl('img/ddextensions/dd-support.png'),
-              }}
-            />
-
-            <h1 className="mt-24 title-font sm:text-4xl text-3xl lg:text-6xl mb-4 font-medium text-gray-900 dark:text-white">
-              Extend capabilities with Podman Desktop plug-ins.
-            </h1>
-            <p>Podman Desktop is using plug-ins under the hood to manage the different container engine.</p>
-            <p>By adding a new plugin, you can extend the capabilities of Podman Desktop.</p>
-            <p>For example plug a new container engine like Podman, Docker, or use Lima.</p>
-
             <ThemedImage
               className="py-4"
               alt="Extensibility diagram"
@@ -57,16 +59,22 @@ export default function Home(): JSX.Element {
               }}
             />
 
-            <h3 className="pt-2 pb-24 font-semibold">
-              <div>Extensions are written in JavaScript/Typescript</div>
-              <div>It consumes a TypeScript definition of the extension model.</div>
-            </h3>
-
-            <h1 className="title-font sm:text-2xl text-2xl lg:text-3xl mb-4 font-medium text-gray-900 dark:text-white">
-              More extension points
+            <h1 className="mt-24 title-font sm:text-4xl text-3xl lg:text-6xl mb-4 font-medium text-gray-900 dark:text-white">
+              Extend capabilities with Docker Desktop extensions.
             </h1>
-            <p>Several extension points exists in addition to adding a Provider connection.</p>
-            <p>User interaction, dialogs, launching commands are among the possible ways to extend Podman Desktop</p>
+            <p>
+              Utilizing Docker Desktop UI extensions, Podman Desktop is equipped with a wrapper that intercepts API
+              calls, enabling seamless integration and extension of its capabilities. Add Docker Desktop extensions to
+              enhance the functionality of Podman Desktop with innovative features.
+            </p>
+            <ThemedImage
+              className="py-4"
+              alt="Extend with Docker Desktop extensions"
+              sources={{
+                light: useBaseUrl('img/extend/extend-dd-light.png'),
+                dark: useBaseUrl('img/extend/extend-dd-dark.png'),
+              }}
+            />
           </div>
         </div>
       </section>
