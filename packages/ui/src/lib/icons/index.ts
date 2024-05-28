0d8d9e0871502094c364f654e15eec36e1cd4d0a
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-28 14:27:32 +0200

    fix: weird file movement
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/ui/src/lib/icons/index.ts
+++ packages/ui/src/lib/icons/index.ts
@@ -16,18 +16,7 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import sveltePreprocess from 'svelte-preprocess';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import ContainerIcon from './ContainerIcon.svelte';
+import StarIcon from './StarIcon.svelte';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess({
-    postcss: {
-      configFilePath: join(__dirname, 'postcss.config.cjs'),
-    },
-  }),
-};
+export { ContainerIcon, StarIcon };
