58ae335dc99cc57bf4456aa4e864051fb2d96b20
Tim deBoer <git@tdeboer.ca>
2024-05-29 11:09:44 -0400

    feat: telemetry settings in json
    Move filtering of telemetry events to a json file so that it is easy to
    configure the filtering of events without changing code.
    
    Prior to this, 'list' events were hardcoded to be filtered to once per day per
    instance of Podman Desktop. Now, this setting is done via a JSON config file,
    and there are three options that we could use for other events:
    - disabled (true/false) - totally disable an event
    - ratio (0.0-1.0) - only send x% of this event
    - frequency - only supports 'dailyPerInstance' to start (for list events),
      but could support things like 'daily' or 'weekly' in the future.
    
    Part of #4970.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/main/src/plugin/telemetry/telemetry.spec.ts
+++ packages/main/src/plugin/telemetry/telemetry.spec.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2023 Red Hat, Inc.
+ * Copyright (C) 2023-2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,19 @@ const configurationRegistryMock = {
   onDidChangeConfiguration: onDidChangeConfigurationMock,
 } as unknown as ConfigurationRegistry;
 
+vi.mock('../../../../../telemetry.json', () => ({
+  default: {
+    rules: [
+      {
+        event: 'dropMe',
+        disabled: true,
+      },
+      { event: 'sometimes', ratio: 0.5 },
+      { event: 'list', frequency: 'dailyPerInstance' },
+    ],
+  },
+}));
+
 class TelemetryTest extends Telemetry {
   constructor() {
     super(configurationRegistryMock, {} as Proxy);
@@ -73,20 +86,37 @@ test('Should not filter out basic event', async () => {
   expect(telemetry.shouldDropEvent('basic')).toBeFalsy();
 });
 
-test('Should not filter out a list event if it is first time', async () => {
+test('Should not filter out a dailyPerInstance event if it is first time', async () => {
   expect(telemetry.shouldDropEvent('listFirstTime')).toBeFalsy();
 });
 
-test('Should filter out a list event if last event was < 24h', async () => {
+test('Should filter out a dailyPerInstance event if last event was < 24h', async () => {
   // last call was 23h ago
   telemetry.getLastTimeEvents().set('listSecondTime', Date.now() - 1000 * 60 * 60 * 23);
   expect(telemetry.shouldDropEvent('listSecondTime')).toBeTruthy();
 });
 
-test('Should not filter out a list event if last event was > 24h', async () => {
+test('Should not filter out a dailyPerInstance event if last event was > 24h', async () => {
   // last call was 25h ago, so it should not be filtered out
   telemetry.getLastTimeEvents().set('listVeryVeryOldime', Date.now() - 1000 * 60 * 60 * 25);
   expect(telemetry.shouldDropEvent('listVeryVeryOldime')).toBeFalsy();
+});
+
+test('Should filter out a disabled event', async () => {
+  expect(telemetry.shouldDropEvent('dropMe')).toBeTruthy();
+});
+
+test('Should filter out a ratio event sometimes', async () => {
+  // this test will flake once every 8x10^62 times. if you are *that* unlucky, sorry
+  let count = 0;
+  for (let i = 0; i < 100; i++) {
+    if (telemetry.shouldDropEvent('sometimes')) {
+      count++;
+    }
+  }
+
+  expect(count === 0).toBeFalsy();
+  expect(count === 100).toBeFalsy();
 });
 
 test('Check Telemetry is enabled', async () => {
