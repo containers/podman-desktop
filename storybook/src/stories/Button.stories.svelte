49083f2b8fab3bc2099952211547f98d48f03901
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-06-03 10:12:16 +0200

    chore(storybook): adding basic setup (#7329)
    * chore(storybook): adding basic setup
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: weird file movement
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: github comments
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix(Storybook): packages.json
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix(Storybook): config files
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: package version
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ storybook/src/stories/Button.stories.svelte
@@ -0,0 +1,68 @@
+<script context="module" lang="ts">
+import { faBell } from '@fortawesome/free-regular-svg-icons';
+import { Button } from '@podman-desktop/ui-svelte';
+import { Story, Template } from '@storybook/addon-svelte-csf';
+
+export const meta = {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs'],
+};
+</script>
+
+<Template let:args>
+  <Button {...args}>{args.content}</Button>
+</Template>
+
+<Story
+  name="Primary"
+  args="{{
+    type: 'primary',
+    content: 'primary',
+  }}" />
+
+<Story
+  name="Secondary"
+  args="{{
+    type: 'secondary',
+    content: 'secondary',
+  }}" />
+
+<Story
+  name="Primary"
+  args="{{
+    type: 'primary',
+    content: 'primary',
+  }}" />
+
+<Story
+  name="Danger"
+  args="{{
+    type: 'danger',
+    content: 'danger',
+  }}" />
+
+<Story
+  name="With icon"
+  args="{{
+    type: 'primary',
+    content: 'with icon',
+    icon: faBell,
+  }}" />
+
+<Story
+  name="Disabled"
+  args="{{
+    type: 'primary',
+    content: 'disabled',
+    disabled: true,
+  }}" />
+
+<Story
+  name="Loading"
+  args="{{
+    type: 'primary',
+    content: 'loading',
+    inProgress: true,
+    icon: faBell,
+  }}" />
