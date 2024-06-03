6254bed1c86286740bff6cf1bd41fd2679ab4e24
Charlie Drage <charlie@charliedrage.com>
2024-06-03 08:06:32 -0400

    chore: k8s deployments to say 'Progressed', sort columns
    ### What does this PR do?
    
    * Sorts the columns by condition (available should be first, then
    progressing, etc.)
    * Say progressed if it has progressed..
    * Updates tests so that they are individual cases against deploymentUI
      rather than using a single one.
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop/issues/7410
    Closes https://github.com/containers/podman-desktop/issues/7411
    
    ### How to test this PR?
    
    <!-- Please explain steps to verify the functionality,
    do not forget to provide unit/component tests -->
    
    - [X] Tests are covering the bug fix or the new feature
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/renderer/src/lib/deployments/DeploymentColumnConditions.svelte
+++ packages/renderer/src/lib/deployments/DeploymentColumnConditions.svelte
@@ -3,25 +3,31 @@ import { faCheckCircle, faExclamationTriangle, faQuestionCircle, faSync } from '
 import { Tooltip } from '@podman-desktop/ui-svelte';
 import Fa from 'svelte-fa';
 
-import type { DeploymentUI } from './DeploymentUI';
+import type { DeploymentCondition, DeploymentUI } from './DeploymentUI';
 
 export let object: DeploymentUI;
 
 // Determine both the icon and color based on the deployment condition
-function getConditionAttributes(type: string) {
-  switch (type) {
+function getConditionAttributes(condition: DeploymentCondition) {
+  let name = condition.type;
+  switch (condition.type) {
     case 'Available':
       // faCheckCircle: Indicates a successful state, typically used to denote availability and operational readiness
-      return { color: 'text-green-600', icon: faCheckCircle };
+      return { color: 'text-green-600', icon: faCheckCircle, name };
     case 'Progressing':
       // faSync: Often used to represent ongoing processes or operations, fitting for a "Progressing" state
-      return { color: 'text-sky-400', icon: faSync };
+      // If reason has NewReplicaSetAvailable then it's progressed
+      if (condition.reason === 'NewReplicaSetAvailable') {
+        name = 'Progressed';
+      }
+
+      return { color: 'text-sky-400', icon: faSync, name };
     case 'ReplicaFailure':
       // faExclamationTriangle: Alerts and warnings
-      return { color: 'text-amber-600', icon: faExclamationTriangle };
+      return { color: 'text-amber-600', icon: faExclamationTriangle, name };
     default:
       // faQuestionCircle: Uncertain / unknown
-      return { color: 'text-gray-900', icon: faQuestionCircle };
+      return { color: 'text-gray-900', icon: faQuestionCircle, name };
   }
 }
 </script>
@@ -33,9 +39,9 @@ function getConditionAttributes(type: string) {
         <div class="flex flex-row bg-charcoal-500 items-center p-1 rounded-md text-xs text-gray-500">
           <Fa
             size="1x"
-            icon="{getConditionAttributes(condition.type).icon}"
-            class="{getConditionAttributes(condition.type).color} mr-1" />
-          {condition.type}
+            icon="{getConditionAttributes(condition).icon}"
+            class="{getConditionAttributes(condition).color} mr-1" />
+          {getConditionAttributes(condition).name}
         </div>
       </svelte:fragment>
       <svelte:fragment slot="tip">
