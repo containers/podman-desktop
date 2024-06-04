f097d84974e5729329ba04f5fc85008dc26af897
Philippe Martin <phmartin@redhat.com>
2024-06-04 16:09:26 +0200

    feat: watch files on existing dirs only Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/main/src/plugin/kubernetes-client.ts
+++ packages/main/src/plugin/kubernetes-client.ts
@@ -189,10 +189,10 @@ export class KubernetesClient {
     const kubernetesConfiguration = this.configurationRegistry.getConfiguration('kubernetes');
     const userKubeconfigPath = kubernetesConfiguration.get<string>('Kubeconfig');
     if (userKubeconfigPath) {
+      this.kubeconfigPath = userKubeconfigPath;
       this.setupWatcher(userKubeconfigPath);
       // check if path exists
       if (existsSync(userKubeconfigPath)) {
-        this.kubeconfigPath = userKubeconfigPath;
         this.refresh().catch(() => console.error('Refresh of kube resources on startup failed'));
       } else {
         console.error(`Kubeconfig path ${userKubeconfigPath} provided does not exist. Skipping.`);
