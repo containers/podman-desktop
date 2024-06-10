7451c29ff662dc7c72f0961fe4971ce804dc7e99
Philippe Martin <phmartin@redhat.com>
2024-06-10 14:07:44 +0200

    Light mode for pref/resources actions (#7529)
    refactor: light mode for pref/resources actions
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/preferences/PreferencesConnectionActions.svelte
+++ packages/renderer/src/lib/preferences/PreferencesConnectionActions.svelte
@@ -157,7 +157,10 @@ function getLoggerHandler(
   {#if connection.lifecycleMethods && connection.lifecycleMethods.length > 0}
     <div class="mt-2 relative">
       <!-- TODO: see action available like machine infos -->
-      <div class="flex bg-charcoal-800 w-fit rounded-lg m-auto" role="group" aria-label="Connection Actions">
+      <div
+        class="flex bg-[var(--pd-action-button-details-bg)] w-fit rounded-lg m-auto"
+        role="group"
+        aria-label="Connection Actions">
         {#if connection.lifecycleMethods.includes('start')}
           <div class="ml-2">
             <LoadingIconButton
