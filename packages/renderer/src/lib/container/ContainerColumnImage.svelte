7615fb51eb8fc943acba43cbe9b9cf408354bd48
Tim deBoer <git@tdeboer.ca>
2024-06-12 09:46:45 -0400

    chore: light mode, type checking
    Response to PR feedback:
    - Use light mode variables in columns.
    - Simplified column type checking.
    - Added missing return types (mostly void).
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/container/ContainerColumnImage.svelte
+++ packages/renderer/src/lib/container/ContainerColumnImage.svelte
@@ -5,7 +5,7 @@ export let object: ContainerInfoUI | ContainerGroupInfoUI;
 </script>
 
 {#if 'image' in object}
-  <div class="text-sm text-gray-700 overflow-hidden text-ellipsis" title="{object.image}">
+  <div class="text-sm text-[var(--pd-table-body-text)] overflow-hidden text-ellipsis" title="{object.image}">
     {object.shortImage}
   </div>
 {/if}
