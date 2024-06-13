e51ac372797f41ed7948ffed5d713d1daaa537bf
Tim deBoer <git@tdeboer.ca>
2024-06-13 14:58:21 -0400

    chore: light mode preference headers (#7639)
    Must have been affected by another change b/c I remember this being ok
    before, but 'No settings found' and preference titles don't have color
    set for light mode.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/preferences/PreferencesRendering.svelte
+++ packages/renderer/src/lib/preferences/PreferencesRendering.svelte
@@ -69,7 +69,7 @@ function updateSearchValue(event: any) {
 <Route path="/" breadcrumb="{key}">
   <SettingsPage title="Preferences">
     <SearchInput slot="header" title="preferences" class="mt-4" on:input="{e => updateSearchValue(e)}" />
-    <div class="flex flex-col space-y-5">
+    <div class="flex flex-col space-y-5 text-[var(--pd-content-header)]">
       {#if matchingRecords.size === 0}
         <div>No Settings Found</div>
       {:else}
