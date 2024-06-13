9efe3d5776deadcb2762713f2e9d6e13035aed20
Charlie Drage <charlie@charliedrage.com>
2024-06-13 10:52:12 -0400

    fix: remove incorrect 'apply' namespace from applyResourcesFromYAML (#7595)
    ### What does this PR do?
    
    Removes the 'apply' namespace from applyResourcesFromYAML which was
    causing problems.
    
    ### Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop/issues/7594
    
    ### How to test this PR?
    
    <!-- Please explain steps to verify the functionality,
    do not forget to provide unit/component tests -->
    
    - [X] Tests are covering the bug fix or the new feature
    
    You can now edit any k8s YAML without having to remove resourceVersion.
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- packages/main/src/plugin/kubernetes-client.spec.ts
+++ packages/main/src/plugin/kubernetes-client.spec.ts
@@ -1144,7 +1144,7 @@ test('Expect applyResourcesFromYAML to correctly call applyResources after loadi
   const applyResourcesSpy = vi.spyOn(client, 'applyResources').mockResolvedValue(expectedObjects);
   const objects = await client.applyResourcesFromYAML('default', podAndDeploymentTestYAML);
   expect(objects).toEqual(expectedObjects);
-  expect(applyResourcesSpy).toHaveBeenCalledWith('default', expectedObjects, 'apply');
+  expect(applyResourcesSpy).toHaveBeenCalledWith('default', expectedObjects);
 });
 
 test('setupWatcher sends kubernetes-context-update when kubeconfig file changes', async () => {
