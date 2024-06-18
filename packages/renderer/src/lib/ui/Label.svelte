a8664e523065267e0483074c9ca3a58f1f4e7431
Tim deBoer <git@tdeboer.ca>
2024-06-18 12:54:28 -0400

    chore: light mode for Kubernetes status label (#7720)

--- packages/renderer/src/lib/ui/Label.svelte
+++ packages/renderer/src/lib/ui/Label.svelte
@@ -3,10 +3,13 @@ import { Tooltip } from '@podman-desktop/ui-svelte';
 
 export let name = '';
 export let tip = '';
+export let role: string | undefined = undefined;
 </script>
 
 <Tooltip top tip="{tip}">
-  <div class="flex items-center bg-[var(--pd-label-bg)] p-1 rounded-md text-xs text-[var(--pd-label-text)] gap-x-1">
+  <div
+    role="{role}"
+    class="flex items-center bg-[var(--pd-label-bg)] p-1 rounded-md text-xs text-[var(--pd-label-text)] gap-x-1">
     <slot></slot>
     <span class="capitalize">
       {name}
