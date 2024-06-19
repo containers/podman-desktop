2822eed13d5e181729ea2385212bf90044c1bd28
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-06-19 09:49:16 +0200

    fix: children object used
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/ui/src/lib/table/Table.svelte
+++ packages/ui/src/lib/table/Table.svelte
@@ -307,7 +307,7 @@ function toggleChildren(name: string | undefined): void {
                   {#if column.info.renderer}
                     <svelte:component
                       this="{column.info.renderer}"
-                      object="{column.info.renderMapping ? column.info.renderMapping(object) : object}"
+                      object="{column.info.renderMapping ? column.info.renderMapping(child) : child}"
                       on:update />
                   {/if}
                 </div>
