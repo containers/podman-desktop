a155143f07957bfc9ea7833cd00cb644a55d0bad
Sonia Sandler <ssandler@redhat.com>
2024-06-24 14:46:08 -0400

    chore: update volumes summary page UI
    *** What does this PR do?
    
    Changes the UI of the summary page of volumes to match other summary pages
    
    *** Screenshot / video of UI
    
    <!-- If this PR is changing UI, please include
    screenshots or screencasts showing the difference -->
    
    *** What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop/issues/7562
    
    *** How to test this PR?
    
    <!-- Please explain steps to verify the functionality,
    do not forget to provide unit/component tests -->
    
    - [X] Tests are covering the bug fix or the new feature
    
    View the summary page of a volume
    
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    refactor: apply suggested changes
    
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/volume/VolumeDetailsSummary.svelte
+++ packages/renderer/src/lib/volume/VolumeDetailsSummary.svelte
@@ -1,47 +1,76 @@
 <script lang="ts">
+import { Link } from '@podman-desktop/ui-svelte';
 import { router } from 'tinro';
 
+import DetailsCell from '../details/DetailsCell.svelte';
+import DetailsTable from '../details/DetailsTable.svelte';
+import DetailsTitle from '../details/DetailsTitle.svelte';
 import type { VolumeInfoUI } from './VolumeInfoUI';
 
 export let volume: VolumeInfoUI;
+const createdTime: Date = new Date(volume.created);
 
 function openContainer(containerID: string) {
   router.goto(`/containers/${containerID}/logs`);
 }
 </script>
 
-<div class="flex px-5 py-4 flex-col h-full overflow-auto text-[var(--pd-details-body-text)]">
-  <div class="w-full">
-    <table>
-      <tbody>
-        <tr>
-          <td class="pr-2">Name:</td>
-          <td>{volume.name}</td>
-        </tr>
-        <tr>
-          <td class="pr-2">Size:</td>
-          <td>{volume.humanSize}</td>
-        </tr>
-        <tr>
-          <td class="pr-2">Age:</td>
-          <td>{volume.age}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+<DetailsTable>
+  <tr>
+    <DetailsTitle>Details</DetailsTitle>
+  </tr>
+  <tr>
+    <DetailsCell>Name</DetailsCell>
+    <DetailsCell>{volume.name}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Size</DetailsCell>
+    <DetailsCell>{volume.humanSize}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Age</DetailsCell>
+    <DetailsCell>{volume.age}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Created</DetailsCell>
+    <DetailsCell>{createdTime}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Status</DetailsCell>
+    <DetailsCell>{volume.status.toLowerCase()}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Mount Point</DetailsCell>
+    <DetailsCell>{volume.mountPoint}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Scope</DetailsCell>
+    <DetailsCell>{volume.scope}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Driver</DetailsCell>
+    <DetailsCell>{volume.driver}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Engine ID</DetailsCell>
+    <DetailsCell>{volume.engineId}</DetailsCell>
+  </tr>
+  <tr>
+    <DetailsCell>Engine Name</DetailsCell>
+    <DetailsCell>{volume.engineName}</DetailsCell>
+  </tr>
   {#if volume.containersUsage.length > 0}
-    <div class="w-full mt-12">
-      <span>Containers using this volume:</span>
-      {#each volume.containersUsage as container}
-        <table>
-          <tbody>
-            <tr class="cursor-pointer" on:click="{() => openContainer(container.id)}">
-              <td class="pr-2">{container.names.join('')}</td>
-              <td>{container.id}</td>
-            </tr>
-          </tbody>
-        </table>
-      {/each}
-    </div>
+    <tr>
+      <DetailsTitle>Container Usage</DetailsTitle>
+    </tr>
+    {#each volume.containersUsage as container}
+      <tr>
+        <DetailsCell>
+          <Link on:click="{() => openContainer(container.id)}"
+            >{container.names.map(name => (name.startsWith('/') ? name.slice(1) : name)).join(' ')}</Link>
+        </DetailsCell>
+        <DetailsCell>{container.id}</DetailsCell>
+      </tr>
+    {/each}
   {/if}
-</div>
+</DetailsTable>
