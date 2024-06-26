5d7bb61c691221bd144f9f6b146cab0b3cf380a0
Tim deBoer <git@tdeboer.ca>
2024-06-26 18:13:24 -0400

    chore: light error and warning colors
    Replaces the hard-coded error and warning colors with values from the registry
    and uses them for ErrorMessage, WarningMessage, TaskManagerItem, MessageBox, and
    Donut.
    
    I wasn't sure what to call this set as they are not quite status; went with state.
    The MessageBox had state colors already defined for modals, but IMHO these should
    be 'global' so I replaced them. These colors might need to be refined for light
    and there are more uses of red-* and green-*, but this felt like enough to review
    and get the color vars in.
    
    Fixes #7936.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/container/ContainerStatistics.spec.ts
+++ packages/renderer/src/lib/container/ContainerStatistics.spec.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2023 Red Hat, Inc.
+ * Copyright (C) 2023-2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -154,7 +154,7 @@ test('Expect memory donut', async () => {
   expect(memTooltip).toBeInTheDocument();
 
   const memArc = screen.getAllByTestId('arc')[1];
-  expect(memArc).toHaveClass('stroke-green-500');
+  expect(memArc).toHaveClass('stroke-[var(--pd-state-success)]');
 });
 
 test('Expect CPU donut', async () => {
@@ -173,5 +173,5 @@ test('Expect CPU donut', async () => {
   expect(cpuTooltip).toBeInTheDocument();
 
   const cpuArc = screen.getAllByTestId('arc')[0];
-  expect(cpuArc).toHaveClass('stroke-red-500');
+  expect(cpuArc).toHaveClass('stroke-[var(--pd-state-error)]');
 });
