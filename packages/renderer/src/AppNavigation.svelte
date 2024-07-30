9a520b2664cae765918acb0bdd67fea68179800c
Evzen Gasta <evzen.ml@seznam.cz>
2024-07-30 09:23:45 +0200

    chore: use dropdownmenu component for always on buddy widget popup menu
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/AppNavigation.svelte
+++ packages/renderer/src/AppNavigation.svelte
@@ -1,10 +1,25 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
+<<<<<<< HEAD
 import { Tooltip } from '@podman-desktop/ui-svelte';
 import { onMount } from 'svelte';
 import type { TinroRouteMeta } from 'tinro';
 
+=======
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
+import { Tooltip } from '@podman-desktop/ui-svelte';
+import { ContainerIcon } from '@podman-desktop/ui-svelte/icons';
+import { onDestroy, onMount } from 'svelte';
+import type { Unsubscriber } from 'svelte/store';
+import Fa from 'svelte-fa';
+import type { TinroRouteMeta } from 'tinro';
+
+import Webviews from '/@/lib/webview/Webviews.svelte';
+import { webviews } from '/@/stores/webviews';
+import type { ImageInfo } from '/@api/image-info';
+
+>>>>>>> c556038c (chore: use dropdownmenu component for always on buddy widget popup menu)
 import AuthActions from './lib/authentication/AuthActions.svelte';
 import { CommandRegistry } from './lib/CommandRegistry';
 import NewContentOnDashboardBadge from './lib/dashboard/NewContentOnDashboardBadge.svelte';
@@ -16,6 +31,7 @@ import NavRegistryEntry from './lib/ui/NavRegistryEntry.svelte';
 import NavSection from './lib/ui/NavSection.svelte';
 import { navigationRegistry } from './stores/navigation/navigation-registry';
 
+<<<<<<< HEAD
 let {
   exitSettingsCallback,
   meta = $bindable(),
@@ -23,6 +39,44 @@ let {
   exitSettingsCallback: () => void;
   meta: TinroRouteMeta;
 } = $props();
+=======
+let podInfoSubscribe: Unsubscriber;
+let containerInfoSubscribe: Unsubscriber;
+let imageInfoSubscribe: Unsubscriber;
+let volumeInfoSubscribe: Unsubscriber;
+let contextsSubscribe: Unsubscriber;
+let nodesSubscribe: Unsubscriber;
+let deploymentsSubscribe: Unsubscriber;
+let persistentVolumeClaimsSubscribe: Unsubscriber;
+let servicesSubscribe: Unsubscriber;
+let ingressesSubscribe: Unsubscriber;
+let routesSubscribe: Unsubscriber;
+let configmapsSubscribe: Unsubscriber;
+let secretsSubscribe: Unsubscriber;
+let combinedInstalledExtensionsSubscribe: Unsubscriber;
+
+let podCount = '';
+let containerCount = '';
+let imageCount = '';
+let volumeCount = '';
+let configmapsCount = 0;
+let secretsCount = 0;
+let configmapSecretsCount = '';
+let persistentVolumeClaimsCount = '';
+let contextCount = 0;
+let deploymentCount = '';
+let nodeCount = '';
+let serviceCount = '';
+let ingressesCount = 0;
+let routesCount = 0;
+let ingressesRoutesCount = '';
+let extensionCount = '';
+let authActions: AuthActions;
+let outsideWindow: HTMLDivElement;
+
+const imageUtils = new ImageUtils();
+export let exitSettingsCallback: () => void;
+>>>>>>> c556038c (chore: use dropdownmenu component for always on buddy widget popup menu)
 
 const iconSize = '22';
 
@@ -85,7 +139,11 @@ function clickSettings(b: boolean) {
   <div bind:this={outsideWindow}>
     <NavItem href="/accounts" tooltip="" bind:meta={meta} onClick={event => authActions?.onButtonClick(event)}>
       <Tooltip bottomRight tip="Accounts">
+<<<<<<< HEAD
         <AccountIcon size={iconSize} />
+=======
+        <Fa class="h-6 w-6 fa-light" icon={faCircleUser} size="2x" style="fa-light" />
+>>>>>>> c556038c (chore: use dropdownmenu component for always on buddy widget popup menu)
       </Tooltip>
       <AuthActions bind:this={authActions} outsideWindow={outsideWindow} />
     </NavItem>
