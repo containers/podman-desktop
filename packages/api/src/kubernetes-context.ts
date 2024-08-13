1ca7a50bad5390376d2518a8fe0b89de614cf284
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-08-13 14:09:28 +0200

    fix: update copyright Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/api/src/kubernetes-context.ts
+++ packages/api/src/kubernetes-context.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2023 Red Hat, Inc.
+ * Copyright (C) 2023-2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
