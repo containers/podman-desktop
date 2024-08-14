3171f0bd41454a21ebcf82026e9f39b2e1f8297a
Tim deBoer <deboer@redhat.com>
2024-08-14 09:54:07 -0400

    chore: add definition
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

--- packages/renderer/src/lib/ui/NavItem.svelte
+++ packages/renderer/src/lib/ui/NavItem.svelte
@@ -16,6 +16,7 @@ export let onClick: MouseEventHandler<HTMLAnchorElement> | undefined = undefined
 export let counter: number | undefined = undefined;
 
 let inSection: boolean = false;
+let uri: string;
 $: uri = encodeURI(href);
 let selected: boolean;
 $: selected = meta.url === uri || (uri !== '/' && meta.url.startsWith(uri));
