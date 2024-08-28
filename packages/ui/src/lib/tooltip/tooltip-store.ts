7cf0766c02f746b6ae3bef2d361c3f887f56e5dd
Sonia Sandler <ssandler@redhat.com>
2024-08-28 19:16:00 -0400

    chore: remove console logs Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/ui/src/lib/tooltip/tooltip-store.ts
+++ packages/ui/src/lib/tooltip/tooltip-store.ts
@@ -4,10 +4,8 @@ export const tooltipHidden = writable(false);
 
 window.addEventListener('context-menu-closed', () => {
   tooltipHidden.update(() => false);
-  console.log('context menu closed tooltip');
 });
 
 window.addEventListener('contextmenu', () => {
   tooltipHidden.update(() => true);
-  console.log('context menu opened tooltip');
 });
