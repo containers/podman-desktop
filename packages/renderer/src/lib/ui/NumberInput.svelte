e78b7858c07216b2784b832a5fc2731808277d3a
Florent Benoit <fbenoit@redhat.com>
2024-09-02 09:20:59 +0200

    fixup! fix: number widget should accept float value for number type Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/ui/NumberInput.svelte
+++ packages/renderer/src/lib/ui/NumberInput.svelte
@@ -37,12 +37,8 @@ function onKeyPress(event: any) {
   // get cursor position
   const cursorPosition = event.target.selectionStart;
 
-  console.log('cursorPosition', cursorPosition);
-
   // add the new character to the cursor position
   const wantedValue = `${event.target.value.slice(0, cursorPosition)}${event.key}${event.target.value.slice(cursorPosition)}`;
-
-  console.log('wantedValue', wantedValue);
 
   // now, check if type is integer if the value is value without digits or with zero fractional part
   if (type === 'integer' && Number.isInteger(Number(wantedValue))) {
