a982c5e811a90d84cbb0433e3dc96769aaedb6f8
Jeff MAURY <jmaury@redhat.com>
2024-09-11 10:31:03 +0200

    fix: apply change suggested by @benoitf
    Co-authored-by: Florent BENOIT <fbenoit@redhat.com>
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- extensions/podman/src/base-check.spec.ts
+++ extensions/podman/src/base-check.spec.ts
@@ -16,7 +16,6 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import type * as extensionApi from '@podman-desktop/api';
 import { describe, expect, test } from 'vitest';
