cc0d8cb8a96a8689afa94b2acb4064c276274b62
Jeff MAURY <jmaury@redhat.com>
2024-09-11 10:49:02 +0200

    fix: update format
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- extensions/podman/src/base-check.spec.ts
+++ extensions/podman/src/base-check.spec.ts
@@ -16,7 +16,6 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-
 import type * as extensionApi from '@podman-desktop/api';
 import { describe, expect, test } from 'vitest';
 
