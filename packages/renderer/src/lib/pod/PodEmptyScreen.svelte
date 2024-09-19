f80ae0f9d61ec8de3423d46cb657fe3b26626a7a
Denis Golovin <dgolovin@redhat.com>
2024-09-18 22:25:54 -0700

    feat: add 'Start my first pod' button for empty pods list page
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/renderer/src/lib/pod/PodEmptyScreen.svelte
+++ packages/renderer/src/lib/pod/PodEmptyScreen.svelte
@@ -1,9 +1,52 @@
 <script lang="ts">
-import { EmptyScreen } from '@podman-desktop/ui-svelte';
+import { Button, EmptyScreen } from '@podman-desktop/ui-svelte';
+
+import { providerInfos } from '/@/stores/providers';
 
 import PodIcon from '../images/PodIcon.svelte';
 
-const commandLine = 'podman pod create --label myFirstPod';
+const myFirstPod = 'myFirstPod';
+const helloImage = 'quay.io/podman/hello:latest';
+const commandLine = `podman run -dt --pod new:${myFirstPod} ${helloImage}`;
+
+let inProgress = false;
+
+$: selectedProviderConnection = $providerInfos
+  .map(provider => provider.containerConnections)
+  .flat()
+  .find(providerContainerConnection => providerContainerConnection.status === 'started');
+
+async function startPod(): Promise<void> {
+  inProgress = true;
+  if (selectedProviderConnection) {
+    try {
+      await window.pullImage(selectedProviderConnection, helloImage, () => {});
+      const listImages = await window.listImages();
+      const image = listImages.find(item => item.RepoTags?.includes(helloImage));
+      await window.createPod({ name: myFirstPod });
+      if (image) {
+        await window.createAndStartContainer(image.engineId, { Image: helloImage, pod: myFirstPod });
+      } else {
+        await window.showMessageBox({
+          title: `Error when running a pod`,
+          message: `Could not find '${helloImage}'' in images`,
+        });
+      }
+    } catch (error) {
+      await window.showMessageBox({
+        title: `Error when running a pod`,
+        message: String(error),
+      });
+    } finally {
+      inProgress = false;
+    }
+  } else {
+    await window.showMessageBox({
+      title: `Error when running a pod`,
+      message: `No provider connections found`,
+    });
+  }
+}
 </script>
 
 <EmptyScreen
@@ -11,4 +54,12 @@ const commandLine = 'podman pod create --label myFirstPod';
   title="No pods"
   message="Run a first pod using the following command line:"
   commandline={commandLine}
-  on:click={() => window.clipboardWriteText(commandLine)} />
+  on:click={() => window.clipboardWriteText(commandLine)}>
+  <div slot="upperContent">
+    <div class="flex gap-2 justify-center p-3">
+      <Button title="Start your first pod" type="primary" inProgress={inProgress} on:click={() => startPod()}
+        >Start your first pod</Button>
+    </div>
+    <h1 class="text-xl text-[var(--pd-details-empty-header)]">OR</h1>
+  </div>
+</EmptyScreen>
