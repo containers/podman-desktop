9a5781b8c34d9603b6fc36aeee63b8b9f5dfd4c9
Philippe Martin <phmartin@redhat.com>
2024-09-20 08:48:18 +0200

    feat: display Kubernetes empty page when no current context Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/renderer/src/lib/kube/KubernetesEmptyPage.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { EmptyScreen } from '@podman-desktop/ui-svelte';
+
+import EmbeddableCatalogExtensionList from '../extensions/EmbeddableCatalogExtensionList.svelte';
+import KubeIcon from '../images/KubeIcon.svelte';
+</script>
+
+<EmptyScreen
+  icon={KubeIcon}
+  title="No Kubernetes cluster"
+  message="Deploy a Kubernetes cluster or connect to a remote one">
+  <div class="flex gap-2 justify-center max-w-[800px]">
+    <EmbeddableCatalogExtensionList category="Kubernetes" showInstalled={false} />
+  </div>
+</EmptyScreen>
