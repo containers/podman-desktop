689f09b893d89e8a26d71f59acff008afcec24d3
Florent Benoit <fbenoit@redhat.com>
2024-09-20 09:52:40 +0200

    fix: apply light theme on preflight checks box
    fixes https://github.com/containers/podman-desktop/issues/8587
    
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/dashboard/PreflightChecks.svelte
+++ packages/renderer/src/lib/dashboard/PreflightChecks.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
-import { Spinner } from '@podman-desktop/ui-svelte';
+import { Link, Spinner } from '@podman-desktop/ui-svelte';
 
 import type { CheckStatus } from '/@api/provider-info';
 
 export let preflightChecks: CheckStatus[] = [];
 
-function openLink(e: MouseEvent, url: string): void {
-  e.preventDefault();
-  e.stopPropagation();
+function openLink(url: string): void {
   window.openExternal(url);
 }
 </script>
 
 {#if preflightChecks.length > 0}
-  <div class="flex flex-col w-full mt-5 px-5 pt-5 pb-0 rounded-lg bg-zinc-600">
+  <div class="flex flex-col w-full p-2 rounded-lg bg-[var(--pd-invert-content-card-bg)]">
     {#each preflightChecks as preCheck}
       <div class="flex flex-col">
         <div class="mb-4 flex flex-row">
@@ -27,19 +25,20 @@ function openLink(e: MouseEvent, url: string): void {
           <div aria-label="precheck-title" class="ml-2">{preCheck.name}</div>
         </div>
         {#if preCheck.description}
-          Details: <p aria-label="precheck-description" class="text-gray-400 w-full break-all">
+          Details: <p
+            aria-label="precheck-description"
+            class="text-[var(--pd-invert-content-card-text)] w-full break-all">
             {preCheck.description}
           </p>
           {#if preCheck.docLinksDescription}
-            <p aria-label="precheck-docLinksDescription" class="text-gray-400 w-full">
+            <p aria-label="precheck-docLinksDescription" class="text-[var(--pd-invert-content-card-text)] w-full">
               {preCheck.docLinksDescription}
             </p>
           {/if}
           {#if preCheck.docLinks}
             See:
             {#each preCheck.docLinks as link}
-              <a aria-label="precheck-link" href={link.url} target="_blank" on:click={e => openLink(e, link.url)}
-                >{link.title}</a>
+              <Link aria-label="precheck-link" on:click={() => openLink(link.url)}>{link.title}</Link>
             {/each}
           {/if}
         {/if}
