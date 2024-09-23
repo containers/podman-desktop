4bce304ac6c6bb3056745f1df71232c2a15a75da
Philippe Martin <phmartin@redhat.com>
2024-09-23 15:22:08 +0200

    feat: separate local and remote providers Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/kube/KubernetesEmptyPage.svelte
+++ packages/renderer/src/lib/kube/KubernetesEmptyPage.svelte
@@ -9,7 +9,18 @@ import KubeIcon from '../images/KubeIcon.svelte';
   icon={KubeIcon}
   title="No Kubernetes cluster"
   message="Deploy a Kubernetes cluster or connect to a remote one">
-  <div class="flex gap-2 justify-center max-w-[800px]">
-    <EmbeddableCatalogExtensionList category="Kubernetes" showInstalled={false} />
+  <div class="max-w-[800px]">
+    <EmbeddableCatalogExtensionList
+      showEmptyScreen={false}
+      title="Extensions to help you deploy Kubernetes clusters on your machine"
+      category="Kubernetes"
+      keywords={['provider', 'local']}
+      showInstalled={false} />
+    <EmbeddableCatalogExtensionList
+      showEmptyScreen={false}
+      title="Extensions to help you connect to remote Kubernetes clusters"
+      category="Kubernetes"
+      keywords={['provider', 'remote']}
+      showInstalled={false} />
   </div>
 </EmptyScreen>
