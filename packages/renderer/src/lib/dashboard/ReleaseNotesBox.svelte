d6057d1e280648fb1291a1994f1275ff800dfc61
Sonia Sandler <ssandler@redhat.com>
2024-09-30 12:11:40 -0400

    chore: modify design Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/dashboard/ReleaseNotesBox.svelte
+++ packages/renderer/src/lib/dashboard/ReleaseNotesBox.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { faCircleArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { Button, CloseButton, Link } from '@podman-desktop/ui-svelte';
-import { onMount } from 'svelte';
+import { onDestroy, onMount } from 'svelte';
 
 import Markdown from '../markdown/Markdown.svelte';
 
@@ -15,7 +15,7 @@ let summary: string = '';
 let imageUrl: string = '';
 let imageAlt: string = '';
 
-window.events?.receive('show-release-notes', () => {
+const receiveShowReleaseNotes = window.events?.receive('show-release-notes', () => {
   showBanner = true;
 });
 
@@ -60,15 +60,22 @@ onMount(async () => {
     });
   await getInfoFromNotes();
 });
+
+onDestroy(async () => {
+  receiveShowReleaseNotes.dispose();
+});
 </script>
 
 {#if showBanner}
   {#if notesAvailable}
-    <div class="flex bg-[var(--pd-content-card-bg)] rounded-md p-5 gap-3 flex-row flex-nowrap h-[230px] items-center">
+    <div class="flex bg-[var(--pd-content-card-bg)] rounded-md p-5 gap-3 flex-row flex-nowrap h-[200px] items-center">
       {#if imageUrl}
-        <img src={imageUrl} class="max-h-[190px] w-auto max-w-[20%] object-contain rounded-md" alt={imageAlt} />
+        <img
+          src={imageUrl}
+          class="max-h-[100%] w-auto max-w-[20%] object-contain rounded-md self-start"
+          alt={imageAlt} />
       {/if}
-      <div class="flex flex-col flex-1">
+      <div class="flex flex-col flex-1 h-100% self-start">
         <div class="flex flex-row items-center justify-between">
           <p class="text-[var(--pd-content-card-header-text)] font-bold text-xl ml-2">
             {title}
@@ -83,15 +90,12 @@ onMount(async () => {
           <Link on:click={openReleaseNotes}>Learn more</Link>
           <Button on:click={updatePodmanDesktop} hidden={!updateAvilable} icon={faCircleArrowUp}>Update</Button>
         </div>
-        <div class="flex justify-end items-center text-[var(--pd-content-card-light-title)] mt-2">
-          Podman Desktop v{currentVersion}
-        </div>
       </div>
     </div>
   {:else}
-    <div class="flex bg-[var(--pd-content-card-bg)] rounded-md p-5 gap-3 flex-row flex-nowrap h-auto items-center">
+    <div class="flex bg-[var(--pd-content-card-bg)] rounded-md p-5 flex-col flex-nowrap h-auto items-center">
       <div class="flex flex-row items-center justify-between w-full">
-        <p class="text-[var(--pd-content-card-header-text)] font-bold text-lg ml-2 w-full items-center">
+        <p class="text-[var(--pd-content-card-header-text)] font-bold text-lg w-full items-center">
           Release notes are currently unavailable, please check again later or try this
           <a href={notesURL} class="text-[var(--pd-link)]">link</a>
         </p>
