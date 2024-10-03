21d10c36f86dd04e6a3da7479c09865191f63c8f
Florent Benoit <fbenoit@redhat.com>
2024-10-03 13:36:02 +0200

    feat: bring  docker compatibility page if flag is enabled
    note: page will be empty (no widgets in it for now)
    
    part of https://github.com/containers/podman-desktop/issues/9025
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/preferences/docker-compat/PreferencesDockerCompatibilityRendering.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import SettingsPage from '../SettingsPage.svelte';
+</script>
+
+<SettingsPage title="Docker compatibility">
+  <div slot="subtitle" class="text-sm font-thin mt-2">
+    Podman Desktop provides compatibility with Docker, allowing you to use your existing Docker commands, images and
+    workflows.
+  </div>
+</SettingsPage>
