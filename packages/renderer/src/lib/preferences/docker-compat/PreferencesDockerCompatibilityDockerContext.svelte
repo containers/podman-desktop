3a60361752a9cf0c3774e03bee86e85020e97df4
Florent Benoit <fbenoit@redhat.com>
2024-10-03 22:34:58 +0200

    feat: add ability to switch the docker CLI context
    fixes https://github.com/containers/podman-desktop/issues/9026
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/preferences/docker-compat/PreferencesDockerCompatibilityDockerContext.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+import { Tooltip } from '@podman-desktop/ui-svelte';
+import { onMount } from 'svelte';
+
+import type { DockerContextInfo } from '/@api/docker-compatibility-info';
+
+let dockerContexts: DockerContextInfo[] = $state([]);
+
+let selectedContext: DockerContextInfo | undefined = $state(undefined);
+
+async function refreshDockerContext(): Promise<void> {
+  const items = await window.getDockerContexts();
+  selectedContext = items.find(context => context.isCurrentContext);
+  // update the list after selected context has been set
+  dockerContexts = items;
+}
+
+onMount(async () => {
+  // ask once to get the result, then it'll be triggered by the user clicking on refresh
+  await refreshDockerContext();
+});
+
+$effect(() => {
+  // get current selected context
+  const currentSelectedContext = dockerContexts.find(context => context.isCurrentContext);
+  if (!selectedContext || (currentSelectedContext && selectedContext?.name === currentSelectedContext.name)) {
+    // do nothing if we're not changing the context
+    return;
+  }
+
+  // set the selected context
+  window.switchDockerContext(selectedContext.name).catch((error: unknown) => {
+    console.error(`Failing to switch docket context to ${selectedContext?.name}`, error);
+  });
+});
+</script>
+
+<div
+  class="bg-[var(--pd-invert-content-card-bg)] rounded-md mt-2 ml-2 divide-x divide-gray-900 flex flex-col lg:flex-row">
+  <div class="flex flex-row grow px-2 py-2 justify-between text-[color:var(--pd-invert-content-card-text)]">
+    <div class="flex flex-col">
+      <div class="flex flex-row items-center text-[color:var(--pd-invert-content-card-text)]">
+        Docker Context
+
+        <div class="mx-2">
+          <Tooltip tip="Refresh the context">
+            <button aria-label="refresh context" class="text-xs text-violet-500" onclick={() => refreshDockerContext()}>
+              <i class="fas fa-undo" aria-hidden="true"></i>
+            </button>
+          </Tooltip>
+        </div>
+      </div>
+      {#if dockerContexts.length === 0}
+        <div class="mt-2">No Docker context found</div>
+      {:else}
+        <div class="mt-2">
+          Display and select between your different Docker-compatible socket contexts.
+          <select
+            class="w-full p-2 outline-none bg-[var(--pd-select-bg)] rounded-sm text-[var(--pd-content-text)]"
+            name="Docker Context selection"
+            id="dockerContextChoice"
+            bind:value={selectedContext}>
+            {#each dockerContexts as dockerContext}
+              <option selected={dockerContext.isCurrentContext} value={dockerContext}
+                >{dockerContext.name} ({dockerContext.endpoints.docker.host})</option>
+            {/each}
+          </select>
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
--- packages/renderer/src/lib/preferences/docker-compat/PreferencesDockerCompatibilityRendering.svelte
+++ packages/renderer/src/lib/preferences/docker-compat/PreferencesDockerCompatibilityRendering.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import SettingsPage from '../SettingsPage.svelte';
 import PreferencesDockerCompatibilitySocketMappingStatus from './PreferencesDockerCompatibilitySocketMappingStatus.svelte';
+import PreferencesDockerCompatibilityDockerContext from './PreferencesDockerCompatibilityDockerContext.svelte';
 </script>
 
 <SettingsPage title="Docker compatibility">
@@ -12,6 +13,12 @@ import PreferencesDockerCompatibilitySocketMappingStatus from './PreferencesDock
     <div class="container" role="list">
       Docker Preferences
       <PreferencesDockerCompatibilitySocketMappingStatus />
+    </div>
+  </div>
+  <div class="flex flex-col space-y-4">
+    <div class="container" role="list">
+      Docker CLI Context
+      <PreferencesDockerCompatibilityDockerContext />
     </div>
   </div>
 </SettingsPage>
