d764e70852fb350593c412e67ff127448f458505
Florent Benoit <fbenoit@redhat.com>
2024-10-03 22:44:24 +0200

    feat: show who is owning the docker system socket
    fixes https://github.com/containers/podman-desktop/issues/9025
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/preferences/docker-compat/PreferencesDockerCompatibilitySocketMappingStatus.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { Tooltip } from '@podman-desktop/ui-svelte';
+import { onMount } from 'svelte';
+import Fa from 'svelte-fa';
+import { router } from 'tinro';
+
+import type { DockerSocketMappingStatusInfo } from '/@api/docker-compatibility-info';
+
+import Label from '../../ui/Label.svelte';
+import ProviderInfoCircle from '../../ui/ProviderInfoCircle.svelte';
+
+let isMac = $state(false);
+let isWindows = $state(false);
+let isLinux = $state(false);
+
+let dockerSocketMappingStatusInfo: DockerSocketMappingStatusInfo | undefined = $state(undefined);
+
+let engineType: 'kubernetes' | 'podman' | 'docker' | undefined = $state(undefined);
+
+async function refreshSocketMappingStatus(): Promise<void> {
+  dockerSocketMappingStatusInfo = await window.getSystemDockerSocketMappingStatus();
+
+  if (dockerSocketMappingStatusInfo?.serverInfo?.type === 'podman') {
+    engineType = 'podman';
+  } else if (dockerSocketMappingStatusInfo?.serverInfo?.type === 'docker') {
+    engineType = 'docker';
+  } else {
+    engineType = undefined;
+  }
+}
+
+onMount(async () => {
+  const platform = await window.getOsPlatform();
+
+  isMac = platform === 'darwin';
+  isLinux = platform === 'linux';
+  isWindows = platform === 'win32';
+
+  // ask once to get the result
+  await refreshSocketMappingStatus();
+});
+</script>
+
+<div
+  class="bg-[var(--pd-invert-content-card-bg)] rounded-md mt-2 ml-2 divide-x divide-gray-900 flex flex-col lg:flex-row">
+  <div class="flex flex-row grow px-2 py-2 justify-between text-[color:var(--pd-invert-content-card-text)]">
+    <div class="flex flex-col">
+      <div class="flex flex-row items-center text-[color:var(--pd-invert-content-card-text)]">
+        System socket status
+
+        <div class="mx-2">
+          <Tooltip tip="Refresh the status">
+            <button
+              aria-label="refresh status"
+              class="text-xs text-violet-500"
+              onclick={() => refreshSocketMappingStatus()}>
+              <i class="fas fa-undo" aria-hidden="true"></i>
+            </button>
+          </Tooltip>
+        </div>
+        {#if dockerSocketMappingStatusInfo?.status === 'running' && dockerSocketMappingStatusInfo?.serverInfo}
+          <Label name="{dockerSocketMappingStatusInfo.serverInfo.type} is listening">
+            <ProviderInfoCircle type={engineType} />
+          </Label>
+        {:else if dockerSocketMappingStatusInfo?.status === 'unreachable'}
+          <Label name="socket not reachable">
+            <ProviderInfoCircle type={undefined} />
+          </Label>
+        {/if}
+      </div>
+      <div class="mt-2">
+        Status of the system {isLinux || isMac ? '/var/run/docker.sock socket' : ''}{isWindows
+          ? '//./pipe/docker_engine'
+          : ''}.
+      </div>
+    </div>
+  </div>
+
+  {#if dockerSocketMappingStatusInfo?.connectionInfo}
+    {@const connectionInfo = dockerSocketMappingStatusInfo.connectionInfo}
+    <div class="flex flex-row grow divide-gray-900 p-2 max-w-64">
+      <div class="flex flex-col grow">
+        <div>Provided by {connectionInfo.provider.name} ({connectionInfo.displayName})</div>
+        {#if dockerSocketMappingStatusInfo.serverInfo?.type === 'podman'}
+          <div class="font-thin text-xs">
+            Any docker commands using this socket are redirected to the Podman Engine instead
+          </div>
+        {/if}
+      </div>
+
+      <Tooltip bottom tip="{connectionInfo.provider.name} details">
+        <button
+          aria-label="{connectionInfo.provider.name} details"
+          type="button"
+          onclick={() => router.goto(connectionInfo.link)}>
+          <Fa icon={faArrowUpRightFromSquare} />
+        </button>
+      </Tooltip>
+    </div>
+  {:else if dockerSocketMappingStatusInfo?.serverInfo}
+    <div class="flex flex-row divide-gray-900 px-2 m-2 font-thin">
+      <div class="grid grid-cols-2 gap-x-1.5 gap-y-0.5">
+        {#each Array.from(Object.entries(dockerSocketMappingStatusInfo.serverInfo)) as entry}
+          <div class="capitalize">{entry[0]}:</div>
+          <div>{entry[1]}</div>
+        {/each}
+      </div>
+    </div>
+  {/if}
+</div>
