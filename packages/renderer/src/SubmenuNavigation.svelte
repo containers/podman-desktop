828b8bef43718f4fcb9850f15cd7122890801809
Charlie Drage <charlie@charliedrage.com>
2024-10-17 11:40:16 -0400

    chore: reduce width and font size subnavigation (#9325)

--- packages/renderer/src/SubmenuNavigation.svelte
+++ packages/renderer/src/SubmenuNavigation.svelte
@@ -13,9 +13,9 @@ export let meta: TinroRouteMeta;
   class="z-1 w-leftsidebar min-w-leftsidebar flex-col justify-between flex transition-all duration-500 ease-in-out bg-[var(--pd-secondary-nav-bg)] border-[var(--pd-global-nav-bg-border)] border-r-[1px]"
   aria-label={title + ' Navigation Bar'}>
   <div class="flex items-center">
-    <div class="pt-4 px-3 mb-10">
+    <div class="pt-4 px-3 mb-5">
       <p
-        class="text-2xl font-semibold text-[color:var(--pd-secondary-nav-header-text)] border-l-[4px] border-transparent">
+        class="text-xl font-semibold text-[color:var(--pd-secondary-nav-header-text)] border-l-[4px] border-transparent">
         {title}
       </p>
     </div>
