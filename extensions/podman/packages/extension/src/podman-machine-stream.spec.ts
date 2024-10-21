306818edf13b12aab9eac081984be0e5a4b59a4c
Evzen Gasta <evzen.ml@seznam.cz>
2024-10-21 13:19:33 +0200

    chore: review
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- extensions/podman/packages/extension/src/podman-machine-stream.spec.ts
+++ extensions/podman/packages/extension/src/podman-machine-stream.spec.ts
@@ -1,5 +1,23 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
 import { Client } from 'ssh2';
-import { beforeEach, describe, expect, test,vi } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 
 import type { MachineInfo } from './extension';
 import { ProviderConnectionShellAccessImpl } from './podman-machine-stream';
@@ -134,9 +152,6 @@ describe('Test SSH Stream', () => {
     // Emit event 'ready'
     client.emit('ready');
 
-    // Wait for the shell callback to be invoked
-    await new Promise(resolve => setTimeout(resolve, 150));
-
     // Adds callback for 'data'
     streamMock.on('data', onStreamData);
     streamMock.emit('data', 'some_data');
@@ -159,9 +174,6 @@ describe('Test SSH Stream', () => {
 
     // Emit event 'ready'
     client.emit('ready');
-
-    // Wait for the shell callback to be invoked
-    await new Promise(resolve => setTimeout(resolve, 150));
 
     // Adds callback for 'close'
     streamMock.on('close', onStreamEnd);
