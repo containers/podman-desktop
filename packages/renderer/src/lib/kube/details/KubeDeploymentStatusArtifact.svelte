fed33674f0001194b35979eb5174e3d4a09d19b8
Philippe Martin <phmartin@redhat.com>
2024-10-23 10:34:06 +0200

    feat: display deployments and nodes conditions in a table Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/kube/details/KubeDeploymentStatusArtifact.svelte
+++ packages/renderer/src/lib/kube/details/KubeDeploymentStatusArtifact.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 import type { V1DeploymentStatus } from '@kubernetes/client-node';
+import humanizeDuration from 'humanize-duration';
+import moment from 'moment';
 
 import Cell from '/@/lib/details/DetailsCell.svelte';
-import Subtitle from '/@/lib/details/DetailsSubtitle.svelte';
+import Table from '/@/lib/details/DetailsTable.svelte';
 import Title from '/@/lib/details/DetailsTitle.svelte';
 
 export let artifact: V1DeploymentStatus | undefined;
@@ -23,26 +25,23 @@ export let artifact: V1DeploymentStatus | undefined;
     <tr>
       <Title>Conditions</Title>
     </tr>
-    {#each artifact.conditions as condition}
-      <tr>
-        <Subtitle>{condition.reason}</Subtitle>
-      </tr>
-      <tr>
-        <Cell>Type</Cell>
-        <Cell>{condition.type}</Cell>
-      </tr>
-      <tr>
-        <Cell>Status</Cell>
-        <Cell>{condition.status}</Cell>
-      </tr>
-      <tr>
-        <Cell>Reason</Cell>
-        <Cell>{condition.reason}</Cell>
-      </tr>
-      <tr>
-        <Cell>Message</Cell>
-        <Cell>{condition.message}</Cell>
-      </tr>
-    {/each}
+    <tr>
+      <td colspan="2">
+        <Table>
+          <tr>
+            <th align="left">Type</th><th align="left">Status</th><th align="left">Updated</th><th align="left">Reason</th><th align="left">Message</th>
+          </tr>
+          {#each artifact.conditions as condition}
+            <tr>
+              <td>{condition.type}</td>
+              <td>{condition.status}</td>
+              <td>{humanizeDuration(moment().diff(condition.lastTransitionTime), { round: true, largest: 1 })}</td>
+              <td>{condition.reason}</td>
+              <td>{condition.message}</td>
+            </tr>
+          {/each}
+        </Table>
+      </td>
+    </tr>
   {/if}
 {/if}
