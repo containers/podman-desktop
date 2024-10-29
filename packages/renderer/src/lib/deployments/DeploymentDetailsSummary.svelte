1a61cdbb701b9386f16980019dd96fd39906a818
Philippe Martin <phmartin@redhat.com>
2024-10-29 17:29:50 +0100

    feat: display Deployments events (#9657)
    * feat: display Deployments events
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * fix: remove unnecessary fields in EventUI
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * refactor: move to Svelte5 and add unit test
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * refactor: use $derived to have invariant prop
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/deployments/DeploymentDetailsSummary.svelte
+++ packages/renderer/src/lib/deployments/DeploymentDetailsSummary.svelte
@@ -2,14 +2,19 @@
 import type { V1Deployment } from '@kubernetes/client-node';
 import { ErrorMessage } from '@podman-desktop/ui-svelte';
 
+import Cell from '/@/lib/details/DetailsCell.svelte';
 import Table from '/@/lib/details/DetailsTable.svelte';
+import Title from '/@/lib/details/DetailsTitle.svelte';
 
+import type { EventUI } from '../events/EventUI';
+import EventsTable from '../kube/details/EventsTable.svelte';
 import KubeDeploymentArtifact from '../kube/details/KubeDeploymentArtifact.svelte';
 import KubeDeploymentStatusArtifact from '../kube/details/KubeDeploymentStatusArtifact.svelte';
 import KubeObjectMetaArtifact from '../kube/details/KubeObjectMetaArtifact.svelte';
 
 export let deployment: V1Deployment | undefined;
 export let kubeError: string | undefined = undefined;
+export let events: EventUI[];
 </script>
 
 <!-- Show the kube error if we're unable to retrieve the data correctly, but we still want to show the
@@ -25,5 +30,13 @@ basic information -->
     <KubeDeploymentArtifact artifact={deployment.spec} />
   {:else}
     <p class="text-[var(--pd-state-info)] font-medium">Loading ...</p>
+  {/if}
+  <tr>
+    <Title>Events</Title>
+  </tr>
+  {#if events.length}
+    <EventsTable events={events} />
+  {:else}
+    <tr><Cell>No events</Cell></tr>
   {/if}
 </Table>
--- packages/renderer/src/lib/events/EventUI.ts
+++ packages/renderer/src/lib/events/EventUI.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2023 Red Hat, Inc.
+ * Copyright (C) 2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +16,12 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-export interface DeploymentUI {
-  uid: string;
-  name: string;
-  status: string;
-  namespace: string;
-  replicas: number;
-  ready: number;
-  created?: Date;
-  selected: boolean;
-  conditions: DeploymentCondition[];
-}
-
-export interface DeploymentCondition {
-  type: string;
+export interface EventUI {
+  count?: number;
+  type?: string;
   reason?: string;
+  firstTimestamp?: Date;
+  lastTimestamp?: Date;
+  reportingComponent?: string;
   message?: string;
 }
