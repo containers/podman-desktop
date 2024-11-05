a2614ced68b724e89b9d756c66980189b3540267
Philippe Martin <phmartin@redhat.com>
2024-11-05 10:39:36 +0100

    fix: tty when container run with it options (#9745)
    * fix: tty when container run with it options
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * fix: show cursor for tty tab
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/renderer/src/lib/container/ContainerDetailsTtyTerminal.svelte
+++ packages/renderer/src/lib/container/ContainerDetailsTtyTerminal.svelte
@@ -13,6 +13,26 @@ export let container: ContainerInfoUI;
 export let screenReaderMode = false;
 let attachContainerTerminal: Terminal;
 let closed = false;
+let callbackId: number;
+
+let listened = false;
+$: listenTerminalData(attachContainerTerminal, callbackId);
+
+// listenTerminalData only when attachContainerTerminal is bound from TerminalWindow component
+// and callbackId is defined
+function listenTerminalData(terminal: Terminal, cbId: number) {
+  if (!attachContainerTerminal || !cbId) {
+    return;
+  }
+  if (listened) {
+    return;
+  }
+  listened = true;
+  // pass data from xterm to container
+  terminal.onData(data => {
+    window.attachContainerSend(callbackId, data);
+  });
+}
 
 // update terminal when receiving data
 function receiveDataCallback(data: Buffer) {
@@ -30,18 +50,13 @@ async function attachToContainer() {
   }
 
   // attach to the container
-  const callbackId = await window.attachContainer(
+  callbackId = await window.attachContainer(
     container.engineId,
     container.id,
     receiveDataCallback,
     () => {},
     receiveEndCallback,
   );
-
-  // pass data from xterm to container
-  attachContainerTerminal?.onData(data => {
-    window.attachContainerSend(callbackId, data);
-  });
 }
 
 onMount(async () => {
@@ -50,7 +65,7 @@ onMount(async () => {
 </script>
 
 <div class="h-full" class:hidden={container.state !== 'RUNNING'}>
-  <TerminalWindow class="h-full" bind:terminal={attachContainerTerminal} screenReaderMode={screenReaderMode} />
+  <TerminalWindow class="h-full" bind:terminal={attachContainerTerminal} screenReaderMode={screenReaderMode} disableStdIn={false} showCursor={true} />
 </div>
 
 <EmptyScreen
