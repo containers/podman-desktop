4b62de19ed4236ff61b2127bac28e267504b038c
Evzen Gasta <evzen.ml@seznam.cz>
2024-11-07 17:25:31 +0100

    chore: updated kubernetes dashboard
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/lib/kube/KubernetesDashboard.svelte
+++ packages/renderer/src/lib/kube/KubernetesDashboard.svelte
@@ -25,11 +25,16 @@ import ServiceIcon from '../images/ServiceIcon.svelte';
 import KubernetesDashboardGuideCard from './KubernetesDashboardGuideCard.svelte';
 import KubernetesDashboardResourceCard from './KubernetesDashboardResourceCard.svelte';
 import KubernetesEmptyPage from './KubernetesEmptyPage.svelte';
+import { fadeSlide } from '../ui/animations';
+import { podsInfos } from '/@/stores/pods';
+import { ContainerUtils } from '../container/container-utils';
+  import type { ContainerInfo } from '@podman-desktop/api';
+  import { containersInfos } from '/@/stores/containers';
 
 let noContexts = $derived($kubernetesCurrentContextState.error === NO_CURRENT_CONTEXT_ERROR);
 let currentContextName = $derived($kubernetesContexts.find(context => context.currentContext)?.name);
-
 let nodeCount = $derived($kubernetesCurrentContextNodes.length);
+let activeNodeCount = $state(0);
 let deploymentCount = $derived($kubernetesCurrentContextDeployments.length);
 let serviceCount = $derived($kubernetesCurrentContextServices.length);
 let ingressRouteCount = $derived($kubernetesCurrentContextIngresses.length + $kubernetesCurrentContextRoutes.length);
@@ -37,10 +42,49 @@ let pvcCount = $derived($kubernetesCurrentContextPersistentVolumeClaims.length);
 let configMapSecretCount = $derived(
   $kubernetesCurrentContextConfigMaps.length + $kubernetesCurrentContextSecrets.length,
 );
+let expandedDetails: boolean = $state(true);
+let expandedGuide: boolean = $state(true);
+let searchTerm: string = $state('');
+const containerUtils = new ContainerUtils();
 
 async function openKubernetesDocumentation(): Promise<void> {
   await window.openExternal('https://podman-desktop.io/docs/kubernetes');
 }
+
+async function updateActiveNodeCount(): Promise<void> {
+  for (const node of $kubernetesCurrentContextNodes) {
+    if (node.metadata?.name) {
+      const getKubeNode = await window.kubernetesReadNode(node.metadata.name);
+      // console.error(getKubeNode?.status);
+
+      if (getKubeNode?.status?.phase === "RUNNING") {
+        activeNodeCount += 1;
+      }
+    }
+  }
+};
+
+$effect(() => {
+  // console.error($kubernetesCurrentContextNodes)
+
+  let containers = $containersInfos
+  .filter(container => 
+    container.State === 'running' &&
+    container.Names.filter(name => 
+      $kubernetesCurrentContextNodes.filter(node => node?.metadata?.name === name)
+    )
+  );
+
+  console.error(containers)
+});
+
+let unsub; 
+$effect(() => {
+  updateActiveNodeCount();
+  unsub = $containersInfos
+  
+})
+
 </script>
 
 <NavPage searchEnabled={false}  title="Dashboard">
@@ -55,35 +99,69 @@ async function openKubernetesDocumentation(): Promise<void> {
         <KubernetesEmptyPage />
     {:else}
         <div class="flex flex-col space-y-4 min-w-full overflow-y-auto">
-            <div class="flex flex-col gap-4 p-5 pb-2">
-                <span>Here you can manage and interact with Kubernetes clusters with features like connecting to clusters, and
-                viewing workloads like deployments and services.</span>
-
-                <span>Get up and running by clicking one of the menu items!</span>
-
-                <Link class="place-self-start" on:click={openKubernetesDocumentation}>Kubernetes documentation</Link>
-            </div>
-
-            <div class="flex flex-col gap-4 bg-[var(--pd-content-card-bg)] grow p-5">
-                {#if currentContextName}
-                    <div class="text-xl pt-2">Metrics</div>
-                    <div class="grid grid-cols-4 gap-4">
-                        <KubernetesDashboardResourceCard type='Nodes' Icon={NodeIcon} count={nodeCount} link='/kubernetes/nodes'/>
-                        <KubernetesDashboardResourceCard type='Deployments' Icon={DeploymentIcon} count={deploymentCount} link='/kubernetes/deployments'/>
-                        <KubernetesDashboardResourceCard type='Services' Icon={ServiceIcon} count={serviceCount} link='/kubernetes/services'/>
-                        <KubernetesDashboardResourceCard type='Ingresses & Routes' Icon={IngressRouteIcon} count={ingressRouteCount} link='/kubernetes/ingressesRoutes'/>
-                        <KubernetesDashboardResourceCard type='Persistent Volume Claims' Icon={PvcIcon} count={pvcCount} link='/kubernetes/persistentvolumeclaims'/>
-                        <KubernetesDashboardResourceCard type='ConfigMaps & Secrets' Icon={ConfigMapSecretIcon} count={configMapSecretCount} link='/kubernetes/configmapsSecrets'/>
-                    </div>
-                {/if}
-
-                <div class="text-xl pt-2">Explore articles and blog posts</div>
-                <div class="grid grid-cols-3 gap-4">
-                    <KubernetesDashboardGuideCard title='Deploy and test Kubernetes containers using Podman Desktop' link='https://developers.redhat.com/articles/2023/06/09/deploy-and-test-kubernetes-containers-using-podman-desktop'/>
-                    <KubernetesDashboardGuideCard title='Working with Kubernetes in Podman Desktop' link='https://developers.redhat.com/articles/2023/11/06/working-kubernetes-podman-desktop'/>
-                    <KubernetesDashboardGuideCard title='Share your local podman images with the Kubernetes cluster' link='https://podman-desktop.io/blog/sharing-podman-images-with-kubernetes-cluster'/>
+          <div class="flex flex-col pl-5 pr-5">
+            {#if expandedDetails}
+              <div role="region" class="flex flex-col p-5 pb-2">
+                <div transition:fadeSlide={{ duration: 500 }} class="flex flex-col gap-4">
+                  <div>Here you can manage and interact with Kubernetes clusters with features like connecting to clusters, and
+                    viewing workloads like deployments and services.</div>
+                  <div>Get up and running by clicking one of the menu items!</div>
+                  <div><Link class="place-self-start" on:click={openKubernetesDocumentation}>Kubernetes documentation</Link></div>
                 </div>
+              </div>
+            {/if}
+            
+            <div class="flex justify-end pr-5">
+              <button onclick={() => (expandedDetails = !expandedDetails)}>
+                <div class="flex flex-row text-[var(--pd-content-card-header-text)]">
+                  {#if expandedDetails}
+                    <i class="fas fa-chevron-down"></i>
+                  {:else}
+                    <i class="fas fa-chevron-left"></i>
+                  {/if}
+                </div>
+              </button>
             </div>
+          </div>
+          <div class="flex flex-col gap-4 bg-[var(--pd-content-card-bg)] grow p-5">
+            {#if currentContextName}
+              <div class="text-xl pt-2">Metrics</div>
+                <div class="grid grid-cols-4 gap-4">
+                    <KubernetesDashboardResourceCard type='Nodes' Icon={NodeIcon} activeCount={activeNodeCount} count={nodeCount} link='/kubernetes/nodes'/>
+                    <KubernetesDashboardResourceCard type='Deployments' Icon={DeploymentIcon} activeCount={2} count={deploymentCount} link='/kubernetes/deployments'/>
+                    <KubernetesDashboardResourceCard type='Services' Icon={ServiceIcon} count={serviceCount} link='/kubernetes/services'/>
+                    <KubernetesDashboardResourceCard type='Ingresses & Routes' Icon={IngressRouteIcon} count={ingressRouteCount} link='/kubernetes/ingressesRoutes'/>
+                    <KubernetesDashboardResourceCard type='Persistent Volume Claims' Icon={PvcIcon} count={pvcCount} link='/kubernetes/persistentvolumeclaims'/>
+                    <KubernetesDashboardResourceCard type='ConfigMaps & Secrets' Icon={ConfigMapSecretIcon} count={configMapSecretCount} link='/kubernetes/configmapsSecrets'/>
+                </div>
+            {/if}
+
+            <div class="flex flex-1 flex-col pt-2">
+              <div>
+                <button onclick={() => (expandedGuide = !expandedGuide)}>
+                  <div class="flex flex-row space-x-2 items-center text-[var(--pd-content-card-header-text)]">
+                    {#if expandedGuide}
+                      <i class="fas fa-chevron-down"></i>
+                    {:else}
+                      <i class="fas fa-chevron-right"></i>
+                    {/if}
+                    <p class="text-xl">Explore articles and blog posts</p>
+                  </div>
+                </button>
+              </div>
+              {#if expandedGuide}
+                <div role="region" class="mt-5">
+                  <div transition:fadeSlide={{ duration: 500 }}>
+                    <div class="grid grid-cols-3 gap-4">
+                      <KubernetesDashboardGuideCard title='Deploy and test Kubernetes containers using Podman Desktop' link='https://developers.redhat.com/articles/2023/06/09/deploy-and-test-kubernetes-containers-using-podman-desktop'/>
+                      <KubernetesDashboardGuideCard title='Working with Kubernetes in Podman Desktop' link='https://developers.redhat.com/articles/2023/11/06/working-kubernetes-podman-desktop'/>
+                      <KubernetesDashboardGuideCard title='Share your local podman images with the Kubernetes cluster' link='https://podman-desktop.io/blog/sharing-podman-images-with-kubernetes-cluster'/>
+                    </div>
+                  </div>
+                </div>
+              {/if}
+            </div>
+          </div>
         </div>
     {/if}
   </div>
