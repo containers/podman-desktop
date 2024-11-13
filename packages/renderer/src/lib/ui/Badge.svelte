5b5688c8a3fce78700152e8c1d98457e5b2b8240
Sonia Sandler <66797193+SoniaSandler@users.noreply.github.com>
2024-11-13 08:21:26 -0500

    chore: update extension badge text color and color name in the registry (#9849)
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/ui/Badge.svelte
+++ packages/renderer/src/lib/ui/Badge.svelte
@@ -26,6 +26,6 @@ onMount(async () => {
 });
 </script>
 
-<div class="text-[var(--pd-card-bg)] text-xs me-2 px-1 py-0.5 rounded select-none {customClass} {$$props.class}" style={customStyle}>
+<div class="text-[var(--pd-badge-text)] text-xs me-2 px-1 py-0.5 rounded select-none {customClass} {$$props.class}" style={customStyle}>
   {label}
 </div>
