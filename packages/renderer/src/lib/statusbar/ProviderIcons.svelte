cd506ea1d99ce0e7780878d25b966f9752faf14b
Sonia Sandler <ssandler@redhat.com>
2024-11-13 09:34:30 -0500

    chore: update status Signed-off-by: Sonia Sandler <ssandler@redhat.com>

---
+++ packages/renderer/src/lib/statusbar/ProviderIcons.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+const statusesStyle = new Map<string, string>([
+  ['Podman', 'fa-regular fa-circle-check'],
+  ['OpenShift Local', 'fa-regular fa-circle-check'],
+  ['Minikube', 'animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent'],
+  ['Kind', 'fa-regular fa-circle-dot'],
+  [
+    'Developer SandBox',
+    'animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent',
+  ],
+  ['failed', 'fa-circle-x'],
+  ['not set up', 'fa-regular fa-exclamation-triangle text-[var(--pd-button-text)]'],
+  ['update available', 'fa-regular fa-circle-arrow-up text-[var(--pd-button-text)]'],
+]);
+console.log(statusesStyle);
+</script>
--- packages/renderer/src/lib/statusbar/ProviderWidget.svelte
+++ packages/renderer/src/lib/statusbar/ProviderWidget.svelte
@@ -5,8 +5,9 @@ import type { ProviderInfo } from '/@api/provider-info';
 
 import IconImage from '../appearance/IconImage.svelte';
 
-export let entry: ProviderInfo;
-export let command = () => router.goto('/preferences/resources');
+let { entry, command = () => router.goto('/preferences/resources') }: { entry: ProviderInfo; command: () => void } =
+  $props();
+let statusStyle = $state('bg-gray-900 text-gray-900');
 
 function tooltipText(entry: ProviderInfo): string {
   let tooltip = '';
@@ -21,27 +22,35 @@ async function executeCommand() {
 }
 
 const statusesStyle = new Map<string, string>([
-  ['ready', 'bg-[var(--pd-status-connected)]'],
-  ['started', 'bg-[var(--pd-status-running)]'],
+  ['ready', 'fa-regular fa-circle-check'],
+  ['started', 'fa-regular fa-circle-check'],
   ['starting', 'animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent'],
   ['stopped', 'fa-regular fa-circle-dot'],
   ['stopping', 'animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent'],
-  ['failed', 'bg-[var(--pd-status-terminated)]'],
-  ['not set up', 'fa-regular fa-exclamation-triangle text-[var(--pd-button-text)]'],
-  ['update available', 'fa-regular fa-circle-up text-[var(--pd-button-text)]'],
+  ['failed', 'fa-circle-x'],
+  ['not-installed', 'fa fa-exclamation-triangle text-[var(--pd-button-text)]'],
+  ['update available', 'fa-regular fa-circle-arrow-up text-[var(--pd-button-text)]'],
+  ['unknown', 'fa-regular fa-moon'],
 ]);
 
-$: statusStyle = statusesStyle.get(entry.status) ?? 'bg-gray-900 text-gray-900';
+$effect(() => {
+  if (entry.containerConnections.length > 0) {
+    statusStyle = statusesStyle.get(entry.containerConnections[0].status) ?? 'bg-gray-900 text-gray-900';
+  } else if (entry.kubernetesConnections.length > 0) {
+    statusStyle = statusesStyle.get(entry.kubernetesConnections[0].status) ?? 'bg-gray-900 text-gray-900';
+  } else {
+    statusStyle = statusesStyle.get(entry.status) ?? 'bg-gray-900 text-gray-900';
+  }
+});
 </script>
   
   <button
-    on:click={() => {
-      executeCommand();
-    }}
+    onclick={executeCommand}
     class="px-2 py-1 gap-x-2 flex h-full min-w-fit items-center hover:bg-[var(--pd-statusbar-hover-bg)] hover:cursor-pointer relative inline-block text-base text-[var(--pd-button-text)]"
     title={tooltipText(entry)}>
-    {#if entry.status}
-        <div aria-label="Connection Status Icon" class="w-3 h-3 rounded-full {statusStyle}"></div>
+    
+    {#if entry.containerConnections || entry.kubernetesConnections || entry.status }
+      <div aria-label="Connection Status Icon" class="w-3 h-3 rounded-full {statusStyle}"></div>
     {/if}
     {#if entry.images.icon}
       <IconImage image={entry.images.icon} class="max-h-3" alt={entry.name}></IconImage>
