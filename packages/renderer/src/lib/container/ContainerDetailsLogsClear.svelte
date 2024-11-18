21ca912cdf2104528c06ca4997f6fd5896c9b0b1
Florent BENOIT <fbenoit@redhat.com>
2024-11-18 14:55:21 +0100

    feat: add a way to clean logs in container's log page (#9528)
    * feat: add a way to clean logs in container's log page
    
    fixes https://github.com/containers/podman-desktop/issues/8311
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ packages/renderer/src/lib/container/ContainerDetailsLogsClear.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { faEraser } from '@fortawesome/free-solid-svg-icons';
+import type { Terminal } from '@xterm/xterm';
+import Fa from 'svelte-fa';
+
+const { terminal }: { terminal: Terminal } = $props();
+
+function clear(): void {
+  terminal.clear();
+}
+</script>
+
+<div class="absolute top-0 right-2 px-1 z-50 m-1 opacity-50 space-x-1">
+  <button title="Clear logs" onclick={clear} class="">
+    <Fa
+      class="cursor-pointer rounded-full bg-[var(--pd-button-disabled)] min-h-8 w-8 p-1.5 hover:bg-[var(--pd-button-primary-hover-bg)]"
+      icon={faEraser}
+    />
+  </button>
+</div>
