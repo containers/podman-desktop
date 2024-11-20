containers/podman-desktop

commit 130f17a819b20376cdc80875fddc566721c8d515

chore: use dropdown component in Kube terminal
Switches the select in the Kube terminal to a Dropdown.

The select control automatically selects the first value if none is provided.
Dropdown wasn't doing that so I added the appropriate line, hoping it is
small enough to justify including it here.

Remove redundant aria-labelledby (label already points to control).

Part of #6199.

Signed-off-by: Tim deBoer <git@tdeboer.ca>

packages/renderer/src/lib/pod/KubernetesTerminalBrowser.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { EmptyScreen } from '@podman-desktop/ui-svelte';
+import { Dropdown, EmptyScreen } from '@podman-desktop/ui-svelte';
 import { onMount } from 'svelte';
 
 import NoLogIcon from '/@/lib/ui/NoLogIcon.svelte';
@@ -53,16 +53,16 @@ function handleSelectionChange(event: Event) {
     </label>
     <div class="w-full">
       {#if pod.containers.length > 1}
-        <select
+        <Dropdown
           on:change={handleSelectionChange}
-          aria-labelledby="listbox-label"
-          class="block w-48 p-1 outline-none text-sm bg-charcoal-800 rounded-sm text-gray-700 placeholder-gray-700"
+          class="w-48"
           name={pod.name}
-          id="input-standard-{pod.name}">
-          {#each pod.containers as container}
-            <option value={container.Names}>{container.Names}</option>
-          {/each}
-        </select>
+          id="input-standard-{pod.name}"
+          options={pod.containers.map(container => ({
+            label: container.Names,
+            value: container.Names,
+          }))}>
+        </Dropdown>
       {:else}
         <span
           id="input-standard-{pod.name}"

commit abd81b501cbdff41162ab6bfb8c7933772efffc1

chore: fix the on:change -> onChange event
fix an issue introduced by https://github.com/podman-desktop/podman-desktop/pull/9510
Signed-off-by: Florent Benoit <fbenoit@redhat.com>

packages/renderer/src/lib/pod/KubernetesTerminalBrowser.svelte
@@ -30,9 +30,8 @@ onMount(() => {
   key++;
 });
 
-function handleSelectionChange(event: Event) {
-  const target = event.target as HTMLSelectElement;
-  currentContainerName = target.value;
+function handleSelectionChange(value: unknown) {
+  currentContainerName = String(value);
   terminalService.ensureTerminalExists(pod.name, currentContainerName);
   key++;
 }
@@ -54,7 +53,7 @@ function handleSelectionChange(event: Event) {
     <div class="w-full">
       {#if pod.containers.length > 1}
         <Dropdown
-          on:change={handleSelectionChange}
+          onChange={handleSelectionChange}
           class="w-48"
           name={pod.name}
           id="input-standard-{pod.name}"