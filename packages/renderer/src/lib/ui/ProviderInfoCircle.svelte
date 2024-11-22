96fe6a29ab2aa1288f0063f91206d9648583245b
Sonia Sandler <ssandler@redhat.com>
2024-11-21 19:08:02 -0500

    chore: remove comment from svelte file Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/ui/ProviderInfoCircle.svelte
+++ packages/renderer/src/lib/ui/ProviderInfoCircle.svelte
@@ -3,12 +3,6 @@ import { providerColors } from './ProviderInfoCircle';
 
 let { type }: { type: 'kubernetes' | 'podman' | 'docker' | undefined } = $props();
 let color = $derived(providerColors[type ?? 'unknown']);
-
-// Each provider has a colour associated to it within tailwind, this is a map of those colours.
-// bg-purple-600 = podman
-// bg-sky-300    = docker
-// bg-sky-600    = kubernetes
-// bg-gray-900   = unknown
 </script>
 
 <div aria-label="Provider info circle" class="w-2 h-2 {color} rounded-full"></div>
