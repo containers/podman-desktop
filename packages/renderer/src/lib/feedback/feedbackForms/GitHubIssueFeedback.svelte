4b874cc9e2fab63626f554fbb8f044cbba3b7d47
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-11-26 11:42:27 +0100

    refactor(github-feedback): removing test ids and factoring code
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/renderer/src/lib/feedback/feedbackForms/GitHubIssueFeedback.svelte
+++ packages/renderer/src/lib/feedback/feedbackForms/GitHubIssueFeedback.svelte
@@ -62,16 +62,26 @@ async function previewOnGitHub(): Promise<void> {
 <FeedbackForm>
   <svelte:fragment slot="content">
     <div class="text-sm text-[var(--pd-state-warning)]">You can search existing {category} issues on <Link aria-label="GitHub issues" onclick={openGitHubIssues}>github.com/podman-desktop/podman-desktop/issues</Link></div>
+    <!-- issue title -->
     <label for="issueTitle" class="block mt-4 mb-2 text-sm font-medium text-[var(--pd-modal-text)]">Title</label>
-    <input type="text" name="issueTitle" id="issueTitle" bind:value={issueTitle} data-testid="issueTitle" placeholder={titlePlaceholder} class="w-full p-2 outline-none text-sm bg-[var(--pd-input-field-focused-bg)] rounded-sm text-[var(--pd-input-field-focused-text)] placeholder-[var(--pd-input-field-placeholder-text)]"/>
+    <input
+      type="text"
+      name="issueTitle"
+      id="issueTitle"
+      aria-label="Issue Title"
+      bind:value={issueTitle}
+      placeholder={titlePlaceholder}
+      class="w-full p-2 outline-none text-sm bg-[var(--pd-input-field-focused-bg)] rounded-sm text-[var(--pd-input-field-focused-text)] placeholder-[var(--pd-input-field-placeholder-text)]"/>
+
+    <!-- issue body -->
     <label for="issueDescription" class="block mt-4 mb-2 text-sm font-medium text-[var(--pd-modal-text)]"
       >Description</label>
     <textarea
       rows="3"
       maxlength="1000"
       name="issueDescription"
+      aria-label="Issue Description"
       id="issueDescription"
-      data-testid="issueDescription"
       bind:value={issueDescription}
       class="w-full p-2 outline-none text-sm bg-[var(--pd-input-field-focused-bg)] rounded-sm text-[var(--pd-input-field-focused-text)] placeholder-[var(--pd-input-field-placeholder-text)]"
       placeholder={descriptionPlaceholder}></textarea>
