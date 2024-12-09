5a8f9963ec1147bbe040244bfcddd66c2d0b4e01
Sonia Sandler <66797193+SoniaSandler@users.noreply.github.com>
2024-12-09 09:31:37 -0500

    chore: update hardcoded colors to use color registry in CustomPick, Image, Preferences, task-manager, and move provider colors to ts file (#9991)
    * chore: update hardcoded colors to use color registry in CustomPick
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    * chore: remove hardcoded color from comment in TaskManager
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    * chore: update hardcoded border colors to use color registry
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>
    
    * chore: move provider colors to ts file
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>

---
+++ packages/renderer/src/lib/ui/ProviderInfoCircle.ts
@@ -0,0 +1,24 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+export const providerColors: Record<string, string> = {
+  podman: 'bg-purple-600',
+  docker: 'bg-sky-400',
+  kubernetes: 'bg-sky-600',
+  unknown: 'bg-gray-900',
+};
