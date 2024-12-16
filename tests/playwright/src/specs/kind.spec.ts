0f740919c7ab72b3bd4766e2d1eec75bf67f8d25
Anton Misskii <amisskii@redhat.com>
2024-12-16 21:28:10 +0100

    chore(test): extract cluster-related operations to a dedicated utility file Signed-off-by: Anton Misskii <amisskii@redhat.com>

--- tests/playwright/src/specs/kind.spec.ts
+++ tests/playwright/src/specs/kind.spec.ts
@@ -16,18 +16,16 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import { ResourceElementActions } from '../model/core/operations';
-import { ContainerState, ResourceElementState } from '../model/core/states';
 import { ResourceConnectionCardPage } from '../model/pages/resource-connection-card-page';
 import { ResourcesPage } from '../model/pages/resources-page';
-import { VolumesPage } from '../model/pages/volumes-page';
-import { expect as playExpect, test } from '../utility/fixtures';
 import {
+  checkClusterResources,
+  clusterOperations,
   createKindCluster,
-  deleteKindCluster,
-  ensureCliInstalled,
-  getVolumeNameForContainer,
-} from '../utility/operations';
+  deleteCluster,
+} from '../utility/cluster-operations';
+import { expect as playExpect, test } from '../utility/fixtures';
+import { ensureCliInstalled } from '../utility/operations';
 import { waitForPodmanMachineStartup } from '../utility/wait';
 
 const RESOURCE_NAME: string = 'kind';
@@ -51,7 +49,7 @@ test.beforeAll(async ({ runner, page, welcomePage }) => {
 
 test.afterAll(async ({ runner, page }) => {
   try {
-    await deleteKindCluster(page, KIND_CONTAINER_NAME, CLUSTER_NAME);
+    await deleteCluster(page, KIND_CONTAINER_NAME, CLUSTER_NAME);
   } finally {
     await runner.close();
   }
@@ -84,7 +82,7 @@ test.describe.serial('Kind End-to-End Tests', { tag: '@k8s_e2e' }, () => {
         await playExpect.poll(async () => resourcesPage.resourceCardIsVisible(RESOURCE_NAME)).toBeTruthy();
       });
     });
-  test.describe('Kind cluster operations', () => {
+  test.describe('Kind cluster validation tests', () => {
     test.skip(
       !!process.env.GITHUB_ACTIONS && process.env.RUNNER_OS === 'Linux',
       'Tests suite should not run on Linux platform',
@@ -94,46 +92,12 @@ test.describe.serial('Kind End-to-End Tests', { tag: '@k8s_e2e' }, () => {
       await createKindCluster(page, CLUSTER_NAME, true, CLUSTER_CREATION_TIMEOUT);
     });
 
-    test('Check resources added with the Kind cluster', async ({ page, navigationBar }) => {
-      const containersPage = await navigationBar.openContainers();
-      await playExpect.poll(async () => containersPage.containerExists(KIND_CONTAINER_NAME)).toBeTruthy();
-      const containerDetailsPage = await containersPage.openContainersDetails(KIND_CONTAINER_NAME);
-      await playExpect.poll(async () => await containerDetailsPage.getState()).toEqual(ContainerState.Running);
-
-      const volumesPage = new VolumesPage(page);
-      const volumeName = await getVolumeNameForContainer(page, KIND_CONTAINER_NAME);
-      if (!volumeName) {
-        throw new Error(`Volume name for container "${KIND_CONTAINER_NAME}" is not defined.`);
-      }
-      const volumeDetailsPage = await volumesPage.openVolumeDetails(volumeName);
-      await playExpect.poll(async () => await volumeDetailsPage.isUsed()).toBeTruthy();
+    test('Check resources added with the Kind cluster', async ({ page }) => {
+      await checkClusterResources(page, KIND_CONTAINER_NAME);
     });
 
-    test('Kind cluster operations - STOP', async ({ navigationBar }) => {
-      await navigationBar.openSettings();
-      await playExpect(kindResourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running);
-      await kindResourceCard.performConnectionAction(ResourceElementActions.Stop);
-      await playExpect(kindResourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Off, {
-        timeout: 50000,
-      });
-    });
-
-    test('Kind cluster operations - START', async () => {
-      await kindResourceCard.performConnectionAction(ResourceElementActions.Start);
-      await playExpect(kindResourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running, {
-        timeout: 50000,
-      });
-    });
-
-    test('Kind cluster operatioms - RESTART', async () => {
-      await kindResourceCard.performConnectionAction(ResourceElementActions.Restart);
-      await playExpect(kindResourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running, {
-        timeout: 50000,
-      });
-    });
-
-    test('Kind cluster operations - DELETE', async ({ page }) => {
-      await deleteKindCluster(page, KIND_CONTAINER_NAME, CLUSTER_NAME);
+    test('Kind cluster operations', async ({ page }) => {
+      await clusterOperations(page, kindResourceCard, KIND_CONTAINER_NAME, CLUSTER_NAME);
     });
   });
 });
---
+++ tests/playwright/src/utility/cluster-operations.ts
@@ -0,0 +1,171 @@
+/**********************************************************************
+ * Copyright (C) 2023-2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import type { Page } from '@playwright/test';
+import test, { expect as playExpect } from '@playwright/test';
+
+import { ResourceElementActions } from '../model/core/operations';
+import { ContainerState, ResourceElementState } from '../model/core/states';
+import type { KindClusterOptions } from '../model/core/types';
+import { CreateKindClusterPage } from '../model/pages/create-kind-cluster-page';
+import { ResourceConnectionCardPage } from '../model/pages/resource-connection-card-page';
+import { ResourcesPage } from '../model/pages/resources-page';
+import { VolumesPage } from '../model/pages/volumes-page';
+import { NavigationBar } from '../model/workbench/navigation';
+import { StatusBar } from '../model/workbench/status-bar';
+import { getVolumeNameForContainer } from './operations';
+
+export async function createKindCluster(
+  page: Page,
+  clusterName: string,
+  usedefaultOptions: boolean,
+  timeout: number = 300_000,
+  { providerType, httpPort, httpsPort, useIngressController, containerImage }: KindClusterOptions = {},
+): Promise<void> {
+  return test.step('Create Kind cluster', async () => {
+    const navigationBar = new NavigationBar(page);
+    const statusBar = new StatusBar(page);
+    const kindResourceCard = new ResourceConnectionCardPage(page, 'kind', clusterName);
+    const createKindClusterPage = new CreateKindClusterPage(page);
+
+    const settingsPage = await navigationBar.openSettings();
+    const resourcesPage = await settingsPage.openTabPage(ResourcesPage);
+    await playExpect(resourcesPage.heading).toBeVisible({ timeout: 10_000 });
+    await playExpect.poll(async () => resourcesPage.resourceCardIsVisible('kind')).toBeTruthy();
+    await playExpect(kindResourceCard.createButton).toBeVisible();
+
+    if (await kindResourceCard.doesResourceElementExist()) {
+      console.log(`Kind cluster [${clusterName}] already present, skipping creation.`);
+      return;
+    }
+
+    await kindResourceCard.createButton.click();
+    if (usedefaultOptions) {
+      await createKindClusterPage.createClusterDefault(clusterName, timeout);
+    } else {
+      await createKindClusterPage.createClusterParametrized(
+        clusterName,
+        {
+          providerType: providerType,
+          httpPort: httpPort,
+          httpsPort: httpsPort,
+          useIngressController: useIngressController,
+          containerImage: containerImage,
+        },
+        timeout,
+      );
+    }
+    await playExpect(kindResourceCard.resourceElement).toBeVisible();
+    await playExpect(kindResourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running, {
+      timeout: 15_000,
+    });
+    await statusBar.validateKubernetesContext(`kind-${clusterName}`);
+  });
+}
+
+export async function deleteCluster(
+  page: Page,
+  resourceName: string = 'kind',
+  containerName: string = 'kind-cluster-control-plane',
+  clusterName: string = 'kind-cluster',
+): Promise<void> {
+  return test.step('Delete Kind cluster', async () => {
+    const navigationBar = new NavigationBar(page);
+    const resourceCard = new ResourceConnectionCardPage(page, resourceName, clusterName);
+    const volumeName = await getVolumeNameForContainer(page, containerName);
+
+    await navigationBar.openSettings();
+    const resourcesPage = new ResourcesPage(page);
+    await playExpect(resourcesPage.heading).toBeVisible({ timeout: 10_000 });
+    if (!(await resourceCard.doesResourceElementExist())) {
+      console.log(`Kind cluster [${clusterName}] not present, skipping deletion.`);
+      return;
+    }
+
+    await resourceCard.performConnectionAction(ResourceElementActions.Stop);
+    await playExpect(resourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Off, {
+      timeout: 50_000,
+    });
+    await resourceCard.performConnectionAction(ResourceElementActions.Delete);
+    await playExpect(resourceCard.markdownContent).toBeVisible({
+      timeout: 50_000,
+    });
+    const containersPage = await navigationBar.openContainers();
+    await playExpect(containersPage.heading).toBeVisible();
+    await playExpect
+      .poll(async () => containersPage.containerExists(containerName), {
+        timeout: 10_000,
+      })
+      .toBeFalsy();
+
+    const volumePage = await navigationBar.openVolumes();
+    await playExpect(volumePage.heading).toBeVisible();
+    await playExpect
+      .poll(async () => await volumePage.waitForVolumeDelete(volumeName), {
+        timeout: 20_000,
+      })
+      .toBeTruthy();
+  });
+}
+
+export async function checkClusterResources(page: Page, containerName: string): Promise<void> {
+  return test.step('Check resources added with cluster', async () => {
+    const navigationBar = new NavigationBar(page);
+    const containersPage = await navigationBar.openContainers();
+    await playExpect.poll(async () => containersPage.containerExists(containerName)).toBeTruthy();
+    const containerDetailsPage = await containersPage.openContainersDetails(containerName);
+    await playExpect.poll(async () => await containerDetailsPage.getState()).toEqual(ContainerState.Running);
+
+    const volumesPage = new VolumesPage(page);
+    const volumeName = await getVolumeNameForContainer(page, containerName);
+    if (!volumeName) {
+      throw new Error(`Volume name for container "${containerName}" is not defined.`);
+    }
+    const volumeDetailsPage = await volumesPage.openVolumeDetails(volumeName);
+    await playExpect.poll(async () => await volumeDetailsPage.isUsed()).toBeTruthy();
+  });
+}
+
+export async function clusterOperations(
+  page: Page,
+  resourceCard: ResourceConnectionCardPage,
+  containerName: string,
+  clusterName: string,
+): Promise<void> {
+  return test.step('Basic cluster operations', async () => {
+    const navigationBar = new NavigationBar(page);
+    await navigationBar.openSettings();
+    await playExpect(resourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running);
+    await resourceCard.performConnectionAction(ResourceElementActions.Stop);
+    await playExpect(resourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Off, {
+      timeout: 50000,
+    });
+
+    await resourceCard.performConnectionAction(ResourceElementActions.Start);
+    await playExpect(resourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running, {
+      timeout: 50000,
+    });
+
+    await resourceCard.performConnectionAction(ResourceElementActions.Restart);
+    await playExpect(resourceCard.resourceElementConnectionStatus).toHaveText(ResourceElementState.Running, {
+      timeout: 50000,
+    });
+
+    await deleteCluster(page, containerName, clusterName);
+  });
+}
