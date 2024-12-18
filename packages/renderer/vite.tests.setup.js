5efbd4e0773e0761be88d82a75c2d53f5e822446
Florent Benoit <fbenoit@redhat.com>
2024-12-18 19:30:09 +0100

    chore: generate all the mocks of exposed methods during the setup of vitest
    avoid to have to think about it in all unit tests.
    Still need to provide the returned value if needed but no need
    to initialize all vi.fn()
    
    fixes https://github.com/podman-desktop/podman-desktop/issues/10420
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/vite.tests.setup.js
+++ packages/renderer/vite.tests.setup.js
@@ -16,6 +16,61 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
 import 'vitest-canvas-mock';
+import typescript from 'typescript';
+import { expect } from 'vitest';
 
 global.window.matchMedia = () => {};
+
+// read the given path and extract the method names from the Window interface
+function extractWindowMethods(filePath) {
+  // Read the content of the .d.ts file
+  const fileContent = readFileSync(filePath, 'utf-8');
+
+  // Create a TypeScript SourceFile
+  const sourceFile = typescript.createSourceFile(filePath, fileContent, typescript.ScriptTarget.Latest, true);
+
+  const methodNames = [];
+
+  // Visit each node in the AST
+  const visit = node => {
+    // Look for the Window interface
+    if (
+      typescript.isInterfaceDeclaration(node) &&
+      node.name.text === 'Window' // Target the "Window" interface
+    ) {
+      for (const member of node.members) {
+        if (typescript.isPropertySignature(member) && member.type && typescript.isFunctionTypeNode(member.type)) {
+          const name = member.name.text;
+          methodNames.push(name);
+        }
+      }
+    }
+
+    typescript.forEachChild(node, visit);
+  };
+
+  visit(sourceFile);
+
+  return methodNames;
+}
+
+// methods being exposed
+const declarationsPath = path.resolve(__dirname, '../preload/exposedInMainWorld.d.ts');
+
+// Extract method names from the Window interface
+const methodNames = extractWindowMethods(declarationsPath);
+
+// assert that we have more than 50 methods
+expect(methodNames.length).toBeGreaterThan(50);
+
+// Dynamically create vi mocks for all the given methods
+for (const methodName of methodNames) {
+  Object.defineProperty(window, methodName, {
+    value: vi.fn(),
+    configurable: true,
+    writable: true,
+  });
+}
